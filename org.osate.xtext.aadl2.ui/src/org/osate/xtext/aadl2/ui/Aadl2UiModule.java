1e888520d133d2e5be09e0ee2b164664d78802b7
Peter Feiler <phf@sei.cmu.edu>
2011-07-08 10:45:17 -0400

    First attempt at a context menu. Extended Outline page. Registered my outline page. Plug-in has an example menu item.

--- org.osate.xtext.aadl2.ui/src/org/osate/xtext/aadl2/ui/Aadl2UiModule.java
+++ org.osate.xtext.aadl2.ui/src/org/osate/xtext/aadl2/ui/Aadl2UiModule.java
@@ -4,6 +4,8 @@
 package org.osate.xtext.aadl2.ui;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.eclipse.ui.views.contentoutline.IContentOutlinePage;
+import org.osate.xtext.aadl2.ui.outline.Aadl2OutlinePage;
 
 /**
  * Use this class to register components to be used within the IDE.
@@ -11,5 +13,9 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
 public class Aadl2UiModule extends org.osate.xtext.aadl2.ui.AbstractAadl2UiModule {
 	public Aadl2UiModule(AbstractUIPlugin plugin) {
 		super(plugin);
+	}
+	@Override
+	public Class<? extends IContentOutlinePage> bindIContentOutlinePage() {
+	return Aadl2OutlinePage.class;
 	}
 }
