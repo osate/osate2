aa225901811094d7fb6186e9d8fcdd5f20aa2531
Peter Feiler <phf@sei.cmu.edu>
2011-07-08 10:43:03 -0400

    added property set to root rule so we can do both from same file extension. Fixed formatting.

--- core/org.osate.xtext.aadl2/src/org/osate/xtext/aadl2/linking/MyLinkingService.java
+++ core/org.osate.xtext.aadl2/src/org/osate/xtext/aadl2/linking/MyLinkingService.java
@@ -10,28 +10,6 @@ import org.eclipse.emf.ecore.EReference;
 import org.eclipse.xtext.linking.impl.DefaultLinkingService;
 import org.eclipse.xtext.linking.impl.IllegalNodeException;
 import org.eclipse.xtext.nodemodel.INode;
-import org.osate.aadl2.Aadl2Package;
-import org.osate.aadl2.AadlPackage;
-import org.osate.aadl2.CallContext;
-import org.osate.aadl2.Classifier;
-import org.osate.aadl2.Connection;
-import org.osate.aadl2.ConnectionEnd;
-import org.osate.aadl2.Context;
-import org.osate.aadl2.EndToEndFlow;
-import org.osate.aadl2.Feature;
-import org.osate.aadl2.FlowSpecification;
-import org.osate.aadl2.Mode;
-import org.osate.aadl2.ModeTransition;
-import org.osate.aadl2.PackageSection;
-import org.osate.aadl2.Port;
-import org.osate.aadl2.PortConnection;
-import org.osate.aadl2.Property;
-import org.osate.aadl2.PropertyConstant;
-import org.osate.aadl2.PropertySet;
-import org.osate.aadl2.PropertyType;
-import org.osate.aadl2.Prototype;
-import org.osate.aadl2.Subcomponent;
-import org.osate.aadl2.SubprogramCall;
 import org.osate.xtext.aadl2.properties.linking.NameResolver;
 
 
