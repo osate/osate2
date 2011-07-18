b7e0534f99846802d5c69acbfc44af2a935e2d19
Peter Feiler <phf@sei.cmu.edu>
2011-07-17 23:05:37 -0400

    added handler

---
+++ core/org.osate.xtext.aadl2.ui/src/org/osate/xtext/aadl2/ui/handlers/InstantiateHandler.java
@@ -0,0 +1,124 @@
+package org.osate.xtext.aadl2.ui.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.jface.text.IRegion;
+import org.eclipse.jface.text.ITextSelection;
+import org.eclipse.jface.text.Region;
+import org.eclipse.jface.text.hyperlink.IHyperlink;
+import org.eclipse.jface.text.hyperlink.IHyperlinkDetector;
+import org.eclipse.jface.text.source.ISourceViewer;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.views.contentoutline.ContentOutline;
+import org.eclipse.xtext.nodemodel.INode;
+import org.eclipse.xtext.resource.EObjectAtOffsetHelper;
+import org.eclipse.xtext.resource.XtextResource;
+import org.eclipse.xtext.ui.editor.XtextEditor;
+import org.eclipse.xtext.ui.editor.outline.impl.EObjectNode;
+import org.eclipse.xtext.ui.editor.utils.EditorUtils;
+import org.eclipse.xtext.util.concurrent.IUnitOfWork;
+import org.osate.aadl2.Aadl2Package;
+import org.osate.aadl2.NamedElement;
+
+import com.google.inject.Inject;
+
+public class InstantiateHandler extends AbstractHandler {
+
+
+	@Inject
+	private EObjectAtOffsetHelper eObjectAtOffsetHelper;
+
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		IWorkbench wb = PlatformUI.getWorkbench();
+		IWorkbenchWindow win = wb.getActiveWorkbenchWindow();
+		IWorkbenchPage page = win.getActivePage();
+		IWorkbenchPart part = page.getActivePart();
+		IEditorPart activeEditor = page.getActiveEditor();
+		if (activeEditor == null)
+			return null;
+		XtextEditor xtextEditor = (XtextEditor) activeEditor.getAdapter(XtextEditor.class);
+		final ISelection selection;
+		if (xtextEditor != null) {
+			if (part instanceof ContentOutline) {
+				selection = ((ContentOutline) part).getSelection();
+			} else {
+				selection = (ITextSelection) xtextEditor.getSelectionProvider().getSelection();
+			}
+
+			xtextEditor.getDocument().readOnly(
+					new IUnitOfWork<EObject, XtextResource>() {
+						public EObject exec(XtextResource resource) throws Exception {
+							EObject targetElement = null;
+							if (selection instanceof IStructuredSelection) {
+								IStructuredSelection ss = (IStructuredSelection) selection;
+								Object eon = ss.getFirstElement();
+								if (eon instanceof EObjectNode) {
+									targetElement = ((EObjectNode)eon).getEObject(resource);
+								}
+							} else {
+								targetElement = eObjectAtOffsetHelper.resolveElementAt(resource,
+										((ITextSelection)selection).getOffset());
+							}
+							
+							if (targetElement != null) {
+								if (targetElement instanceof NamedElement){
+									System.out.println("instantiate " + ((NamedElement)targetElement).getName());
+								} else {
+									System.out.println("instantiate" + targetElement.toString());
+								}
+								return null;
+							}
+							return null;
+						}
+					});
+		}
+		return null;
+	}
+	
+//	@Override
+//	public boolean isEnabled() {
+//		IWorkbench wb = PlatformUI.getWorkbench();
+//		IWorkbenchWindow win = wb.getActiveWorkbenchWindow();
+//		IWorkbenchPage page = win.getActivePage();
+//		IWorkbenchPart part = page.getActivePart();
+//		IEditorPart activeEditor = page.getActiveEditor();
+//		if (activeEditor == null)
+//			return false;
+//		XtextEditor xtextEditor = (XtextEditor) activeEditor.getAdapter(XtextEditor.class);
+//		final ISelection selection;
+//		if (xtextEditor != null) {
+//			if (part instanceof ContentOutline) {
+//				selection = ((ContentOutline) part).getSelection();
+//			} else {
+//				selection = (ITextSelection) xtextEditor.getSelectionProvider().getSelection();
+//			}
+//		}
+//		Resource resource = xtextEditor.getResource();
+//		Resource resource = xtextEditor.getEditorSite().g;
+//		EObject targetElement = null;
+//		if (selection instanceof IStructuredSelection) {
+//			IStructuredSelection ss = (IStructuredSelection) selection;
+//			Object eon = ss.getFirstElement();
+//			if (eon instanceof EObjectNode) {
+//				targetElement = ((EObjectNode)eon).getEObject(resource);
+//			}
+//		} else {
+//			targetElement = eObjectAtOffsetHelper.resolveElementAt(resource,
+//					((ITextSelection)selection).getOffset());
+//		}
+//
+//		return  true;
+//	}
+
+}
