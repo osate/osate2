8f6bdaa8e9fca5309296f33543542d76c89753e3
Peter Feiler <phf@sei.cmu.edu>
2011-07-21 21:19:12 -0400

    fixed EnumerationValue to NamedValue in PropertyUtil

--- core/org.osate.aadl2.modelsupport/src/org/osate/aadl2/modelsupport/properties/PropertyUtils.java
+++ core/org.osate.aadl2.modelsupport/src/org/osate/aadl2/modelsupport/properties/PropertyUtils.java
@@ -34,10 +34,11 @@
 package org.osate.aadl2.modelsupport.properties;
 
 import org.osate.aadl2.Aadl2Factory;
+import org.osate.aadl2.AbstractNamedValue;
 import org.osate.aadl2.BooleanLiteral;
 import org.osate.aadl2.Classifier;
 import org.osate.aadl2.EnumerationLiteral;
-import org.osate.aadl2.EnumerationValue;
+import org.osate.aadl2.NamedValue;
 import org.osate.aadl2.IntegerLiteral;
 import org.osate.aadl2.NamedElement;
 import org.osate.aadl2.NumberType;
@@ -283,7 +284,10 @@ public class PropertyUtils {
 			IllegalArgumentException, PropertyDoesNotApplyToHolderException, PropertyIsListException,
 			ClassCastException {
 		final PropertyExpression pv = getSimplePropertyValue(ph, pd);
-		return ((EnumerationValue) pv).getLiteral();
+		AbstractNamedValue nv = ((NamedValue) pv).getNamedValue();
+		if (nv instanceof EnumerationLiteral)
+			return (EnumerationLiteral)nv;
+		return null;
 	}
 
 	/**
@@ -776,12 +780,12 @@ public class PropertyUtils {
 	 * @return An enumeration value for the given literal
 	 * @throws IllegalArgumentException Thrown if literal is null
 	 */
-	public static EnumerationValue createEnumValue(EnumerationLiteral literal) throws IllegalArgumentException {
+	public static NamedValue createEnumValue(EnumerationLiteral literal) throws IllegalArgumentException {
 		if (literal == null) {
 			throw new IllegalArgumentException("Enumeration literal is null.");
 		}
-		EnumerationValue newPropertyValue = Aadl2Factory.eINSTANCE.createEnumerationValue();
-		newPropertyValue.setLiteral(literal);
+		NamedValue newPropertyValue = Aadl2Factory.eINSTANCE.createNamedValue();
+		newPropertyValue.setNamedValue(literal);
 		return newPropertyValue;
 	}
 
