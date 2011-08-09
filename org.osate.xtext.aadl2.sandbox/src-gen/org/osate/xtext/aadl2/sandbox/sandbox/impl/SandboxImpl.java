e4e328a2944d70bd54e17e8c7f5dd988626151d1
Peter Feiler <phf@sei.cmu.edu>
2011-08-09 09:30:52 -0400

    initial sandbox

---
+++ org.osate.xtext.aadl2.sandbox/src-gen/org/osate/xtext/aadl2/sandbox/sandbox/impl/SandboxImpl.java
@@ -0,0 +1,244 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+
+ */
+package org.osate.xtext.aadl2.sandbox.sandbox.impl;
+
+import java.util.Collection;
+
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.notify.NotificationChain;
+
+import org.eclipse.emf.common.util.EList;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.InternalEObject;
+
+import org.eclipse.emf.ecore.impl.ENotificationImpl;
+import org.eclipse.emf.ecore.impl.MinimalEObjectImpl;
+
+import org.eclipse.emf.ecore.util.EObjectContainmentEList;
+import org.eclipse.emf.ecore.util.InternalEList;
+
+import org.osate.xtext.aadl2.sandbox.sandbox.Sandbox;
+import org.osate.xtext.aadl2.sandbox.sandbox.SandboxPackage;
+import org.osate.xtext.aadl2.sandbox.sandbox.SystemConfiguration;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Sandbox</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * The following features are implemented:
+ * <ul>
+ *   <li>{@link org.osate.xtext.aadl2.sandbox.sandbox.impl.SandboxImpl#getName <em>Name</em>}</li>
+ *   <li>{@link org.osate.xtext.aadl2.sandbox.sandbox.impl.SandboxImpl#getSystemInstance <em>System Instance</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @generated
+ */
+public class SandboxImpl extends MinimalEObjectImpl.Container implements Sandbox
+{
+  /**
+   * The default value of the '{@link #getName() <em>Name</em>}' attribute.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see #getName()
+   * @generated
+   * @ordered
+   */
+  protected static final String NAME_EDEFAULT = null;
+
+  /**
+   * The cached value of the '{@link #getName() <em>Name</em>}' attribute.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see #getName()
+   * @generated
+   * @ordered
+   */
+  protected String name = NAME_EDEFAULT;
+
+  /**
+   * The cached value of the '{@link #getSystemInstance() <em>System Instance</em>}' containment reference list.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see #getSystemInstance()
+   * @generated
+   * @ordered
+   */
+  protected EList<SystemConfiguration> systemInstance;
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  protected SandboxImpl()
+  {
+    super();
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  protected EClass eStaticClass()
+  {
+    return SandboxPackage.Literals.SANDBOX;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public String getName()
+  {
+    return name;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public void setName(String newName)
+  {
+    String oldName = name;
+    name = newName;
+    if (eNotificationRequired())
+      eNotify(new ENotificationImpl(this, Notification.SET, SandboxPackage.SANDBOX__NAME, oldName, name));
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public EList<SystemConfiguration> getSystemInstance()
+  {
+    if (systemInstance == null)
+    {
+      systemInstance = new EObjectContainmentEList<SystemConfiguration>(SystemConfiguration.class, this, SandboxPackage.SANDBOX__SYSTEM_INSTANCE);
+    }
+    return systemInstance;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  public NotificationChain eInverseRemove(InternalEObject otherEnd, int featureID, NotificationChain msgs)
+  {
+    switch (featureID)
+    {
+      case SandboxPackage.SANDBOX__SYSTEM_INSTANCE:
+        return ((InternalEList<?>)getSystemInstance()).basicRemove(otherEnd, msgs);
+    }
+    return super.eInverseRemove(otherEnd, featureID, msgs);
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  public Object eGet(int featureID, boolean resolve, boolean coreType)
+  {
+    switch (featureID)
+    {
+      case SandboxPackage.SANDBOX__NAME:
+        return getName();
+      case SandboxPackage.SANDBOX__SYSTEM_INSTANCE:
+        return getSystemInstance();
+    }
+    return super.eGet(featureID, resolve, coreType);
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @SuppressWarnings("unchecked")
+  @Override
+  public void eSet(int featureID, Object newValue)
+  {
+    switch (featureID)
+    {
+      case SandboxPackage.SANDBOX__NAME:
+        setName((String)newValue);
+        return;
+      case SandboxPackage.SANDBOX__SYSTEM_INSTANCE:
+        getSystemInstance().clear();
+        getSystemInstance().addAll((Collection<? extends SystemConfiguration>)newValue);
+        return;
+    }
+    super.eSet(featureID, newValue);
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  public void eUnset(int featureID)
+  {
+    switch (featureID)
+    {
+      case SandboxPackage.SANDBOX__NAME:
+        setName(NAME_EDEFAULT);
+        return;
+      case SandboxPackage.SANDBOX__SYSTEM_INSTANCE:
+        getSystemInstance().clear();
+        return;
+    }
+    super.eUnset(featureID);
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  public boolean eIsSet(int featureID)
+  {
+    switch (featureID)
+    {
+      case SandboxPackage.SANDBOX__NAME:
+        return NAME_EDEFAULT == null ? name != null : !NAME_EDEFAULT.equals(name);
+      case SandboxPackage.SANDBOX__SYSTEM_INSTANCE:
+        return systemInstance != null && !systemInstance.isEmpty();
+    }
+    return super.eIsSet(featureID);
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  public String toString()
+  {
+    if (eIsProxy()) return super.toString();
+
+    StringBuffer result = new StringBuffer(super.toString());
+    result.append(" (name: ");
+    result.append(name);
+    result.append(')');
+    return result.toString();
+  }
+
+} //SandboxImpl
---
+++ org.osate.xtext.aadl2.sandbox/src-gen/org/osate/xtext/aadl2/sandbox/sandbox/util/SandboxAdapterFactory.java
@@ -0,0 +1,176 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+
+ */
+package org.osate.xtext.aadl2.sandbox.sandbox.util;
+
+import org.eclipse.emf.common.notify.Adapter;
+import org.eclipse.emf.common.notify.Notifier;
+
+import org.eclipse.emf.common.notify.impl.AdapterFactoryImpl;
+
+import org.eclipse.emf.ecore.EObject;
+
+import org.osate.xtext.aadl2.sandbox.sandbox.*;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Adapter Factory</b> for the model.
+ * It provides an adapter <code>createXXX</code> method for each class of the model.
+ * <!-- end-user-doc -->
+ * @see org.osate.xtext.aadl2.sandbox.sandbox.SandboxPackage
+ * @generated
+ */
+public class SandboxAdapterFactory extends AdapterFactoryImpl
+{
+  /**
+   * The cached model package.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  protected static SandboxPackage modelPackage;
+
+  /**
+   * Creates an instance of the adapter factory.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public SandboxAdapterFactory()
+  {
+    if (modelPackage == null)
+    {
+      modelPackage = SandboxPackage.eINSTANCE;
+    }
+  }
+
+  /**
+   * Returns whether this factory is applicable for the type of the object.
+   * <!-- begin-user-doc -->
+   * This implementation returns <code>true</code> if the object is either the model's package or is an instance object of the model.
+   * <!-- end-user-doc -->
+   * @return whether this factory is applicable for the type of the object.
+   * @generated
+   */
+  @Override
+  public boolean isFactoryForType(Object object)
+  {
+    if (object == modelPackage)
+    {
+      return true;
+    }
+    if (object instanceof EObject)
+    {
+      return ((EObject)object).eClass().getEPackage() == modelPackage;
+    }
+    return false;
+  }
+
+  /**
+   * The switch that delegates to the <code>createXXX</code> methods.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  protected SandboxSwitch<Adapter> modelSwitch =
+    new SandboxSwitch<Adapter>()
+    {
+      @Override
+      public Adapter caseSandbox(Sandbox object)
+      {
+        return createSandboxAdapter();
+      }
+      @Override
+      public Adapter caseSystemConfiguration(SystemConfiguration object)
+      {
+        return createSystemConfigurationAdapter();
+      }
+      @Override
+      public Adapter caseSystemInstanceProxy(SystemInstanceProxy object)
+      {
+        return createSystemInstanceProxyAdapter();
+      }
+      @Override
+      public Adapter defaultCase(EObject object)
+      {
+        return createEObjectAdapter();
+      }
+    };
+
+  /**
+   * Creates an adapter for the <code>target</code>.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @param target the object to adapt.
+   * @return the adapter for the <code>target</code>.
+   * @generated
+   */
+  @Override
+  public Adapter createAdapter(Notifier target)
+  {
+    return modelSwitch.doSwitch((EObject)target);
+  }
+
+
+  /**
+   * Creates a new adapter for an object of class '{@link org.osate.xtext.aadl2.sandbox.sandbox.Sandbox <em>Sandbox</em>}'.
+   * <!-- begin-user-doc -->
+   * This default implementation returns null so that we can easily ignore cases;
+   * it's useful to ignore a case when inheritance will catch all the cases anyway.
+   * <!-- end-user-doc -->
+   * @return the new adapter.
+   * @see org.osate.xtext.aadl2.sandbox.sandbox.Sandbox
+   * @generated
+   */
+  public Adapter createSandboxAdapter()
+  {
+    return null;
+  }
+
+  /**
+   * Creates a new adapter for an object of class '{@link org.osate.xtext.aadl2.sandbox.sandbox.SystemConfiguration <em>System Configuration</em>}'.
+   * <!-- begin-user-doc -->
+   * This default implementation returns null so that we can easily ignore cases;
+   * it's useful to ignore a case when inheritance will catch all the cases anyway.
+   * <!-- end-user-doc -->
+   * @return the new adapter.
+   * @see org.osate.xtext.aadl2.sandbox.sandbox.SystemConfiguration
+   * @generated
+   */
+  public Adapter createSystemConfigurationAdapter()
+  {
+    return null;
+  }
+
+  /**
+   * Creates a new adapter for an object of class '{@link org.osate.xtext.aadl2.sandbox.sandbox.SystemInstanceProxy <em>System Instance Proxy</em>}'.
+   * <!-- begin-user-doc -->
+   * This default implementation returns null so that we can easily ignore cases;
+   * it's useful to ignore a case when inheritance will catch all the cases anyway.
+   * <!-- end-user-doc -->
+   * @return the new adapter.
+   * @see org.osate.xtext.aadl2.sandbox.sandbox.SystemInstanceProxy
+   * @generated
+   */
+  public Adapter createSystemInstanceProxyAdapter()
+  {
+    return null;
+  }
+
+  /**
+   * Creates a new adapter for the default case.
+   * <!-- begin-user-doc -->
+   * This default implementation returns null.
+   * <!-- end-user-doc -->
+   * @return the new adapter.
+   * @generated
+   */
+  public Adapter createEObjectAdapter()
+  {
+    return null;
+  }
+
+} //SandboxAdapterFactory
