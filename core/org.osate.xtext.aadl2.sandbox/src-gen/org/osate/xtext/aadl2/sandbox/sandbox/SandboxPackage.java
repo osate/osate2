6e7bf50fe8af21b3d3d8bcc853858fbd536dd2e7
Peter Feiler <phf@sei.cmu.edu>
2011-08-09 11:02:37 -0400

    initial sandbox

---
+++ core/org.osate.xtext.aadl2.sandbox/src-gen/org/osate/xtext/aadl2/sandbox/sandbox/SandboxPackage.java
@@ -0,0 +1,374 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+
+ */
+package org.osate.xtext.aadl2.sandbox.sandbox;
+
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EReference;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Package</b> for the model.
+ * It contains accessors for the meta objects to represent
+ * <ul>
+ *   <li>each class,</li>
+ *   <li>each feature of each class,</li>
+ *   <li>each enum,</li>
+ *   <li>and each data type</li>
+ * </ul>
+ * <!-- end-user-doc -->
+ * @see org.osate.xtext.aadl2.sandbox.sandbox.SandboxFactory
+ * @model kind="package"
+ * @generated
+ */
+public interface SandboxPackage extends EPackage
+{
+  /**
+   * The package name.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  String eNAME = "sandbox";
+
+  /**
+   * The package namespace URI.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  String eNS_URI = "http://www.osate.org/xtext/aadl2/sandbox/Sandbox";
+
+  /**
+   * The package namespace name.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  String eNS_PREFIX = "sandbox";
+
+  /**
+   * The singleton instance of the package.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  SandboxPackage eINSTANCE = org.osate.xtext.aadl2.sandbox.sandbox.impl.SandboxPackageImpl.init();
+
+  /**
+   * The meta object id for the '{@link org.osate.xtext.aadl2.sandbox.sandbox.impl.SandboxImpl <em>Sandbox</em>}' class.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see org.osate.xtext.aadl2.sandbox.sandbox.impl.SandboxImpl
+   * @see org.osate.xtext.aadl2.sandbox.sandbox.impl.SandboxPackageImpl#getSandbox()
+   * @generated
+   */
+  int SANDBOX = 0;
+
+  /**
+   * The feature id for the '<em><b>Name</b></em>' attribute.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   * @ordered
+   */
+  int SANDBOX__NAME = 0;
+
+  /**
+   * The feature id for the '<em><b>System Instance</b></em>' containment reference list.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   * @ordered
+   */
+  int SANDBOX__SYSTEM_INSTANCE = 1;
+
+  /**
+   * The number of structural features of the '<em>Sandbox</em>' class.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   * @ordered
+   */
+  int SANDBOX_FEATURE_COUNT = 2;
+
+  /**
+   * The meta object id for the '{@link org.osate.xtext.aadl2.sandbox.sandbox.impl.SystemConfigurationImpl <em>System Configuration</em>}' class.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see org.osate.xtext.aadl2.sandbox.sandbox.impl.SystemConfigurationImpl
+   * @see org.osate.xtext.aadl2.sandbox.sandbox.impl.SandboxPackageImpl#getSystemConfiguration()
+   * @generated
+   */
+  int SYSTEM_CONFIGURATION = 1;
+
+  /**
+   * The feature id for the '<em><b>Name</b></em>' attribute.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   * @ordered
+   */
+  int SYSTEM_CONFIGURATION__NAME = 0;
+
+  /**
+   * The feature id for the '<em><b>System Implementation</b></em>' reference.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   * @ordered
+   */
+  int SYSTEM_CONFIGURATION__SYSTEM_IMPLEMENTATION = 1;
+
+  /**
+   * The feature id for the '<em><b>System Instance Proxy</b></em>' containment reference.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   * @ordered
+   */
+  int SYSTEM_CONFIGURATION__SYSTEM_INSTANCE_PROXY = 2;
+
+  /**
+   * The number of structural features of the '<em>System Configuration</em>' class.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   * @ordered
+   */
+  int SYSTEM_CONFIGURATION_FEATURE_COUNT = 3;
+
+  /**
+   * The meta object id for the '{@link org.osate.xtext.aadl2.sandbox.sandbox.impl.SystemInstanceProxyImpl <em>System Instance Proxy</em>}' class.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see org.osate.xtext.aadl2.sandbox.sandbox.impl.SystemInstanceProxyImpl
+   * @see org.osate.xtext.aadl2.sandbox.sandbox.impl.SandboxPackageImpl#getSystemInstanceProxy()
+   * @generated
+   */
+  int SYSTEM_INSTANCE_PROXY = 2;
+
+  /**
+   * The feature id for the '<em><b>Instance Ref</b></em>' reference.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   * @ordered
+   */
+  int SYSTEM_INSTANCE_PROXY__INSTANCE_REF = 0;
+
+  /**
+   * The number of structural features of the '<em>System Instance Proxy</em>' class.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   * @ordered
+   */
+  int SYSTEM_INSTANCE_PROXY_FEATURE_COUNT = 1;
+
+
+  /**
+   * Returns the meta object for class '{@link org.osate.xtext.aadl2.sandbox.sandbox.Sandbox <em>Sandbox</em>}'.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return the meta object for class '<em>Sandbox</em>'.
+   * @see org.osate.xtext.aadl2.sandbox.sandbox.Sandbox
+   * @generated
+   */
+  EClass getSandbox();
+
+  /**
+   * Returns the meta object for the attribute '{@link org.osate.xtext.aadl2.sandbox.sandbox.Sandbox#getName <em>Name</em>}'.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return the meta object for the attribute '<em>Name</em>'.
+   * @see org.osate.xtext.aadl2.sandbox.sandbox.Sandbox#getName()
+   * @see #getSandbox()
+   * @generated
+   */
+  EAttribute getSandbox_Name();
+
+  /**
+   * Returns the meta object for the containment reference list '{@link org.osate.xtext.aadl2.sandbox.sandbox.Sandbox#getSystemInstance <em>System Instance</em>}'.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return the meta object for the containment reference list '<em>System Instance</em>'.
+   * @see org.osate.xtext.aadl2.sandbox.sandbox.Sandbox#getSystemInstance()
+   * @see #getSandbox()
+   * @generated
+   */
+  EReference getSandbox_SystemInstance();
+
+  /**
+   * Returns the meta object for class '{@link org.osate.xtext.aadl2.sandbox.sandbox.SystemConfiguration <em>System Configuration</em>}'.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return the meta object for class '<em>System Configuration</em>'.
+   * @see org.osate.xtext.aadl2.sandbox.sandbox.SystemConfiguration
+   * @generated
+   */
+  EClass getSystemConfiguration();
+
+  /**
+   * Returns the meta object for the attribute '{@link org.osate.xtext.aadl2.sandbox.sandbox.SystemConfiguration#getName <em>Name</em>}'.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return the meta object for the attribute '<em>Name</em>'.
+   * @see org.osate.xtext.aadl2.sandbox.sandbox.SystemConfiguration#getName()
+   * @see #getSystemConfiguration()
+   * @generated
+   */
+  EAttribute getSystemConfiguration_Name();
+
+  /**
+   * Returns the meta object for the reference '{@link org.osate.xtext.aadl2.sandbox.sandbox.SystemConfiguration#getSystemImplementation <em>System Implementation</em>}'.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return the meta object for the reference '<em>System Implementation</em>'.
+   * @see org.osate.xtext.aadl2.sandbox.sandbox.SystemConfiguration#getSystemImplementation()
+   * @see #getSystemConfiguration()
+   * @generated
+   */
+  EReference getSystemConfiguration_SystemImplementation();
+
+  /**
+   * Returns the meta object for the containment reference '{@link org.osate.xtext.aadl2.sandbox.sandbox.SystemConfiguration#getSystemInstanceProxy <em>System Instance Proxy</em>}'.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return the meta object for the containment reference '<em>System Instance Proxy</em>'.
+   * @see org.osate.xtext.aadl2.sandbox.sandbox.SystemConfiguration#getSystemInstanceProxy()
+   * @see #getSystemConfiguration()
+   * @generated
+   */
+  EReference getSystemConfiguration_SystemInstanceProxy();
+
+  /**
+   * Returns the meta object for class '{@link org.osate.xtext.aadl2.sandbox.sandbox.SystemInstanceProxy <em>System Instance Proxy</em>}'.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return the meta object for class '<em>System Instance Proxy</em>'.
+   * @see org.osate.xtext.aadl2.sandbox.sandbox.SystemInstanceProxy
+   * @generated
+   */
+  EClass getSystemInstanceProxy();
+
+  /**
+   * Returns the meta object for the reference '{@link org.osate.xtext.aadl2.sandbox.sandbox.SystemInstanceProxy#getInstanceRef <em>Instance Ref</em>}'.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return the meta object for the reference '<em>Instance Ref</em>'.
+   * @see org.osate.xtext.aadl2.sandbox.sandbox.SystemInstanceProxy#getInstanceRef()
+   * @see #getSystemInstanceProxy()
+   * @generated
+   */
+  EReference getSystemInstanceProxy_InstanceRef();
+
+  /**
+   * Returns the factory that creates the instances of the model.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return the factory that creates the instances of the model.
+   * @generated
+   */
+  SandboxFactory getSandboxFactory();
+
+  /**
+   * <!-- begin-user-doc -->
+   * Defines literals for the meta objects that represent
+   * <ul>
+   *   <li>each class,</li>
+   *   <li>each feature of each class,</li>
+   *   <li>each enum,</li>
+   *   <li>and each data type</li>
+   * </ul>
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  interface Literals
+  {
+    /**
+     * The meta object literal for the '{@link org.osate.xtext.aadl2.sandbox.sandbox.impl.SandboxImpl <em>Sandbox</em>}' class.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @see org.osate.xtext.aadl2.sandbox.sandbox.impl.SandboxImpl
+     * @see org.osate.xtext.aadl2.sandbox.sandbox.impl.SandboxPackageImpl#getSandbox()
+     * @generated
+     */
+    EClass SANDBOX = eINSTANCE.getSandbox();
+
+    /**
+     * The meta object literal for the '<em><b>Name</b></em>' attribute feature.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    EAttribute SANDBOX__NAME = eINSTANCE.getSandbox_Name();
+
+    /**
+     * The meta object literal for the '<em><b>System Instance</b></em>' containment reference list feature.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    EReference SANDBOX__SYSTEM_INSTANCE = eINSTANCE.getSandbox_SystemInstance();
+
+    /**
+     * The meta object literal for the '{@link org.osate.xtext.aadl2.sandbox.sandbox.impl.SystemConfigurationImpl <em>System Configuration</em>}' class.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @see org.osate.xtext.aadl2.sandbox.sandbox.impl.SystemConfigurationImpl
+     * @see org.osate.xtext.aadl2.sandbox.sandbox.impl.SandboxPackageImpl#getSystemConfiguration()
+     * @generated
+     */
+    EClass SYSTEM_CONFIGURATION = eINSTANCE.getSystemConfiguration();
+
+    /**
+     * The meta object literal for the '<em><b>Name</b></em>' attribute feature.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    EAttribute SYSTEM_CONFIGURATION__NAME = eINSTANCE.getSystemConfiguration_Name();
+
+    /**
+     * The meta object literal for the '<em><b>System Implementation</b></em>' reference feature.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    EReference SYSTEM_CONFIGURATION__SYSTEM_IMPLEMENTATION = eINSTANCE.getSystemConfiguration_SystemImplementation();
+
+    /**
+     * The meta object literal for the '<em><b>System Instance Proxy</b></em>' containment reference feature.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    EReference SYSTEM_CONFIGURATION__SYSTEM_INSTANCE_PROXY = eINSTANCE.getSystemConfiguration_SystemInstanceProxy();
+
+    /**
+     * The meta object literal for the '{@link org.osate.xtext.aadl2.sandbox.sandbox.impl.SystemInstanceProxyImpl <em>System Instance Proxy</em>}' class.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @see org.osate.xtext.aadl2.sandbox.sandbox.impl.SystemInstanceProxyImpl
+     * @see org.osate.xtext.aadl2.sandbox.sandbox.impl.SandboxPackageImpl#getSystemInstanceProxy()
+     * @generated
+     */
+    EClass SYSTEM_INSTANCE_PROXY = eINSTANCE.getSystemInstanceProxy();
+
+    /**
+     * The meta object literal for the '<em><b>Instance Ref</b></em>' reference feature.
+     * <!-- begin-user-doc -->
+     * <!-- end-user-doc -->
+     * @generated
+     */
+    EReference SYSTEM_INSTANCE_PROXY__INSTANCE_REF = eINSTANCE.getSystemInstanceProxy_InstanceRef();
+
+  }
+
+} //SandboxPackage
---
+++ core/org.osate.xtext.aadl2.sandbox/src-gen/org/osate/xtext/aadl2/sandbox/sandbox/util/SandboxSwitch.java
@@ -0,0 +1,169 @@
+/**
+ * <copyright>
+ * </copyright>
+ *
+
+ */
+package org.osate.xtext.aadl2.sandbox.sandbox.util;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EPackage;
+
+import org.eclipse.emf.ecore.util.Switch;
+
+import org.osate.xtext.aadl2.sandbox.sandbox.*;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Switch</b> for the model's inheritance hierarchy.
+ * It supports the call {@link #doSwitch(EObject) doSwitch(object)}
+ * to invoke the <code>caseXXX</code> method for each class of the model,
+ * starting with the actual class of the object
+ * and proceeding up the inheritance hierarchy
+ * until a non-null result is returned,
+ * which is the result of the switch.
+ * <!-- end-user-doc -->
+ * @see org.osate.xtext.aadl2.sandbox.sandbox.SandboxPackage
+ * @generated
+ */
+public class SandboxSwitch<T> extends Switch<T>
+{
+  /**
+   * The cached model package
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  protected static SandboxPackage modelPackage;
+
+  /**
+   * Creates an instance of the switch.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public SandboxSwitch()
+  {
+    if (modelPackage == null)
+    {
+      modelPackage = SandboxPackage.eINSTANCE;
+    }
+  }
+
+  /**
+   * Checks whether this is a switch for the given package.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @parameter ePackage the package in question.
+   * @return whether this is a switch for the given package.
+   * @generated
+   */
+  @Override
+  protected boolean isSwitchFor(EPackage ePackage)
+  {
+    return ePackage == modelPackage;
+  }
+
+  /**
+   * Calls <code>caseXXX</code> for each class of the model until one returns a non null result; it yields that result.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @return the first non-null result returned by a <code>caseXXX</code> call.
+   * @generated
+   */
+  @Override
+  protected T doSwitch(int classifierID, EObject theEObject)
+  {
+    switch (classifierID)
+    {
+      case SandboxPackage.SANDBOX:
+      {
+        Sandbox sandbox = (Sandbox)theEObject;
+        T result = caseSandbox(sandbox);
+        if (result == null) result = defaultCase(theEObject);
+        return result;
+      }
+      case SandboxPackage.SYSTEM_CONFIGURATION:
+      {
+        SystemConfiguration systemConfiguration = (SystemConfiguration)theEObject;
+        T result = caseSystemConfiguration(systemConfiguration);
+        if (result == null) result = defaultCase(theEObject);
+        return result;
+      }
+      case SandboxPackage.SYSTEM_INSTANCE_PROXY:
+      {
+        SystemInstanceProxy systemInstanceProxy = (SystemInstanceProxy)theEObject;
+        T result = caseSystemInstanceProxy(systemInstanceProxy);
+        if (result == null) result = defaultCase(theEObject);
+        return result;
+      }
+      default: return defaultCase(theEObject);
+    }
+  }
+
+  /**
+   * Returns the result of interpreting the object as an instance of '<em>Sandbox</em>'.
+   * <!-- begin-user-doc -->
+   * This implementation returns null;
+   * returning a non-null result will terminate the switch.
+   * <!-- end-user-doc -->
+   * @param object the target of the switch.
+   * @return the result of interpreting the object as an instance of '<em>Sandbox</em>'.
+   * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
+   * @generated
+   */
+  public T caseSandbox(Sandbox object)
+  {
+    return null;
+  }
+
+  /**
+   * Returns the result of interpreting the object as an instance of '<em>System Configuration</em>'.
+   * <!-- begin-user-doc -->
+   * This implementation returns null;
+   * returning a non-null result will terminate the switch.
+   * <!-- end-user-doc -->
+   * @param object the target of the switch.
+   * @return the result of interpreting the object as an instance of '<em>System Configuration</em>'.
+   * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
+   * @generated
+   */
+  public T caseSystemConfiguration(SystemConfiguration object)
+  {
+    return null;
+  }
+
+  /**
+   * Returns the result of interpreting the object as an instance of '<em>System Instance Proxy</em>'.
+   * <!-- begin-user-doc -->
+   * This implementation returns null;
+   * returning a non-null result will terminate the switch.
+   * <!-- end-user-doc -->
+   * @param object the target of the switch.
+   * @return the result of interpreting the object as an instance of '<em>System Instance Proxy</em>'.
+   * @see #doSwitch(org.eclipse.emf.ecore.EObject) doSwitch(EObject)
+   * @generated
+   */
+  public T caseSystemInstanceProxy(SystemInstanceProxy object)
+  {
+    return null;
+  }
+
+  /**
+   * Returns the result of interpreting the object as an instance of '<em>EObject</em>'.
+   * <!-- begin-user-doc -->
+   * This implementation returns null;
+   * returning a non-null result will terminate the switch, but this is the last case anyway.
+   * <!-- end-user-doc -->
+   * @param object the target of the switch.
+   * @return the result of interpreting the object as an instance of '<em>EObject</em>'.
+   * @see #doSwitch(org.eclipse.emf.ecore.EObject)
+   * @generated
+   */
+  @Override
+  public T defaultCase(EObject object)
+  {
+    return null;
+  }
+
+} //SandboxSwitch
