1f6e238d7a390003c08f623a1bc3674a6727c0d8
Lutz Wrage <lutz.wrage@gmail.com>
2011-08-10 10:06:46 -0400

    Tycho build for osate core

--- org.osate.contribution.sei/src/org/osate/contribution/sei/Activator.java
+++ org.osate.contribution.sei/src/org/osate/contribution/sei/Activator.java
@@ -9,7 +9,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends Plugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "edu.cmu.sei.aadl.properties.SEI";
+	public static final String PLUGIN_ID = "org.osate.contribution.sei";
 
 	// The shared instance
 	private static Activator plugin;
