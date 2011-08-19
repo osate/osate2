ad4d7ba9c7577159eb584ca25b65d566615f4079
Peter Feiler <phf@sei.cmu.edu>
2011-08-19 11:02:53 -0400

    project wizard does not create aadl and aaxl folder.

--- org.osate.ui/src/org/osate/ui/wizards/AadlProjectWizard.java
+++ org.osate.ui/src/org/osate/ui/wizards/AadlProjectWizard.java
@@ -341,19 +341,19 @@ public class AadlProjectWizard extends BasicNewResourceWizard implements
         updatePerspective();
         selectAndReveal(newProject);
         final IProject p = getNewProject();
-        final IFolder defModDir = p.getFolder(WorkspacePlugin.DEFAULT_MODEL_DIR);
-        final IFolder xmlPack = defModDir.getFolder(WorkspacePlugin.AADL_PACKAGES_DIR);
-        final IFolder xmlPSet = defModDir.getFolder(WorkspacePlugin.PROPERTY_SETS_DIR);
-        final IFolder defSrcDir = p.getFolder(WorkspacePlugin.DEFAULT_SOURCE_DIR);
-        final IFolder srcPack = defSrcDir.getFolder(WorkspacePlugin.AADL_PACKAGES_DIR);
-        final IFolder srcPSet = defSrcDir.getFolder(WorkspacePlugin.PROPERTY_SETS_DIR);
+//        final IFolder defModDir = p.getFolder(WorkspacePlugin.DEFAULT_MODEL_DIR);
+        final IFolder xmlPack = p.getFolder(WorkspacePlugin.AADL_PACKAGES_DIR);
+        final IFolder xmlPSet = p.getFolder(WorkspacePlugin.PROPERTY_SETS_DIR);
+//        final IFolder defSrcDir = p.getFolder(WorkspacePlugin.DEFAULT_SOURCE_DIR);
+//        final IFolder srcPack = defSrcDir.getFolder(WorkspacePlugin.AADL_PACKAGES_DIR);
+//        final IFolder srcPSet = defSrcDir.getFolder(WorkspacePlugin.PROPERTY_SETS_DIR);
         
         try
         {
             CoreUtility.createFolder(xmlPack, true, true, null);
             CoreUtility.createFolder(xmlPSet, true, true, null);
-            CoreUtility.createFolder(srcPack, true, true, null);
-            CoreUtility.createFolder(srcPSet, true, true, null);
+//            CoreUtility.createFolder(srcPack, true, true, null);
+//            CoreUtility.createFolder(srcPSet, true, true, null);
         } catch (CoreException e)
         {
             // TODO Auto-generated catch block
@@ -363,10 +363,10 @@ public class AadlProjectWizard extends BasicNewResourceWizard implements
                 .getRawLocation().toString();
 
         PreferenceStore ps = new PreferenceStore(filepath);
-        ps.setValue(WorkspacePlugin.PROJECT_SOURCE_DIR, 
-                WorkspacePlugin.DEFAULT_SOURCE_DIR);
-        ps.setValue(WorkspacePlugin.PROJECT_MODEL_DIR,
-                WorkspacePlugin.DEFAULT_MODEL_DIR);
+//        ps.setValue(WorkspacePlugin.PROJECT_SOURCE_DIR, 
+//                WorkspacePlugin.DEFAULT_SOURCE_DIR);
+//        ps.setValue(WorkspacePlugin.PROJECT_MODEL_DIR,
+//                WorkspacePlugin.DEFAULT_MODEL_DIR);
         try
         {
             ps.save();
