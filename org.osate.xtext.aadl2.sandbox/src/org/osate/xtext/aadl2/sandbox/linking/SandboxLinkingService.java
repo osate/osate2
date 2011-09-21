7cfc65f125fbd377066dcd86f1461a9ec476ecf4
phf <phf@RM-GRASSSNAKE.ad.sei.cmu.edu>
2011-09-21 13:43:16 -0400

    deleted sandbox

--- org.osate.xtext.aadl2.sandbox/src/org/osate/xtext/aadl2/sandbox/linking/SandboxLinkingService.java
+++
@@ -1,6 +0,0 @@
-package org.osate.xtext.aadl2.sandbox.linking;
-
-import org.eclipse.xtext.linking.impl.DefaultLinkingService;
-
-public class SandboxLinkingService extends DefaultLinkingService {
-}
