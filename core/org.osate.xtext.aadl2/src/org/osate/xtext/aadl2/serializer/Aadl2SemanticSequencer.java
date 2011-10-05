dc3c26ad1c82b3e81ab0d66d3685e9a943e1a597
phf <phf@RM-GRASSSNAKE.ad.sei.cmu.edu>
2011-10-04 20:41:15 -0400

    new serializer files

---
+++ core/org.osate.xtext.aadl2/src/org/osate/xtext/aadl2/serializer/Aadl2SemanticSequencer.java
@@ -0,0 +1,4 @@
+package org.osate.xtext.aadl2.serializer;
+
+public class Aadl2SemanticSequencer extends AbstractAadl2SemanticSequencer {
+}
---
+++ core/org.osate.xtext.aadl2/src/org/osate/xtext/aadl2/serializer/Aadl2SyntacticSequencer.java
@@ -0,0 +1,4 @@
+package org.osate.xtext.aadl2.serializer;
+
+public class Aadl2SyntacticSequencer extends AbstractAadl2SyntacticSequencer {
+}
