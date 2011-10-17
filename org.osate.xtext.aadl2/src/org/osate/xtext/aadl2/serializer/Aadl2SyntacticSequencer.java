b9fe5fd57fbd8b43a9bb4b2473bd06f2a6c2b6ed
Peter Feiler <phf@sei.cmu.edu>
2011-10-17 09:50:11 +0200

    added code to handle ID and FULLINAME as unassigned rules in serializing

--- org.osate.xtext.aadl2/src/org/osate/xtext/aadl2/serializer/Aadl2SyntacticSequencer.java
+++ org.osate.xtext.aadl2/src/org/osate/xtext/aadl2/serializer/Aadl2SyntacticSequencer.java
@@ -1,4 +1,28 @@
 package org.osate.xtext.aadl2.serializer;
 
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.xtext.RuleCall;
+import org.eclipse.xtext.nodemodel.INode;
+import org.osate.aadl2.AadlPackage;
+import org.osate.aadl2.Classifier;
+import org.osate.aadl2.ComponentImplementation;
+
 public class Aadl2SyntacticSequencer extends AbstractAadl2SyntacticSequencer {
+
+	@Override
+	protected String getIDToken(EObject semanticObject, RuleCall ruleCall, INode node) {
+		if (semanticObject instanceof Classifier)
+			return ((Classifier)semanticObject).getName();
+		if(semanticObject instanceof AadlPackage)
+			return ((AadlPackage)semanticObject).getName();
+		return super.getIDToken(semanticObject, ruleCall, node);
+	}
+	
+	@Override
+	protected String getFULLINAMEToken(EObject semanticObject, RuleCall ruleCall, INode node) {
+		if (semanticObject instanceof ComponentImplementation)
+ 			return ((ComponentImplementation)semanticObject).getName();
+		return super.getFULLINAMEToken(semanticObject, ruleCall, node);
+	}
+
 }
