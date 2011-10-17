30c8b4c43ab2657e7ae927af69678f699d5cb209
Peter Feiler <phf@sei.cmu.edu>
2011-10-18 00:03:21 +0200

    Fixed issue with unit literals, enumeration literals, and record field definitions. They needed to be qualified within their scope.

--- org.osate.xtext.aadl2/src/org/osate/xtext/aadl2/naming/Aadl2QualifiedNameProvider.java
+++ org.osate.xtext.aadl2/src/org/osate/xtext/aadl2/naming/Aadl2QualifiedNameProvider.java
@@ -4,8 +4,10 @@ import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtext.naming.DefaultDeclarativeQualifiedNameProvider;
 import org.eclipse.xtext.naming.QualifiedName;
 import org.osate.aadl2.AadlPackage;
+import org.osate.aadl2.BasicProperty;
 import org.osate.aadl2.Classifier;
 import org.osate.aadl2.Element;
+import org.osate.aadl2.EnumerationLiteral;
 import org.osate.aadl2.NamedElement;
 import org.osate.aadl2.Namespace;
 import org.osate.aadl2.PackageSection;
@@ -13,6 +15,7 @@ import org.osate.aadl2.Property;
 import org.osate.aadl2.PropertyConstant;
 import org.osate.aadl2.PropertySet;
 import org.osate.aadl2.PropertyType;
+import org.osate.aadl2.UnitLiteral;
 
 public class Aadl2QualifiedNameProvider extends DefaultDeclarativeQualifiedNameProvider {
 	
@@ -43,10 +46,18 @@ public class Aadl2QualifiedNameProvider extends DefaultDeclarativeQualifiedNameP
 		//		return getConverter().toQualifiedName(obj.getName());
 		return getConverter().toQualifiedName(obj.getQualifiedName());
 	}
-//	
-//	public QualifiedName qualifiedName(final UnitLiteral obj) {
-//		return null;//getConverter().toQualifiedName(obj.getQualifiedName());
-//	}
+	
+	public QualifiedName qualifiedName(final BasicProperty obj) {
+		return null;//getConverter().toQualifiedName(obj.getQualifiedName());
+	}
+	
+	public QualifiedName qualifiedName(final UnitLiteral obj) {
+		return null;//getConverter().toQualifiedName(obj.getQualifiedName());
+	}
+	
+	public QualifiedName qualifiedName(final EnumerationLiteral obj) {
+		return null;//getConverter().toQualifiedName(obj.getQualifiedName());
+	}
 	
 	public QualifiedName qualifiedName(final AadlPackage obj) {
 		return getConverter().toQualifiedName(obj.getName());
