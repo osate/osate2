ef9daa48d05fc89177f72d8e0ddfdf7afbb4e59a
phf <phf@RM-GRASSSNAKE.ad.sei.cmu.edu>
2011-10-24 17:24:12 -0400

    added hint for reference to classifier.

--- org.osate.xtext.aadl2.properties.ui/src/org/osate/xtext/aadl2/properties/ui/linking/PropertiesLinkingDiagnosticMessageProvider.java
+++ org.osate.xtext.aadl2.properties.ui/src/org/osate/xtext/aadl2/properties/ui/linking/PropertiesLinkingDiagnosticMessageProvider.java
@@ -12,12 +12,13 @@ public class PropertiesLinkingDiagnosticMessageProvider extends LinkingDiagnosti
 	@Override
 	public DiagnosticMessage getUnresolvedProxyMessage(ILinkingDiagnosticContext context) {
 		EClass referenceType = context.getReference().getEReferenceType();
-		String targetName = referenceType.getName();
+		String targetName = null;
 		if (Aadl2Package.eINSTANCE.getAbstractNamedValue() == referenceType){
 			targetName = "Property Constant, Property Definition, Enumeration or Unit literal";
+			String msg = "Couldn't resolve reference to " + targetName + " '" + context.getLinkText() + "'." + " For classifier references use classifier( <ref> ).";
+			return new DiagnosticMessage(msg, Severity.ERROR, Diagnostic.LINKING_DIAGNOSTIC);
 		}
-		String msg = "Couldn't resolve reference to " + targetName + " '" + context.getLinkText() + "'.";
-		return new DiagnosticMessage(msg, Severity.ERROR, Diagnostic.LINKING_DIAGNOSTIC);
+		return super.getUnresolvedProxyMessage(context);
 	}
 
 }
