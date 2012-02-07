ab36e6b94fd638ea5d8d256cf027f2ea5983dbe9
phf <phf@RM-GRASSSNAKE.ad.sei.cmu.edu>
2012-02-07 11:26:55 -0500

    switched to new serializer

---
+++ org.osate.xtext.aadl2.propertyset/src-gen/org/osate/xtext/aadl2/propertyset/serializer/AbstractPropertysetSyntacticSequencer.java
@@ -0,0 +1,54 @@
+package org.osate.xtext.aadl2.propertyset.serializer;
+
+import com.google.inject.Inject;
+import java.util.List;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.xtext.IGrammarAccess;
+import org.eclipse.xtext.RuleCall;
+import org.eclipse.xtext.nodemodel.INode;
+import org.eclipse.xtext.serializer.analysis.GrammarAlias.AbstractElementAlias;
+import org.eclipse.xtext.serializer.analysis.ISyntacticSequencerPDAProvider.ISynTransition;
+import org.eclipse.xtext.serializer.sequencer.AbstractSyntacticSequencer;
+import org.osate.xtext.aadl2.propertyset.services.PropertysetGrammarAccess;
+
+@SuppressWarnings("restriction")
+public class AbstractPropertysetSyntacticSequencer extends AbstractSyntacticSequencer {
+
+	protected PropertysetGrammarAccess grammarAccess;
+	
+	@Inject
+	protected void init(IGrammarAccess access) {
+		grammarAccess = (PropertysetGrammarAccess) access;
+	}
+	
+	@Override
+	protected String getUnassignedRuleCallToken(EObject semanticObject, RuleCall ruleCall, INode node) {
+		if(ruleCall.getRule() == grammarAccess.getIDRule())
+			return getIDToken(semanticObject, ruleCall, node);
+		else if(ruleCall.getRule() == grammarAccess.getSTARRule())
+			return getSTARToken(semanticObject, ruleCall, node);
+		return "";
+	}
+	
+	protected String getIDToken(EObject semanticObject, RuleCall ruleCall, INode node) {
+		if (node != null)
+			return getTokenText(node);
+		return "";
+	}
+	protected String getSTARToken(EObject semanticObject, RuleCall ruleCall, INode node) {
+		if (node != null)
+			return getTokenText(node);
+		return "*";
+	}
+	
+	@Override
+	protected void emitUnassignedTokens(EObject semanticObject, ISynTransition transition, INode fromNode, INode toNode) {
+		if (transition.getAmbiguousSyntaxes().isEmpty()) return;
+		List<INode> transitionNodes = collectNodes(fromNode, toNode);
+		for (AbstractElementAlias syntax : transition.getAmbiguousSyntaxes()) {
+			List<INode> syntaxNodes = getNodesFor(transitionNodes, syntax);
+			acceptNodes(getLastNavigableState(), syntaxNodes);
+		}
+	}
+
+}
