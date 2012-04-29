63f43b22154846d343e4a0c8beb5fca7ba50707e
Peter Feiler <phf@sei.cmu.edu>
2012-04-28 23:47:33 -0400

    transient value for call context as it is not assigned by grammar rule. Provide dotted name for suprogram call reference (context.calledsub).

--- org.osate.xtext.aadl2/src/org/osate/xtext/aadl2/serializer/Aadl2CrossReferenceSerializer.java
+++ org.osate.xtext.aadl2/src/org/osate/xtext/aadl2/serializer/Aadl2CrossReferenceSerializer.java
@@ -6,12 +6,14 @@ import org.eclipse.xtext.scoping.IScope;
 import org.eclipse.xtext.serializer.diagnostic.ISerializationDiagnostic.Acceptor;
 import org.eclipse.xtext.serializer.tokens.CrossReferenceSerializer;
 import org.osate.aadl2.AadlPackage;
+import org.osate.aadl2.CallContext;
 import org.osate.aadl2.Classifier;
 import org.osate.aadl2.Element;
 import org.osate.aadl2.NamedElement;
 import org.osate.aadl2.Property;
 import org.osate.aadl2.PropertyConstant;
 import org.osate.aadl2.PropertyType;
+import org.osate.aadl2.SubprogramCall;
 import org.osate.aadl2.modelsupport.util.AadlUtil;
 
 public class Aadl2CrossReferenceSerializer extends CrossReferenceSerializer {
@@ -26,6 +28,17 @@ public class Aadl2CrossReferenceSerializer extends CrossReferenceSerializer {
 		} else if (target instanceof Property || target instanceof PropertyType ||
 				target instanceof PropertyConstant){
 			return AadlUtil.getPropertySetElementName((NamedElement)target);
+		} else if (semanticObject instanceof SubprogramCall){
+			String s = "";
+			CallContext sc = ((SubprogramCall)semanticObject).getContext();
+			if (sc != null){
+				if (sc instanceof Classifier){
+					s = AadlUtil.getClassifierName((Classifier)sc, (Element) semanticObject)+".";
+				} else if (sc instanceof NamedElement){
+					s = ((NamedElement)sc).getName()+".";
+				}
+			}
+			return s+((NamedElement)target).getName();
 		} else if (target instanceof NamedElement){
 			return ((NamedElement)target).getName();
 		} else return "<noname>";
