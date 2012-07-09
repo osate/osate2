8a5bdc9be22e333e5435c62d67a4d0b629ab9058
Joe Seibel <jseibel@sei.cmu.edu>
2012-07-09 14:02:10 -0400

    Fixed null check when looking up a feature in a feature group for a flow end. The null check was supposed to prevent a NullPointerException when a feature group didn't have a feature group type, but it actually resulted in not finding the internal feature when the feature group did have a type.

--- org.osate.xtext.aadl2.properties/src/org/osate/xtext/aadl2/properties/linking/PropertiesLinkingService.java
+++ org.osate.xtext.aadl2.properties/src/org/osate/xtext/aadl2/properties/linking/PropertiesLinkingService.java
@@ -303,7 +303,7 @@ public class PropertiesLinkingService extends DefaultLinkingService {
 				Context flowendcxt = ((FlowEnd)context).getContext();
 				if (flowendcxt instanceof FeatureGroup){
 					FeatureGroupType fgt = ((FeatureGroup)flowendcxt).getAllFeatureGroupType();
-					if (fgt != null ) return Collections.emptyList();
+					if (fgt == null ) return Collections.emptyList();
 					if ( fgt.getInverse()!=null&& fgt.getAllFeatures().isEmpty()){
 						ns = fgt.getInverse();
 					}else {
