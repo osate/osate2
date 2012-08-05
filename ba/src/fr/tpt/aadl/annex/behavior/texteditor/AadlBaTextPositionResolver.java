e392c52e3c7c482769b509e806bfe9c192d79374
Etienne Borde <etienne.borde@telecom-paristech.fr>
2012-08-05 22:23:21 +0000

    first draft of position resolver, used for hyperlinking
    git-svn-id: https://eve.enst.fr/svn/aadl-eclipse-dev/aadlba/trunk/fr.tpt.aadl.annex.behavior@1354 974160da-3573-0410-8cc1-8b9333dd3547

---
+++ ba/src/fr/tpt/aadl/annex/behavior/texteditor/AadlBaTextPositionResolver.java
@@ -0,0 +1,83 @@
+package fr.tpt.aadl.annex.behavior.texteditor;
+
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.xtext.resource.XtextResource;
+import org.osate.aadl2.Element;
+import org.osate.annexsupport.AnnexTextPositionResolver;
+import org.osate.annexsupport.TextPositionInfo;
+
+import fr.tpt.aadl.annex.behavior.AadlBaResolver;
+import fr.tpt.aadl.annex.behavior.aadlba.BehaviorAnnex;
+import fr.tpt.aadl.annex.behavior.aadlba.BehaviorElement;
+import fr.tpt.aadl.annex.behavior.aadlba.BehaviorState;
+import fr.tpt.aadl.annex.behavior.aadlba.ElementHolder;
+import fr.tpt.aadl.annex.behavior.utils.AadlBaLocationReference;
+
+public class AadlBaTextPositionResolver implements AnnexTextPositionResolver{
+
+	private BehaviorAnnex ba;
+	
+	private TextPositionInfo resolveBehaviorAnnexElementAt(int offset)
+	{
+		BehaviorElement e = XtextAadlBaHyperlink.getLinkedElement(ba, offset);
+		
+		if(e==null)
+			return new TextPositionInfo(null, 0, 0);
+		
+		AadlBaLocationReference loc = XtextAadlBaHyperlink.getSourceOffsetElement(ba, offset);
+		TextPositionInfo positionInfo;
+		if(e instanceof ElementHolder)
+		{
+			ElementHolder beh = (ElementHolder) e;
+			positionInfo = new TextPositionInfo(beh.getElement(), loc.getOffset(), loc.getLength());
+		}
+		else
+		{
+			positionInfo = new TextPositionInfo(e, loc.getOffset(), loc.getLength());
+		}
+		return positionInfo;
+	}
+
+	private TextPositionInfo resolveBehaviorAnnexCrossReferencedElementAt(int offset)
+	{
+		BehaviorElement e = XtextAadlBaHyperlink.getLinkedElement(ba, offset);
+		
+		if(e==null)
+			return new TextPositionInfo(null, offset, 0);
+		AadlBaLocationReference loc = XtextAadlBaHyperlink.getSourceOffsetElement(ba, offset);
+		TextPositionInfo positionInfo;
+		if(e instanceof ElementHolder)
+		{
+			ElementHolder beh = (ElementHolder) e;
+			positionInfo = new TextPositionInfo(beh.getElement(), loc.getOffset(), loc.getLength());
+		}
+		else
+		{
+			positionInfo = new TextPositionInfo(e, e.getAadlBaLocationReference().getOffset(), e.getAadlBaLocationReference().getLength());
+		}
+		return positionInfo;
+	}
+	
+	@Override
+	public TextPositionInfo resolveElementAt(EObject annexRoot, int offset) {
+		if(annexRoot instanceof BehaviorAnnex)
+		{
+			ba = (BehaviorAnnex) annexRoot;
+			return this.resolveBehaviorAnnexElementAt(offset);
+		}
+		return null;
+	}
+
+	@Override
+	public TextPositionInfo resolveCrossReferencedElementAt(EObject annexRoot,
+			int offset) {
+		
+		if(annexRoot instanceof BehaviorAnnex)
+		{
+			ba = (BehaviorAnnex) annexRoot;
+			return resolveBehaviorAnnexCrossReferencedElementAt(offset);
+		}
+		return null;
+	}
+
+}
