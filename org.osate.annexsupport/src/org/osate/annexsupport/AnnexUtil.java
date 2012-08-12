70558685c730fbd59c1bf168aa5722626b2ebca6
Peter Feiler <phf@sei.cmu.edu>
2012-08-12 15:42:17 -0400

    updated getLineNumberFor to look up the default annex line number even for annex specific subclasue/library object.

--- org.osate.annexsupport/src/org/osate/annexsupport/AnnexUtil.java
+++ org.osate.annexsupport/src/org/osate/annexsupport/AnnexUtil.java
@@ -4,6 +4,7 @@ import org.eclipse.emf.common.notify.Adapter;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtext.nodemodel.INode;
 import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
+import org.osate.aadl2.AnnexSubclause;
 
 public class AnnexUtil {
 	
@@ -42,6 +43,11 @@ public class AnnexUtil {
 	 * @return line number
 	 */
 	public static int getLineNumberFor(EObject obj)	{
+		if (obj == null) return 0;
+		AnnexParseResult apr = AnnexUtil.getAnnexParseResult(obj);
+		if (apr != null){
+			obj = (EObject) apr.getTarget();
+		}
 		INode node = NodeModelUtils.findActualNodeFor(obj);
 		if (node != null){
 			return node.getStartLine();
