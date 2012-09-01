50b19a393dc252e36415d8070dfa09d937c16b2b
Lutz Wrage <lwrage@sei.cmu.edu>
2012-09-01 15:50:19 -0400

    Fill in SOMs for end to end flow instances

--- org.osate.aadl2/src/org/osate/aadl2/instance/impl/EndToEndFlowInstanceImpl.java
+++ org.osate.aadl2/src/org/osate/aadl2/instance/impl/EndToEndFlowInstanceImpl.java
@@ -38,7 +38,6 @@ package org.osate.aadl2.instance.impl;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 
 import org.eclipse.emf.common.notify.Notification;
