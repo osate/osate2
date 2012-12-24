e1625190ec9e92d47866be24d1df058b8fd1c241
Julien <julien@gunnm.org>
2012-12-24 14:40:01 -0500

    * Add a class dedicated to debugging to    ease enablement/disabling debug functions

---
+++ org.osate.aadl2/src/org/osate/aadl2/util/OsateDebug.java
@@ -0,0 +1,14 @@
+package org.osate.aadl2.util;
+
+public class OsateDebug {
+
+	static boolean DEBUG_ENABLE = true;
+	
+	public static void osateDebug (String str)
+	{
+		if (DEBUG_ENABLE)
+		{
+			System.out.println (str);
+		}
+	}
+}
