543dca54ef549af0343f035f42fa6b2be6ef68bf
Julien Delange <julien@gunnm.org>
2013-01-09 13:47:24 -0500

    * Ability to execute LUTE theorems on the instance model

--- analyses/org.osate.validation.lute/src/org/osate/validation/lute/LuteAction.java
+++ analyses/org.osate.validation.lute/src/org/osate/validation/lute/LuteAction.java
@@ -19,6 +19,41 @@ FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TOR
 ARISING FROM, OUT OF OR IN CONNECTION WITH THE DATA OR THE USE OR OTHER DEALINGS IN THE DATA.
 */
 
+/**
+ * <copyright>
+ * Copyright  2013 by Carnegie Mellon University, all rights reserved.
+ * 
+ * Use of the Open Source AADL Tool Environment (OSATE) is subject to the terms of the license set forth
+ * at http://www.eclipse.org/org/documents/epl-v10.html.
+ * 
+ * NO WARRANTY
+ * 
+ * ANY INFORMATION, MATERIALS, SERVICES, INTELLECTUAL PROPERTY OR OTHER PROPERTY OR RIGHTS GRANTED OR PROVIDED BY
+ * CARNEGIE MELLON UNIVERSITY PURSUANT TO THIS LICENSE (HEREINAFTER THE ''DELIVERABLES'') ARE ON AN ''AS-IS'' BASIS.
+ * CARNEGIE MELLON UNIVERSITY MAKES NO WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED AS TO ANY MATTER INCLUDING,
+ * BUT NOT LIMITED TO, WARRANTY OF FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABILITY, INFORMATIONAL CONTENT,
+ * NONINFRINGEMENT, OR ERROR-FREE OPERATION. CARNEGIE MELLON UNIVERSITY SHALL NOT BE LIABLE FOR INDIRECT, SPECIAL OR
+ * CONSEQUENTIAL DAMAGES, SUCH AS LOSS OF PROFITS OR INABILITY TO USE SAID INTELLECTUAL PROPERTY, UNDER THIS LICENSE,
+ * REGARDLESS OF WHETHER SUCH PARTY WAS AWARE OF THE POSSIBILITY OF SUCH DAMAGES. LICENSEE AGREES THAT IT WILL NOT
+ * MAKE ANY WARRANTY ON BEHALF OF CARNEGIE MELLON UNIVERSITY, EXPRESS OR IMPLIED, TO ANY PERSON CONCERNING THE
+ * APPLICATION OF OR THE RESULTS TO BE OBTAINED WITH THE DELIVERABLES UNDER THIS LICENSE.
+ * 
+ * Licensee hereby agrees to defend, indemnify, and hold harmless Carnegie Mellon University, its trustees, officers,
+ * employees, and agents from all claims or demands made against them (and any related losses, expenses, or
+ * attorney's fees) arising out of, or relating to Licensee's and/or its sub licensees' negligent use or willful
+ * misuse of or negligent conduct or willful misconduct regarding the Software, facilities, or other rights or
+ * assistance granted by Carnegie Mellon University under this License, including, but not limited to, any claims of
+ * product liability, personal injury, death, damage to property, or violation of any laws or regulations.
+ * 
+ * Carnegie Mellon University Software Engineering Institute authored documents are sponsored by the U.S. Department
+ * of Defense under Contract F19628-00-C-0003. Carnegie Mellon University retains copyrights in all material produced
+ * under this contract. The U.S. Government retains a non-exclusive, royalty-free license to publish or reproduce these
+ * documents, or allow others to do so, for U.S. Government purposes only pursuant to the copyright license
+ * under the contract clause at 252.227.7013.
+ * </copyright>
+ * 
+ */
+
 package org.osate.validation.lute;
 
 import java.io.InputStream;
@@ -43,38 +78,52 @@ public abstract class LuteAction extends AadlAction {
 	protected Logger log;
 	
 	@Override
-	public IStatus runJob(Element sel, IProgressMonitor monitor, Logger log) {
+	public IStatus runJob(Element sel, IProgressMonitor monitor, Logger log)
+	{
+		SystemImplementation sysimpl;
+		SystemInstance si;
+		
+		sysimpl = null;
+		si = null;
+		
+		System.out.println (" sel = " + sel);
 		log.info("");
 
-		if (!(sel instanceof SystemImplementation)) 
+		if ( (!(sel instanceof SystemImplementation)) && ( ! ( sel instanceof SystemInstance)))
 		{
-			log.error("Must select a system implementation");
+			log.error("Must select a system implementation or a System Instance");
 			return Status.CANCEL_STATUS;
 		}
-		
-		final SystemImplementation sysimpl = (SystemImplementation) sel;
 
 		monitor.beginTask("Running Lute", IProgressMonitor.UNKNOWN);
-		InstantiateModel im = new InstantiateModel(new NullProgressMonitor(), getErrorManager());
-		URI uri = OsateResourceUtil.getInstanceModelURI(sysimpl);
-		Resource resource = OsateResourceUtil.getEmptyAaxl2Resource(uri);
-		
-		try
-		{
-			SystemInstance si = im.createSystemInstance(sysimpl, resource);
-			LuteInterpreter interpreter = new LuteInterpreter(si, log);
-			InputStream stream = getLuteInput();
-			if (stream == null) return Status.CANCEL_STATUS;
-			interpreter.run(stream);
-			return Status.OK_STATUS;
-		}
-		catch (Exception e)
-		{
-			Dialog.showError("Model Instantiate", "Error while re-instantiating the model: " + e.getMessage());
-			return Status.CANCEL_STATUS;
-		}
-		
 
+		if (sel instanceof SystemImplementation)
+		{
+			sysimpl = (SystemImplementation) sel;
+			InstantiateModel im = new InstantiateModel(new NullProgressMonitor(), getErrorManager());
+			URI uri = OsateResourceUtil.getInstanceModelURI(sysimpl);
+			Resource resource = OsateResourceUtil.getEmptyAaxl2Resource(uri);
+			try 
+			{
+				si = im.createSystemInstance(sysimpl, resource);
+			} 
+			catch (Exception e) 
+			{
+				Dialog.showError("Model Instantiate", "Error while re-instantiating the model: " + e.getMessage());
+				return Status.CANCEL_STATUS;
+			}
+		}
+		else
+		{
+			si = (SystemInstance) sel;
+		}
+
+
+		LuteInterpreter interpreter = new LuteInterpreter(si, log);
+		InputStream stream = getLuteInput();
+		if (stream == null) return Status.CANCEL_STATUS;
+		interpreter.run(stream);
+		return Status.OK_STATUS;
 	}
 	
 	abstract public InputStream getLuteInput();
