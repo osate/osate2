e4caddffd778e4578812966259355580b16e6dc0
Julien Delange <julien@gunnm.org>
2013-01-24 10:19:55 -0500

    * Remove equals methods

--- org.osate.aadl2/src/org/osate/aadl2/impl/NamedValueImpl.java
+++ org.osate.aadl2/src/org/osate/aadl2/impl/NamedValueImpl.java
@@ -186,26 +186,7 @@ public class NamedValueImpl extends PropertyValueImpl implements NamedValue {
 		return super.eIsSet(featureID);
 	}
 
-	public int compareTo(Object arg0) {
 
-		return (this == arg0) ? 0 : 1;
-	}
 
-	public boolean equals(Object arg0) {
 
-		//System.out.println ("equals on " +  arg0);
-		if (arg0 instanceof NamedValueImpl) {
-			NamedValueImpl nvi = (NamedValueImpl) arg0;
-
-			if ((this.getNamedValue() instanceof EnumerationLiteralImpl)
-					&& (nvi.getNamedValue() instanceof EnumerationLiteralImpl)) {
-				String str1 = ((EnumerationLiteralImpl) this.getNamedValue()).getName().toString();
-				String str2 = ((EnumerationLiteralImpl) nvi.getNamedValue()).getName().toString();
-				return (str1 == str2);
-			}
-
-		}
-
-		return (this == arg0);
-	}
 } // NamedValueImpl
