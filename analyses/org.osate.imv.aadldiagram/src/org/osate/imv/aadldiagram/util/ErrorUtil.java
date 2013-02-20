dc8a1d4a61977bbaad702d787466c1859f9649e2
Julien Delange <julien@gunnm.org>
2013-02-20 14:42:22 -0500

    * Handle connections going through different components

--- analyses/org.osate.imv.aadldiagram/src/org/osate/imv/aadldiagram/util/ErrorUtil.java
+++ analyses/org.osate.imv.aadldiagram/src/org/osate/imv/aadldiagram/util/ErrorUtil.java
@@ -9,6 +9,7 @@ import org.osate.aadl2.instance.ComponentInstance;
 import org.osate.aadl2.instance.ConnectionInstance;
 import org.osate.aadl2.instance.ConnectionReference;
 import org.osate.aadl2.instance.FeatureInstance;
+import org.osate.aadl2.instance.InstanceObject;
 import org.osate.aadl2.util.OsateDebug;
 import org.osate.xtext.aadl2.errormodel.errorModel.ErrorPropagation;
 import org.osate.xtext.aadl2.errormodel.errorModel.ErrorType;
@@ -72,62 +73,97 @@ public class ErrorUtil {
 		{
 			//OsateDebug.osateDebug("source feature=" + fi);
 			connectionInstances = new ArrayList<ConnectionInstance>();
-			connectionInstances.addAll(fi.getDstConnectionInstances());
-			connectionInstances.addAll(fi.getSrcConnectionInstances());
+			connectionInstances.addAll(source.getContainingComponentInstance().getConnectionInstances());
 
-			for (ConnectionInstance ci : connectionInstances)
+			for (ConnectionInstance ci : source.getSystemInstance().allConnectionInstances())
 			{
-				//OsateDebug.osateDebug("ci=" + ci);
+				boolean dstOK = false;
+				boolean srcOK  = false;
+				FeatureInstance dstFeature = null;
+				//OsateDebug.osateDebug("   ci=" + ci);
+				
+				for (InstanceObject io : ci.getThroughFeatureInstances())
+				{
+					if (io.getContainingComponentInstance() == source)
+					{
+						srcOK = true;
+					}
+
+				}
 
 				for (ConnectionReference cr : ci.getConnectionReferences())
 				{
-					//OsateDebug.osateDebug("dest=" + cr.getDestination());
+					//OsateDebug.osateDebug("   cr=" + cr);
+
+				//	OsateDebug.osateDebug("      dest1=" + cr.getDestination());
+					//OsateDebug.osateDebug("      dest2=" + cr.getDestination().getContainingComponentInstance());
+
 					if ((cr.getDestination() instanceof FeatureInstance) && 
 					    (cr.getDestination().getContainingComponentInstance() == dest))
 					{
-						OsateDebug.osateDebug("same");
-						FeatureInstance destinationFeature = (FeatureInstance) cr.getDestination();
-						sourcePropagation = EM2Util.getOutgoingErrorPropagation (fi);
-						destinationPropagation = EM2Util.getOutgoingErrorPropagation (destinationFeature);
-						
-						if (sourcePropagation == null)
-						{
-							continue;
-						}
-						
-						if (destinationPropagation == null)
-						{
-							continue;
-						}
-						
-						TypeSet sourceTypes = sourcePropagation.getTypeSet();
-						TypeSet destinationTypes = sourcePropagation.getTypeSet();
-						for (TypeToken tt1 : sourceTypes.getElementType())
-						{
-							for (ErrorType type1 : tt1.getType())
-							{
-								for (TypeToken tt2 : destinationTypes.getElementType())
-								{
-									for (ErrorType type2 : tt2.getType())
-									{
-										if (type1.getName().equalsIgnoreCase(type2.getName()))
-										{
-											/*
-											 * Here, it means that we have the
-											 * same error propagation
-											 * between the source and the destination.
-											 */
-											OsateDebug.osateDebug("same type");
-											return DEFAULT_FACTOR;
-										}
-									}
-								}
+						//OsateDebug.osateDebug("      dest3=" + cr.getDestination() + "|"+ cr.getDestination().getContainingComponentInstance() );
+					
+						//OsateDebug.osateDebug("      dest4=" + cr.getSource() + "|"+ cr.getSource().getContainingComponentInstance() );
 
-							}
-						}
+						dstOK = true;
+						dstFeature = (FeatureInstance) cr.getDestination();
+					}
+					if ((cr.getSource() instanceof FeatureInstance) && 
+						    (cr.getSource().getContainingComponentInstance() == source))
+					{			
+						//OsateDebug.osateDebug("   srcok");
 
+						srcOK = true;
 					}
 					
+				}
+				
+				if (dstOK && srcOK)
+				{
+
+					//OsateDebug.osateDebug("same");
+					sourcePropagation = EM2Util.getOutgoingErrorPropagation (fi);
+					//destinationPropagation = EM2Util.getOutgoingErrorPropagation (destinationFeature);
+					destinationPropagation = EM2Util.getIncomingErrorPropagation (dstFeature);
+
+					if (sourcePropagation == null)
+					{
+						OsateDebug.osateDebug("sourcePropagation null");
+						continue;
+					}
+					
+					if (destinationPropagation == null)
+					{
+						OsateDebug.osateDebug("destinationPropagation null, fi = " + dstFeature);
+
+						continue;
+					}
+					
+					TypeSet sourceTypes = sourcePropagation.getTypeSet();
+					TypeSet destinationTypes = sourcePropagation.getTypeSet();
+					for (TypeToken tt1 : sourceTypes.getElementType())
+					{
+						for (ErrorType type1 : tt1.getType())
+						{
+							for (TypeToken tt2 : destinationTypes.getElementType())
+							{
+								for (ErrorType type2 : tt2.getType())
+								{
+									if (type1.getName().equalsIgnoreCase(type2.getName()))
+									{
+										/*
+										 * Here, it means that we have the
+										 * same error propagation
+										 * between the source and the destination.
+										 */
+										//OsateDebug.osateDebug("same type");
+										return DEFAULT_FACTOR;
+									}
+								}
+							}
+
+						}
+					}
 				}
 			}
 			
