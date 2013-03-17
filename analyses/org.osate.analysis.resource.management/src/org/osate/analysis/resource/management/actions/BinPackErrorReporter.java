cb3f6110d10dbfa62f615254669d1bc913b07e7d
Julien Delange <julien@gunnm.org>
2013-03-17 09:09:24 -0400

    Avoid build failure

--- analyses/org.osate.analysis.resource.management/src/org/osate/analysis/resource/management/actions/BinPackErrorReporter.java
+++ analyses/org.osate.analysis.resource.management/src/org/osate/analysis/resource/management/actions/BinPackErrorReporter.java
@@ -1,14 +1,15 @@
 package org.osate.analysis.resource.management.actions;
 
+import org.osate.aadl2.util.OsateDebug;
+
 import EAnalysis.BinPacking.AssignmentProblem;
 import EAnalysis.BinPacking.BPErrorReporter;
 
 public class BinPackErrorReporter implements BPErrorReporter {
 
-	@Override
-	public void reportError(int errorCode, AssignmentProblem problem) {
-		System.out.println("error"+errorCode);
-
+	public void reportError(int errorCode, AssignmentProblem problem) 
+	{
+		OsateDebug.osateDebug ("error"+errorCode);
 	}
 
 }
