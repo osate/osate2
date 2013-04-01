e296eeb7e069f79733944f6622f90ce7a3f216af
phf <phf@RM-CHAMOMILE.ad.sei.cmu.edu>
2013-04-01 17:04:25 -0400

    Fixes issue #214 We now resolve modes correctly for contained property associations, namely in the context of the model element being referenced.

--- core/org.osate.xtext.aadl2.properties/src/org/osate/xtext/aadl2/properties/linking/PropertiesLinkingService.java
+++ core/org.osate.xtext.aadl2.properties/src/org/osate/xtext/aadl2/properties/linking/PropertiesLinkingService.java
@@ -479,26 +479,25 @@ public class PropertiesLinkingService extends DefaultLinkingService {
 						if (sub != null) res = sub.findNamedElement(name);
 				}
 			} else {
-				res = AadlUtil.getContainingClassifier(context)
-						.findNamedElement(name);
-				if (res == null){
-					// check about in modes in a contained property association
-					PropertyAssociation pa = AadlUtil.getContainingPropertyAssociation(context);
-					if (!Aadl2Util.isNull(pa)&&!pa.getAppliesTos().isEmpty()){
-						ContainedNamedElement path = pa.getAppliesTos().get(0);
-						EList<ContainmentPathElement> cpelist = path.getContainmentPathElements();
-						Classifier cpecl = null;
-						for (ContainmentPathElement containmentPathElement : cpelist) {
-							if (containmentPathElement.getNamedElement() instanceof Subcomponent){
-								cpecl = ((Subcomponent)containmentPathElement.getNamedElement()).getClassifier();
-							} else {
-								break;
-							}
-						}
-						if (cpecl != null){
-							res = cpecl.findNamedElement(name);
+				// check about in modes in a contained property association
+				PropertyAssociation pa = AadlUtil.getContainingPropertyAssociation(context);
+				if (pa != null &&!pa.getAppliesTos().isEmpty()){
+					ContainedNamedElement path = pa.getAppliesTos().get(0);
+					EList<ContainmentPathElement> cpelist = path.getContainmentPathElements();
+					Classifier cpecl = null;
+					for (ContainmentPathElement containmentPathElement : cpelist) {
+						if (containmentPathElement.getNamedElement() instanceof Subcomponent){
+							cpecl = ((Subcomponent)containmentPathElement.getNamedElement()).getClassifier();
+						} else {
+							break;
 						}
 					}
+					if (cpecl != null){
+						res = cpecl.findNamedElement(name);
+					}
+				} else {
+					res = AadlUtil.getContainingClassifier(context)
+							.findNamedElement(name);
 				}
 			}
 			if (res != null && res instanceof Mode) {
