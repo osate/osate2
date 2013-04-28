15a8e1868110ea90e0f47a478b73335012dc6b41
phf <phf@RM-CHAMOMILE.ad.sei.cmu.edu>
2013-04-28 17:05:02 -0400

    Added Data_Rate_Units as constant for property lookup

--- org.osate.xtext.aadl2.properties/src/org/osate/xtext/aadl2/properties/util/AadlProject.java
+++ org.osate.xtext.aadl2.properties/src/org/osate/xtext/aadl2/properties/util/AadlProject.java
@@ -113,6 +113,8 @@ public final class AadlProject {
 	public static final String MB_LITERAL = "MByte";
 	public static final String GB_LITERAL = "GByte";
 	
+	public static final String DATA_RATE_UNITS = "Data_Rate_Units";
+	
 	public static final String DATA_VOLUME_UNITS = "Data_Volume_Units";
 	public static final String BITSPS_LITERAL = "bitsps";
 	public static final String BPS_LITERAL = "Bytesps";
