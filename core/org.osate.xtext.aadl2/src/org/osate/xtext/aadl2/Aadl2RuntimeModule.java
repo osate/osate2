bd33d8d2a95920e7b1a93c5c18bb11e9b20e8355
phf <phf@RM-CHAMOMILE.ad.sei.cmu.edu>
2013-04-28 15:26:11 -0400

    commented out the hook in of the fragment provider. It was not correctly showing the model hierarhcy in th eoutline view but remained stuck at the package.

--- core/org.osate.xtext.aadl2/src/org/osate/xtext/aadl2/Aadl2RuntimeModule.java
+++ core/org.osate.xtext.aadl2/src/org/osate/xtext/aadl2/Aadl2RuntimeModule.java
@@ -56,10 +56,11 @@ public class Aadl2RuntimeModule extends org.osate.xtext.aadl2.AbstractAadl2Runti
 		return org.osate.xtext.aadl2.naming.Aadl2QualifiedNameProvider.class;
 	}
 	
-	@Override
-	public Class<? extends IFragmentProvider> bindIFragmentProvider() {
-		return org.osate.xtext.aadl2.util.Aadl2QualifiedNameFragmentProvider.class;
-	}
+// It has some problems. It recurses on the package in the outline view
+	//	@Override
+//	public Class<? extends IFragmentProvider> bindIFragmentProvider() {
+//		return org.osate.xtext.aadl2.util.Aadl2QualifiedNameFragmentProvider.class;
+//	}
 
 	@Override
 	public Class<? extends org.eclipse.xtext.linking.ILinker> bindILinker() {
