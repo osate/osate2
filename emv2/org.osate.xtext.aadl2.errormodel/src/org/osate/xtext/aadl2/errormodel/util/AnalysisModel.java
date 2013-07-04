f09f8283f25252a83baf7beadb21ce9012766dc2
phf <phf@RM-CHAMOMILE.ad.sei.cmu.edu>
2013-07-04 19:21:44 -0400

    PathEnds now point to the ConnectionInstanceEnd. The methods to get srcCI and dstCI still exist, but we can now also get the COnnectionInstanceEnd instead of having to find it based on the error propagation when needed.

--- emv2/org.osate.xtext.aadl2.errormodel/src/org/osate/xtext/aadl2/errormodel/util/AnalysisModel.java
+++ emv2/org.osate.xtext.aadl2.errormodel/src/org/osate/xtext/aadl2/errormodel/util/AnalysisModel.java
@@ -106,14 +106,14 @@ public class AnalysisModel {
 		EList<ConnectionReference> connrefs = connectionInstance.getConnectionReferences();
 		if (connrefs.isEmpty()) return;
 		ErrorPropagation srcprop = null;
-		ComponentInstance srcCI = null;
+		ConnectionInstanceEnd srcCIE = null;
 		ErrorPropagation dstprop = null;
-		ComponentInstance dstCI = null;
+		ConnectionInstanceEnd dstCIE = null;
 		ConnectionReference first = connrefs.get(0);
 		boolean inonly = (first.getSource().getContainingComponentInstance() == first.getContext());
 		ConnectionReference last = connrefs.get(connrefs.size()-1);
 		boolean outonly = (last.getDestination().getContainingComponentInstance() == last.getContext());
-		EList<ComponentInstance> addlSrcCI = new BasicEList<ComponentInstance>();
+		EList<ConnectionInstanceEnd> addlSrcCIE = new BasicEList<ConnectionInstanceEnd>();
 		EList<ErrorPropagation> addlSrcEP = new BasicEList<ErrorPropagation>();
 		for (ConnectionReference connectionReference : connrefs) {
 			ConnectionInstanceEnd src = connectionReference.getSource();
@@ -137,7 +137,7 @@ public class AnalysisModel {
 				}
 				if(foundEP != null){
 					srcprop = foundEP;
-					srcCI = src.getComponentInstance();
+					srcCIE = src;
 				}
 			} else {
 				// for enclosing components record an outgoing path 
@@ -159,7 +159,7 @@ public class AnalysisModel {
 				}
 				if(foundEP != null){
 					addlSrcEP.add(foundEP);
-					addlSrcCI.add(src.getComponentInstance());
+					addlSrcCIE.add(src);
 				}
 
 			}
@@ -182,25 +182,25 @@ public class AnalysisModel {
 			if (founddst != null){
 				// remember the last destination with EP
 				dstprop = founddst;
-				dstCI = dst.getComponentInstance();
+				dstCIE = dst;
 			}
 
 		}
 		if (srcprop!= null && dstprop!=null){
-			PropagationPath path = new PropagationPath(srcCI, srcprop, dstCI, dstprop,connectionInstance);
+			PropagationPath path = new PropagationPath(srcCIE, srcprop, dstCIE, dstprop,connectionInstance);
 			propagationPaths.add(path);
-			subcomponents.add(srcCI);
-			subcomponents.add(dstCI);
-			for (int i = 0; i<addlSrcCI.size();i++) {
-				srcCI = addlSrcCI.get(i);
+			subcomponents.add(srcCIE.getComponentInstance());
+			subcomponents.add(dstCIE.getComponentInstance());
+			for (int i = 0; i<addlSrcCIE.size();i++) {
+				srcCIE = addlSrcCIE.get(i);
 				srcprop = addlSrcEP.get(i);
-				path = new PropagationPath(srcCI, srcprop, dstCI, dstprop,connectionInstance);
+				path = new PropagationPath(srcCIE, srcprop, dstCIE, dstprop,connectionInstance);
 				propagationPaths.add(path);
-				subcomponents.add(srcCI);
+				subcomponents.add(srcCIE.getComponentInstance());
 			}
 		}
 		if (connectionInstance.isBidirectional()){
-			addlSrcCI.clear();
+			addlSrcCIE.clear();
 			addlSrcEP.clear();
 			for (int i= connrefs.size()-1; i>= 0 ;i--) {
 				ConnectionReference connectionReference = connrefs.get(i);
@@ -225,7 +225,7 @@ public class AnalysisModel {
 					}
 					if(foundEP != null){
 						srcprop = foundEP;
-						srcCI = src.getComponentInstance();
+						srcCIE = src;
 					}
 				} else {
 					// for enclosing components record an outgoing path 
@@ -247,7 +247,7 @@ public class AnalysisModel {
 					}
 					if(foundEP != null){
 						addlSrcEP.add(foundEP);
-						addlSrcCI.add(src.getComponentInstance());
+						addlSrcCIE.add(src);
 					}
 
 				}
@@ -270,21 +270,21 @@ public class AnalysisModel {
 				if (founddst != null){
 					// remember the last destination with EP
 					dstprop = founddst;
-					dstCI = dst.getComponentInstance();
+					dstCIE = dst;
 				}
 
 			}
 			if (srcprop!= null && dstprop!=null){
-				PropagationPath path = new PropagationPath(srcCI, srcprop, dstCI, dstprop,connectionInstance);
+				PropagationPath path = new PropagationPath(srcCIE, srcprop, dstCIE, dstprop,connectionInstance);
 				propagationPaths.add(path);
-				subcomponents.add(srcCI);
-				subcomponents.add(dstCI);
-				for (int i = 0; i<addlSrcCI.size();i++) {
-					srcCI = addlSrcCI.get(i);
+				subcomponents.add(srcCIE.getComponentInstance());
+				subcomponents.add(dstCIE.getComponentInstance());
+				for (int i = 0; i<addlSrcCIE.size();i++) {
+					srcCIE = addlSrcCIE.get(i);
 					srcprop = addlSrcEP.get(i);
-					path = new PropagationPath(srcCI, srcprop, dstCI, dstprop,connectionInstance);
+					path = new PropagationPath(srcCIE, srcprop, dstCIE, dstprop,connectionInstance);
 					propagationPaths.add(path);
-					subcomponents.add(srcCI);
+					subcomponents.add(srcCIE.getComponentInstance());
 				}
 			}
 		}
--- emv2/org.osate.xtext.aadl2.errormodel/src/org/osate/xtext/aadl2/errormodel/util/PropagationPath.java
+++ emv2/org.osate.xtext.aadl2.errormodel/src/org/osate/xtext/aadl2/errormodel/util/PropagationPath.java
@@ -2,6 +2,7 @@ package org.osate.xtext.aadl2.errormodel.util;
 
 import org.osate.aadl2.instance.ComponentInstance;
 import org.osate.aadl2.instance.ConnectionInstance;
+import org.osate.aadl2.instance.ConnectionInstanceEnd;
 import org.osate.aadl2.instance.ConnectionReference;
 import org.osate.xtext.aadl2.errormodel.errorModel.ErrorPropagation;
 /**
@@ -17,8 +18,8 @@ public class PropagationPath {
 	
 	
 	public PropagationPath(
-			ComponentInstance srcCI, ErrorPropagation srcEP,
-			ComponentInstance dstCI, ErrorPropagation dstEP, ConnectionInstance conni) {
+			ConnectionInstanceEnd srcCI, ErrorPropagation srcEP,
+			ConnectionInstanceEnd dstCI, ErrorPropagation dstEP, ConnectionInstance conni) {
 		this.pathSrc = new PropagationPathEnd(srcCI, srcEP);
 		this.pathDst = new PropagationPathEnd(dstCI, dstEP);
 		this.conni = conni;
@@ -27,14 +28,14 @@ public class PropagationPath {
 	
 	public PropagationPath(
 			ConnectionInstance srcConni, 
-			ComponentInstance dstCI, ErrorPropagation dstEP) {
+			ConnectionInstanceEnd dstCI, ErrorPropagation dstEP) {
 		this.pathSrc = new PropagationPathEnd(srcConni, null);
 		this.pathDst = new PropagationPathEnd(dstCI, dstEP);
 		this.conni = null;
 	}
 	
 	public PropagationPath(
-			ComponentInstance srcCI, ErrorPropagation srcEP,
+			ConnectionInstanceEnd srcCI, ErrorPropagation srcEP,
 			ConnectionInstance dstConni) {
 		this.pathSrc = new PropagationPathEnd(srcCI, srcEP);
 		this.pathDst = new PropagationPathEnd(dstConni, null);
@@ -50,26 +51,14 @@ public class PropagationPath {
 		return pathSrc;
 	}
 	public ComponentInstance getSrcCI(){
-		if (pathSrc == null)
-		{
-			return null;
-		}
-		if (pathSrc.getComponentInstance() != null)
-		{
 			return pathSrc.getComponentInstance();
-		}
-		return null;
 	}
+	public ConnectionInstanceEnd getSrcCIE(){
+		return pathSrc.getConnectionInstanceEnd();
+	}
+	
 	public ConnectionInstance getSrcConni(){
-		if (pathSrc == null)
-		{
-			return null;
-		}
-		if (pathSrc.getComponentInstance() != null)
-		{
 			return pathSrc.getConnectionInstance();
-		}
-		return null;
 	}
 	public void setPathSrc(PropagationPathEnd pathSrc) {
 		this.pathSrc = pathSrc;
@@ -79,26 +68,14 @@ public class PropagationPath {
 	}
 	public ComponentInstance getDstCI()
 	{
-		if (pathDst == null)
-		{
-			return null;
-		}
-		if (pathDst.getComponentInstance() != null)
-		{
 			return pathDst.getComponentInstance();
-		}
-		return null;
+	}
+	public ConnectionInstanceEnd getDstCIE()
+	{
+			return pathDst.getConnectionInstanceEnd();
 	}
 	public ConnectionInstance getDstConni(){
-		if (pathDst == null)
-		{
-			return null;
-		}
-		if (pathDst.getComponentInstance() != null)
-		{
 			return pathDst.getConnectionInstance();
-		}
-		return null;
 	}
 	public void setPathDst(PropagationPathEnd pathDst) {
 		this.pathDst = pathDst;
