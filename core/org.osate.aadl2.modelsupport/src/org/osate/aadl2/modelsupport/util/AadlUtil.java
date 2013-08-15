19b14831f1d211144e895845ae7f3b69d2c45a33
Peter Feiler <phf@sei.cmu.edu>
2013-08-15 11:23:11 -0400

    Fixes Issue #274 null pointer exception in getElement. We were retrieving the file extension on a project, which is null and then checked for equality with an extension string. THe test now uses the extension as parameter and we only do it for IFile instead of IResource.

--- core/org.osate.aadl2.modelsupport/src/org/osate/aadl2/modelsupport/util/AadlUtil.java
+++ core/org.osate.aadl2.modelsupport/src/org/osate/aadl2/modelsupport/util/AadlUtil.java
@@ -1332,15 +1332,15 @@ public final class AadlUtil {
 			if (theElement != null)
 				return theElement;
 		}
-		if (object instanceof IResource
-				&& ((IResource) object).getFileExtension().equalsIgnoreCase(WorkspacePlugin.INSTANCE_FILE_EXT)) {
+		if (object instanceof IFile
+				&& WorkspacePlugin.INSTANCE_FILE_EXT.equalsIgnoreCase(((IFile) object).getFileExtension())) {
 			Resource res = OsateResourceUtil.getResource((IResource) object);
 			EList<EObject> rl = res.getContents();
 			if (!rl.isEmpty() && rl.get(0) instanceof Element)
 				return (Element) rl.get(0);
 		}
-		if (object instanceof IResource
-				&& ((IResource) object).getFileExtension().equalsIgnoreCase(WorkspacePlugin.SOURCE_FILE_EXT)) {
+		if (object instanceof IFile
+				&& WorkspacePlugin.SOURCE_FILE_EXT.equalsIgnoreCase(((IFile) object).getFileExtension())) {
 			Resource res = OsateResourceUtil.getResource((IResource) object);
 			EList<EObject> rl = res.getContents();
 			
