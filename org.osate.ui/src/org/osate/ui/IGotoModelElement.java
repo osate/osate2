0ab6b0afded976bc39f8ad4a0e1ec34425dcf156
Peter Feiler <phf@sei.cmu.edu>
2013-10-21 16:09:31 -0400

    changed from IGotoAObject to IGotoModelElement

--- org.osate.ui/src/org/osate/ui/IGotoModelElement.java
+++ org.osate.ui/src/org/osate/ui/IGotoModelElement.java
@@ -6,14 +6,14 @@ import org.osate.aadl2.Element;
 
 /**
  * Abstract interface for objects that know how to open up and display
- * a particular AObject.  The intention is that AADL Editors be <em>adaptable</em>
+ * a particular Model Element.  The intention is that AADL Editors be <em>adaptable</em>
  * to this class.  That is, they should implement {@link org.eclipse.core.runtime.IAdaptable#getAdapter(java.lang.Class)}
- * so that it understands <code>GotoAObject.class</code>.  THis allows views
- * to trigger editors to focus on particular AObjects without having to know
+ * so that it understands <code>GotoModel Element.class</code>.  THis allows views
+ * to trigger editors to focus on particular Model Elements without having to know
  * what kind of editor the current editor is.
  * 
  * @author aarong
  */
-public interface IGotoAObject {
-	public void gotoAObject(Element obj);
+public interface IGotoModelElement {
+	public void gotoModelElement(Element obj);
 }
