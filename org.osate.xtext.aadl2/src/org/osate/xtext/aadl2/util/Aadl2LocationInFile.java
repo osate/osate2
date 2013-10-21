4259fe83a2f2e938f6cba2f76ab03db8314655a9
Peter Feiler <phf@sei.cmu.edu>
2013-10-21 14:51:55 -0400

    Added getLocationReference as util method to replace the method in AObject.

--- org.osate.xtext.aadl2/src/org/osate/xtext/aadl2/util/Aadl2LocationInFile.java
+++ org.osate.xtext.aadl2/src/org/osate/xtext/aadl2/util/Aadl2LocationInFile.java
@@ -3,6 +3,7 @@ package org.osate.xtext.aadl2.util;
 import java.util.Collections;
 import java.util.List;
 
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.xtext.nodemodel.ICompositeNode;
 import org.eclipse.xtext.nodemodel.INode;
@@ -10,6 +11,7 @@ import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
 import org.eclipse.xtext.resource.DefaultLocationInFileProvider;
 import org.eclipse.xtext.util.ITextRegion;
 import org.eclipse.xtext.util.TextRegion;
+import org.osate.aadl2.Element;
 import org.osate.aadl2.parsesupport.AObject;
 import org.osate.aadl2.parsesupport.LocationReference;
 
@@ -38,6 +40,14 @@ public class Aadl2LocationInFile extends DefaultLocationInFileProvider {
 		if (nodes == null || nodes.isEmpty())
 			nodes = Collections.<INode>singletonList(node);
 		return createRegion(nodes);
+	}
+	
+	public LocationReference getLocationReference(Element obj) {
+		ICompositeNode node = NodeModelUtils.findActualNodeFor(obj);
+		LocationReference locref = new LocationReference();
+		locref.setLine(node.getTotalStartLine());
+		locref.setFilename(obj.eResource().getURI().toString());
+		return locref;
 	}
 
 }
