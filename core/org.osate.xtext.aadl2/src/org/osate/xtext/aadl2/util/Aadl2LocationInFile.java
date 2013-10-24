0c3ce8cf7c2cbcfa8501393bc6ac3ff650d22e5e
Julien Delange <julien@gunnm.org>
2013-10-24 09:58:37 -0400

    Can now correctly parse the BA

--- core/org.osate.xtext.aadl2/src/org/osate/xtext/aadl2/util/Aadl2LocationInFile.java
+++ core/org.osate.xtext.aadl2/src/org/osate/xtext/aadl2/util/Aadl2LocationInFile.java
@@ -9,7 +9,9 @@ import org.eclipse.xtext.nodemodel.INode;
 import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
 import org.eclipse.xtext.resource.DefaultLocationInFileProvider;
 import org.eclipse.xtext.util.ITextRegion;
+import org.eclipse.xtext.util.TextRegion;
 import org.osate.aadl2.Element;
+import org.osate.aadl2.parsesupport.AObject;
 import org.osate.aadl2.parsesupport.LocationReference;
 
 public class Aadl2LocationInFile extends DefaultLocationInFileProvider {
@@ -17,6 +19,30 @@ public class Aadl2LocationInFile extends DefaultLocationInFileProvider {
 	@Override
 	protected ITextRegion getTextRegion(EObject obj, boolean isSignificant) {
 		ICompositeNode node = NodeModelUtils.findActualNodeFor(obj);
+		
+		/**
+		 * This is an old code that was initially removed but
+		 * added again to fix some bugs.
+		 * See. https://github.com/osate/osate2-ba/issues/1
+		 */
+		if (node == null) {
+			// it may be in annex
+			if (obj instanceof AObject){
+				LocationReference locref = ((AObject)obj).getLocationReference();
+				if (locref != null){
+					return new TextRegion(locref.getOffset(), locref.getLength());
+				} else {
+					//try AnnexSource adapter
+				}
+			}
+			if (obj.eContainer() == null)
+				return ITextRegion.EMPTY_REGION;
+			return getTextRegion(obj.eContainer(), isSignificant);
+		}
+		/**
+		 * end of handling error with the behavior annex
+		 */
+		
 		List<INode> nodes = null;
 		if (isSignificant)
 			nodes = getLocationNodes(obj);
