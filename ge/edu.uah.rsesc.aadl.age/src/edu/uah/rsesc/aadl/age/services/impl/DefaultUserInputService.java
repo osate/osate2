7d0fdc6105e3ec26c84109daf1501eb3d2e7097b
Philip Alldredge <pwa0001@uah.edu>
2013-12-09 12:54:06 -0600

    Added license notice.

--- ge/edu.uah.rsesc.aadl.age/src/edu/uah/rsesc/aadl/age/services/impl/DefaultUserInputService.java
+++ ge/edu.uah.rsesc.aadl.age/src/edu/uah/rsesc/aadl/age/services/impl/DefaultUserInputService.java
@@ -1,3 +1,11 @@
+/*******************************************************************************
+ * Copyright (C) 2013 University of Alabama in Huntsville (UAH)
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * The US Government has unlimited rights in this work in accordance with W31P4Q-10-D-0092 DO 0073.
+ *******************************************************************************/
 package edu.uah.rsesc.aadl.age.services.impl;
 
 import java.text.MessageFormat;
