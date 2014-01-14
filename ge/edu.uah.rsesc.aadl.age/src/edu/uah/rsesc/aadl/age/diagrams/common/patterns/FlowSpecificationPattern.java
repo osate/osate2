1eee19363723cfc3b803096df33dd95e0df91b4a
Philip Alldredge <pwa0001@uah.edu>
2014-01-14 10:49:00 -0600

    Implemented deletion of flow specifications.

--- ge/edu.uah.rsesc.aadl.age/src/edu/uah/rsesc/aadl/age/diagrams/common/patterns/FlowSpecificationPattern.java
+++ ge/edu.uah.rsesc.aadl.age/src/edu/uah/rsesc/aadl/age/diagrams/common/patterns/FlowSpecificationPattern.java
@@ -10,41 +10,64 @@ package edu.uah.rsesc.aadl.age.diagrams.common.patterns;
 
 import javax.inject.Inject;
 
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.graphiti.features.context.IDeleteContext;
 import org.eclipse.graphiti.mm.GraphicsAlgorithmContainer;
 import org.eclipse.graphiti.mm.algorithms.GraphicsAlgorithm;
 import org.eclipse.graphiti.mm.algorithms.Polyline;
 import org.eclipse.graphiti.mm.algorithms.Text;
 import org.eclipse.graphiti.mm.algorithms.styles.Style;
 import org.eclipse.graphiti.mm.pictograms.Anchor;
+import org.eclipse.graphiti.mm.pictograms.AnchorContainer;
 import org.eclipse.graphiti.mm.pictograms.Connection;
 import org.eclipse.graphiti.mm.pictograms.ConnectionDecorator;
 import org.eclipse.graphiti.mm.pictograms.ContainerShape;
+import org.eclipse.graphiti.mm.pictograms.PictogramElement;
+import org.eclipse.graphiti.mm.pictograms.Shape;
 import org.eclipse.graphiti.services.Graphiti;
 import org.eclipse.graphiti.services.IGaService;
 import org.eclipse.graphiti.services.IPeCreateService;
+import org.osate.aadl2.ComponentClassifier;
 import org.osate.aadl2.FlowSpecification;
-
 import edu.uah.rsesc.aadl.age.diagrams.common.AadlElementWrapper;
+import edu.uah.rsesc.aadl.age.services.AadlModificationService;
+import edu.uah.rsesc.aadl.age.services.BusinessObjectResolutionService;
 import edu.uah.rsesc.aadl.age.services.ConnectionService;
+import edu.uah.rsesc.aadl.age.services.DiagramModificationService;
 import edu.uah.rsesc.aadl.age.services.GraphicsAlgorithmManipulationService;
 import edu.uah.rsesc.aadl.age.services.HighlightingService;
+import edu.uah.rsesc.aadl.age.services.ShapeService;
 import edu.uah.rsesc.aadl.age.services.StyleService;
+import edu.uah.rsesc.aadl.age.services.UserInputService;
 import edu.uah.rsesc.aadl.age.services.VisibilityService;
+import edu.uah.rsesc.aadl.age.services.AadlModificationService.AbstractModifier;
 
 public class FlowSpecificationPattern extends AgeConnectionPattern {
 	private final StyleService styleUtil;
 	private final GraphicsAlgorithmManipulationService graphicsAlgorithmUtil;
 	private final HighlightingService highlightingHelper;
 	private final ConnectionService connectionHelper;
+	private final ShapeService shapeService;
+	private final AadlModificationService aadlModService;
+	private final DiagramModificationService diagramModService;
+	private final UserInputService userInputService;
+	private final BusinessObjectResolutionService bor;
 	
 	@Inject
-	public FlowSpecificationPattern(final VisibilityService visibilityHelper,
-			final StyleService styleUtil, final GraphicsAlgorithmManipulationService graphicsAlgorithmUtil, final HighlightingService highlightingHelper, final ConnectionService connectionHelper) {
+	public FlowSpecificationPattern(final VisibilityService visibilityHelper, final StyleService styleUtil, final GraphicsAlgorithmManipulationService graphicsAlgorithmUtil, 
+			final HighlightingService highlightingHelper, final ConnectionService connectionHelper, final ShapeService shapeService, AadlModificationService aadlModService, 
+			final DiagramModificationService diagramModService, final UserInputService userInputService, final BusinessObjectResolutionService bor) {
 		super(visibilityHelper);
 		this.styleUtil = styleUtil;
 		this.graphicsAlgorithmUtil = graphicsAlgorithmUtil;
 		this.highlightingHelper = highlightingHelper;
 		this.connectionHelper = connectionHelper;
+		this.shapeService = shapeService;
+		this.aadlModService = aadlModService;
+		this.diagramModService = diagramModService;
+		this.userInputService = userInputService;
+		this.bor = bor;
 	}
 
 	@Override
@@ -151,5 +174,66 @@ public class FlowSpecificationPattern extends AgeConnectionPattern {
 		final FlowSpecification fs = getFlowSpecification(connection);
 		final ContainerShape ownerShape = connectionHelper.getOwnerShape(connection);
 		return (ownerShape == null) ? null : connectionHelper.getAnchors(ownerShape, fs);		
+	}
+	
+	@Override
+	public boolean canDelete(final IDeleteContext context) {
+		final Object bo = bor.getBusinessObjectForPictogramElement(context.getPictogramElement());
+		if(!(bo instanceof FlowSpecification)) {
+			return false;
+		}
+		
+		final FlowSpecification fs = (FlowSpecification)bo;
+		final Connection connection = (Connection)context.getPictogramElement();
+		if(connection.getStart() == null) {
+			return false; 
+		}
+		
+		final AnchorContainer startContainer = connection.getStart().getParent();
+		if(!(startContainer instanceof Shape)) {
+			return false;
+		}
+		
+		final ComponentClassifier cc = getComponentClassifier((Shape)startContainer);
+		return fs.getContainingClassifier() == cc;
+	}
+	
+	@Override
+	public void delete(final IDeleteContext context) {
+		if(!userInputService.confirmDelete(context)) {
+			return;
+		}
+
+		// Make the modification
+		final Connection connection = (Connection)context.getPictogramElement();
+		final FlowSpecification fs = (FlowSpecification)bor.getBusinessObjectForPictogramElement(connection);
+		aadlModService.modify(fs, new AbstractModifier<FlowSpecification, Object>() {
+			private DiagramModificationService.Modification diagramMod;
+			
+			@Override
+			public Object modify(final Resource resource, final FlowSpecification fs) {
+				// Start the diagram modification
+	 			diagramMod = diagramModService.startModification();	 			
+	 			
+	 			// Mark other diagrams for updating
+ 				diagramMod.markRelatedDiagramsAsDirty(fs.getContainingClassifier());
+
+	 			EcoreUtil.remove(fs);
+				
+				return null;
+			}
+			
+	 		@Override
+			public void beforeCommit(final Resource resource, final FlowSpecification fs, final Object modificationResult) {
+				diagramMod.commit();
+			}
+		});	
+		
+		// Clear selection
+		getFeatureProvider().getDiagramTypeProvider().getDiagramBehavior().getDiagramContainer().selectPictogramElements(new PictogramElement[0]);
+	}
+	
+	private ComponentClassifier getComponentClassifier(final Shape shape) {
+		return shapeService.getClosestBusinessObjectOfType(shape, ComponentClassifier.class);
 	}
 }
