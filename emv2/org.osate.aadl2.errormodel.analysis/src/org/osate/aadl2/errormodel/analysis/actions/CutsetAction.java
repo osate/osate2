9b40e06f1057a32b9b0d7e5b421a3a71ae424804
Julien Delange <julien@gunnm.org>
2014-01-15 22:02:43 -0500

    Add a first draft for the cutset action

---
+++ emv2/org.osate.aadl2.errormodel.analysis/src/org/osate/aadl2/errormodel/analysis/actions/CutsetAction.java
@@ -0,0 +1,145 @@
+/*
+ * <copyright>
+ * Copyright  2012-2013 by Carnegie Mellon University, all rights reserved.
+ *
+ * Use of the Open Source AADL Tool Environment (OSATE) is subject to the terms of the license set forth
+ * at http://www.eclipse.org/org/documents/epl-v10.html.
+ *
+ * NO WARRANTY
+ *
+ * ANY INFORMATION, MATERIALS, SERVICES, INTELLECTUAL PROPERTY OR OTHER PROPERTY OR RIGHTS GRANTED OR PROVIDED BY
+ * CARNEGIE MELLON UNIVERSITY PURSUANT TO THIS LICENSE (HEREINAFTER THE "DELIVERABLES") ARE ON AN "AS-IS" BASIS.
+ * CARNEGIE MELLON UNIVERSITY MAKES NO WARRANTIES OF ANY KIND, EITHER EXPRESS OR IMPLIED AS TO ANY MATTER INCLUDING,
+ * BUT NOT LIMITED TO, WARRANTY OF FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABILITY, INFORMATIONAL CONTENT,
+ * NONINFRINGEMENT, OR ERROR-FREE OPERATION. CARNEGIE MELLON UNIVERSITY SHALL NOT BE LIABLE FOR INDIRECT, SPECIAL OR
+ * CONSEQUENTIAL DAMAGES, SUCH AS LOSS OF PROFITS OR INABILITY TO USE SAID INTELLECTUAL PROPERTY, UNDER THIS LICENSE,
+ * REGARDLESS OF WHETHER SUCH PARTY WAS AWARE OF THE POSSIBILITY OF SUCH DAMAGES. LICENSEE AGREES THAT IT WILL NOT
+ * MAKE ANY WARRANTY ON BEHALF OF CARNEGIE MELLON UNIVERSITY, EXPRESS OR IMPLIED, TO ANY PERSON CONCERNING THE
+ * APPLICATION OF OR THE RESULTS TO BE OBTAINED WITH THE DELIVERABLES UNDER THIS LICENSE.
+ *
+ * Licensee hereby agrees to defend, indemnify, and hold harmless Carnegie Mellon University, its trustees, officers,
+ * employees, and agents from all claims or demands made against them (and any related losses, expenses, or
+ * attorney's fees) arising out of, or relating to Licensee's and/or its sub licensees' negligent use or willful
+ * misuse of or negligent conduct or willful misconduct regarding the Software, facilities, or other rights or
+ * assistance granted by Carnegie Mellon University under this License, including, but not limited to, any claims of
+ * product liability, personal injury, death, damage to property, or violation of any laws or regulations.
+ *
+ * Carnegie Mellon Carnegie Mellon University Software Engineering Institute authored documents are sponsored by the U.S. Department
+ * of Defense under Contract F19628-00-C-0003. Carnegie Mellon University retains copyrights in all material produced
+ * under this contract. The U.S. Government retains a non-exclusive, royalty-free license to publish or reproduce these
+ * documents, or allow others to do so, for U.S. Government purposes only pursuant to the copyright license
+ * under the contract clause at 252.227.7013.
+ * </copyright>
+ */
+package org.osate.aadl2.errormodel.analysis.actions;
+
+
+
+/**
+ * Also, this class implement the following consistency rule from
+ * the official documentation:
+ * C1, C5, C7, C11, C12 
+ * 
+ */
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.emf.common.util.BasicEList;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.uml2.uml.Classifier;
+import org.eclipse.xtext.EcoreUtil2;
+import org.osate.aadl2.AbstractConnectionEnd;
+import org.osate.aadl2.ComponentClassifier;
+import org.osate.aadl2.ComponentImplementation;
+import org.osate.aadl2.ConnectedElement;
+import org.osate.aadl2.Connection;
+import org.osate.aadl2.ContainedNamedElement;
+import org.osate.aadl2.DirectionType;
+import org.osate.aadl2.Element;
+import org.osate.aadl2.Feature;
+import org.osate.aadl2.Subcomponent;
+import org.osate.aadl2.impl.ConnectedElementImpl;
+import org.osate.aadl2.instance.ComponentInstance;
+import org.osate.aadl2.instance.ConnectionInstance;
+import org.osate.aadl2.instance.ConnectionInstanceEnd;
+import org.osate.aadl2.instance.ConnectionReference;
+import org.osate.aadl2.instance.FeatureInstance;
+import org.osate.aadl2.instance.InstanceObject;
+import org.osate.aadl2.instance.SystemInstance;
+import org.osate.aadl2.util.OsateDebug;
+import org.osate.ui.actions.AaxlReadOnlyActionAsJob;
+import org.osate.xtext.aadl2.errormodel.errorModel.CompositeState;
+import org.osate.xtext.aadl2.errormodel.errorModel.ConditionElement;
+import org.osate.xtext.aadl2.errormodel.errorModel.ErrorBehaviorEvent;
+import org.osate.xtext.aadl2.errormodel.errorModel.ErrorBehaviorState;
+import org.osate.xtext.aadl2.errormodel.errorModel.ErrorBehaviorTransition;
+import org.osate.xtext.aadl2.errormodel.errorModel.ErrorEvent;
+import org.osate.xtext.aadl2.errormodel.errorModel.ErrorFlow;
+import org.osate.xtext.aadl2.errormodel.errorModel.ErrorPath;
+import org.osate.xtext.aadl2.errormodel.errorModel.ErrorPropagation;
+import org.osate.xtext.aadl2.errormodel.errorModel.ErrorSink;
+import org.osate.xtext.aadl2.errormodel.errorModel.ErrorSource;
+import org.osate.xtext.aadl2.errormodel.errorModel.EventOrPropagation;
+import org.osate.xtext.aadl2.errormodel.errorModel.OutgoingPropagationCondition;
+import org.osate.xtext.aadl2.errormodel.errorModel.SubcomponentElement;
+import org.osate.xtext.aadl2.errormodel.errorModel.TypeToken;
+import org.osate.xtext.aadl2.errormodel.util.AnalysisModel;
+import org.osate.xtext.aadl2.errormodel.util.EM2TypeSetUtil;
+import org.osate.xtext.aadl2.errormodel.util.EMV2Properties;
+import org.osate.xtext.aadl2.errormodel.util.EMV2Util;
+import org.osate.xtext.aadl2.errormodel.util.PropagationPathRecord;
+import org.osate.xtext.aadl2.errormodel.util.PropagationPathEnd;
+
+public final class CutsetAction extends AaxlReadOnlyActionAsJob {
+	AnalysisModel model;
+	
+	protected String getMarkerType() {
+		return "org.osate.aadl2.errormodel.analysis.FaultImpactMarker";
+	}
+
+	protected String getActionName() {
+		return "Consistency";
+	}
+
+	public void doAaxlAction(IProgressMonitor monitor, Element obj) {
+		monitor.beginTask("Generating Cutset", IProgressMonitor.UNKNOWN);
+		List<PropagationPathEnd> sources;
+		List<PropagationPathEnd> destinations;
+		sources = new ArrayList<PropagationPathEnd>();
+		destinations = new ArrayList<PropagationPathEnd>();
+		// Get the system instance (if any)
+		SystemInstance si;
+		if (obj instanceof InstanceObject){
+			si = ((InstanceObject)obj).getSystemInstance();
+		}
+		else return;
+
+	
+		model = new AnalysisModel(si);
+		
+		EList<PropagationPathRecord> pathlist = model.getPropagationPaths();
+		for (PropagationPathRecord ppr : pathlist)
+		{
+			sources.add(ppr.getPathSrc());
+			destinations.add(ppr.getPathSrc());
+		}
+		
+		for (PropagationPathEnd src : sources)
+		{
+			OsateDebug.osateDebug("[SRC] ci=" + src.getComponentInstance() + ";ep=" + src.getErrorPropagation());
+		}
+		for (PropagationPathEnd dst : destinations)
+		{
+			OsateDebug.osateDebug("[DST] ci=" + dst.getComponentInstance() + ";ep=" + dst.getErrorPropagation());
+		}
+
+		
+		monitor.done();
+	}
+	
+
+		
+
+}
