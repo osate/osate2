09f68d73479cc02973a226e95feb5d7db9f17dea
Peter Feiler <phf@sei.cmu.edu>
2014-09-02 00:48:19 -0400

    New class for latency analysis

---
+++ analyses/org.osate.analysis.flows/src/org/osate/analysis/flows/reporting/model/ReportedCell.java
@@ -0,0 +1,46 @@
+package org.osate.analysis.flows.reporting.model;
+
+public class ReportedCell {
+
+	private String msg;
+	private ReportSeverity severity;
+
+	public ReportedCell(ReportSeverity sev, String msg) {
+		this.msg = msg;
+		this.severity = sev;
+	}
+
+	public ReportedCell(String msg) {
+		this.msg = msg;
+		this.severity = ReportSeverity.UNKNOWN;
+	}
+
+	public String getMessage() {
+		return this.msg;
+	}
+
+	public void setMessage(String msg) {
+		this.msg = msg;
+	}
+
+	public ReportSeverity getSeverity() {
+		return this.severity;
+	}
+
+	public boolean isError() {
+		return this.severity.equals(ReportSeverity.ERROR);
+	}
+
+	public boolean isWarning() {
+		return this.severity.equals(ReportSeverity.WARNING);
+	}
+
+	public boolean isSuccess() {
+		return this.severity.equals(ReportSeverity.SUCCESS);
+	}
+
+	public boolean isUnknown() {
+		return this.severity.equals(ReportSeverity.UNKNOWN);
+	}
+
+}
