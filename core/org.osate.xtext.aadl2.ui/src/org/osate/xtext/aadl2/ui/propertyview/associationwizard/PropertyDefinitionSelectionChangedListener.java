adea929db5b2ee80d094d96e35653244a316091b
Joe Seibel <jseibel@sei.cmu.edu>
2015-01-07 15:28:43 -0500

    Removed the New Property Association wizard from the property view.

--- core/org.osate.xtext.aadl2.ui/src/org/osate/xtext/aadl2/ui/propertyview/associationwizard/PropertyDefinitionSelectionChangedListener.java
+++
@@ -1,5 +0,0 @@
-package org.osate.xtext.aadl2.ui.propertyview.associationwizard;
-
-public interface PropertyDefinitionSelectionChangedListener {
-	public void propertyDefinitionSelectionChanged();
-}
