560a3e9d26da660276f7614416877ebda5b73c99
ryan.mcilnay <rm0016@uah.edu>
2015-01-13 13:18:09 -0600

    Issue 101 - Switch Direction of a Connection
    	-Ryan Mcilnay

---
+++ ge/org.osate.ge/src/org/osate/ge/diagrams/common/features/SwitchDirectionOfConnectionFeature.java
@@ -0,0 +1,75 @@
+package org.osate.ge.diagrams.common.features;
+
+import javax.inject.Inject;
+
+import org.eclipse.emf.ecore.resource.Resource;
+
+import org.eclipse.graphiti.features.IFeatureProvider;
+import org.eclipse.graphiti.features.context.ICustomContext;
+import org.eclipse.graphiti.features.custom.AbstractCustomFeature;
+import org.eclipse.graphiti.features.context.IContext;
+import org.eclipse.graphiti.mm.pictograms.PictogramElement;
+import org.osate.aadl2.ConnectedElement;
+import org.osate.aadl2.Connection;
+import org.osate.aadl2.Element;
+import org.osate.aadl2.NamedElement;
+import org.osate.ge.services.AadlModificationService;
+import org.osate.ge.services.BusinessObjectResolutionService;
+import org.osate.ge.services.AadlModificationService.AbstractModifier;
+
+public class SwitchDirectionOfConnectionFeature extends AbstractCustomFeature {
+	private BusinessObjectResolutionService bor;
+	private final AadlModificationService aadlModService;
+		
+	@Inject
+	public SwitchDirectionOfConnectionFeature(final IFeatureProvider fp, final BusinessObjectResolutionService bor, 
+			AadlModificationService aadlModService) {
+		super(fp);
+		this.bor = bor;
+		this.aadlModService = aadlModService;
+	}
+
+	@Override
+	public String getName() {
+		return "Switch Direction";
+	}
+		
+	@Override
+	public String getDescription() {
+		return "Switches the selected connection's directional flow between source and destination.";
+	}
+
+	@Override
+	public boolean canExecute(ICustomContext context) {
+		return true;
+	}
+
+	@Override
+	public boolean isAvailable(IContext context) {
+		final PictogramElement pe = getFeatureProvider().getDiagramTypeProvider().getDiagramBehavior().getDiagramContainer().getSelectedPictogramElements()[0];
+		final Element feature = (Element) bor.getBusinessObjectForPictogramElement(pe);
+		if (feature instanceof org.osate.aadl2.Connection) {
+			return true;
+		}
+		return false;
+	}
+
+	@Override
+	public void execute(final ICustomContext context) {
+		final PictogramElement pe = getFeatureProvider().getDiagramTypeProvider().getDiagramBehavior().getDiagramContainer().getSelectedPictogramElements()[0];
+		final NamedElement feature = (NamedElement)bor.getBusinessObjectForPictogramElement(pe);
+		// Make modifications			
+		aadlModService.modify(feature, new AbstractModifier<NamedElement, Object>() {
+			@Override
+			public Object modify(final Resource resource, final NamedElement feature) {
+				org.osate.aadl2.Connection connection = (Connection) feature;
+				final ConnectedElement ceSource = connection.getSource();
+				connection.setSource(connection.getDestination());
+				connection.setDestination(ceSource);	
+				return null;
+			}
+		});
+	}
+}
+
+
