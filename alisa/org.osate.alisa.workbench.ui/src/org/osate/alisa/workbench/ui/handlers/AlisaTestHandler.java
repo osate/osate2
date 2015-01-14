90c860b1011aa439f29c02b4063a329f57103714
Julien <julien@gunnm.org>
2015-01-14 13:56:03 -0500

    Commit new class loader with arguments

--- alisa/org.osate.alisa.workbench.ui/src/org/osate/alisa/workbench/ui/handlers/AlisaTestHandler.java
+++ alisa/org.osate.alisa.workbench.ui/src/org/osate/alisa/workbench/ui/handlers/AlisaTestHandler.java
@@ -1,13 +1,5 @@
 package org.osate.alisa.workbench.ui.handlers;
 
-import java.io.File;
-import java.lang.reflect.Method;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.util.ArrayList;
-import java.util.List;
-
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
@@ -17,10 +9,8 @@ import org.osate.aadl2.ComponentImplementation;
 import org.osate.aadl2.Element;
 import org.osate.aadl2.instance.SystemInstance;
 import org.osate.aadl2.instantiation.InstantiateModel;
-import org.osate.alisa.workbench.ui.internal.AlisaActivator;
+import org.osate.alisa.workbench.ui.utils.AlisaLoader;
 import org.osate.ui.dialogs.Dialog;
-import org.osgi.framework.Bundle;
-import org.osgi.framework.BundleContext;
 
 public class AlisaTestHandler extends AadlHandler {
 	private static final String RERUN_ID = "org.osate.ilca.commands.rerunIlca";
@@ -59,66 +49,10 @@ public class AlisaTestHandler extends AadlHandler {
 //		InstantiateCase mycase = new InstantiateCase();
 //		String result = mycase.generate(si);
 //		System.out.println(result);
-		String className = "testresourcebudget.BudgetTest";
-		String methodName = "testme";
-		try {
-			// see
-// http://stackoverflow.com/questions/12714368/load-a-class-to-java-classpath-dynamically-using-eclipse-and-osgi
-
-			ClassLoader cl = Thread.currentThread().getContextClassLoader();
-			String cp = ResourcesPlugin.getWorkspace().getRoot().getLocation().toString();
-			cp = cp + "/testresourcebudget/bin/";
-			System.out.println("test=" + cp);
-			ClassLoader cl2 = ClassLoader.getSystemClassLoader();
-
-			URL[] urls = ((URLClassLoader) cl2).getURLs();
-
-//			for (URL url : urls) {
-//				System.out.println("url=" + url.getFile());
-//			}
-
-			URL mainURL = urls[0];
-			File mainFile = new File(mainURL.getFile());
-			File parent = new File(mainFile.getParent());
-			List<String> toadd = new ArrayList<String>();
-			toadd.add(cp);
-
-			for (File f : parent.listFiles()) {
-				toadd.add(f.getAbsolutePath());
-				System.out.println("file=" + f.getAbsolutePath());
-			}
-			BundleContext ctx = AlisaActivator.getInstance().getBundle().getBundleContext();
-			Bundle[] bundles = ctx.getBundles();
-			for (Bundle b : bundles) {
-				String loc = b.getLocation();
-				if (loc.endsWith("jar")) {
-					continue;
-				}
-				loc = loc.replaceAll("reference:file:/", "");
-//				System.out.println("loc1=" + loc);
-				if (loc.contains("/")) {
-					loc = loc.replace('/', '\\');
-				}
-				loc = loc + "bin\\";
-				System.out.println("loc2=" + loc);
-				toadd.add(loc);
-
-			}
-			URL[] urllist = new URL[toadd.size()];
-			for (int i = 0; i < toadd.size(); i++) {
-				urllist[i] = new File(toadd.get(i)).toURL();
-			}
-			URLClassLoader urlClassLoader = new URLClassLoader(urllist, cl);
-			Thread.currentThread().setContextClassLoader(urlClassLoader);
-			Class c = Class.forName(className, true, urlClassLoader);
-			Object o = c.newInstance();
-			Method m = c.getMethod(methodName, null);
-			m.invoke(o, null);
-
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		Object[] args;
+		args = new Object[1];
+		args[0] = si;
+		AlisaLoader.alisaInvoke("testresourcebudget.JulienTest", "julien", args);
 
 		long stop = System.currentTimeMillis();
 		System.out.println("Evaluation time: " + (stop - start) / 1000.0 + "s");
---
+++ alisa/org.osate.alisa.workbench.ui/src/org/osate/alisa/workbench/ui/utils/AlisaLoader.java
@@ -0,0 +1,124 @@
+package org.osate.alisa.workbench.ui.utils;
+
+import java.io.File;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.lang.SystemUtils;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.osate.alisa.workbench.ui.internal.AlisaActivator;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+
+public class AlisaLoader {
+	public static boolean alisaInvoke(String className, String methodName, Object[] args) {
+
+		try {
+			// see
+			// http://stackoverflow.com/questions/12714368/load-a-class-to-java-classpath-dynamically-using-eclipse-and-osgi
+
+			/**
+			 * List of the files referenced in the classpath
+			 */
+			List<String> classPathFiles;
+			classPathFiles = new ArrayList<String>();
+
+			/**
+			 * Try to list all the projects and see where there are class files
+			 * to load (typically, in the bin/ directory).
+			 */
+//			String cp = ResourcesPlugin.getWorkspace().getRoot().getLocation().toString();
+//			cp = cp + "/testresourcebudget/bin/";
+//			System.out.println("test=" + cp);
+//			classPathFiles.add(cp);
+			for (IProject project : ResourcesPlugin.getWorkspace().getRoot().getProjects()) {
+				String path;
+				path = project.getLocation().toString();
+				path = path + "/bin";
+//				System.out.println("test=" + path);
+				classPathFiles.add(path);
+
+			}
+
+			/**
+			 * Add all the paths from the current system class loader.
+			 */
+			ClassLoader systemClassLoader = ClassLoader.getSystemClassLoader();
+
+			URL[] systemLoaderurls = ((URLClassLoader) systemClassLoader).getURLs();
+
+			for (URL url : systemLoaderurls) {
+				System.out.println("url=" + url.getFile());
+				File mainFile = new File(url.getFile());
+				File parent = new File(mainFile.getParent());
+				for (File f : parent.listFiles()) {
+					classPathFiles.add(f.getAbsolutePath());
+//					System.out.println("file=" + f.getAbsolutePath());
+				}
+			}
+
+			/**
+			 * Get all bundles and add them to the classpath as well.
+			 */
+			BundleContext bundleContext = AlisaActivator.getInstance().getBundle().getBundleContext();
+			Bundle[] allBundles = bundleContext.getBundles();
+			for (Bundle bundle : allBundles) {
+				String bundleLocation = bundle.getLocation();
+				if (bundleLocation.endsWith("jar")) {
+					continue;
+				}
+
+				// remove the prefix to find the file
+				// and adapt to the OS naming scheme
+				bundleLocation = bundleLocation.replaceAll("reference:file:/", "");
+//				System.out.println("loc1=" + loc);
+				if (SystemUtils.IS_OS_WINDOWS) {
+					if (bundleLocation.contains("/")) {
+						bundleLocation = bundleLocation.replace('/', '\\');
+					}
+					bundleLocation = bundleLocation + "bin\\";
+				} else {
+					bundleLocation = "/" + bundleLocation + "bin/";
+				}
+
+//				System.out.println("loc2=" + loc);
+				classPathFiles.add(bundleLocation);
+
+			}
+
+			/**
+			 * Now, build the URL list of all classpaths
+			 * and put each of them in an array.
+			 */
+			URL[] urllist = new URL[classPathFiles.size()];
+			for (int i = 0; i < classPathFiles.size(); i++) {
+				urllist[i] = new File(classPathFiles.get(i)).toURL();
+			}
+
+			/**
+			 * Finally build the classloader and invoke the method.
+			 */
+			ClassLoader cl = Thread.currentThread().getContextClassLoader();
+			URLClassLoader urlClassLoader = new URLClassLoader(urllist, cl);
+			Thread.currentThread().setContextClassLoader(urlClassLoader);
+			Class c = Class.forName(className, true, urlClassLoader);
+			Object o = c.newInstance();
+			Class[] parametersTypes = new Class[args.length];
+			for (int i = 0; i < args.length; i++) {
+				parametersTypes[i] = args[i].getClass();
+			}
+			Method m = c.getMethod(methodName, parametersTypes);
+			m.invoke(o, args);
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	}
+}
