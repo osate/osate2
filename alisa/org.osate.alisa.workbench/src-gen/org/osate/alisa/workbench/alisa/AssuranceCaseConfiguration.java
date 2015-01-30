9725386842a406fa1164d076f9410cda9f17613f
Peter Feiler <phf@sei.cmu.edu>
2015-01-30 12:15:44 -0500

    a new class

---
+++ alisa/org.osate.alisa.workbench/src-gen/org/osate/alisa/workbench/alisa/AssuranceCaseConfiguration.java
@@ -0,0 +1,194 @@
+/**
+ */
+package org.osate.alisa.workbench.alisa;
+
+import org.eclipse.emf.common.util.EList;
+
+import org.eclipse.emf.ecore.EObject;
+
+import org.osate.aadl2.ComponentImplementation;
+
+import org.osate.alisa.common.common.Description;
+import org.osate.alisa.common.common.FinalValue;
+
+import org.osate.categories.categories.VerificationCategory;
+
+import org.osate.verify.verify.VerificationPlan;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Assurance Case Configuration</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * <ul>
+ *   <li>{@link org.osate.alisa.workbench.alisa.AssuranceCaseConfiguration#getName <em>Name</em>}</li>
+ *   <li>{@link org.osate.alisa.workbench.alisa.AssuranceCaseConfiguration#getTitle <em>Title</em>}</li>
+ *   <li>{@link org.osate.alisa.workbench.alisa.AssuranceCaseConfiguration#getSystem <em>System</em>}</li>
+ *   <li>{@link org.osate.alisa.workbench.alisa.AssuranceCaseConfiguration#getDescription <em>Description</em>}</li>
+ *   <li>{@link org.osate.alisa.workbench.alisa.AssuranceCaseConfiguration#getConstant <em>Constant</em>}</li>
+ *   <li>{@link org.osate.alisa.workbench.alisa.AssuranceCaseConfiguration#getPlans <em>Plans</em>}</li>
+ *   <li>{@link org.osate.alisa.workbench.alisa.AssuranceCaseConfiguration#getSelectionFilter <em>Selection Filter</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @see org.osate.alisa.workbench.alisa.AlisaPackage#getAssuranceCaseConfiguration()
+ * @model
+ * @generated
+ */
+public interface AssuranceCaseConfiguration extends EObject
+{
+  /**
+   * Returns the value of the '<em><b>Name</b></em>' attribute.
+   * <!-- begin-user-doc -->
+   * <p>
+   * If the meaning of the '<em>Name</em>' attribute isn't clear,
+   * there really should be more of a description here...
+   * </p>
+   * <!-- end-user-doc -->
+   * @return the value of the '<em>Name</em>' attribute.
+   * @see #setName(String)
+   * @see org.osate.alisa.workbench.alisa.AlisaPackage#getAssuranceCaseConfiguration_Name()
+   * @model
+   * @generated
+   */
+  String getName();
+
+  /**
+   * Sets the value of the '{@link org.osate.alisa.workbench.alisa.AssuranceCaseConfiguration#getName <em>Name</em>}' attribute.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @param value the new value of the '<em>Name</em>' attribute.
+   * @see #getName()
+   * @generated
+   */
+  void setName(String value);
+
+  /**
+   * Returns the value of the '<em><b>Title</b></em>' attribute.
+   * <!-- begin-user-doc -->
+   * <p>
+   * If the meaning of the '<em>Title</em>' attribute isn't clear,
+   * there really should be more of a description here...
+   * </p>
+   * <!-- end-user-doc -->
+   * @return the value of the '<em>Title</em>' attribute.
+   * @see #setTitle(String)
+   * @see org.osate.alisa.workbench.alisa.AlisaPackage#getAssuranceCaseConfiguration_Title()
+   * @model
+   * @generated
+   */
+  String getTitle();
+
+  /**
+   * Sets the value of the '{@link org.osate.alisa.workbench.alisa.AssuranceCaseConfiguration#getTitle <em>Title</em>}' attribute.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @param value the new value of the '<em>Title</em>' attribute.
+   * @see #getTitle()
+   * @generated
+   */
+  void setTitle(String value);
+
+  /**
+   * Returns the value of the '<em><b>System</b></em>' reference.
+   * <!-- begin-user-doc -->
+   * <p>
+   * If the meaning of the '<em>System</em>' reference isn't clear,
+   * there really should be more of a description here...
+   * </p>
+   * <!-- end-user-doc -->
+   * @return the value of the '<em>System</em>' reference.
+   * @see #setSystem(ComponentImplementation)
+   * @see org.osate.alisa.workbench.alisa.AlisaPackage#getAssuranceCaseConfiguration_System()
+   * @model
+   * @generated
+   */
+  ComponentImplementation getSystem();
+
+  /**
+   * Sets the value of the '{@link org.osate.alisa.workbench.alisa.AssuranceCaseConfiguration#getSystem <em>System</em>}' reference.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @param value the new value of the '<em>System</em>' reference.
+   * @see #getSystem()
+   * @generated
+   */
+  void setSystem(ComponentImplementation value);
+
+  /**
+   * Returns the value of the '<em><b>Description</b></em>' containment reference.
+   * <!-- begin-user-doc -->
+   * <p>
+   * If the meaning of the '<em>Description</em>' containment reference isn't clear,
+   * there really should be more of a description here...
+   * </p>
+   * <!-- end-user-doc -->
+   * @return the value of the '<em>Description</em>' containment reference.
+   * @see #setDescription(Description)
+   * @see org.osate.alisa.workbench.alisa.AlisaPackage#getAssuranceCaseConfiguration_Description()
+   * @model containment="true"
+   * @generated
+   */
+  Description getDescription();
+
+  /**
+   * Sets the value of the '{@link org.osate.alisa.workbench.alisa.AssuranceCaseConfiguration#getDescription <em>Description</em>}' containment reference.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @param value the new value of the '<em>Description</em>' containment reference.
+   * @see #getDescription()
+   * @generated
+   */
+  void setDescription(Description value);
+
+  /**
+   * Returns the value of the '<em><b>Constant</b></em>' containment reference list.
+   * The list contents are of type {@link org.osate.alisa.common.common.FinalValue}.
+   * <!-- begin-user-doc -->
+   * <p>
+   * If the meaning of the '<em>Constant</em>' containment reference list isn't clear,
+   * there really should be more of a description here...
+   * </p>
+   * <!-- end-user-doc -->
+   * @return the value of the '<em>Constant</em>' containment reference list.
+   * @see org.osate.alisa.workbench.alisa.AlisaPackage#getAssuranceCaseConfiguration_Constant()
+   * @model containment="true"
+   * @generated
+   */
+  EList<FinalValue> getConstant();
+
+  /**
+   * Returns the value of the '<em><b>Plans</b></em>' reference list.
+   * The list contents are of type {@link org.osate.verify.verify.VerificationPlan}.
+   * <!-- begin-user-doc -->
+   * <p>
+   * If the meaning of the '<em>Plans</em>' reference list isn't clear,
+   * there really should be more of a description here...
+   * </p>
+   * <!-- end-user-doc -->
+   * @return the value of the '<em>Plans</em>' reference list.
+   * @see org.osate.alisa.workbench.alisa.AlisaPackage#getAssuranceCaseConfiguration_Plans()
+   * @model
+   * @generated
+   */
+  EList<VerificationPlan> getPlans();
+
+  /**
+   * Returns the value of the '<em><b>Selection Filter</b></em>' reference list.
+   * The list contents are of type {@link org.osate.categories.categories.VerificationCategory}.
+   * <!-- begin-user-doc -->
+   * <p>
+   * If the meaning of the '<em>Selection Filter</em>' reference list isn't clear,
+   * there really should be more of a description here...
+   * </p>
+   * <!-- end-user-doc -->
+   * @return the value of the '<em>Selection Filter</em>' reference list.
+   * @see org.osate.alisa.workbench.alisa.AlisaPackage#getAssuranceCaseConfiguration_SelectionFilter()
+   * @model
+   * @generated
+   */
+  EList<VerificationCategory> getSelectionFilter();
+
+} // AssuranceCaseConfiguration
---
+++ alisa/org.osate.alisa.workbench/src-gen/org/osate/alisa/workbench/alisa/impl/AssuranceCaseConfigurationImpl.java
@@ -0,0 +1,514 @@
+/**
+ */
+package org.osate.alisa.workbench.alisa.impl;
+
+import java.util.Collection;
+
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.notify.NotificationChain;
+
+import org.eclipse.emf.common.util.EList;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.InternalEObject;
+
+import org.eclipse.emf.ecore.impl.ENotificationImpl;
+import org.eclipse.emf.ecore.impl.MinimalEObjectImpl;
+
+import org.eclipse.emf.ecore.util.EObjectContainmentEList;
+import org.eclipse.emf.ecore.util.EObjectResolvingEList;
+import org.eclipse.emf.ecore.util.InternalEList;
+
+import org.osate.aadl2.ComponentImplementation;
+
+import org.osate.alisa.common.common.Description;
+import org.osate.alisa.common.common.FinalValue;
+
+import org.osate.alisa.workbench.alisa.AlisaPackage;
+import org.osate.alisa.workbench.alisa.AssuranceCaseConfiguration;
+
+import org.osate.categories.categories.VerificationCategory;
+
+import org.osate.verify.verify.VerificationPlan;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Assurance Case Configuration</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * The following features are implemented:
+ * <ul>
+ *   <li>{@link org.osate.alisa.workbench.alisa.impl.AssuranceCaseConfigurationImpl#getName <em>Name</em>}</li>
+ *   <li>{@link org.osate.alisa.workbench.alisa.impl.AssuranceCaseConfigurationImpl#getTitle <em>Title</em>}</li>
+ *   <li>{@link org.osate.alisa.workbench.alisa.impl.AssuranceCaseConfigurationImpl#getSystem <em>System</em>}</li>
+ *   <li>{@link org.osate.alisa.workbench.alisa.impl.AssuranceCaseConfigurationImpl#getDescription <em>Description</em>}</li>
+ *   <li>{@link org.osate.alisa.workbench.alisa.impl.AssuranceCaseConfigurationImpl#getConstant <em>Constant</em>}</li>
+ *   <li>{@link org.osate.alisa.workbench.alisa.impl.AssuranceCaseConfigurationImpl#getPlans <em>Plans</em>}</li>
+ *   <li>{@link org.osate.alisa.workbench.alisa.impl.AssuranceCaseConfigurationImpl#getSelectionFilter <em>Selection Filter</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @generated
+ */
+public class AssuranceCaseConfigurationImpl extends MinimalEObjectImpl.Container implements AssuranceCaseConfiguration
+{
+  /**
+   * The default value of the '{@link #getName() <em>Name</em>}' attribute.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see #getName()
+   * @generated
+   * @ordered
+   */
+  protected static final String NAME_EDEFAULT = null;
+
+  /**
+   * The cached value of the '{@link #getName() <em>Name</em>}' attribute.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see #getName()
+   * @generated
+   * @ordered
+   */
+  protected String name = NAME_EDEFAULT;
+
+  /**
+   * The default value of the '{@link #getTitle() <em>Title</em>}' attribute.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see #getTitle()
+   * @generated
+   * @ordered
+   */
+  protected static final String TITLE_EDEFAULT = null;
+
+  /**
+   * The cached value of the '{@link #getTitle() <em>Title</em>}' attribute.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see #getTitle()
+   * @generated
+   * @ordered
+   */
+  protected String title = TITLE_EDEFAULT;
+
+  /**
+   * The cached value of the '{@link #getSystem() <em>System</em>}' reference.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see #getSystem()
+   * @generated
+   * @ordered
+   */
+  protected ComponentImplementation system;
+
+  /**
+   * The cached value of the '{@link #getDescription() <em>Description</em>}' containment reference.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see #getDescription()
+   * @generated
+   * @ordered
+   */
+  protected Description description;
+
+  /**
+   * The cached value of the '{@link #getConstant() <em>Constant</em>}' containment reference list.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see #getConstant()
+   * @generated
+   * @ordered
+   */
+  protected EList<FinalValue> constant;
+
+  /**
+   * The cached value of the '{@link #getPlans() <em>Plans</em>}' reference list.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see #getPlans()
+   * @generated
+   * @ordered
+   */
+  protected EList<VerificationPlan> plans;
+
+  /**
+   * The cached value of the '{@link #getSelectionFilter() <em>Selection Filter</em>}' reference list.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see #getSelectionFilter()
+   * @generated
+   * @ordered
+   */
+  protected EList<VerificationCategory> selectionFilter;
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  protected AssuranceCaseConfigurationImpl()
+  {
+    super();
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  protected EClass eStaticClass()
+  {
+    return AlisaPackage.Literals.ASSURANCE_CASE_CONFIGURATION;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public String getName()
+  {
+    return name;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public void setName(String newName)
+  {
+    String oldName = name;
+    name = newName;
+    if (eNotificationRequired())
+      eNotify(new ENotificationImpl(this, Notification.SET, AlisaPackage.ASSURANCE_CASE_CONFIGURATION__NAME, oldName, name));
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public String getTitle()
+  {
+    return title;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public void setTitle(String newTitle)
+  {
+    String oldTitle = title;
+    title = newTitle;
+    if (eNotificationRequired())
+      eNotify(new ENotificationImpl(this, Notification.SET, AlisaPackage.ASSURANCE_CASE_CONFIGURATION__TITLE, oldTitle, title));
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public ComponentImplementation getSystem()
+  {
+    if (system != null && ((EObject)system).eIsProxy())
+    {
+      InternalEObject oldSystem = (InternalEObject)system;
+      system = (ComponentImplementation)eResolveProxy(oldSystem);
+      if (system != oldSystem)
+      {
+        if (eNotificationRequired())
+          eNotify(new ENotificationImpl(this, Notification.RESOLVE, AlisaPackage.ASSURANCE_CASE_CONFIGURATION__SYSTEM, oldSystem, system));
+      }
+    }
+    return system;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public ComponentImplementation basicGetSystem()
+  {
+    return system;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public void setSystem(ComponentImplementation newSystem)
+  {
+    ComponentImplementation oldSystem = system;
+    system = newSystem;
+    if (eNotificationRequired())
+      eNotify(new ENotificationImpl(this, Notification.SET, AlisaPackage.ASSURANCE_CASE_CONFIGURATION__SYSTEM, oldSystem, system));
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public Description getDescription()
+  {
+    return description;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public NotificationChain basicSetDescription(Description newDescription, NotificationChain msgs)
+  {
+    Description oldDescription = description;
+    description = newDescription;
+    if (eNotificationRequired())
+    {
+      ENotificationImpl notification = new ENotificationImpl(this, Notification.SET, AlisaPackage.ASSURANCE_CASE_CONFIGURATION__DESCRIPTION, oldDescription, newDescription);
+      if (msgs == null) msgs = notification; else msgs.add(notification);
+    }
+    return msgs;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public void setDescription(Description newDescription)
+  {
+    if (newDescription != description)
+    {
+      NotificationChain msgs = null;
+      if (description != null)
+        msgs = ((InternalEObject)description).eInverseRemove(this, EOPPOSITE_FEATURE_BASE - AlisaPackage.ASSURANCE_CASE_CONFIGURATION__DESCRIPTION, null, msgs);
+      if (newDescription != null)
+        msgs = ((InternalEObject)newDescription).eInverseAdd(this, EOPPOSITE_FEATURE_BASE - AlisaPackage.ASSURANCE_CASE_CONFIGURATION__DESCRIPTION, null, msgs);
+      msgs = basicSetDescription(newDescription, msgs);
+      if (msgs != null) msgs.dispatch();
+    }
+    else if (eNotificationRequired())
+      eNotify(new ENotificationImpl(this, Notification.SET, AlisaPackage.ASSURANCE_CASE_CONFIGURATION__DESCRIPTION, newDescription, newDescription));
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public EList<FinalValue> getConstant()
+  {
+    if (constant == null)
+    {
+      constant = new EObjectContainmentEList<FinalValue>(FinalValue.class, this, AlisaPackage.ASSURANCE_CASE_CONFIGURATION__CONSTANT);
+    }
+    return constant;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public EList<VerificationPlan> getPlans()
+  {
+    if (plans == null)
+    {
+      plans = new EObjectResolvingEList<VerificationPlan>(VerificationPlan.class, this, AlisaPackage.ASSURANCE_CASE_CONFIGURATION__PLANS);
+    }
+    return plans;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public EList<VerificationCategory> getSelectionFilter()
+  {
+    if (selectionFilter == null)
+    {
+      selectionFilter = new EObjectResolvingEList<VerificationCategory>(VerificationCategory.class, this, AlisaPackage.ASSURANCE_CASE_CONFIGURATION__SELECTION_FILTER);
+    }
+    return selectionFilter;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  public NotificationChain eInverseRemove(InternalEObject otherEnd, int featureID, NotificationChain msgs)
+  {
+    switch (featureID)
+    {
+      case AlisaPackage.ASSURANCE_CASE_CONFIGURATION__DESCRIPTION:
+        return basicSetDescription(null, msgs);
+      case AlisaPackage.ASSURANCE_CASE_CONFIGURATION__CONSTANT:
+        return ((InternalEList<?>)getConstant()).basicRemove(otherEnd, msgs);
+    }
+    return super.eInverseRemove(otherEnd, featureID, msgs);
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  public Object eGet(int featureID, boolean resolve, boolean coreType)
+  {
+    switch (featureID)
+    {
+      case AlisaPackage.ASSURANCE_CASE_CONFIGURATION__NAME:
+        return getName();
+      case AlisaPackage.ASSURANCE_CASE_CONFIGURATION__TITLE:
+        return getTitle();
+      case AlisaPackage.ASSURANCE_CASE_CONFIGURATION__SYSTEM:
+        if (resolve) return getSystem();
+        return basicGetSystem();
+      case AlisaPackage.ASSURANCE_CASE_CONFIGURATION__DESCRIPTION:
+        return getDescription();
+      case AlisaPackage.ASSURANCE_CASE_CONFIGURATION__CONSTANT:
+        return getConstant();
+      case AlisaPackage.ASSURANCE_CASE_CONFIGURATION__PLANS:
+        return getPlans();
+      case AlisaPackage.ASSURANCE_CASE_CONFIGURATION__SELECTION_FILTER:
+        return getSelectionFilter();
+    }
+    return super.eGet(featureID, resolve, coreType);
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @SuppressWarnings("unchecked")
+  @Override
+  public void eSet(int featureID, Object newValue)
+  {
+    switch (featureID)
+    {
+      case AlisaPackage.ASSURANCE_CASE_CONFIGURATION__NAME:
+        setName((String)newValue);
+        return;
+      case AlisaPackage.ASSURANCE_CASE_CONFIGURATION__TITLE:
+        setTitle((String)newValue);
+        return;
+      case AlisaPackage.ASSURANCE_CASE_CONFIGURATION__SYSTEM:
+        setSystem((ComponentImplementation)newValue);
+        return;
+      case AlisaPackage.ASSURANCE_CASE_CONFIGURATION__DESCRIPTION:
+        setDescription((Description)newValue);
+        return;
+      case AlisaPackage.ASSURANCE_CASE_CONFIGURATION__CONSTANT:
+        getConstant().clear();
+        getConstant().addAll((Collection<? extends FinalValue>)newValue);
+        return;
+      case AlisaPackage.ASSURANCE_CASE_CONFIGURATION__PLANS:
+        getPlans().clear();
+        getPlans().addAll((Collection<? extends VerificationPlan>)newValue);
+        return;
+      case AlisaPackage.ASSURANCE_CASE_CONFIGURATION__SELECTION_FILTER:
+        getSelectionFilter().clear();
+        getSelectionFilter().addAll((Collection<? extends VerificationCategory>)newValue);
+        return;
+    }
+    super.eSet(featureID, newValue);
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  public void eUnset(int featureID)
+  {
+    switch (featureID)
+    {
+      case AlisaPackage.ASSURANCE_CASE_CONFIGURATION__NAME:
+        setName(NAME_EDEFAULT);
+        return;
+      case AlisaPackage.ASSURANCE_CASE_CONFIGURATION__TITLE:
+        setTitle(TITLE_EDEFAULT);
+        return;
+      case AlisaPackage.ASSURANCE_CASE_CONFIGURATION__SYSTEM:
+        setSystem((ComponentImplementation)null);
+        return;
+      case AlisaPackage.ASSURANCE_CASE_CONFIGURATION__DESCRIPTION:
+        setDescription((Description)null);
+        return;
+      case AlisaPackage.ASSURANCE_CASE_CONFIGURATION__CONSTANT:
+        getConstant().clear();
+        return;
+      case AlisaPackage.ASSURANCE_CASE_CONFIGURATION__PLANS:
+        getPlans().clear();
+        return;
+      case AlisaPackage.ASSURANCE_CASE_CONFIGURATION__SELECTION_FILTER:
+        getSelectionFilter().clear();
+        return;
+    }
+    super.eUnset(featureID);
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  public boolean eIsSet(int featureID)
+  {
+    switch (featureID)
+    {
+      case AlisaPackage.ASSURANCE_CASE_CONFIGURATION__NAME:
+        return NAME_EDEFAULT == null ? name != null : !NAME_EDEFAULT.equals(name);
+      case AlisaPackage.ASSURANCE_CASE_CONFIGURATION__TITLE:
+        return TITLE_EDEFAULT == null ? title != null : !TITLE_EDEFAULT.equals(title);
+      case AlisaPackage.ASSURANCE_CASE_CONFIGURATION__SYSTEM:
+        return system != null;
+      case AlisaPackage.ASSURANCE_CASE_CONFIGURATION__DESCRIPTION:
+        return description != null;
+      case AlisaPackage.ASSURANCE_CASE_CONFIGURATION__CONSTANT:
+        return constant != null && !constant.isEmpty();
+      case AlisaPackage.ASSURANCE_CASE_CONFIGURATION__PLANS:
+        return plans != null && !plans.isEmpty();
+      case AlisaPackage.ASSURANCE_CASE_CONFIGURATION__SELECTION_FILTER:
+        return selectionFilter != null && !selectionFilter.isEmpty();
+    }
+    return super.eIsSet(featureID);
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  public String toString()
+  {
+    if (eIsProxy()) return super.toString();
+
+    StringBuffer result = new StringBuffer(super.toString());
+    result.append(" (name: ");
+    result.append(name);
+    result.append(", title: ");
+    result.append(title);
+    result.append(')');
+    return result.toString();
+  }
+
+} //AssuranceCaseConfigurationImpl
