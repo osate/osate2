302fb9e9d8e87115b4076959bf82c6b1934b9730
Peter Feiler <phf@sei.cmu.edu>
2015-01-31 11:29:09 -0500

    new classes for the library.

---
+++ alisa/org.osate.reqspec/src-gen/org/osate/reqspec/reqSpec/ReqLib.java
@@ -0,0 +1,172 @@
+/**
+ */
+package org.osate.reqspec.reqSpec;
+
+import org.eclipse.emf.common.util.EList;
+
+import org.eclipse.emf.ecore.EObject;
+
+import org.osate.aadl2.Classifier;
+
+import org.osate.alisa.common.common.FinalValue;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Req Lib</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * <ul>
+ *   <li>{@link org.osate.reqspec.reqSpec.ReqLib#getName <em>Name</em>}</li>
+ *   <li>{@link org.osate.reqspec.reqSpec.ReqLib#getTitle <em>Title</em>}</li>
+ *   <li>{@link org.osate.reqspec.reqSpec.ReqLib#getTarget <em>Target</em>}</li>
+ *   <li>{@link org.osate.reqspec.reqSpec.ReqLib#getTargetDescription <em>Target Description</em>}</li>
+ *   <li>{@link org.osate.reqspec.reqSpec.ReqLib#getConstants <em>Constants</em>}</li>
+ *   <li>{@link org.osate.reqspec.reqSpec.ReqLib#getContent <em>Content</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @see org.osate.reqspec.reqSpec.ReqSpecPackage#getReqLib()
+ * @model
+ * @generated
+ */
+public interface ReqLib extends EObject
+{
+  /**
+   * Returns the value of the '<em><b>Name</b></em>' attribute.
+   * <!-- begin-user-doc -->
+   * <p>
+   * If the meaning of the '<em>Name</em>' attribute isn't clear,
+   * there really should be more of a description here...
+   * </p>
+   * <!-- end-user-doc -->
+   * @return the value of the '<em>Name</em>' attribute.
+   * @see #setName(String)
+   * @see org.osate.reqspec.reqSpec.ReqSpecPackage#getReqLib_Name()
+   * @model
+   * @generated
+   */
+  String getName();
+
+  /**
+   * Sets the value of the '{@link org.osate.reqspec.reqSpec.ReqLib#getName <em>Name</em>}' attribute.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @param value the new value of the '<em>Name</em>' attribute.
+   * @see #getName()
+   * @generated
+   */
+  void setName(String value);
+
+  /**
+   * Returns the value of the '<em><b>Title</b></em>' attribute.
+   * <!-- begin-user-doc -->
+   * <p>
+   * If the meaning of the '<em>Title</em>' attribute isn't clear,
+   * there really should be more of a description here...
+   * </p>
+   * <!-- end-user-doc -->
+   * @return the value of the '<em>Title</em>' attribute.
+   * @see #setTitle(String)
+   * @see org.osate.reqspec.reqSpec.ReqSpecPackage#getReqLib_Title()
+   * @model
+   * @generated
+   */
+  String getTitle();
+
+  /**
+   * Sets the value of the '{@link org.osate.reqspec.reqSpec.ReqLib#getTitle <em>Title</em>}' attribute.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @param value the new value of the '<em>Title</em>' attribute.
+   * @see #getTitle()
+   * @generated
+   */
+  void setTitle(String value);
+
+  /**
+   * Returns the value of the '<em><b>Target</b></em>' reference.
+   * <!-- begin-user-doc -->
+   * <p>
+   * If the meaning of the '<em>Target</em>' reference isn't clear,
+   * there really should be more of a description here...
+   * </p>
+   * <!-- end-user-doc -->
+   * @return the value of the '<em>Target</em>' reference.
+   * @see #setTarget(Classifier)
+   * @see org.osate.reqspec.reqSpec.ReqSpecPackage#getReqLib_Target()
+   * @model
+   * @generated
+   */
+  Classifier getTarget();
+
+  /**
+   * Sets the value of the '{@link org.osate.reqspec.reqSpec.ReqLib#getTarget <em>Target</em>}' reference.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @param value the new value of the '<em>Target</em>' reference.
+   * @see #getTarget()
+   * @generated
+   */
+  void setTarget(Classifier value);
+
+  /**
+   * Returns the value of the '<em><b>Target Description</b></em>' attribute.
+   * <!-- begin-user-doc -->
+   * <p>
+   * If the meaning of the '<em>Target Description</em>' attribute isn't clear,
+   * there really should be more of a description here...
+   * </p>
+   * <!-- end-user-doc -->
+   * @return the value of the '<em>Target Description</em>' attribute.
+   * @see #setTargetDescription(String)
+   * @see org.osate.reqspec.reqSpec.ReqSpecPackage#getReqLib_TargetDescription()
+   * @model
+   * @generated
+   */
+  String getTargetDescription();
+
+  /**
+   * Sets the value of the '{@link org.osate.reqspec.reqSpec.ReqLib#getTargetDescription <em>Target Description</em>}' attribute.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @param value the new value of the '<em>Target Description</em>' attribute.
+   * @see #getTargetDescription()
+   * @generated
+   */
+  void setTargetDescription(String value);
+
+  /**
+   * Returns the value of the '<em><b>Constants</b></em>' containment reference list.
+   * The list contents are of type {@link org.osate.alisa.common.common.FinalValue}.
+   * <!-- begin-user-doc -->
+   * <p>
+   * If the meaning of the '<em>Constants</em>' containment reference list isn't clear,
+   * there really should be more of a description here...
+   * </p>
+   * <!-- end-user-doc -->
+   * @return the value of the '<em>Constants</em>' containment reference list.
+   * @see org.osate.reqspec.reqSpec.ReqSpecPackage#getReqLib_Constants()
+   * @model containment="true"
+   * @generated
+   */
+  EList<FinalValue> getConstants();
+
+  /**
+   * Returns the value of the '<em><b>Content</b></em>' containment reference list.
+   * The list contents are of type {@link org.eclipse.emf.ecore.EObject}.
+   * <!-- begin-user-doc -->
+   * <p>
+   * If the meaning of the '<em>Content</em>' containment reference list isn't clear,
+   * there really should be more of a description here...
+   * </p>
+   * <!-- end-user-doc -->
+   * @return the value of the '<em>Content</em>' containment reference list.
+   * @see org.osate.reqspec.reqSpec.ReqSpecPackage#getReqLib_Content()
+   * @model containment="true"
+   * @generated
+   */
+  EList<EObject> getContent();
+
+} // ReqLib
---
+++ alisa/org.osate.reqspec/src-gen/org/osate/reqspec/reqSpec/impl/ReqLibImpl.java
@@ -0,0 +1,459 @@
+/**
+ */
+package org.osate.reqspec.reqSpec.impl;
+
+import java.util.Collection;
+
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.emf.common.notify.NotificationChain;
+
+import org.eclipse.emf.common.util.EList;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.InternalEObject;
+
+import org.eclipse.emf.ecore.impl.ENotificationImpl;
+import org.eclipse.emf.ecore.impl.MinimalEObjectImpl;
+
+import org.eclipse.emf.ecore.util.EObjectContainmentEList;
+import org.eclipse.emf.ecore.util.InternalEList;
+
+import org.osate.aadl2.Classifier;
+
+import org.osate.alisa.common.common.FinalValue;
+
+import org.osate.reqspec.reqSpec.ReqLib;
+import org.osate.reqspec.reqSpec.ReqSpecPackage;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Req Lib</b></em>'.
+ * <!-- end-user-doc -->
+ * <p>
+ * The following features are implemented:
+ * <ul>
+ *   <li>{@link org.osate.reqspec.reqSpec.impl.ReqLibImpl#getName <em>Name</em>}</li>
+ *   <li>{@link org.osate.reqspec.reqSpec.impl.ReqLibImpl#getTitle <em>Title</em>}</li>
+ *   <li>{@link org.osate.reqspec.reqSpec.impl.ReqLibImpl#getTarget <em>Target</em>}</li>
+ *   <li>{@link org.osate.reqspec.reqSpec.impl.ReqLibImpl#getTargetDescription <em>Target Description</em>}</li>
+ *   <li>{@link org.osate.reqspec.reqSpec.impl.ReqLibImpl#getConstants <em>Constants</em>}</li>
+ *   <li>{@link org.osate.reqspec.reqSpec.impl.ReqLibImpl#getContent <em>Content</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @generated
+ */
+public class ReqLibImpl extends MinimalEObjectImpl.Container implements ReqLib
+{
+  /**
+   * The default value of the '{@link #getName() <em>Name</em>}' attribute.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see #getName()
+   * @generated
+   * @ordered
+   */
+  protected static final String NAME_EDEFAULT = null;
+
+  /**
+   * The cached value of the '{@link #getName() <em>Name</em>}' attribute.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see #getName()
+   * @generated
+   * @ordered
+   */
+  protected String name = NAME_EDEFAULT;
+
+  /**
+   * The default value of the '{@link #getTitle() <em>Title</em>}' attribute.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see #getTitle()
+   * @generated
+   * @ordered
+   */
+  protected static final String TITLE_EDEFAULT = null;
+
+  /**
+   * The cached value of the '{@link #getTitle() <em>Title</em>}' attribute.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see #getTitle()
+   * @generated
+   * @ordered
+   */
+  protected String title = TITLE_EDEFAULT;
+
+  /**
+   * The cached value of the '{@link #getTarget() <em>Target</em>}' reference.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see #getTarget()
+   * @generated
+   * @ordered
+   */
+  protected Classifier target;
+
+  /**
+   * The default value of the '{@link #getTargetDescription() <em>Target Description</em>}' attribute.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see #getTargetDescription()
+   * @generated
+   * @ordered
+   */
+  protected static final String TARGET_DESCRIPTION_EDEFAULT = null;
+
+  /**
+   * The cached value of the '{@link #getTargetDescription() <em>Target Description</em>}' attribute.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see #getTargetDescription()
+   * @generated
+   * @ordered
+   */
+  protected String targetDescription = TARGET_DESCRIPTION_EDEFAULT;
+
+  /**
+   * The cached value of the '{@link #getConstants() <em>Constants</em>}' containment reference list.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see #getConstants()
+   * @generated
+   * @ordered
+   */
+  protected EList<FinalValue> constants;
+
+  /**
+   * The cached value of the '{@link #getContent() <em>Content</em>}' containment reference list.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @see #getContent()
+   * @generated
+   * @ordered
+   */
+  protected EList<EObject> content;
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  protected ReqLibImpl()
+  {
+    super();
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  protected EClass eStaticClass()
+  {
+    return ReqSpecPackage.Literals.REQ_LIB;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public String getName()
+  {
+    return name;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public void setName(String newName)
+  {
+    String oldName = name;
+    name = newName;
+    if (eNotificationRequired())
+      eNotify(new ENotificationImpl(this, Notification.SET, ReqSpecPackage.REQ_LIB__NAME, oldName, name));
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public String getTitle()
+  {
+    return title;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public void setTitle(String newTitle)
+  {
+    String oldTitle = title;
+    title = newTitle;
+    if (eNotificationRequired())
+      eNotify(new ENotificationImpl(this, Notification.SET, ReqSpecPackage.REQ_LIB__TITLE, oldTitle, title));
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public Classifier getTarget()
+  {
+    if (target != null && ((EObject)target).eIsProxy())
+    {
+      InternalEObject oldTarget = (InternalEObject)target;
+      target = (Classifier)eResolveProxy(oldTarget);
+      if (target != oldTarget)
+      {
+        if (eNotificationRequired())
+          eNotify(new ENotificationImpl(this, Notification.RESOLVE, ReqSpecPackage.REQ_LIB__TARGET, oldTarget, target));
+      }
+    }
+    return target;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public Classifier basicGetTarget()
+  {
+    return target;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public void setTarget(Classifier newTarget)
+  {
+    Classifier oldTarget = target;
+    target = newTarget;
+    if (eNotificationRequired())
+      eNotify(new ENotificationImpl(this, Notification.SET, ReqSpecPackage.REQ_LIB__TARGET, oldTarget, target));
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public String getTargetDescription()
+  {
+    return targetDescription;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public void setTargetDescription(String newTargetDescription)
+  {
+    String oldTargetDescription = targetDescription;
+    targetDescription = newTargetDescription;
+    if (eNotificationRequired())
+      eNotify(new ENotificationImpl(this, Notification.SET, ReqSpecPackage.REQ_LIB__TARGET_DESCRIPTION, oldTargetDescription, targetDescription));
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public EList<FinalValue> getConstants()
+  {
+    if (constants == null)
+    {
+      constants = new EObjectContainmentEList<FinalValue>(FinalValue.class, this, ReqSpecPackage.REQ_LIB__CONSTANTS);
+    }
+    return constants;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  public EList<EObject> getContent()
+  {
+    if (content == null)
+    {
+      content = new EObjectContainmentEList<EObject>(EObject.class, this, ReqSpecPackage.REQ_LIB__CONTENT);
+    }
+    return content;
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  public NotificationChain eInverseRemove(InternalEObject otherEnd, int featureID, NotificationChain msgs)
+  {
+    switch (featureID)
+    {
+      case ReqSpecPackage.REQ_LIB__CONSTANTS:
+        return ((InternalEList<?>)getConstants()).basicRemove(otherEnd, msgs);
+      case ReqSpecPackage.REQ_LIB__CONTENT:
+        return ((InternalEList<?>)getContent()).basicRemove(otherEnd, msgs);
+    }
+    return super.eInverseRemove(otherEnd, featureID, msgs);
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  public Object eGet(int featureID, boolean resolve, boolean coreType)
+  {
+    switch (featureID)
+    {
+      case ReqSpecPackage.REQ_LIB__NAME:
+        return getName();
+      case ReqSpecPackage.REQ_LIB__TITLE:
+        return getTitle();
+      case ReqSpecPackage.REQ_LIB__TARGET:
+        if (resolve) return getTarget();
+        return basicGetTarget();
+      case ReqSpecPackage.REQ_LIB__TARGET_DESCRIPTION:
+        return getTargetDescription();
+      case ReqSpecPackage.REQ_LIB__CONSTANTS:
+        return getConstants();
+      case ReqSpecPackage.REQ_LIB__CONTENT:
+        return getContent();
+    }
+    return super.eGet(featureID, resolve, coreType);
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @SuppressWarnings("unchecked")
+  @Override
+  public void eSet(int featureID, Object newValue)
+  {
+    switch (featureID)
+    {
+      case ReqSpecPackage.REQ_LIB__NAME:
+        setName((String)newValue);
+        return;
+      case ReqSpecPackage.REQ_LIB__TITLE:
+        setTitle((String)newValue);
+        return;
+      case ReqSpecPackage.REQ_LIB__TARGET:
+        setTarget((Classifier)newValue);
+        return;
+      case ReqSpecPackage.REQ_LIB__TARGET_DESCRIPTION:
+        setTargetDescription((String)newValue);
+        return;
+      case ReqSpecPackage.REQ_LIB__CONSTANTS:
+        getConstants().clear();
+        getConstants().addAll((Collection<? extends FinalValue>)newValue);
+        return;
+      case ReqSpecPackage.REQ_LIB__CONTENT:
+        getContent().clear();
+        getContent().addAll((Collection<? extends EObject>)newValue);
+        return;
+    }
+    super.eSet(featureID, newValue);
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  public void eUnset(int featureID)
+  {
+    switch (featureID)
+    {
+      case ReqSpecPackage.REQ_LIB__NAME:
+        setName(NAME_EDEFAULT);
+        return;
+      case ReqSpecPackage.REQ_LIB__TITLE:
+        setTitle(TITLE_EDEFAULT);
+        return;
+      case ReqSpecPackage.REQ_LIB__TARGET:
+        setTarget((Classifier)null);
+        return;
+      case ReqSpecPackage.REQ_LIB__TARGET_DESCRIPTION:
+        setTargetDescription(TARGET_DESCRIPTION_EDEFAULT);
+        return;
+      case ReqSpecPackage.REQ_LIB__CONSTANTS:
+        getConstants().clear();
+        return;
+      case ReqSpecPackage.REQ_LIB__CONTENT:
+        getContent().clear();
+        return;
+    }
+    super.eUnset(featureID);
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  public boolean eIsSet(int featureID)
+  {
+    switch (featureID)
+    {
+      case ReqSpecPackage.REQ_LIB__NAME:
+        return NAME_EDEFAULT == null ? name != null : !NAME_EDEFAULT.equals(name);
+      case ReqSpecPackage.REQ_LIB__TITLE:
+        return TITLE_EDEFAULT == null ? title != null : !TITLE_EDEFAULT.equals(title);
+      case ReqSpecPackage.REQ_LIB__TARGET:
+        return target != null;
+      case ReqSpecPackage.REQ_LIB__TARGET_DESCRIPTION:
+        return TARGET_DESCRIPTION_EDEFAULT == null ? targetDescription != null : !TARGET_DESCRIPTION_EDEFAULT.equals(targetDescription);
+      case ReqSpecPackage.REQ_LIB__CONSTANTS:
+        return constants != null && !constants.isEmpty();
+      case ReqSpecPackage.REQ_LIB__CONTENT:
+        return content != null && !content.isEmpty();
+    }
+    return super.eIsSet(featureID);
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  public String toString()
+  {
+    if (eIsProxy()) return super.toString();
+
+    StringBuffer result = new StringBuffer(super.toString());
+    result.append(" (name: ");
+    result.append(name);
+    result.append(", title: ");
+    result.append(title);
+    result.append(", targetDescription: ");
+    result.append(targetDescription);
+    result.append(')');
+    return result.toString();
+  }
+
+} //ReqLibImpl
