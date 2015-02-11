e7d60729ac88e6409e457cc0546b4e8268bc4e7e
Peter Feiler <phf@sei.cmu.edu>
2015-02-11 08:07:14 -0500

    updated weight analysis to produce a better report and not use dialogs.

--- analyses/org.osate.analysis.architecture/src/org/osate/analysis/architecture/PropertyTotals.java
+++ analyses/org.osate.analysis.architecture/src/org/osate/analysis/architecture/PropertyTotals.java
@@ -42,6 +42,7 @@ package org.osate.analysis.architecture;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.emf.common.util.EList;
 import org.osate.aadl2.ComponentCategory;
+import org.osate.aadl2.NamedElement;
 import org.osate.aadl2.instance.ComponentInstance;
 import org.osate.aadl2.instance.ConnectionInstance;
 import org.osate.aadl2.instance.SystemInstance;
@@ -52,64 +53,68 @@ import org.osate.xtext.aadl2.properties.util.GetProperties;
 /**
  * @author phf
  */
-public /* final */ class PropertyTotals extends AadlProcessingSwitchWithProgress {
-	/* Counters to keep track of occurrences of different
+public/* final */class PropertyTotals extends AadlProcessingSwitchWithProgress {
+	/*
+	 * Counters to keep track of occurrences of different
 	 * objects in the model.
 	 */
 	private double WeightSum = 0.0;
-	private String ResultMsg = "Weight Analysis";
 	private AbstractAaxlAction action;
-	
+
 	public PropertyTotals(final IProgressMonitor monitor, AbstractAaxlAction action) {
 		super(monitor, PROCESS_PRE_ORDER_ALL);
 		this.action = action;
 	}
 
 	protected final void initSwitches() {
-	    /* We overwrite the case method for a class in the meta model
-	     * specific switches.
-         */
-		
+		/*
+		 * We overwrite the case method for a class in the meta model
+		 * specific switches.
+		 */
+
 	}
-	
-	public final double calcWeight(ComponentInstance ci){
+
+	public final double calcWeight(ComponentInstance ci) {
 		String header = "Element,type,net weight, net/gross\n\r";
-    	csvlog(header);
-		double total = doCalcWeight(ci, true,"");
+		csvlog(header);
+		double total = doCalcWeight(ci, true, "");
 		return total;
-		
+
 	}
-	
-	private double doCalcWeight(ComponentInstance ci, boolean needWeight, String indent){
+
+	private double doCalcWeight(ComponentInstance ci, boolean needWeight, String indent) {
 
 		double net = GetProperties.getNetWeight(ci, 0.0);
 		double weight = 0.0;
 		double gross = GetProperties.getGrossWeight(ci, 0.0);
 		double sublimit = 0.0;
-		logWeight(getPrintName(ci), ci.getCategory().getName() , net>0.0?net:gross,net>0.0);
+		reportWeight(getPrintName(ci), ci.getCategory().getName(), net > 0.0 ? net : gross, net > 0.0);
 		EList<ComponentInstance> cil = ci.getComponentInstances();
 		for (ComponentInstance subi : cil) {
 			ComponentCategory subcat = subi.getCategory();
-			if(!(subcat.equals(ComponentCategory.PROCESS)||subcat.equals(ComponentCategory.VIRTUAL_BUS)||subcat.equals(ComponentCategory.VIRTUAL_PROCESSOR))){
-				double subweight = doCalcWeight(subi,(needWeight&&(gross == 0.0||net > 0.0)),indent+" ");
+			if (!(subcat.equals(ComponentCategory.PROCESS) || subcat.equals(ComponentCategory.VIRTUAL_BUS) || subcat
+					.equals(ComponentCategory.VIRTUAL_PROCESSOR))) {
+				double subweight = doCalcWeight(subi, (needWeight && (gross == 0.0 || net > 0.0)), indent + " ");
 				weight += subweight;
-				sublimit += GetProperties.getWeightLimit(subi,  0.0);
+				sublimit += GetProperties.getWeightLimit(subi, 0.0);
 			}
 		}
 		EList<ConnectionInstance> connl = ci.getConnectionInstances();
 		for (ConnectionInstance connectionInstance : connl) {
 			double netconn = GetProperties.getNetWeight(connectionInstance, 0.0);
-				double grossconn = GetProperties.getGrossWeight(connectionInstance, 0.0);
-			weight += netconn>0?netconn:grossconn;
-			logWeight(connectionInstance.getName(), "Connection " , netconn>0?netconn:grossconn,netconn>0);
-			if (netconn>0||grossconn>0){
-				ResultMsg = indent+String.format(connectionInstance.getName()+": Weight of access connection %.3f kg",netconn>0?netconn:grossconn);
-				info(connectionInstance,ResultMsg);
+			double grossconn = GetProperties.getGrossWeight(connectionInstance, 0.0);
+			weight += netconn > 0 ? netconn : grossconn;
+			reportWeight(connectionInstance.getName(), "Connection ", netconn > 0 ? netconn : grossconn, netconn > 0);
+			if (netconn > 0 || grossconn > 0) {
+				String ResultMsg = indent
+						+ String.format(connectionInstance.getName() + ": Weight of access connection %.3f kg",
+								netconn > 0 ? netconn : grossconn);
+				reportinfo(connectionInstance, ResultMsg);
 			}
-			sublimit += GetProperties.getWeightLimit(connectionInstance,  0.0);
+			sublimit += GetProperties.getWeightLimit(connectionInstance, 0.0);
 		}
-		if (weight == 0.0 && cil.isEmpty()){
-			if (gross == 0 && net > 0){
+		if (weight == 0.0 && cil.isEmpty()) {
+			if (gross == 0 && net > 0) {
 				weight = net;
 			} else {
 				weight = gross;
@@ -117,56 +122,79 @@ public /* final */ class PropertyTotals extends AadlProcessingSwitchWithProgress
 		} else {
 			weight += net;
 		}
-		if (gross > 0.0){
-			if (weight > gross){
+		if (gross > 0.0) {
+			if (weight > gross) {
 				// problem
-				warning(ci,indent+String.format(getPrintName(ci)+": Sum of weights %.3f kg exceeds grossweight %.3f kg",weight,gross));
+				reportwarning(
+						ci,
+						indent
+								+ String.format(getPrintName(ci)
+										+ ":[G] Sum of weights %.3f kg exceeds grossweight %.3f kg", weight, gross));
 				// Set gross weight
-			} else if (weight > 0 && weight < gross){
+			} else if (weight > 0 && weight < gross) {
 				// problem
-				warning(ci,indent+String.format(getPrintName(ci)+": Sum of weights %.3f kg less than grossweight %.3f kg (using gross weight)",weight,gross));
+				reportwarning(
+						ci,
+						indent
+								+ String.format(
+										getPrintName(ci)
+												+ ":[G] Sum of weights %.3f kg less than grossweight %.3f kg (using gross weight)",
+										weight, gross));
 				weight = gross;
 			}
-			if (weight == 0.0){
+			if (weight == 0.0) {
 				weight = gross;
 			}
 		}
-		double limit = GetProperties.getWeightLimit(ci,  0.0);
-		if (limit > 0.0){
-			if( weight > limit){
+		double limit = GetProperties.getWeightLimit(ci, 0.0);
+		if (limit > 0.0) {
+			if (weight > limit) {
 				// problem
-				ResultMsg = indent+String.format(getPrintName(ci)+": Sum of weights %.3f kg exceeds weight limit %.3f kg",weight,limit);
-				error(ci,ResultMsg);
+				String ResultMsg = indent
+						+ String.format(getPrintName(ci) + ":[A] Sum of weights %.3f kg exceeds weight limit %.3f kg",
+								weight, limit);
+				reporterror(ci, ResultMsg);
 			} else {
-				if (sublimit > limit){
+				if (sublimit > limit) {
 					// problem
-					warning(ci,indent+String.format(getPrintName(ci)+": Sum of subcomponent weight limits %.3f kg exceeds weight limit %.3f kg",sublimit,limit));
+					reportwarning(
+							ci,
+							indent
+									+ String.format(
+											getPrintName(ci)
+													+ ":[L] Sum of subcomponent weight limits %.3f kg exceeds weight limit %.3f kg",
+											sublimit, limit));
 				}
-				if ( weight < limit){
-					ResultMsg = indent+String.format(getPrintName(ci)+": Sum of weights %.3f kg below weight limit %.3f kg (%.1f %% Weight slack)",
-							weight, limit,(limit-weight)/limit*100);
-					info(ci,ResultMsg);
+				if (weight < limit) {
+					String ResultMsg = indent
+							+ String.format(getPrintName(ci)
+									+ ":[A] Sum of weights %.3f kg below weight limit %.3f kg (%.1f %% Weight slack)",
+									weight, limit, (limit - weight) / limit * 100);
+					reportinfo(ci, ResultMsg);
 				}
 			}
 		} else {
-			if (weight > 0.0){
-				ResultMsg = indent+String.format(String.format(getPrintName(ci)+": Sum of weights / Gross weight %.3f kg (no limit specified", weight));
-				info(ci,ResultMsg);
-			} else if (needWeight){
-				ResultMsg = indent+getPrintName(ci)+": no net weight plus subcomomponent weight, or no grossweight";
-				warning(ci,ResultMsg);
+			if (weight > 0.0) {
+				String ResultMsg = indent
+						+ String.format(String.format(getPrintName(ci)
+								+ ":[L] Sum of weights / Gross weight %.3f kg (no limit specified", weight));
+				reportinfo(ci, ResultMsg);
+			} else if (needWeight) {
+				String ResultMsg = indent + getPrintName(ci)
+						+ ":[L] no net weight plus subcomomponent weight, or no grossweight";
+				reportwarning(ci, ResultMsg);
 			}
 		}
 		return weight;
 	}
-	
-	public String getPrintName(ComponentInstance ci){
+
+	public String getPrintName(ComponentInstance ci) {
 		String res = null;
-		if (ci instanceof SystemInstance){
+		if (ci instanceof SystemInstance) {
 			res = ci.getComponentClassifier().getName();
 		} else {
-			if (ci.getComponentClassifier() != null){
-			res = ci.getName()+": "+ci.getComponentClassifier().getName();
+			if (ci.getComponentClassifier() != null) {
+				res = ci.getName() + ": " + ci.getComponentClassifier().getName();
 			} else {
 				res = ci.getName();
 			}
@@ -174,19 +202,25 @@ public /* final */ class PropertyTotals extends AadlProcessingSwitchWithProgress
 		return res;
 	}
 
-	public String getModelResult() {
-		return ResultMsg;
-	}
-	
-	
-	private void csvlog(String s){
+	private void csvlog(String s) {
 		action.logInfo(s);
 	}
-	
-	private void logWeight(String ownerStr,  String elementType, 
-			double elementWeight, boolean net) {
-		if (elementWeight > 0 )
-		csvlog( ownerStr +","+elementType+","+elementWeight+", "+(net?"net":"gross"));
+
+	private void reportWeight(String ownerStr, String elementType, double elementWeight, boolean net) {
+		if (elementWeight > 0)
+			csvlog(ownerStr + "," + elementType + "," + elementWeight + ", " + (net ? "net" : "gross"));
+	}
+
+	private void reportwarning(final NamedElement obj, final String msg) {
+		action.warning(obj, msg);
+	}
+
+	private void reporterror(final NamedElement obj, final String msg) {
+		action.error(obj, msg);
+	}
+
+	private void reportinfo(final NamedElement obj, final String msg) {
+		action.info(obj, msg);
 	}
 
 }
