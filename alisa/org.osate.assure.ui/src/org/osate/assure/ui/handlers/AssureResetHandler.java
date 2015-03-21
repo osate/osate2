ede301e413b38288b96c905f94bd7ae0dc958bfb
Peter Feiler <phf@sei.cmu.edu>
2015-03-21 00:53:08 -0400

    added assureresethandler

---
+++ alisa/org.osate.assure.ui/src/org/osate/assure/ui/handlers/AssureResetHandler.java
@@ -0,0 +1,141 @@
+package org.osate.assure.ui.handlers;
+
+import static org.osate.assure.util.AssureUtilExtension.recomputeAllCounts;
+import static org.osate.assure.util.AssureUtilExtension.resetToTBD;
+
+import java.io.IOException;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.resources.WorkspaceJob;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.xtext.resource.XtextResource;
+import org.eclipse.xtext.ui.editor.XtextEditor;
+import org.eclipse.xtext.ui.editor.outline.impl.EObjectNode;
+import org.eclipse.xtext.ui.editor.utils.EditorUtils;
+import org.eclipse.xtext.util.concurrent.IUnitOfWork;
+import org.osate.assure.assure.AssuranceEvidence;
+import org.osate.assure.assure.impl.AssuranceEvidenceImpl;
+
+public class AssureResetHandler extends AbstractHandler {
+
+	private IWorkbenchWindow window;
+	private ExecutionEvent executionEvent;
+
+	protected ExecutionEvent getExecutionEvent() {
+		return this.executionEvent;
+	}
+
+	@Override
+	public Object execute(ExecutionEvent event) {
+		EObjectNode node = getEObjectNode(HandlerUtil.getCurrentSelection(event));
+		this.executionEvent = event;
+		if (node == null) {
+			return null;
+		}
+		final URI uri = node.getEObjectURI();
+
+		window = HandlerUtil.getActiveWorkbenchWindow(event);
+		if (window == null) {
+			return null;
+		}
+
+		return executeURI(uri);
+	}
+
+	public Object executeURI(final URI uri) {
+		final XtextEditor xtextEditor = EditorUtils.getActiveXtextEditor();
+		if (xtextEditor == null) {
+			return null;
+		}
+
+		if (!saveChanges(window.getActivePage().getDirtyEditors())) {
+			return null;
+		}
+
+		WorkspaceJob job = new WorkspaceJob(getJobName()) {
+			@Override
+			public IStatus runInWorkspace(final IProgressMonitor monitor) {
+				return xtextEditor.getDocument().readOnly(new IUnitOfWork<IStatus, XtextResource>() {
+					@Override
+					public IStatus exec(XtextResource resource) throws Exception {
+						EObject eobj = resource.getResourceSet().getEObject(uri, true);
+						if (eobj instanceof AssuranceEvidenceImpl) {
+							return runJob((AssuranceEvidenceImpl) eobj, monitor);
+						} else {
+							return Status.CANCEL_STATUS;
+						}
+					}
+				});
+			}
+		};
+
+		job.setRule(ResourcesPlugin.getWorkspace().getRoot());
+		job.schedule();
+		return null;
+	}
+
+	private boolean saveChanges(IEditorPart[] dirtyEditors) {
+		if (dirtyEditors.length == 0) {
+			return true;
+		}
+
+		if (MessageDialog.openConfirm(window.getShell(), "Save editors", "Save editors and continue?")) {
+			NullProgressMonitor monitor = new NullProgressMonitor();
+			for (IEditorPart e : dirtyEditors) {
+				e.doSave(monitor);
+			}
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	private EObjectNode getEObjectNode(ISelection currentSelection) {
+		if (currentSelection instanceof IStructuredSelection) {
+			IStructuredSelection iss = (IStructuredSelection) currentSelection;
+			if (iss.size() == 1) {
+				return (EObjectNode) iss.getFirstElement();
+			}
+		}
+		return null;
+	}
+
+	protected IWorkbenchWindow getWindow() {
+		return window;
+	}
+
+	protected String getJobName() {
+		return "Reset assure states and counts";
+	}
+
+	protected IStatus runJob(AssuranceEvidence rootCaseResult, IProgressMonitor monitor) {
+
+		long start = System.currentTimeMillis();
+		resetToTBD(rootCaseResult);
+		recomputeAllCounts(rootCaseResult);
+		try {
+			rootCaseResult.eResource().save(null);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		long stop = System.currentTimeMillis();
+		System.out.println("Evaluation time: " + (stop - start) / 1000.0 + "s");
+
+		return Status.OK_STATUS;
+	}
+}
