f1cb80f9560edb8deafbb90ec2496943caa1b642
Joe Seibel <jseibel@sei.cmu.edu>
2015-05-20 13:55:21 -0400

    Modified the TypeDefinition parser rule so that a simple error type could be serialized.

--- emv2/org.osate.xtext.aadl2.errormodel/src-gen/org/osate/xtext/aadl2/errormodel/AbstractErrorModelRuntimeModule.java
+++ emv2/org.osate.xtext.aadl2.errormodel/src-gen/org/osate/xtext/aadl2/errormodel/AbstractErrorModelRuntimeModule.java
@@ -6,7 +6,6 @@ package org.osate.xtext.aadl2.errormodel;
 import java.util.Properties;
 
 import org.eclipse.xtext.Constants;
-import org.eclipse.xtext.service.DefaultRuntimeModule;
 
 import com.google.inject.Binder;
 import com.google.inject.name.Names;
@@ -14,8 +13,8 @@ import com.google.inject.name.Names;
 /**
  * Manual modifications go to {org.osate.xtext.aadl2.errormodel.ErrorModelRuntimeModule}
  */
- @SuppressWarnings("all")
-public abstract class AbstractErrorModelRuntimeModule extends DefaultRuntimeModule {
+@SuppressWarnings("all")
+public abstract class AbstractErrorModelRuntimeModule extends org.eclipse.xtext.service.DefaultRuntimeModule {
 
 	protected Properties properties = null;
 
