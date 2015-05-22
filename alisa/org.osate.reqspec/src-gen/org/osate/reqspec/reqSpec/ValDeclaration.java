b573bc08479c9cc8d09e9f403ab3a5b04a555aa1
Peter Feiler <phf@sei.cmu.edu>
2015-05-22 13:38:22 -0400

    missing class (not used yet)

---
+++ alisa/org.osate.reqspec/src-gen/org/osate/reqspec/reqSpec/ValDeclaration.java
@@ -0,0 +1,51 @@
+/**
+ */
+package org.osate.reqspec.reqSpec;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Val Declaration</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * <p>
+ * The following features are supported:
+ * <ul>
+ *   <li>{@link org.osate.reqspec.reqSpec.ValDeclaration#getName <em>Name</em>}</li>
+ * </ul>
+ * </p>
+ *
+ * @see org.osate.reqspec.reqSpec.ReqSpecPackage#getValDeclaration()
+ * @model
+ * @generated
+ */
+public interface ValDeclaration extends EObject
+{
+  /**
+   * Returns the value of the '<em><b>Name</b></em>' attribute.
+   * <!-- begin-user-doc -->
+   * <p>
+   * If the meaning of the '<em>Name</em>' attribute isn't clear,
+   * there really should be more of a description here...
+   * </p>
+   * <!-- end-user-doc -->
+   * @return the value of the '<em>Name</em>' attribute.
+   * @see #setName(String)
+   * @see org.osate.reqspec.reqSpec.ReqSpecPackage#getValDeclaration_Name()
+   * @model
+   * @generated
+   */
+  String getName();
+
+  /**
+   * Sets the value of the '{@link org.osate.reqspec.reqSpec.ValDeclaration#getName <em>Name</em>}' attribute.
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @param value the new value of the '<em>Name</em>' attribute.
+   * @see #getName()
+   * @generated
+   */
+  void setName(String value);
+
+} // ValDeclaration
