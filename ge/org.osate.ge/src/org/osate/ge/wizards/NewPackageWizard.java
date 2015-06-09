2ddc18f93475095c6c58ca38566ab607de6c6437
Kenneth Ream <klream@sei.cmu.edu>
2015-06-09 16:15:05 -0400

    When starting the create AadlPackage/PropertySet and Aadl Package Graphical Wizards, if in a workspace with no projects instead of the controls on the wizard page there is a message that its required to create a project in the workspace before you can create am AadlPackage part offix for osate/osate2-core #599

--- ge/org.osate.ge/src/org/osate/ge/wizards/NewPackageWizard.java
+++ ge/org.osate.ge/src/org/osate/ge/wizards/NewPackageWizard.java
@@ -1,4 +1,5 @@
 package org.osate.ge.wizards;
+
 /*
  * <copyright>
  * Copyright  2009 by Carnegie Mellon University, all rights reserved.
@@ -80,6 +81,7 @@ import org.eclipse.ui.actions.WorkspaceModifyOperation;
 import org.eclipse.ui.dialogs.ContainerGenerator;
 import org.eclipse.ui.model.WorkbenchContentProvider;
 import org.eclipse.ui.model.WorkbenchLabelProvider;
+import org.eclipse.ui.part.PageBook;
 import org.eclipse.ui.statushandlers.StatusManager;
 import org.eclipse.xtext.resource.XtextResourceSet;
 import org.osate.aadl2.NamedElement;
@@ -90,39 +92,35 @@ import org.osate.ui.OsateUiPlugin;
 import org.osate.workspace.IResourceUtility;
 import org.osate.workspace.WorkspacePlugin;
 
-
 /**
  * This is a simple wizard for creating a new model file.
  * It is based on edu.cmu.sei.aadl.model.core.presentation.CoreModelWizard from OSATE 1.
  * 
  * @author jseibel
  */
-public class NewPackageWizard extends Wizard implements INewWizard
-{	
+public class NewPackageWizard extends Wizard implements INewWizard {
 	/**
 	 * This is the only page of the wizard.  Instantiated in addPages and accessed
 	 * in performFinish.
 	 */
 	private NewPackageWizardNewPackageCreationPage newPackageCreationPage = null;
-	
+
 	/**
 	 * Selected project in the package explorer.  This will become the selected project
 	 * in the wizard's page.
 	 */
 	private IProject project = null;
-	
+
 	/**
 	 * This just records the information.
 	 */
 //	@Override
-	public void init(IWorkbench workbench, IStructuredSelection selection)
-	{
-		if (selection != null)
-		{
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		if (selection != null) {
 			Object selectedElement = selection.getFirstElement();
-			if (selectedElement instanceof IResource)
-			{
-				IProject project = ((IResource)selectedElement).getProject();
+			if (selectedElement instanceof IResource) {
+				IProject project = ((IResource) selectedElement).getProject();
 				if (!project.getName().equals(OsateResourceUtil.PLUGIN_RESOURCES_DIRECTORY_NAME))
 					this.project = project;
 				else
@@ -132,58 +130,52 @@ public class NewPackageWizard extends Wizard implements INewWizard
 		setWindowTitle("New Aadl Package");
 		setDefaultPageImageDescriptor(OsateUiPlugin.getImageDescriptor("icons/NewAadl2.gif"));
 	}
-	
+
 	/**
 	 * Do the work after everything is specified.
 	 * Generates the appropriate files with the appropriate code within.
 	 */
 	@Override
-	public boolean performFinish()
-	{
-			return addAadlSource(newObjectRelativePath());
+	public boolean performFinish() {
+		return addAadlSource(newObjectRelativePath());
 	}
-	
+
 	/**
 	 * Generates a path to the object to create.  The path is based on the settings of
 	 * newObjectCreationPage and dependent on the workspace.  An example: "/MyProject/aadl/NewSourceFile.aadl".
 	 */
-	private IPath newObjectRelativePath()
-	{
+	private IPath newObjectRelativePath() {
 		IProject selectedProject = newPackageCreationPage.getSelectedProject();
 		IPath filePath = new Path("/" + selectedProject.getName());
-		String fileExtension =  "." + WorkspacePlugin.SOURCE_FILE_EXT;
+		String fileExtension = "." + WorkspacePlugin.SOURCE_FILE_EXT;
 		filePath = new Path(filePath.addTrailingSeparator() + WorkspacePlugin.AADL_PACKAGES_DIR);
-		return new Path(filePath.addTrailingSeparator() + newPackageCreationPage.getNewObjectName().replaceAll(
-				WorkspacePlugin.AADL_PACKAGE_SEPARATOR, WorkspacePlugin.FILE_PACKAGE_SEPARATOR) + fileExtension);
+		return new Path(filePath.addTrailingSeparator()
+				+ newPackageCreationPage.getNewObjectName().replaceAll(WorkspacePlugin.AADL_PACKAGE_SEPARATOR,
+						WorkspacePlugin.FILE_PACKAGE_SEPARATOR) + fileExtension);
 	}
-	
+
 	/**
 	 * Generates a path to the object to create.  The path is based on the settings of
 	 * newObjectCreationPage and is a full system dependent path.  An example:
 	 * "C:\projects\aadlProjects\MyProject\aadl\NewSourceFile.aadl".
 	 */
-	private IPath newObjectAbsolutePath()
-	{
+	private IPath newObjectAbsolutePath() {
 		IProject selectedProject = newPackageCreationPage.getSelectedProject();
 		IPath filePath = selectedProject.getLocation();
 		String fileExtension = "." + WorkspacePlugin.SOURCE_FILE_EXT;
 		filePath = new Path(filePath.addTrailingSeparator() + WorkspacePlugin.AADL_PACKAGES_DIR);
-		return new Path(filePath.addTrailingSeparator() + newPackageCreationPage.getNewObjectName().replaceAll(
-				WorkspacePlugin.AADL_PACKAGE_SEPARATOR, WorkspacePlugin.FILE_PACKAGE_SEPARATOR) + fileExtension);
+		return new Path(filePath.addTrailingSeparator()
+				+ newPackageCreationPage.getNewObjectName().replaceAll(WorkspacePlugin.AADL_PACKAGE_SEPARATOR,
+						WorkspacePlugin.FILE_PACKAGE_SEPARATOR) + fileExtension);
 	}
-	
-	
-	private boolean addAadlSource(final IPath sourcePath)
-	{
+
+	private boolean addAadlSource(final IPath sourcePath) {
 		final IFile newFile = ResourcesPlugin.getWorkspace().getRoot().getFile(sourcePath);
 		final InputStream initialContents = getInitialSourceContents();
-		WorkspaceModifyOperation operation = new WorkspaceModifyOperation()
-		{
+		WorkspaceModifyOperation operation = new WorkspaceModifyOperation() {
 			@Override
-			protected void execute(IProgressMonitor monitor) throws CoreException
-			{
-				try
-				{
+			protected void execute(IProgressMonitor monitor) throws CoreException {
+				try {
 					monitor.beginTask("Creating", 2000);
 					ContainerGenerator generator = new ContainerGenerator(sourcePath.removeLastSegments(1));
 					generator.generateContainer(new SubProgressMonitor(monitor, 1000));
@@ -191,102 +183,94 @@ public class NewPackageWizard extends Wizard implements INewWizard
 					IResourceUtility.setGenerated(newFile, false);
 					if (monitor.isCanceled())
 						throw new OperationCanceledException();
-				}
-				finally
-				{
+				} finally {
 					monitor.done();
 				}
 			}
 		};
-		try
-		{
+		try {
 			getContainer().run(true, true, operation);
-			
+
 			// Build the project so that the index will be updated
-			if(project != null) {
+			if (project != null) {
 				try {
 					project.build(IncrementalProjectBuilder.INCREMENTAL_BUILD, new NullProgressMonitor());
-				} catch(CoreException ex) {
-					final Status status = new Status(IStatus.ERROR, Activator.getPluginId(), "An error building the AADL project after creating a new AADL file.", ex);
+				} catch (CoreException ex) {
+					final Status status = new Status(IStatus.ERROR, Activator.getPluginId(),
+							"An error building the AADL project after creating a new AADL file.", ex);
 					StatusManager.getManager().handle(status, StatusManager.SHOW | StatusManager.LOG);
-				}
-				catch(RuntimeException ex) {
-					final Status status = new Status(IStatus.ERROR, Activator.getPluginId(), "An error building the AADL project after creating a new AADL file.", ex);
+				} catch (RuntimeException ex) {
+					final Status status = new Status(IStatus.ERROR, Activator.getPluginId(),
+							"An error building the AADL project after creating a new AADL file.", ex);
 					StatusManager.getManager().handle(status, StatusManager.SHOW | StatusManager.LOG);
 				}
 			}
-			
+
 			// Get the element from the newly created AADL package
 			final URI uri = URI.createPlatformResourceURI(newFile.getFullPath().toString(), true);
 			final XtextResourceSet resourceSet = new XtextResourceSet();
 			final Resource pkgResource = resourceSet.getResource(uri, true);
-			if(pkgResource.getContents().size() > 0 && pkgResource.getContents().get(0) instanceof NamedElement) {
+			if (pkgResource.getContents().size() > 0 && pkgResource.getContents().get(0) instanceof NamedElement) {
 				// Open the diagram
-				final DiagramService diagramService = (DiagramService)PlatformUI.getWorkbench().getActiveWorkbenchWindow().getService(DiagramService.class);
-				diagramService.openOrCreateDiagramForRootBusinessObject((NamedElement)pkgResource.getContents().get(0));
+				final DiagramService diagramService = (DiagramService) PlatformUI.getWorkbench()
+						.getActiveWorkbenchWindow().getService(DiagramService.class);
+				diagramService
+						.openOrCreateDiagramForRootBusinessObject((NamedElement) pkgResource.getContents().get(0));
 			} else {
-				final Status status = new Status(IStatus.ERROR, Activator.getPluginId(), "Unable to retrieve package from resource.", null);
+				final Status status = new Status(IStatus.ERROR, Activator.getPluginId(),
+						"Unable to retrieve package from resource.", null);
 				StatusManager.getManager().handle(status, StatusManager.LOG);
 				ErrorDialog.openError(getContainer().getShell(), "Unable to Create Diagram", null, status);
 			}
 			return true;
-		}
-		catch (InterruptedException e)
-		{
+		} catch (InterruptedException e) {
 			return false;
-		}
-		catch (InvocationTargetException e)
-		{
-			if (e.getTargetException() instanceof CoreException)
-			{
+		} catch (InvocationTargetException e) {
+			if (e.getTargetException() instanceof CoreException) {
 				ErrorDialog.openError(getContainer().getShell(), "Creation Problems", null,
-						((CoreException)e.getTargetException()).getStatus());
-			}
-			else
-			{
+						((CoreException) e.getTargetException()).getStatus());
+			} else {
 				OsateUiPlugin.log(e.getTargetException());
-				MessageDialog.openError(getContainer().getShell(), "Creation Problems", e.getTargetException().getMessage());
+				MessageDialog.openError(getContainer().getShell(), "Creation Problems", e.getTargetException()
+						.getMessage());
 			}
 			return false;
 		}
 	}
-		
-	private InputStream getInitialSourceContents()
-	{
+
+	private InputStream getInitialSourceContents() {
 		String contents = null;
-		contents = "package " + newPackageCreationPage.getNewObjectName() + "\n" +
-				"public\n" +
-				"end " + newPackageCreationPage.getNewObjectName() + ";";
+		contents = "package " + newPackageCreationPage.getNewObjectName() + "\n" + "public\n" + "end "
+				+ newPackageCreationPage.getNewObjectName() + ";";
 		return new ByteArrayInputStream(contents.getBytes());
 	}
-	
+
 	/**
 	 * Allows the user to select options for the new aadl file.  The user can specify which
 	 * project the new file should be in, the name of the new file, whether the new file is
 	 * text or object, and whether the new file is a package or property set.
 	 */
-	private class NewPackageWizardNewPackageCreationPage extends WizardPage
-	{		
-		//All buttons are radio buttons.
+	private class NewPackageWizardNewPackageCreationPage extends WizardPage {
+		// All buttons are radio buttons.
 		private Text nameTextField = null;
 		private TableViewer projectViewer = null;
-		
-		public NewPackageWizardNewPackageCreationPage(String pageName)
-		{
+
+		public NewPackageWizardNewPackageCreationPage(String pageName) {
 			super(pageName, "New Aadl Package", null);
 			setDescription("Create a new Aadl package.");
 		}
-		
-		public void createControl(Composite parent)
-		{
+
+		@Override
+		public void createControl(Composite parent) {
 			createAndLayoutWidgets(parent);
 			ButtonAndTextListener listener = new ButtonAndTextListener(this);
 			nameTextField.addModifyListener(listener);
 		}
-		
-		private void createAndLayoutWidgets(Composite parent)
-		{
-			Composite composite = new Composite(parent, SWT.NONE);
+
+		private void createAndLayoutWidgets(Composite parent) {
+			PageBook pageBook = new PageBook(parent, SWT.NONE);
+
+			Composite composite = new Composite(pageBook, SWT.NONE);
 			composite.setSize(parent.getSize());
 			composite.setLayout(new GridLayout());
 
@@ -296,178 +280,161 @@ public class NewPackageWizard extends Wizard implements INewWizard
 			layoutData = new GridData(SWT.LEFT, SWT.CENTER, false, false);
 			layoutData.verticalIndent = 15;
 			nameFieldLabel.setLayoutData(layoutData);
-			
+
 			nameTextField = new Text(composite, SWT.BORDER);
 			nameTextField.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
 			nameTextField.setFocus();
-			
+
 			Label projectViewerLabel = new Label(composite, SWT.NONE);
 			projectViewerLabel.setText("Create in project:");
 			layoutData = new GridData(SWT.LEFT, SWT.CENTER, false, false);
-			
+
 			projectViewer = new TableViewer(composite, SWT.BORDER);
 			projectViewer.setLabelProvider(WorkbenchLabelProvider.getDecoratingWorkbenchLabelProvider());
-			projectViewer.setContentProvider(
-					new WorkbenchContentProvider()
-					{
-						@Override
-						public Object[] getChildren(Object element)
-						{
-							if (!(element instanceof IWorkspace))
-								return new Object[0];
-							IProject[] projects = ((IWorkspace)element).getRoot().getProjects();
-							ArrayList<IProject> openProjects = new ArrayList<IProject>();
-							for (IProject project : projects)
-								if (project.isOpen() && !project.getName().equals(OsateResourceUtil.PLUGIN_RESOURCES_DIRECTORY_NAME))
-									openProjects.add(project);
-							return openProjects.size() == 0 ? new Object[0] : openProjects.toArray();
-						}
-					});
+			projectViewer.setContentProvider(new WorkbenchContentProvider() {
+				@Override
+				public Object[] getChildren(Object element) {
+					if (!(element instanceof IWorkspace))
+						return new Object[0];
+					IProject[] projects = ((IWorkspace) element).getRoot().getProjects();
+					ArrayList<IProject> openProjects = new ArrayList<IProject>();
+					for (IProject project : projects)
+						if (project.isOpen()
+								&& !project.getName().equals(OsateResourceUtil.PLUGIN_RESOURCES_DIRECTORY_NAME))
+							openProjects.add(project);
+					return openProjects.size() == 0 ? new Object[0] : openProjects.toArray();
+				}
+			});
 			projectViewer.setInput(ResourcesPlugin.getWorkspace());
+			int projectItemCount = projectViewer.getTable().getItemCount();
 			boolean selectedProjectInList = false;
-			for (int i = 0; i < projectViewer.getTable().getItemCount(); i++)
-			{
-				if (projectViewer.getElementAt(i).equals(project))
-				{
+			for (int i = 0; i < projectItemCount; i++) {
+				if (projectViewer.getElementAt(i).equals(project)) {
 					selectedProjectInList = true;
 					break;
 				}
 			}
 			if ((project != null) && selectedProjectInList)
 				projectViewer.setSelection(new StructuredSelection(project), true);
-			else
+			else if (projectItemCount > 0) {
 				projectViewer.getTable().setSelection(0);
+			}
 			projectViewer.getTable().setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
-			
-			setControl(composite);
+
+			Composite noProjectComposite = new Composite(pageBook, SWT.NONE);
+			noProjectComposite.setSize(parent.getSize());
+			noProjectComposite.setLayout(new GridLayout());
+			Label noProjectLabel = new Label(noProjectComposite, SWT.NONE);
+			noProjectLabel
+					.setText("The workspace contains no projects. Before an Aadl Package can be created you must first create a project.");
+			if (projectItemCount < 1) {
+				pageBook.showPage(noProjectComposite);
+			} else {
+				pageBook.showPage(composite);
+			}
+			setControl(pageBook);
 		}
-		
+
 		@Override
-		public boolean isPageComplete()
-		{
-			if (validateEnteredName() && !fileExists())
-			{
+		public boolean isPageComplete() {
+			if (validateEnteredName() && !fileExists()) {
 				setErrorMessage(null);
 				return true;
-			}
-			else
+			} else
 				return false;
 		}
-		
-		public IProject getSelectedProject()
-		{
-			return (IProject)((IStructuredSelection)projectViewer.getSelection()).getFirstElement();
+
+		public IProject getSelectedProject() {
+			return (IProject) ((IStructuredSelection) projectViewer.getSelection()).getFirstElement();
 		}
-		
-		public String getNewObjectName()
-		{
+
+		public String getNewObjectName() {
 			return nameTextField.getText();
 		}
-		
+
 		/**
 		 * Generates an error message if the name is invalid.
 		 */
-		private boolean validateEnteredName()
-		{
-			if (nameTextField.getText().length() == 0)
-			{
-				//No error message is specified.  We assume that the user knows that
-				//a new Aadl file must have a name.
+		private boolean validateEnteredName() {
+			if (nameTextField.getText().length() == 0) {
+				// No error message is specified. We assume that the user knows that
+				// a new Aadl file must have a name.
 				setErrorMessage(null);
 				return false;
-			}
-			else
-			{
-				if (nameTextField.getText().startsWith(":"))
-				{
+			} else {
+				if (nameTextField.getText().startsWith(":")) {
 					setErrorMessage("Package path must start with an identifier.");
 					return false;
-				}
-				else if (nameTextField.getText().endsWith(":"))
-				{
+				} else if (nameTextField.getText().endsWith(":")) {
 					setErrorMessage("Package path must end with an identifier.");
 					return false;
 				}
 				int startingIndex = 0;
-				for (int i = 1; i < nameTextField.getText().length() - 1; i++)
-				{
+				for (int i = 1; i < nameTextField.getText().length() - 1; i++) {
 					if ((nameTextField.getText().charAt(i) == ':') && (nameTextField.getText().charAt(i - 1) != ':'))
 						startingIndex = i;
-					if ((nameTextField.getText().charAt(i) == ':') && (nameTextField.getText().charAt(i + 1) != ':') &&
-							(i - startingIndex != 1))
-					{
+					if ((nameTextField.getText().charAt(i) == ':') && (nameTextField.getText().charAt(i + 1) != ':')
+							&& (i - startingIndex != 1)) {
 						setErrorMessage("Use two colons(::) to separate identifiers in the package path.");
 						return false;
 					}
 				}
 				StringTokenizer tokenizer = new StringTokenizer(nameTextField.getText(), "::");
-				while (tokenizer.hasMoreTokens())
-				{
+				while (tokenizer.hasMoreTokens()) {
 					if (!validateIdentifier(tokenizer.nextToken()))
 						return false;
 				}
 				return true;
 			}
 		}
-		
-		private boolean validateIdentifier(String identifier)
-		{
-			if (!Character.isLetter(identifier.charAt(0)))
-			{
+
+		private boolean validateIdentifier(String identifier) {
+			if (!Character.isLetter(identifier.charAt(0))) {
 				setErrorMessage("Identifier must start with a letter.");
 				return false;
 			}
-			for (int i = 1; i < identifier.length(); i++)
-			{
-				if (!(Character.isLetterOrDigit(identifier.charAt(i)) || identifier.charAt(i) == '_'))
-				{
+			for (int i = 1; i < identifier.length(); i++) {
+				if (!(Character.isLetterOrDigit(identifier.charAt(i)) || identifier.charAt(i) == '_')) {
 					setErrorMessage("Identifier can only contain letters, digits, and the underscore character.");
 					return false;
 				}
 			}
 			return true;
 		}
-		
-		private boolean fileExists()
-		{
-			if (newObjectAbsolutePath().toFile().exists())
-			{
+
+		private boolean fileExists() {
+			if (newObjectAbsolutePath().toFile().exists()) {
 				setErrorMessage(nameTextField.getText() + " already exists.");
 				return true;
-			}
-			else
+			} else
 				return false;
 		}
 	}
-	
-	private static class ButtonAndTextListener extends SelectionAdapter implements ModifyListener
-	{
+
+	private static class ButtonAndTextListener extends SelectionAdapter implements ModifyListener {
 		private final WizardPage wizardPage;
-		
-		public ButtonAndTextListener(final WizardPage wp)
-		{
+
+		public ButtonAndTextListener(final WizardPage wp) {
 			wizardPage = wp;
 		}
-		
+
 		@Override
-		public void widgetSelected(SelectionEvent e)
-		{
+		public void widgetSelected(SelectionEvent e) {
 			wizardPage.setPageComplete(wizardPage.isPageComplete());
 		}
-		
+
 //		@Override
-		public void modifyText(ModifyEvent e)
-		{
+		@Override
+		public void modifyText(ModifyEvent e) {
 			wizardPage.setPageComplete(wizardPage.isPageComplete());
 		}
 	}
-	
+
 	/**
 	 * The framework calls this to create the contents of the wizard.
 	 */
 	@Override
-	public void addPages()
-	{
+	public void addPages() {
 		newPackageCreationPage = new NewPackageWizardNewPackageCreationPage("New Package");
 		addPage(newPackageCreationPage);
 	}
