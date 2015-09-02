ec883e578c13f8a63b039127bc72385217600c66
ryan.mcilnay <rm0016@uah.edu>
2015-09-02 15:01:03 -0500

    End-To-End flow creation bug fix

--- ge/org.osate.ge/src/org/osate/ge/ui/tools/CreateEndToEndFlowTool.java
+++ ge/org.osate.ge/src/org/osate/ge/ui/tools/CreateEndToEndFlowTool.java
@@ -515,8 +515,8 @@ public class CreateEndToEndFlowTool {
 								|| flowFeature == destCE
 								|| flowFeature == srcCE;
 					} else {
-						return flowFeature == destCE
-								|| inFlowFeature == srcCE;
+						return flowFeature == srcCE
+								|| inFlowFeature == destCE;
 					}
 				} else if (fs.getKind() == FlowKind.SINK) {
 					return flowFeature == srcCE || flowFeature == destCE;
