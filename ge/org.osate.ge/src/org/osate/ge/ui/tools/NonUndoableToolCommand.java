755d9fce5756593d4b7425996232685ff1a125ae
Philip Alldredge <pwa0001@uah.edu>
2015-09-21 09:25:49 -0500

    Reduced visibility of NonUndoableToolCommand to package-private.

--- ge/org.osate.ge/src/org/osate/ge/ui/tools/NonUndoableToolCommand.java
+++ ge/org.osate.ge/src/org/osate/ge/ui/tools/NonUndoableToolCommand.java
@@ -3,7 +3,7 @@ package org.osate.ge.ui.tools;
 import org.eclipse.emf.common.command.AbstractCommand;
 
 // An abstract command class which is always prepared for execution and cannot be undone.
-public abstract class NonUndoableToolCommand extends AbstractCommand {
+abstract class NonUndoableToolCommand extends AbstractCommand {
 	public NonUndoableToolCommand() {
 		super("Tool");
 	}
