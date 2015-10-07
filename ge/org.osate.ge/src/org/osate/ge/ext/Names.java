f91f33801c5cdafd8bb6c5c87769d4bafc3e41a8
Philip Alldredge <pwa0001@uah.edu>
2015-10-07 10:35:45 -0500

    Updated comments.

--- ge/org.osate.ge/src/org/osate/ge/ext/Names.java
+++ ge/org.osate.ge/src/org/osate/ge/ext/Names.java
@@ -1,5 +1,9 @@
 package org.osate.ge.ext;
 
+/**
+ * Constants for named parameters. For use with the @Named annotation.
+ *
+ */
 public interface Names {
 	public static final String SELECTED_PICTOGRAM_ELEMENTS = "selected_pictogram_elements";
 	public static final String STYLE_ID = "style_id";
