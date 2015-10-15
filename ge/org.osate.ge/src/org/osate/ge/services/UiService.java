dd01ba0bc1bd76a85e5ffe7e80903463ea6a3e23
Philip Alldredge <pwa0001@uah.edu>
2015-10-15 09:02:47 -0500

    Remove comment.

--- ge/org.osate.ge/src/org/osate/ge/services/UiService.java
+++ ge/org.osate.ge/src/org/osate/ge/services/UiService.java
@@ -3,7 +3,6 @@ package org.osate.ge.services;
 /**
  * Contains methods for interacting with the graphical editor's user interface components.
  * All methods must be called from the display thread
- * @author philip.alldredge
  *
  */
 public interface UiService {
