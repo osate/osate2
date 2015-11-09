5e9d7e7c692b89cd5118ad57dd27651af4d5394a
Peter Feiler <phf@sei.cmu.edu>
2015-11-08 21:20:44 -0500

    new object for PhaseCategory

---
+++ alisa/org.osate.categories/src-gen/org/osate/categories/categories/PhaseCategory.java
@@ -0,0 +1,18 @@
+/**
+ */
+package org.osate.categories.categories;
+
+
+/**
+ * <!-- begin-user-doc -->
+ * A representation of the model object '<em><b>Phase Category</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ *
+ * @see org.osate.categories.categories.CategoriesPackage#getPhaseCategory()
+ * @model
+ * @generated
+ */
+public interface PhaseCategory extends Category
+{
+} // PhaseCategory
---
+++ alisa/org.osate.categories/src-gen/org/osate/categories/categories/impl/PhaseCategoryImpl.java
@@ -0,0 +1,40 @@
+/**
+ */
+package org.osate.categories.categories.impl;
+
+import org.eclipse.emf.ecore.EClass;
+
+import org.osate.categories.categories.CategoriesPackage;
+import org.osate.categories.categories.PhaseCategory;
+
+/**
+ * <!-- begin-user-doc -->
+ * An implementation of the model object '<em><b>Phase Category</b></em>'.
+ * <!-- end-user-doc -->
+ *
+ * @generated
+ */
+public class PhaseCategoryImpl extends CategoryImpl implements PhaseCategory
+{
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  protected PhaseCategoryImpl()
+  {
+    super();
+  }
+
+  /**
+   * <!-- begin-user-doc -->
+   * <!-- end-user-doc -->
+   * @generated
+   */
+  @Override
+  protected EClass eStaticClass()
+  {
+    return CategoriesPackage.Literals.PHASE_CATEGORY;
+  }
+
+} //PhaseCategoryImpl
