3e7b552d1ab0081d6ea18937c361366e4b5238d2
Peter Feiler <phf@sei.cmu.edu>
2015-11-11 16:32:27 -0500

    more cleanup to get xbase indpendence

--- alisa/org.osate.alisa.common/src/org/osate/alisa/common/scoping/AlisaAbstractDeclarativeScopeProvider.java
+++ alisa/org.osate.alisa.common/src/org/osate/alisa/common/scoping/AlisaAbstractDeclarativeScopeProvider.java
@@ -7,19 +7,19 @@ import com.google.inject.name.Named;
 
 public abstract class AlisaAbstractDeclarativeScopeProvider extends CommonScopeProvider { // AbstractDeclarativeScopeProvider {
 
-	public final static String NAMED_DELEGATE = "org.osate.alisa.common.scoping.AlisaAbstractDeclarativeScopeProvider.delegate";
-
-	@Inject
-	@Named(NAMED_DELEGATE)
-	private IScopeProvider myDelegate;
-
-	@Override
-	public void setDelegate(IScopeProvider delegate) {
-		this.myDelegate = delegate;
-	}
-
-	@Override
-	public IScopeProvider getDelegate() {
-		return myDelegate;
-	}
+//	public final static String NAMED_DELEGATE = "org.osate.alisa.common.scoping.AlisaAbstractDeclarativeScopeProvider.delegate";
+//
+//	@Inject
+//	@Named(NAMED_DELEGATE)
+//	private IScopeProvider myDelegate;
+//
+//	@Override
+//	public void setDelegate(IScopeProvider delegate) {
+//		this.myDelegate = delegate;
+//	}
+//
+//	@Override
+//	public IScopeProvider getDelegate() {
+//		return myDelegate;
+//	}
 }
