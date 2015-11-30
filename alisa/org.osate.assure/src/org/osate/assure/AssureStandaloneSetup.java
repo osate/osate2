e556f556d20a702772f45c61866a53d20c1554f0
Lutz Wrage <lutz.wrage@gmail.com>
2015-11-30 08:54:26 -0500

    Formatting

--- alisa/org.osate.assure/src/org/osate/assure/AssureStandaloneSetup.java
+++ alisa/org.osate.assure/src/org/osate/assure/AssureStandaloneSetup.java
@@ -23,10 +23,9 @@ package org.osate.assure;
  * Initialization support for running Xtext languages 
  * without equinox extension registry
  */
-public class AssureStandaloneSetup extends AssureStandaloneSetupGenerated{
+public class AssureStandaloneSetup extends AssureStandaloneSetupGenerated {
 
 	public static void doSetup() {
 		new AssureStandaloneSetup().createInjectorAndDoEMFRegistration();
 	}
 }
-
