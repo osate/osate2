ad72c25d4ca0620b8b77754eaf484efdbfdef0c5
Philip Alldredge <pwa0001@uah.edu>
2015-12-08 17:01:19 -0600

    Merged changes from develop.
    Merged changes from develop. Updated ErrorModelReferenceHandler to be
    compatible with changes to reference handling extension point.

--- ge/org.osate.ge.errormodel/src/org/osate/ge/errormodel/ErrorModelReferenceHandler.java
+++ ge/org.osate.ge.errormodel/src/org/osate/ge/errormodel/ErrorModelReferenceHandler.java
@@ -3,19 +3,17 @@ package org.osate.ge.errormodel;
 import javax.inject.Named;
 
 import org.osate.ge.ext.Names;
-import org.osate.ge.ext.annotations.GetReference;
-import org.osate.ge.ext.annotations.GetReferencedObject;
+import org.osate.ge.ext.annotations.BuildReference;
+import org.osate.ge.ext.annotations.ResolveReference;
 
 public class ErrorModelReferenceHandler {
-	@GetReference
-	public String getReference(final @Named(Names.BUSINESS_OBJECT) Object bo) {
-		System.err.println("GET REFERENCE");
+	@BuildReference
+	public String[] getReference(final @Named(Names.BUSINESS_OBJECT) Object bo) {
 		return null;
 	}
 	
-	@GetReferencedObject
-	public Object getReferencedObject(final @Named(Names.REFERENCE) String reference, final @Named(Names.REFERENCE_SEGMENTS) String[] refSegs) {
-		System.err.println("GET REFERENCED OBJECT");
+	@ResolveReference
+	public Object getReferencedObject(final @Named(Names.REFERENCE) String[] ref) {
 		return null;
 	}
 }
