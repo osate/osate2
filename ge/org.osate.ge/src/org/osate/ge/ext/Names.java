2cd5ecc585098a9b52f74668bf65e8c80aa735de
Philip Alldredge <pwa0001@uah.edu>
2015-12-08 16:58:33 -0600

    Refactoring and cleanup of reference handling.

--- ge/org.osate.ge/src/org/osate/ge/ext/Names.java
+++ ge/org.osate.ge/src/org/osate/ge/ext/Names.java
@@ -9,5 +9,4 @@ public interface Names {
 	public static final String BUSINESS_OBJECT = "bo";
 	public static final String STYLE_ID = "style_id";
 	public static final String REFERENCE = "ref";
-	public static final String REFERENCE_SEGMENTS = "ref_segs";
 }
--- ge/org.osate.ge/src/org/osate/ge/ext/annotations/BuildReference.java
+++ ge/org.osate.ge/src/org/osate/ge/ext/annotations/BuildReference.java
@@ -7,11 +7,11 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Use this annotation to tag methods called to get the object referenced by a reference.
+ * Use this annotation to tag methods called to get a reference for a business object
  * This annotation must not be applied to more than one method per class.
  */
 @Documented
 @Target({ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
-public @interface GetReferencedObject {
+public @interface BuildReference {
 }
