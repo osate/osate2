9d89fa5ccb16542aece9e9f16680d3c2ad120327
Peter Feiler <phf@sei.cmu.edu>
2016-02-09 11:00:33 -0500

    import conflict due to rebase resolved.

--- emv2/org.osate.xtext.aadl2.errormodel/src/org/osate/xtext/aadl2/errormodel/util/EMV2Util.java
+++ emv2/org.osate.xtext.aadl2.errormodel/src/org/osate/xtext/aadl2/errormodel/util/EMV2Util.java
@@ -4,6 +4,8 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+
+import java.util.Iterator;
 import java.util.Set;
 import java.util.stream.Collectors;
 
