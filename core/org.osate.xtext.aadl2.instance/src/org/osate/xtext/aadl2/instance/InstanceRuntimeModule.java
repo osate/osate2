b2758d2e99b5d6c2d82fdea9c37f4ccf305bcb50
Joe Seibel <jseibel@sei.cmu.edu>
2016-02-09 16:31:37 -0500

    Removed ValueConverter because it isn't needed.

--- core/org.osate.xtext.aadl2.instance/src/org/osate/xtext/aadl2/instance/InstanceRuntimeModule.java
+++ core/org.osate.xtext.aadl2.instance/src/org/osate/xtext/aadl2/instance/InstanceRuntimeModule.java
@@ -3,9 +3,6 @@
  */
 package org.osate.xtext.aadl2.instance;
 
-import org.eclipse.xtext.conversion.IValueConverterService;
-import org.osate.xtext.aadl2.instance.valueconversion.InstanceValueConverter;
-
 /**
  * Use this class to register components to be used at runtime / without the Equinox extension registry.
  */
@@ -18,10 +15,4 @@ public class InstanceRuntimeModule extends org.osate.xtext.aadl2.instance.Abstra
 	public Class<? extends org.eclipse.xtext.naming.IQualifiedNameConverter> bindIQualifiedNameConverter() {
 		return org.osate.xtext.aadl2.naming.Aadl2QualifiedNameConverter.class;
 	}
-
-	@Override
-	public Class<? extends IValueConverterService> bindIValueConverterService() {
-		return InstanceValueConverter.class;
-	}
-
 }
