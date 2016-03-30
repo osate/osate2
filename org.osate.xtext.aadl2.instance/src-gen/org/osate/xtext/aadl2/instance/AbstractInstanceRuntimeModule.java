2da2c17b6f4c9193861fd4de3fcebd43991e9974
Joe Seibel <jseibel@sei.cmu.edu>
2016-03-30 12:11:59 -0400

    Removed scope provider. It is not needed since we have a custom cross reference serializer and linking service.

--- org.osate.xtext.aadl2.instance/src-gen/org/osate/xtext/aadl2/instance/AbstractInstanceRuntimeModule.java
+++ org.osate.xtext.aadl2.instance/src-gen/org/osate/xtext/aadl2/instance/AbstractInstanceRuntimeModule.java
@@ -98,26 +98,6 @@ public abstract class AbstractInstanceRuntimeModule extends org.eclipse.xtext.se
 		return org.osate.xtext.aadl2.instance.validation.InstanceValidator.class;
 	}
 
-	// contributed by org.eclipse.xtext.generator.scoping.AbstractScopingFragment
-	public Class<? extends org.eclipse.xtext.scoping.IScopeProvider> bindIScopeProvider() {
-		return org.osate.xtext.aadl2.instance.scoping.InstanceScopeProvider.class;
-	}
-
-	// contributed by org.eclipse.xtext.generator.scoping.AbstractScopingFragment
-	public void configureIScopeProviderDelegate(com.google.inject.Binder binder) {
-		binder.bind(org.eclipse.xtext.scoping.IScopeProvider.class).annotatedWith(com.google.inject.name.Names.named(org.eclipse.xtext.scoping.impl.AbstractDeclarativeScopeProvider.NAMED_DELEGATE)).to(org.eclipse.xtext.scoping.impl.ImportedNamespaceAwareLocalScopeProvider.class);
-	}
-
-	// contributed by org.eclipse.xtext.generator.scoping.AbstractScopingFragment
-	public Class<? extends org.eclipse.xtext.scoping.IGlobalScopeProvider> bindIGlobalScopeProvider() {
-		return org.eclipse.xtext.scoping.impl.DefaultGlobalScopeProvider.class;
-	}
-
-	// contributed by org.eclipse.xtext.generator.scoping.AbstractScopingFragment
-	public void configureIgnoreCaseLinking(com.google.inject.Binder binder) {
-		binder.bindConstant().annotatedWith(org.eclipse.xtext.scoping.IgnoreCaseLinking.class).to(false);
-	}
-
 	// contributed by org.eclipse.xtext.generator.exporting.QualifiedNamesFragment
 	public Class<? extends org.eclipse.xtext.naming.IQualifiedNameProvider> bindIQualifiedNameProvider() {
 		return org.eclipse.xtext.naming.DefaultDeclarativeQualifiedNameProvider.class;
