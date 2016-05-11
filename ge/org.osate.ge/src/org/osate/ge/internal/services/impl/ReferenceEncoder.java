7bb0598be7c846eb082f5424811466146166e3b0
Philip Alldredge <pwa0001@uah.edu>
2016-05-11 09:59:17 -0500

    Fixed reference encoding not being backwards compatible.

--- ge/org.osate.ge/src/org/osate/ge/internal/services/impl/ReferenceEncoder.java
+++ ge/org.osate.ge/src/org/osate/ge/internal/services/impl/ReferenceEncoder.java
@@ -2,22 +2,24 @@ package org.osate.ge.internal.services.impl;
 
 // Helper class for encoding and decoding references. Handles escaping characters. Encoded strings are guaranteed not to include spaces
 public class ReferenceEncoder {
-	private static final char ESCAPE_CHAR = '$';
+	private static final char OUTER_ESCAPE_CHAR = '$'; // Used to start an escape sequence or to indicate a space when used afte ran inner escape character
+	private static final char INNER_ESCAPE_CHAR = '%';
+	private static final String ESCAPED_INNER_ESCAPE_CHAR = Character.toString(INNER_ESCAPE_CHAR) + INNER_ESCAPE_CHAR;	
 	
 	public static String encodeSegment(final String seg) {
 		final StringBuilder sb = new StringBuilder(); // TODO: PASS IT IN?
 		for(int i = 0; i < seg.length(); i++) {
 			final char ch = seg.charAt(i);
-			if(ch == ESCAPE_CHAR) {
-				sb.append(ESCAPE_CHAR);
+			if(ch == OUTER_ESCAPE_CHAR) {
+				sb.append(OUTER_ESCAPE_CHAR);
 				sb.append(ch);
-			} else if(ch == '_') {
-				sb.append(ESCAPE_CHAR);
-				sb.append("__");
+			} else if(ch == INNER_ESCAPE_CHAR) {
+				sb.append(OUTER_ESCAPE_CHAR);
+				sb.append(ESCAPED_INNER_ESCAPE_CHAR);
 			} else if(ch == ' ') {
-				sb.append(ESCAPE_CHAR);
-				sb.append('_');
-				sb.append(ESCAPE_CHAR);
+				sb.append(OUTER_ESCAPE_CHAR);
+				sb.append(INNER_ESCAPE_CHAR);
+				sb.append(OUTER_ESCAPE_CHAR);
 			} else {
 				sb.append(ch);
 			}
@@ -31,17 +33,17 @@ public class ReferenceEncoder {
 		final int len = seg.length();
 		for(int i = 0; i < len; i++) {
 			char ch = seg.charAt(i);
-			if(ch == ESCAPE_CHAR) {
+			if(ch == OUTER_ESCAPE_CHAR) {
 				i++;				
 				if(i < len) {
 					ch = seg.charAt(i);
-					if(ch == ESCAPE_CHAR) {
+					if(ch == OUTER_ESCAPE_CHAR) {
 						sb.append(ch);
-					} else if(ch == '_') {
+					} else if(ch == INNER_ESCAPE_CHAR) {
 						i++;
 						if(i < len) {
 							ch = seg.charAt(i);
-							if(ch == ESCAPE_CHAR) {
+							if(ch == OUTER_ESCAPE_CHAR) {
 								sb.append(' ');
 							} else {
 								sb.append(ch);
