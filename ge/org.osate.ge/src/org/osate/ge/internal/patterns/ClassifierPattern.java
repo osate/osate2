f39ddb57044e8bc4252857161f0ca44f1a4a433f
Philip Alldredge <pwa0001@uah.edu>
2016-10-04 14:38:12 -0500

    Make classifier label non-active to improve selection.

--- ge/org.osate.ge/src/org/osate/ge/internal/patterns/ClassifierPattern.java
+++ ge/org.osate.ge/src/org/osate/ge/internal/patterns/ClassifierPattern.java
@@ -414,11 +414,13 @@ public class ClassifierPattern extends AgePattern implements Categorized {
 			
 			// Create label
 	        final String name = getLabelText(sc);
-	        labelService.createLabelShape(shape, labelShapeName, sc, name);
-
+	        final Shape labelShape = labelService.createLabelShape(shape, labelShapeName, sc, name);
+	        labelShape.setActive(false);
+	        
 	        // Create Subcomponent Type Indicator
 	        final String subcomponentTypeName = getTypeText(sc);
 	        final Shape subcomponentTypeShape = labelService.createLabelShape(shape, subcomponentTypeLabelShapeName, sc, subcomponentTypeName);
+	        subcomponentTypeShape.setActive(false);
 	        if(subcomponentTypeName.length() == 0) {
 	        	subcomponentTypeShape.getGraphicsAlgorithm().setWidth(0);
 	        	subcomponentTypeShape.getGraphicsAlgorithm().setHeight(0);
