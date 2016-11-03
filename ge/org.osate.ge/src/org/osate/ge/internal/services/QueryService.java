47ca3edfc94ed88c71a009288ff75afde2417051
Philip Alldredge <pwa0001@uah.edu>
2016-11-03 12:13:01 -0500

    Minor adjustments to internal query interface.

--- ge/org.osate.ge/src/org/osate/ge/internal/services/QueryService.java
+++ ge/org.osate.ge/src/org/osate/ge/internal/services/QueryService.java
@@ -5,6 +5,5 @@ import org.osate.ge.internal.query.StandaloneDiagramElementQuery;
 
 // TODO: Make API once stabilized.
 public interface QueryService {
-	// TODO: root should be some sort of reference/object that is part of the API?
 	Object getFirstBusinessObject(StandaloneDiagramElementQuery query, DiagramElementProxy rootElement);
 }
