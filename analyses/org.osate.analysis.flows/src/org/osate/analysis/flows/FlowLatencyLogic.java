c2bae41f904f0690d9050ec74803ad3c581aa271
Peter Feiler <phf@sei.cmu.edu>
2016-11-18 22:02:46 -0500

    Fixes issue #60 When traversing ETEF we did not call on the processing method for componentInstances if the reference was not to a flowinstance. The processing method was already able to handle both a flow instanvce and a component instance.

--- analyses/org.osate.analysis.flows/src/org/osate/analysis/flows/FlowLatencyLogic.java
+++ analyses/org.osate.analysis.flows/src/org/osate/analysis/flows/FlowLatencyLogic.java
@@ -1,5 +1,6 @@
 package org.osate.analysis.flows;
 
+import org.osate.aadl2.instance.ComponentInstance;
 import org.osate.aadl2.instance.ConnectionInstance;
 import org.osate.aadl2.instance.EndToEndFlowInstance;
 import org.osate.aadl2.instance.FlowElementInstance;
@@ -11,7 +12,8 @@ public class FlowLatencyLogic {
 	public static void mapFlowElementInstance(final EndToEndFlowInstance etef,
 			final FlowElementInstance flowElementInstance, LatencyReportEntry entry) {
 
-		if (flowElementInstance instanceof FlowSpecificationInstance) {
+		if (flowElementInstance instanceof FlowSpecificationInstance
+				|| flowElementInstance instanceof ComponentInstance) {
 
 			FlowLatencyLogicComponent.mapComponentInstance(etef, flowElementInstance, entry);
 		}
