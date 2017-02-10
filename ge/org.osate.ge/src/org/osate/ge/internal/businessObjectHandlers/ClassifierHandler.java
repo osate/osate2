6ab7ca9774c37cb689c4b4469b9cb334e8f3a597
Philip Alldredge <pwa0001@uah.edu>
2017-02-10 16:20:08 -0600

    Fixed typo.

--- ge/org.osate.ge/src/org/osate/ge/internal/businessObjectHandlers/ClassifierHandler.java
+++ ge/org.osate.ge/src/org/osate/ge/internal/businessObjectHandlers/ClassifierHandler.java
@@ -413,7 +413,7 @@ public class ClassifierHandler {
     		// Check that the name specified both a type and implementation name
     		final String[] segs = value.split("\\.");
     		if(segs.length != 2) {
-    			return "The name is not a valid name for a component implementaiton";
+    			return "The name is not a valid name for a component implementation";
     		}
     		
     		// Ensure both segments are valid identifiers
