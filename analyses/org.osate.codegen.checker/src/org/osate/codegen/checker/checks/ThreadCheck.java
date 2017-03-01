6f2189d71f66a928e65b576c353f7e840cd239f8
Joe Seibel <jseibel@sei.cmu.edu>
2017-03-01 14:48:26 -0500

    Fixed dispatch protocol check.

--- analyses/org.osate.codegen.checker/src/org/osate/codegen/checker/checks/ThreadCheck.java
+++ analyses/org.osate.codegen.checker/src/org/osate/codegen/checker/checks/ThreadCheck.java
@@ -38,12 +38,14 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.osate.aadl2.ComponentCategory;
+import org.osate.aadl2.EnumerationLiteral;
 import org.osate.aadl2.NamedElement;
 import org.osate.aadl2.SubprogramCall;
 import org.osate.aadl2.ThreadImplementation;
 import org.osate.aadl2.instance.ComponentInstance;
 import org.osate.aadl2.instance.SystemInstance;
 import org.osate.codegen.checker.report.ErrorReport;
+import org.osate.xtext.aadl2.properties.util.AadlProject;
 import org.osate.xtext.aadl2.properties.util.GetProperties;
 
 public class ThreadCheck extends AbstractCheck {
@@ -54,17 +56,15 @@ public class ThreadCheck extends AbstractCheck {
 				.filter(comp -> (comp.getCategory() == ComponentCategory.THREAD)).collect(Collectors.toList());
 
 		/**
-		 * Each thread needs to specify its dispatch protocol
+		 * Each thread needs to specify the dispatch protocol "periodic" or
+		 * "sporadic"
 		 */
-		final List<ComponentInstance> threadMissingDispatch = allThreads.stream()
-				.filter(comp -> (GetProperties.getDispatchProtocol(comp) != null)
-						&& ((GetProperties.getDispatchProtocol(comp).toString().equalsIgnoreCase("periodic"))
-								|| (GetProperties.getDispatchProtocol(comp).toString().equalsIgnoreCase("sporadic"))))
-				.collect(Collectors.toList());
-
-		for (ComponentInstance thr : threadMissingDispatch) {
-			addError(new ErrorReport(thr, "Need to define the Dispatch_Protocol property"));
-		}
+		allThreads.stream().filter(comp -> {
+			EnumerationLiteral protocol = GetProperties.getDispatchProtocol(comp);
+			return protocol == null || !(protocol.toString().equalsIgnoreCase(AadlProject.PERIODIC_LITERAL)
+					|| protocol.toString().equalsIgnoreCase(AadlProject.SPORADIC_LITERAL));
+		}).forEach(
+				thr -> addError(new ErrorReport(thr, "Thread needs a dispatch protocol of 'periodic' or 'sporadic'")));
 
 		/**
 		 * Each thread needs to specify period
