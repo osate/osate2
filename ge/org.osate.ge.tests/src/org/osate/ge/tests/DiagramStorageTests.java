0d5f6cc1bff8b5d88866b74d08b2817355589357
Philip Alldredge <philip@philipalldredge.com>
2017-04-10 09:03:22 -0500

    [WIP] Added proof of concept for JSON trasnformation.

--- ge/org.osate.ge.tests/src/org/osate/ge/tests/DiagramStorageTests.java
+++ ge/org.osate.ge.tests/src/org/osate/ge/tests/DiagramStorageTests.java
@@ -10,6 +10,8 @@ import javax.json.JsonObject;
 import javax.json.JsonReader;
 import javax.json.stream.JsonGenerator;
 import javax.json.stream.JsonGeneratorFactory;
+import javax.json.stream.JsonParser;
+import javax.json.stream.JsonParser.Event;
 
 import org.junit.Test;
 
@@ -29,8 +31,11 @@ public class DiagramStorageTests {
 			try(JsonGenerator generator = generatorFactory.createGenerator(os)) {
 				generator
 				.writeStartObject()
-					.write("test", "test2")
-					.write("test", "test2")
+					.write("reference", "test2")
+					.write("test2", "test2")
+					.write("test3", true)
+					.writeStartObject("a")
+					.writeEnd()
 				.writeEnd();
 			}
 
@@ -51,17 +56,95 @@ public class DiagramStorageTests {
 		// More complicated?
 			
 		// TODO: Read Example
+		System.err.println("READ:");
 		try(final ByteArrayInputStream is = new ByteArrayInputStream(data)) {
 			try(JsonReader reader = Json.createReader(is)) {
 				final JsonObject test = reader.readObject();
-				System.err.println(test.getString("test"));
 			}
-		} catch (IOException e) {
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
-		// TODO: For reference changing, need to read and write using stream API while only making small changes.
+		// TODO: Example of transforming JSON using the streaming API.
+		System.err.println("TRANFORM");
+		byte[] transformedData = null;
+		try(final ByteArrayOutputStream os = new ByteArrayOutputStream()) {
+            JsonGeneratorFactory generatorFactory = Json.createGeneratorFactory(Collections.singletonMap(JsonGenerator.PRETTY_PRINTING, true));            
+			try(JsonGenerator generator = generatorFactory.createGenerator(os)) {			
+				try(final ByteArrayInputStream is = new ByteArrayInputStream(data)) {
+					try(JsonParser parser = Json.createParser(is)) {
+						String name = null;
+						while(parser.hasNext()) {
+							final Event e = parser.next();
+							switch(e) {
+							case START_OBJECT:
+								if(name == null) {
+									generator.writeStartObject();
+								} else {
+									generator.writeStartObject(name);
+								}
+								break;
+								
+							case END_OBJECT:
+								generator.writeEnd();
+								break;
+								
+							case START_ARRAY:
+								if(name == null) {
+									generator.writeStartArray();
+								} else {
+									generator.writeStartArray(name);
+								}
+								break;
+								
+							case END_ARRAY:
+								generator.writeEnd();
+								break;
+								
+							case KEY_NAME:
+								name = parser.getString();
+								break;
+								
+							case VALUE_STRING:
+								String value = parser.getString();
+								if("reference".equals(name)) {
+									value = "TRANSFORMED";
+								}
+								
+								generator.write(name, value);
+								break;
+								
+							case VALUE_NUMBER:
+								generator.write(name, parser.getBigDecimal());
+								break;
+								
+							case VALUE_TRUE:
+								generator.write(name, true);
+								break;
+								
+							case VALUE_FALSE:
+								generator.write(name, false);
+								break;
+								
+							case VALUE_NULL:
+								generator.writeNull(name);
+								break;						
+							}
+						}
+					}
+				}
+			}
+			
+			transformedData = os.toByteArray();
+		} catch (Exception e) {
+			System.err.println("Error");
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
+		final String transformedJson = new String(transformedData); // TODO: Need to specify encoding?
+		System.err.println("TRANSFORMED:");
+		System.err.println(transformedJson);		
 	}
 }
