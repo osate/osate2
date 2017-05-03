bdae3f1334412177b4934657ea053983eeb40c75
Joe Seibel <jseibel@sei.cmu.edu>
2017-05-03 17:45:58 -0400

    Added methods to GetProperties for connection quality of service.

--- core/org.osate.xtext.aadl2.properties/src/org/osate/xtext/aadl2/properties/util/DeploymentProperties.java
+++ core/org.osate.xtext.aadl2.properties/src/org/osate/xtext/aadl2/properties/util/DeploymentProperties.java
@@ -53,6 +53,8 @@ public final class DeploymentProperties {
 	public static final String ACTUAL_SUBPROGRAM_CALL = "Actual_Subprogram_Call";
 
 	public static final String REQUIRED_VIRTUAL_BUS_CLASS = "Required_Virtual_Bus_Class";
+	public static final String PROVIDED_CONNECTION_QUALITY_OF_SERVICE = "Provided_Connection_Quality_Of_Service";
+	public static final String REQUIRED_CONNECTION_QUALITY_OF_SERVICE = "Required_Connection_Quality_Of_Service";
 
 	public static final String ACTUAL_FUNCTION_BINDING = "Actual_Function_Binding";
 
