317fbe9b609eee5d5ecb734cfb6c1e6f833e4ac9
Joe Seibel <jseibel@sei.cmu.edu>
2017-06-26 15:24:57 -0400

    Converted org.osate.importer.scade action sets to commands.

--- analyses/org.osate.importer.scade/src/org/osate/importer/scade/handlers/DoImportModel.java
+++ analyses/org.osate.importer.scade/src/org/osate/importer/scade/handlers/DoImportModel.java
@@ -28,65 +28,36 @@
  * 
  */
 
-package org.osate.importer.scade.actions;
+package org.osate.importer.scade.handlers;
 
 import java.io.File;
 import java.util.List;
 
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.jobs.Job;
-import org.eclipse.jface.action.IAction;
-import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbenchWindow;
-import org.eclipse.ui.IWorkbenchWindowActionDelegate;
 import org.eclipse.ui.PlatformUI;
 import org.osate.importer.Utils;
 import org.osate.importer.model.Model;
-import org.osate.importer.scade.Activator;
 import org.osate.importer.scade.FileImport;
 import org.osate.importer.scade.generator.AadlProjectCreator;
-import org.osgi.framework.Bundle;
 
-public final class DoImportModel implements IWorkbenchWindowActionDelegate {
-
+public final class DoImportModel extends AbstractHandler {
 	private String inputFile;
 	private String outputDirectory;
-	List<String> selectedModules;
 	private static String workingDirectory;
-	private static boolean filterSystem = false;
-
-	public static boolean filterSystem() {
-		return filterSystem;
-	}
-
-	public static void setFilterSystem(boolean f) {
-		filterSystem = f;
-	}
-
-	public static String getWorkingDirectory() {
-		return workingDirectory;
-	}
-
-	protected Bundle getBundle() {
-		return Activator.getDefault().getBundle();
-	}
-
-	public String getMarkerType() {
-		return "org.osate.importer.SimulinkImporterMarker";
-	}
-
-	protected String getActionName() {
-		return "Simulink Importer";
-	}
-
-	public void run(IAction action) {
-
+	
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
 		final Display d = PlatformUI.getWorkbench().getDisplay();
 		String fileSuffix;
 
@@ -97,15 +68,14 @@ public final class DoImportModel implements IWorkbenchWindowActionDelegate {
 			org.osate.ui.dialogs.Dialog.showInfo("SCADE Importer",
 					"Please select a directory where the model will be created");
 
-			return;
+			return null;
 		}
 
 		d.syncExec(new Runnable() {
-
+			@Override
 			public void run() {
 				IWorkbenchWindow window;
 				Shell sh;
-				List<String> modulesList;
 
 				window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
 				sh = window.getShell();
@@ -117,7 +87,6 @@ public final class DoImportModel implements IWorkbenchWindowActionDelegate {
 				if (parentDirectory != null) {
 					workingDirectory = parentDirectory;
 				}
-//				OsateDebug.osateDebug("parent=" + parentDirectory);
 				/**
 				 * Then, we open a new window 
 				 * to choose the module to be included
@@ -141,6 +110,7 @@ public final class DoImportModel implements IWorkbenchWindowActionDelegate {
 
 			if (fileSuffix.equalsIgnoreCase("xscade")) {
 				aadlGenerator = new Job("SCADE2AADL") {
+					@Override
 					protected IStatus run(IProgressMonitor monitor) {
 						monitor.beginTask("Generating AADL files from SCADE", 100);
 
@@ -157,12 +127,12 @@ public final class DoImportModel implements IWorkbenchWindowActionDelegate {
 
 			if (fileSuffix.equalsIgnoreCase("etp")) {
 				aadlGenerator = new Job("SCADE2AADL") {
+					@Override
 					protected IStatus run(IProgressMonitor monitor) {
 						List<Model> projectModels;
 
 						monitor.beginTask("Generating AADL files from SCADE", 100);
 
-						Model genericModel = new Model();
 						projectModels = FileImport.loadProjectFile(inputFile, workingDirectory);
 
 						for (Model m : projectModels) {
@@ -183,19 +153,8 @@ public final class DoImportModel implements IWorkbenchWindowActionDelegate {
 		} else {
 			org.osate.ui.dialogs.Dialog.showInfo("SCADE Importer",
 					"Please select a file with the xscade extension. Found suffix: " + fileSuffix);
-			return;
+			return null;
 		}
-	}
-
-	public void selectionChanged(IAction action, ISelection selection) {
-		// TODO Auto-generated method stub
-
-	}
-
-	public void dispose() {
-
-	}
-
-	public void init(IWorkbenchWindow window) {
+		return null;
 	}
 }
