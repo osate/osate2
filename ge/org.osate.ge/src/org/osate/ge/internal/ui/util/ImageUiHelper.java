b95cbd289f9c0924ff2d6ba28310d6080e346908
Philip Alldredge <pwa0001@uah.edu>
2017-07-06 09:09:21 -0500

    Fix bug with icons which caused invalid icon to be used when an icon was not available.

--- ge/org.osate.ge/src/org/osate/ge/internal/ui/util/ImageUiHelper.java
+++ ge/org.osate.ge/src/org/osate/ge/internal/ui/util/ImageUiHelper.java
@@ -3,6 +3,7 @@ package org.osate.ge.internal.ui.util;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.graphiti.ui.services.GraphitiUi;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.ImageData;
 import org.osate.ge.internal.graphiti.AgeDiagramTypeProvider;
 import org.osate.ge.internal.util.ImageHelper;
 
@@ -13,10 +14,25 @@ public class ImageUiHelper {
 	 * @return
 	 */
     public static Image getImage(final Object bo) {
-    	if(bo instanceof EObject) {
-			return GraphitiUi.getImageService().getImageForId(AgeDiagramTypeProvider.id, ImageHelper.getImage(((EObject)bo).eClass().getName()));
+    	if(!(bo instanceof EObject)) {
+    		return null;
+    	}
+    	
+    	final String imageId = ImageHelper.getImage(((EObject)bo).eClass().getName());
+    	final Image img = GraphitiUi.getImageService().getImageForId(AgeDiagramTypeProvider.id, imageId); // TODO: Remove
+    	if(img == null) {
+    		return null;
+    	}
+    	
+    	final ImageData imageData = img.getImageData();
+    	if(imageData == null) {
+    		return null;
+    	}
+    	
+		if(imageData.type < 0) {
+			return null;
 		}
     	
-    	return null;
+    	return img;
     }
 }
