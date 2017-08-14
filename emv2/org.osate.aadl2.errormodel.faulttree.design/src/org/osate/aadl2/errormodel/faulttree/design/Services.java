4f0055113942258ec76f7739c4dd2e2700dcedd2
Peter Feiler <phf@sei.cmu.edu>
2017-08-14 13:00:55 -0400

    updated it to use description for label and hazard description in tooltip.

--- emv2/org.osate.aadl2.errormodel.faulttree.design/src/org/osate/aadl2/errormodel/faulttree/design/Services.java
+++ emv2/org.osate.aadl2.errormodel.faulttree.design/src/org/osate/aadl2/errormodel/faulttree/design/Services.java
@@ -3,8 +3,12 @@ package org.osate.aadl2.errormodel.faulttree.design;
 import org.eclipse.emf.common.util.BasicEList;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
+import org.osate.aadl2.NamedElement;
 import org.osate.aadl2.errormodel.FaultTree.Event;
 import org.osate.aadl2.errormodel.FaultTree.FaultTree;
+import org.osate.aadl2.instance.InstanceObject;
+import org.osate.xtext.aadl2.errormodel.errorModel.ErrorTypes;
+import org.osate.xtext.aadl2.errormodel.util.EMV2Properties;
 
 public class Services {
 
@@ -38,7 +42,7 @@ public class Services {
 		return childrenToReturn;
 	}
 
-	public String ProbabilityToString(EObject context) {
+	public String getDescriptionAndProbability(EObject context) {
 		if (context instanceof Event) {
 			double val = ((Event) context).getProbability();
 			String labeltext = ((Event) context).getDescription();
@@ -46,6 +50,27 @@ public class Services {
 				labeltext = ((Event) context).getName();
 			}
 			return String.format("%1$s\n(%2$.3E)", labeltext, val);
+		}
+		return "";
+	}
+
+	public String getHazardDescriptionAndProbability(EObject context) {
+		if (context instanceof Event) {
+			Event ev = (Event) context;
+			double val = ev.getProbability();
+			String hazardDescription = EMV2Properties.getHazardDescription(
+					(InstanceObject) ev.getRelatedInstanceObject(), (NamedElement) ev.getRelatedEMV2Object(),
+					(ErrorTypes) ev.getRelatedErrorType());
+			String labeltext = ((Event) context).getDescription();
+			if (labeltext == null || labeltext.isEmpty()) {
+				labeltext = ((Event) context).getName();
+			}
+			if (hazardDescription == null) {
+				return String.format("%1$s\nOccurrence probability %2$.3E", labeltext, val);
+			} else {
+				return String.format("%1$s\n%3$s\nOccurrence probability %2$.3E", labeltext, val,
+						"Hazard: " + hazardDescription);
+			}
 		}
 		return "";
 	}
