b70857acc4bb3e97ad1145da8fa08f9fad7c10fa
Lutz Wrage <lutz.wrage@gmail.com>
2017-08-31 13:39:00 -0400

    Fix keyword sequence serialization for Properties and AADL2

--- org.osate.xtext.aadl2.properties/src/org/osate/xtext/aadl2/properties/serializer/PropertiesSyntacticSequencer.java
+++ org.osate.xtext.aadl2.properties/src/org/osate/xtext/aadl2/properties/serializer/PropertiesSyntacticSequencer.java
@@ -34,5 +34,48 @@
  */
 package org.osate.xtext.aadl2.properties.serializer;
 
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.xtext.RuleCall;
+import org.eclipse.xtext.nodemodel.INode;
+
 public class PropertiesSyntacticSequencer extends AbstractPropertiesSyntacticSequencer {
+	/**
+	 * AppliesToKeywords:
+	 * 	'applies' 'to'
+	 * ;
+	 */
+	@Override
+	protected String getAppliesToKeywordsToken(EObject semanticObject, RuleCall ruleCall, INode node) {
+		if (node != null) {
+			return getTokenText(node);
+		}
+		return "applies to";
+	}
+
+	/**
+	 * InBindingKeywords:
+	 * 	'in' 'binding'
+	 * ;
+	 */
+	@Override
+	protected String getInBindingKeywordsToken(EObject semanticObject, RuleCall ruleCall, INode node) {
+		if (node != null) {
+			return getTokenText(node);
+		}
+		return "in binding";
+	}
+
+	/**
+	 * InModesKeywords:
+	 * 	'in' 'modes'
+	 * ;
+	 */
+	@Override
+	protected String getInModesKeywordsToken(EObject semanticObject, RuleCall ruleCall, INode node) {
+		if (node != null) {
+			return getTokenText(node);
+		}
+		return "in modes";
+	}
+
 }
