40a66548604b1de57ba7b9dcedcf5a3b0cb8d033
Peter Feiler <phf@sei.cmu.edu>
2018-03-05 13:27:54 -0500

    now check for isAllincoming/outgoing to avoid null pointer on name. all paths are not filtered.

--- emv2/org.osate.xtext.aadl2.errormodel/src/org/osate/xtext/aadl2/errormodel/util/EMV2Util.java
+++ emv2/org.osate.xtext.aadl2.errormodel/src/org/osate/xtext/aadl2/errormodel/util/EMV2Util.java
@@ -738,14 +738,12 @@ public class EMV2Util {
 			ErrorTypes sourceType, ErrorPropagation flowTarget, ErrorTypes targetType) {
 		EList<ErrorFlow> result = new BasicEList<ErrorFlow>();
 		for (ErrorFlow ef : efs) {
-			if (ef instanceof ErrorPath && flowSource != null
-			) {
+			if (ef instanceof ErrorPath && flowSource != null) {
 				ErrorPath ep = (ErrorPath) ef;
-				if (EMV2Util.isSame(flowSource, ep.getIncoming()) &&
-				EMV2Util.isSame(flowTarget, ep.getOutgoing())) {
+				if ((!ep.isAllIncoming() && EMV2Util.isSame(flowSource, ep.getIncoming()))
+						&& (!ep.isAllOutgoing() && EMV2Util.isSame(flowTarget, ep.getOutgoing()))) {
 					if (EM2TypeSetUtil.contains(ep.getTypeTokenConstraint(), sourceType)
-							&&
-					EM2TypeSetUtil.contains(ep.getTargetToken(), targetType)) {
+							&& EM2TypeSetUtil.contains(ep.getTargetToken(), targetType)) {
 						result.add(ep);
 					}
 				}
