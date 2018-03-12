61d1b1c8eeb796369b01ecf33bb992b340c56de2
Peter Feiler <phf@sei.cmu.edu>
2018-03-12 12:00:28 -0400

    create methods do not use diagnostic parameter

--- org.osate.results/src/org/osate/result/util/ResultUtil.java
+++ org.osate.results/src/org/osate/result/util/ResultUtil.java
@@ -13,36 +13,35 @@ import org.osate.result.StringValue;
 
 public class ResultUtil {
 
-	public static Issue createError(String msg, EObject target, String diagnosticId) {
-		return createIssue(msg, target, IssueType.ERROR, diagnosticId);
+	public static Issue createError(String msg, EObject target) {
+		return createIssue(msg, target, IssueType.ERROR);
 	}
 
-	public static Issue createWarning(String msg, EObject target, String diagnosticId) {
-		return createIssue(msg, target, IssueType.WARNING, diagnosticId);
+	public static Issue createWarning(String msg, EObject target) {
+		return createIssue(msg, target, IssueType.WARNING);
 	}
 
-	public static Issue createInfo(String msg, EObject target, String diagnosticId) {
-		return createIssue(msg, target, IssueType.INFO, diagnosticId);
+	public static Issue createInfo(String msg, EObject target) {
+		return createIssue(msg, target, IssueType.INFO);
 	}
 
-	public static Issue createSuccess(String msg, EObject target, String diagnosticId) {
-		return createIssue(msg, target, IssueType.SUCCESS, diagnosticId);
+	public static Issue createSuccess(String msg, EObject target) {
+		return createIssue(msg, target, IssueType.SUCCESS);
 	}
 
-	public static Issue createFail(String msg, EObject target, String diagnosticId) {
-		return createIssue(msg, target, IssueType.FAIL, diagnosticId);
+	public static Issue createFail(String msg, EObject target) {
+		return createIssue(msg, target, IssueType.FAIL);
 	}
 
-	public static Issue createNone(String msg, EObject target, String diagnosticId) {
-		return createIssue(msg, target, IssueType.NONE, diagnosticId);
+	public static Issue createNone(String msg, EObject target) {
+		return createIssue(msg, target, IssueType.NONE);
 	}
 
-	public static Issue createIssue(String msg, EObject target, IssueType rit, String diagnosticId) {
+	public static Issue createIssue(String msg, EObject target, IssueType rit) {
 		Issue issue = ResultFactory.eINSTANCE.createIssue();
 		issue.setSourceReference(target);
 		issue.setMessage(msg);
 		issue.setIssueType(rit);
-		issue.setDiagnostic(diagnosticId);
 		return issue;
 	}
 
