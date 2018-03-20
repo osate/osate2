58a7a88cd2d55fac55f934ef379c6587fc0a1701
aarong <aarong@sei.cmu.edu>
2018-03-20 11:59:22 -0400

    Added workspace preference page for FHA.  Need to change the analysis drivers to make use of the preference now.

--- emv2/org.osate.aadl2.errormodel.analysis/src/org/osate/aadl2/errormodel/analysis/Activator.java
+++ emv2/org.osate.aadl2.errormodel.analysis/src/org/osate/aadl2/errormodel/analysis/Activator.java
@@ -1,6 +1,7 @@
 package org.osate.aadl2.errormodel.analysis;
 
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osate.aadl2.errormodel.analysis.fha.FHAReport;
 import org.osgi.framework.BundleContext;
 
 /**
@@ -10,6 +11,12 @@ public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "org.osate.aadl2.errormodel.analysis"; //$NON-NLS-1$
+
+	/**
+	 * Name of the preference for which version of the analysis to use.
+	 */
+	public static final String HAZARD_FORMAT_PREF = "hazardFormat";
+	public static final String HAZARD_FORMAT_DEFAULT = FHAReport.HazardFormat.EMV2.name();
 
 	// The shared instance
 	private static Activator plugin;
@@ -22,7 +29,7 @@ public class Activator extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
 	 */
 	@Override
@@ -33,7 +40,7 @@ public class Activator extends AbstractUIPlugin {
 
 	/*
 	 * (non-Javadoc)
-	 * 
+	 *
 	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
 	 */
 	@Override
