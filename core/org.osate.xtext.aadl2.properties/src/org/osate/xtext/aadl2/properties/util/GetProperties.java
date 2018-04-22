d36f24e1c687f5992aa7e531775df2cac01751fa
Peter Feiler <phf@sei.cmu.edu>
2018-04-22 11:34:02 -0400

    Revert "Get properties of ARINC653 schedule did not handle incomplete record fields correctly."
    This reverts commit ae55f54b82a8302928a53fd6e43179b59c84daa1.

--- core/org.osate.xtext.aadl2.properties/src/org/osate/xtext/aadl2/properties/util/GetProperties.java
+++ core/org.osate.xtext.aadl2.properties/src/org/osate/xtext/aadl2/properties/util/GetProperties.java
@@ -1407,9 +1407,8 @@ public class GetProperties {
 	}
 
 	public static double getDataSizeInBytes(final NamedElement ne) {
-		if (ne == null) {
+		if (ne == null)
 			return 0.0;
-		}
 		Property SourceDataSize = lookupPropertyDefinition(ne, MemoryProperties._NAME, MemoryProperties.DATA_SIZE);
 		UnitLiteral Bytes = findUnitLiteral(SourceDataSize, AadlProject.B_LITERAL);
 		return getDataSize(ne, Bytes);
@@ -1891,18 +1890,11 @@ public class GetProperties {
 					windowStartProcessing = (BooleanLiteralImpl) PropertyUtils.getRecordFieldValue(window,
 							"periodic_processing_start");
 					windowPartition = (InstanceReferenceValue) PropertyUtils.getRecordFieldValue(window, "partition");
-					if (windowPartition != null) {
-						part = (ComponentInstance) windowPartition.getReferencedInstanceObject();
-					}
-					if (windowTime == null) {
-						time = 0;
-					} else {
-						time = ((NumberValue) windowTime).getScaledValue(milliseconds);
-					}
-					if (part != null) {
-						scheduleWindow = new ARINC653ScheduleWindow(part, time, startProcessing);
-						windows.add(scheduleWindow);
-					}
+
+					part = (ComponentInstance) windowPartition.getReferencedInstanceObject();
+					time = ((NumberValue) windowTime).getScaledValue(milliseconds);
+					scheduleWindow = new ARINC653ScheduleWindow(part, time, startProcessing);
+					windows.add(scheduleWindow);
 				}
 			}
 		} catch (PropertyLookupException e) {
