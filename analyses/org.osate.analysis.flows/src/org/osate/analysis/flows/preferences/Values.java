a7e26706c992a74b0fac1c570a24297bbfabc7f7
Peter Feiler <phf@sei.cmu.edu>
2018-05-08 23:09:26 -0400

    Removed unused methods

--- analyses/org.osate.analysis.flows/src/org/osate/analysis/flows/preferences/Values.java
+++ analyses/org.osate.analysis.flows/src/org/osate/analysis/flows/preferences/Values.java
@@ -24,12 +24,6 @@ public class Values {
 		String policy = store.getString(Constants.PARTITONING_POLICY);
 		return policy.equalsIgnoreCase(Constants.PARTITIONING_POLICY_MAJOR_FRAME_DELAYED_STR);
 	}
-//
-//	public static boolean doReportSubtotals() {
-//		IPreferenceStore store = FlowanalysisPlugin.getDefault().getPreferenceStore();
-//		String policy = store.getString(Constants.REPORT_SUBTOTALS);
-//		return policy.equalsIgnoreCase(Constants.REPORT_SUBTOTALS_YES);
-//	}
 
 	public static boolean doWorstCaseDeadline() {
 		if (prefs != null && prefs.length > 2) {
@@ -39,12 +33,6 @@ public class Values {
 		String policy = store.getString(Constants.WORST_CASE_DEADLINE);
 		return policy.equalsIgnoreCase(Constants.WORST_CASE_DEADLINE_YES);
 	}
-//
-//	public static boolean doDataSetProcessing() {
-//		IPreferenceStore store = FlowanalysisPlugin.getDefault().getPreferenceStore();
-//		String policy = store.getString(Constants.DATASET_PROCESSING);
-//		return policy.equalsIgnoreCase(Constants.DATASET_PROCESSING_YES);
-//	}
 
 	public static boolean doBestcaseEmptyQueue() {
 		if (prefs != null && prefs.length > 3) {
@@ -64,22 +52,12 @@ public class Values {
 				: Constants.PARTITIONING_POLICY_PARTITION_END_STR;
 	}
 
-	public static String getMajorFrameDelayName() {
-		IPreferenceStore store = FlowanalysisPlugin.getDefault().getPreferenceStore();
-		return store.getString(Constants.PARTITONING_POLICY);
-	}
-
 	public static String getSynchronousSystemLabel() {
 		return Values.doSynchronousSystem() ? "SS" : "AS";
 	}
 
 	public static String getSynchronousSystemDescription() {
 		return Values.doSynchronousSystem() ? Constants.SYNCHRONOUS_SYSTEM_YES : Constants.SYNCHRONOUS_SYSTEM_NO;
-	}
-
-	public static String getSynchronousSystemName() {
-		IPreferenceStore store = FlowanalysisPlugin.getDefault().getPreferenceStore();
-		return store.getString(Constants.SYNCHRONOUS_SYSTEM);
 	}
 
 	public static String getWorstCaseDeadlineLabel() {
@@ -90,10 +68,6 @@ public class Values {
 		return Values.doWorstCaseDeadline() ? Constants.WORST_CASE_DEADLINE_YES : Constants.WORST_CASE_DEADLINE_NO;
 	}
 
-	public static String getWorstCaseDeadlineName() {
-		IPreferenceStore store = FlowanalysisPlugin.getDefault().getPreferenceStore();
-		return store.getString(Constants.WORST_CASE_DEADLINE);
-	}
 
 	public static String getBestcaseEmptyQueueLabel() {
 		return Values.doBestcaseEmptyQueue() ? "EQ" : "FQ";
@@ -101,15 +75,6 @@ public class Values {
 
 	public static String getBestcaseEmptyQueueDescription() {
 		return Values.doBestcaseEmptyQueue() ? Constants.BESTCASE_EMPTY_QUEUE_YES : Constants.BESTCASE_EMPTY_QUEUE_NO;
-	}
-
-	public static String getBestcaseEmptyQueueName() {
-		IPreferenceStore store = FlowanalysisPlugin.getDefault().getPreferenceStore();
-		return store.getString(Constants.BESTCASE_EMPTY_QUEUE);
-	}
-
-	public static String[] getPrefs() {
-		return prefs;
 	}
 
 	public static void setPrefs(String[] prefs) {
