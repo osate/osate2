abf1626f5ec97a1978b2bc51295127a52322c850
Lutz Wrage <lutz.wrage@gmail.com>
2018-05-30 15:31:05 -0400

    Remove unloaded resources from resource set

--- core/org.osate.testsupport/src/org/osate/testsupport/TestResourceSet.java
+++ core/org.osate.testsupport/src/org/osate/testsupport/TestResourceSet.java
@@ -3,6 +3,7 @@ package org.osate.testsupport;
 import java.util.List;
 import java.util.Map;
 
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.xtext.resource.XtextResourceSet;
@@ -13,12 +14,12 @@ import com.google.inject.Provider;
 
 public class TestResourceSet {
 
+	@Inject
+	protected Provider<XtextResourceSet> rsp;
+
 	protected static XtextResourceSet set;
 
 	private static List<URI> contributed = PluginSupportUtil.getContributedAadl();
-
-	@Inject
-	protected Provider<XtextResourceSet> rsp;
 
 	public XtextResourceSet get() {
 		if (set == null) {
@@ -40,6 +41,10 @@ public class TestResourceSet {
 	}
 
 	protected void clean() {
+		EList<Resource> resources = set.getResources();
 		set.getResources().stream().skip(contributed.size()).forEach(Resource::unload);
+		for (int i = resources.size(); i < contributed.size(); i--) {
+			resources.remove(i);
+		}
 	}
 }
