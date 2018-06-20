9383cdcfb20bba8db0209318538f95dda48a2aea
Philip Alldredge <pwa0001@uah.edu>
2018-06-20 15:12:29 -0500

    Added command for removing all bendpoints from the selected connections.

---
+++ ge/org.osate.ge/src/org/osate/ge/internal/ui/handlers/RemoveBendpointsHandler.java
@@ -0,0 +1,51 @@
+package org.osate.ge.internal.ui.handlers;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.osate.ge.internal.diagram.runtime.AgeDiagram;
+import org.osate.ge.internal.diagram.runtime.DiagramElement;
+import org.osate.ge.internal.diagram.runtime.DiagramElementPredicates;
+import org.osate.ge.internal.ui.util.SelectionUtil;
+import org.osate.ge.internal.ui.util.UiUtil;
+
+public class RemoveBendpointsHandler extends AbstractHandler {
+	@Override
+	public Object execute(final ExecutionEvent event) throws ExecutionException {
+		final List<DiagramElement> selectedDiagramElements = AgeHandlerUtil.getSelectedDiagramElements(event);
+		final AgeDiagram diagram = UiUtil.getDiagram(selectedDiagramElements);
+		if (diagram == null) {
+			throw new RuntimeException("Unable to get diagram");
+		}
+
+		diagram.modify("Remove Bendpoints", m -> {
+			for (final DiagramElement de : selectedDiagramElements) {
+				if (DiagramElementPredicates.isConnection(de) && de.isBendpointsSet()) {
+					m.setBendpoints(de, Collections.emptyList());
+				}
+			}
+		});
+
+		return null;
+	}
+
+	@Override
+	public void setEnabled(final Object evaluationContext) {
+		final IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+		final boolean enabled;
+		if (window == null) {
+			enabled = false;
+		} else {
+			enabled = SelectionUtil.getSelectedDiagramElements(window.getActivePage().getSelection()).stream()
+					.anyMatch(de -> DiagramElementPredicates.isConnection(de) && de.isBendpointsSet());
+		}
+
+		setBaseEnabled(enabled);
+	}
+
+}
