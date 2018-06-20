4b635c4169e688f744862386bb000fadd4d68afb
aarong <aarong@sei.cmu.edu>
2018-06-20 13:42:43 -0400

    Added methods to allow change listeners to be added to the property store.  Need this so that the FHA Workflow can find out when the type of FHA analysis is changed.

--- emv2/org.osate.aadl2.errormodel.analysis/src/org/osate/aadl2/errormodel/analysis/fha/FHAReport.java
+++ emv2/org.osate.aadl2.errormodel.analysis/src/org/osate/aadl2/errormodel/analysis/fha/FHAReport.java
@@ -71,8 +71,8 @@ import org.osate.xtext.aadl2.errormodel.errorModel.ErrorTypes;
 import org.osate.xtext.aadl2.errormodel.errorModel.EventOrPropagation;
 import org.osate.xtext.aadl2.errormodel.errorModel.TypeSet;
 import org.osate.xtext.aadl2.errormodel.errorModel.TypeToken;
-import org.osate.xtext.aadl2.errormodel.util.EMV2TypeSetUtil;
 import org.osate.xtext.aadl2.errormodel.util.EMV2Properties;
+import org.osate.xtext.aadl2.errormodel.util.EMV2TypeSetUtil;
 import org.osate.xtext.aadl2.errormodel.util.EMV2Util;
 import org.osate.xtext.aadl2.properties.util.GetProperties;
 
