2044940dea319dbfe4eafcb67cec05f4693c01de
Lutz Wrage <lutz.wrage@gmail.com>
2018-08-05 16:11:16 -0400

    Use AdapterFactoryLabelProvider for expanded AADL files

--- core/org.osate.ui/src/org/osate/ui/navigator/AadlElementLabelProvider.java
+++ core/org.osate.ui/src/org/osate/ui/navigator/AadlElementLabelProvider.java
@@ -1,8 +1,30 @@
 package org.osate.ui.navigator;
 
-import org.eclipse.ui.model.WorkbenchLabelProvider;
+import org.eclipse.emf.edit.ui.provider.AdapterFactoryLabelProvider;
+import org.eclipse.swt.graphics.Image;
+import org.osate.aadl2.Element;
+import org.osate.ui.UiUtil;
 
-public class AadlElementLabelProvider extends WorkbenchLabelProvider {
+public class AadlElementLabelProvider extends AdapterFactoryLabelProvider {
 
+	public AadlElementLabelProvider() {
+		super(UiUtil.getAdapterFactory());
+	}
+
+	@Override
+	public Image getImage(Object object) {
+		if (object instanceof Element) {
+			return super.getImage(object);
+		}
+		return null;
+	}
+
+	@Override
+	public String getText(Object object) {
+		if (object instanceof Element) {
+			return super.getText(object);
+		}
+		return null;
+	}
 
 }
