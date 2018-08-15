44ea00c8234928c93610a810222c19da5592a453
Lutz Wrage <lutz.wrage@gmail.com>
2018-08-15 12:17:22 -0400

    Use an IElementComparator to keep expanded file content open on save
    Also simplified content and label provider for expanded AADL

--- core/org.osate.ui/src/org/osate/ui/navigator/AadlElementContentProvider.java
+++ core/org.osate.ui/src/org/osate/ui/navigator/AadlElementContentProvider.java
@@ -1,138 +1,127 @@
 package org.osate.ui.navigator;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.eclipse.core.resources.IFile;
-import org.eclipse.emf.common.util.EList;
-import org.eclipse.emf.ecore.EObject;
-import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceChangeEvent;
+import org.eclipse.core.resources.IResourceChangeListener;
+import org.eclipse.core.resources.IResourceDelta;
+import org.eclipse.core.resources.IResourceDeltaVisitor;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.edit.ui.provider.AdapterFactoryContentProvider;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.eclipse.jface.viewers.Viewer;
+import org.eclipse.swt.widgets.Display;
 import org.osate.aadl2.AadlPackage;
-import org.osate.aadl2.Classifier;
 import org.osate.aadl2.PackageSection;
-import org.osate.aadl2.Property;
-import org.osate.aadl2.PropertyAssociation;
-import org.osate.aadl2.PropertyConstant;
 import org.osate.aadl2.PropertySet;
-import org.osate.aadl2.PropertyType;
-import org.osate.aadl2.instance.ConnectionInstance;
-import org.osate.aadl2.instance.InstanceObject;
-import org.osate.aadl2.modelsupport.resources.OsateResourceUtil;
+import org.osate.aadl2.instance.ComponentInstance;
+import org.osate.ui.UiUtil;
 
-public class AadlElementContentProvider implements ITreeContentProvider {
+public class AadlElementContentProvider extends AdapterFactoryContentProvider
+		implements IResourceChangeListener, IResourceDeltaVisitor {
 
 	private static final Object[] NO_CHILDREN = new Object[0];
-	private static final Object AADL_EXT = "aadl";
-	private static final Object AAXL2_EXT = "aaxl2";
+
+	private static ResourceSet resourceSet = new ResourceSetImpl();
+
+	public AadlElementContentProvider() {
+		super(UiUtil.getAdapterFactory());
+		ResourcesPlugin.getWorkspace().addResourceChangeListener(this, IResourceChangeEvent.POST_CHANGE);
+	}
 
 	@Override
 	public Object[] getElements(Object inputElement) {
-		if (inputElement instanceof IFile) {
-			IFile modelFile = (IFile) inputElement;
-			if (AADL_EXT.equals(modelFile.getFileExtension())) {
-				EList<EObject> contents = OsateResourceUtil.getResource(modelFile).getContents();
-				if (null != contents && !contents.isEmpty()) {
-					EObject root = contents.get(0);
-					if (root instanceof AadlPackage) {
-						return new AadlPackage[] { (AadlPackage) root };
-					} else if (root instanceof PropertySet) {
-						return new PropertySet[] { (PropertySet) root };
-					}
-				}
-			} else if (AAXL2_EXT.equals(modelFile.getFileExtension())) {
-				EList<EObject> contents = OsateResourceUtil.getResource(modelFile).getContents();
-				if (null != contents && !contents.isEmpty()) {
-					EObject root = contents.get(0);
-					if (root instanceof InstanceObject) {
-						return new InstanceObject[] { (InstanceObject) root };
-					}
-				}
-			}
-		}
-		return NO_CHILDREN;
+		return getChildren(inputElement);
 	}
 
 	@Override
 	public Object[] getChildren(Object parentElement) {
 		if (parentElement instanceof IFile) {
-			IFile modelFile = (IFile) parentElement;
-			if (AADL_EXT.equals(modelFile.getFileExtension())) {
-				EList<EObject> contents = OsateResourceUtil.getResource(modelFile).getContents();
-				if (null != contents && !contents.isEmpty()) {
-					EObject root = contents.get(0);
-					if (root instanceof AadlPackage) {
-						return new AadlPackage[] { (AadlPackage) root };
-					} else if (root instanceof PropertySet) {
-						return new PropertySet[] { (PropertySet) root };
-					}
-				}
-			} else if (AAXL2_EXT.equals(modelFile.getFileExtension())) {
-				EList<EObject> contents = OsateResourceUtil.getResource(modelFile).getContents();
-				if (null != contents && !contents.isEmpty()) {
-					EObject root = contents.get(0);
-					if (root instanceof InstanceObject) {
-						return new InstanceObject[] { (InstanceObject) root };
-					}
-				}
-			}
-		} else if (parentElement instanceof AadlPackage) {
-			AadlPackage aadlPackage = (AadlPackage) parentElement;
-			List<PackageSection> packageSections = new ArrayList<PackageSection>();
-			PackageSection publicPackageSection = aadlPackage.getOwnedPublicSection();
-			if (null != publicPackageSection) {
-				packageSections.add(publicPackageSection);
-			}
-			PackageSection privatePackageSection = aadlPackage.getOwnedPrivateSection();
-			if (null != privatePackageSection) {
-				packageSections.add(privatePackageSection);
-			}
-			return packageSections.toArray();
-		} else if (parentElement instanceof PackageSection) {
-			PackageSection pkg = (PackageSection) parentElement;
-			if (null != pkg) {
-				return pkg.getOwnedClassifiers().toArray();
-			}
-		} else if (parentElement instanceof PropertySet) {
-			List<Property> properties = ((PropertySet) parentElement).getOwnedProperties();
-			List<PropertyType> propertyTypes = ((PropertySet) parentElement).getOwnedPropertyTypes();
-			List<PropertyConstant> propertyConstants = ((PropertySet) parentElement).getOwnedPropertyConstants();
-			List<Object> propertiesConstantsTypes = new ArrayList<Object>();
-			propertiesConstantsTypes.addAll(properties);
-			propertiesConstantsTypes.addAll(propertyConstants);
-			propertiesConstantsTypes.addAll(propertyTypes);
-			return propertiesConstantsTypes.toArray();
-		} else if (parentElement instanceof ConnectionInstance) {
-			return NO_CHILDREN;
-		} else if (parentElement instanceof InstanceObject) {
-			List<EObject> instances = ((InstanceObject) parentElement).eContents();
-			List<EObject> finalInstances = new ArrayList<EObject>();
-			for (EObject eObject : instances) {
-				if (!(eObject instanceof PropertyAssociation)) {
-					finalInstances.add(eObject);
-				}
-			}
-			return finalInstances.toArray();
-		} else if (parentElement instanceof Classifier) {
+			String path = ((IFile) parentElement).getFullPath().toString();
+			URI uri = URI.createPlatformResourceURI(path, true);
+			parentElement = resourceSet.getResource(uri, true);
+		} else if (!shouldExpand(parentElement)) {
 			return NO_CHILDREN;
 		}
-		return NO_CHILDREN;
+
+		return super.getChildren(parentElement);
 	}
 
 	@Override
 	public Object getParent(Object element) {
-		// TODO Auto-generated method stub
-		return null;
+		if (element instanceof IFile) {
+			return null;
+		}
+		return super.getParent(element);
 	}
 
 	@Override
 	public boolean hasChildren(Object element) {
 		if (element instanceof IFile) {
 			return true;
+		} else if (!shouldExpand(element)) {
+			return false;
 		}
-		if (element instanceof AadlPackage) {
-			return true;
+		return super.hasChildren(element);
+	}
+
+	protected boolean shouldExpand(Object element) {
+		return element instanceof AadlPackage || element instanceof PropertySet || element instanceof PackageSection
+				|| element instanceof ComponentInstance;
+	}
+
+	@Override
+	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+		TreeViewer tv = (TreeViewer) viewer;
+		if (tv.getComparer() == null) {
+			tv.setComparer(new AadlElementComparer());
 		}
-		return getChildren(element).length > 0;
+		super.inputChanged(viewer, oldInput, newInput);
+	}
+
+	@Override
+	public void resourceChanged(IResourceChangeEvent event) {
+		try {
+			IResourceDelta delta = event.getDelta();
+			delta.accept(this);
+		} catch (CoreException e) {
+			// FIXME: ignored
+		}
+		Display.getDefault().asyncExec(() -> {
+			TreeViewer tv = (TreeViewer) viewer;
+			Object[] expanded = tv.getExpandedElements();
+			viewer.refresh();
+			tv.setExpandedElements(expanded);
+		});
+	}
+
+	@Override
+	public boolean visit(IResourceDelta delta) throws CoreException {
+		IResource changedResource = delta.getResource();
+		if (changedResource.getType() == IResource.FILE && (changedResource.getFileExtension().equals("aadl")
+				|| changedResource.getFileExtension().equals("aaxl2"))) {
+			try {
+				String path = ((IFile) changedResource).getFullPath().toString();
+				URI uri = URI.createPlatformResourceURI(path, true);
+				Resource res = resourceSet.getResource(uri, true);
+				res.unload();
+				res.load(resourceSet.getLoadOptions());
+			} catch (Exception e) {
+				// FIXME: ignored
+			}
+			return false;
+		}
+		return true;
+	}
+
+	@Override
+	public void dispose() {
+		ResourcesPlugin.getWorkspace().removeResourceChangeListener(this);
 	}
 
 }
