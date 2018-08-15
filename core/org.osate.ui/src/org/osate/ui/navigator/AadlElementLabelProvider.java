be05e145e974360ca8686a96f9acf8e18f4baf7d
Lutz Wrage <lutz.wrage@gmail.com>
2018-08-15 12:17:22 -0400

    Use an IElementComparator to keep expanded file content open on save
    Also simplified content and label provider for expanded AADL

--- core/org.osate.ui/src/org/osate/ui/navigator/AadlElementLabelProvider.java
+++ core/org.osate.ui/src/org/osate/ui/navigator/AadlElementLabelProvider.java
@@ -3,6 +3,8 @@ package org.osate.ui.navigator;
 import org.eclipse.emf.edit.ui.provider.AdapterFactoryLabelProvider;
 import org.eclipse.swt.graphics.Image;
 import org.osate.aadl2.Element;
+import org.osate.aadl2.PrivatePackageSection;
+import org.osate.aadl2.PublicPackageSection;
 import org.osate.ui.UiUtil;
 
 public class AadlElementLabelProvider extends AdapterFactoryLabelProvider {
@@ -21,6 +23,12 @@ public class AadlElementLabelProvider extends AdapterFactoryLabelProvider {
 
 	@Override
 	public String getText(Object object) {
+		if (object instanceof PublicPackageSection) {
+			return "public";
+		}
+		if (object instanceof PrivatePackageSection) {
+			return "private";
+		}
 		if (object instanceof Element) {
 			return super.getText(object);
 		}
