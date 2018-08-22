a0830b5f0d6b723914b0f6185575b3eaf6ec2912
Joe Seibel <1824701+joeseibel@users.noreply.github.com>
2018-08-22 14:40:23 -0400

    Added EObject import back.

--- core/org.osate.ui/src/org/osate/ui/navigator/AadlElementContentProvider.java
+++ core/org.osate.ui/src/org/osate/ui/navigator/AadlElementContentProvider.java
@@ -9,6 +9,7 @@ import org.eclipse.core.resources.IResourceDeltaVisitor;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
