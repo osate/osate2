10aaf6394df1635443147b3114083555b6bbab91
Lutz Wrage <lutz.wrage@gmail.com>
2018-09-11 08:26:02 -0400

    Add status bar text for AADL elements

--- core/org.osate.ui/src/org/osate/ui/navigator/AadlElementLabelProvider.java
+++ core/org.osate.ui/src/org/osate/ui/navigator/AadlElementLabelProvider.java
@@ -1,13 +1,19 @@
 package org.osate.ui.navigator;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.edit.ui.provider.AdapterFactoryLabelProvider;
 import org.eclipse.swt.graphics.Image;
+import org.eclipse.ui.navigator.IDescriptionProvider;
 import org.osate.aadl2.Element;
 import org.osate.aadl2.PrivatePackageSection;
 import org.osate.aadl2.PublicPackageSection;
 import org.osate.ui.UiUtil;
 
-public class AadlElementLabelProvider extends AdapterFactoryLabelProvider {
+public class AadlElementLabelProvider extends AdapterFactoryLabelProvider implements IDescriptionProvider {
 
 	public AadlElementLabelProvider() {
 		super(UiUtil.getAdapterFactory());
@@ -33,6 +39,30 @@ public class AadlElementLabelProvider extends AdapterFactoryLabelProvider {
 			return super.getText(object);
 		}
 		return null;
+	}
+
+	@Override
+	public String getDescription(Object element) {
+		String description = null;
+		if (element instanceof EObject) {
+			URI uri = ((EObject) element).eResource().getURI();
+			if (uri.isPlatformPlugin()) {
+				// contributed
+				String[] segments = uri.segments();
+				int i = 2;
+				while (segments[i].startsWith("resource") || segments[i].startsWith("package")
+						|| segments[i].startsWith("propert")) {
+					i++;
+				}
+				description = "Plug-in Contributions/"
+						+ Arrays.asList(segments).stream().skip(i).collect(Collectors.joining("/"));
+			} else {
+				String[] segments = uri.segments();
+				description = Arrays.asList(segments).stream().skip(1).collect(Collectors.joining("/"));
+			}
+			description = getText(element) + " - " + description;
+		}
+		return description;
 	}
 
 }
