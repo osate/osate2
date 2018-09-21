0cfcb5b832d7793199a40d7e850e097b4b843ef8
Lutz Wrage <lutz.wrage@gmail.com>
2018-09-21 15:57:51 -0400

    Change removal version to 2.4

--- core/org.osate.ui/src/org/osate/ui/dialogs/ComponentTypeMemberInputValidator.java
+++ core/org.osate.ui/src/org/osate/ui/dialogs/ComponentTypeMemberInputValidator.java
@@ -6,7 +6,7 @@ import java.util.Set;
 import org.osate.aadl2.ComponentType;
 
 /**
- * @deprecated this class will be removed in 2.3.6
+ * @deprecated this class will be removed in 2.4
  */
 @Deprecated
 public class ComponentTypeMemberInputValidator extends AadlIdentifierInputValidator {
