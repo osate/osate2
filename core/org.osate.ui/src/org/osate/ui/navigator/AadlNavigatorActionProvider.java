542e3438f4f983118a088ac9fd5ef5230f44cdd6
aarong <aarong@sei.cmu.edu>
2018-09-27 14:37:28 -0400

    Updated the plugin.xml so that the AadlNavigatorActionProvider is also enabled for AObjects that are contributed by the AadlElementContentProvider.  Updated the action provider to (1) add the action to the context menu, and (2) for AObjects to open the editor using the URI of the AObject from ECoreUtil.getURI().

--- core/org.osate.ui/src/org/osate/ui/navigator/AadlNavigatorActionProvider.java
+++ core/org.osate.ui/src/org/osate/ui/navigator/AadlNavigatorActionProvider.java
@@ -1,13 +1,14 @@
 package org.osate.ui.navigator;
 
-import java.util.Arrays;
-import java.util.stream.Stream;
-
+import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.navigator.CommonActionProvider;
 import org.eclipse.ui.navigator.ICommonActionConstants;
+import org.eclipse.ui.navigator.ICommonMenuConstants;
 import org.eclipse.xtext.ui.editor.IURIEditorOpener;
+import org.osate.aadl2.parsesupport.AObject;
 import org.osate.xtext.aadl2.ui.internal.Aadl2Activator;
 import org.osate.xtext.aadl2.ui.resource.ContributedAadlStorage;
 
@@ -15,24 +16,45 @@ import com.google.inject.Injector;
 
 public class AadlNavigatorActionProvider extends CommonActionProvider {
 	private final IURIEditorOpener editorOpener;
+	private final Action openFileAction;
 
 	public AadlNavigatorActionProvider() {
 		Injector injector = Aadl2Activator.getInstance().getInjector(Aadl2Activator.ORG_OSATE_XTEXT_AADL2_AADL2);
 		editorOpener = injector.getInstance(IURIEditorOpener.class);
+		openFileAction = new Action() {
+			{
+				setText("&Open");
+				setToolTipText("Open the contributed library in an AADL editor");
+			}
+
+			@Override
+			public void run() {
+				Object[] selectedElements = getActionSite().getStructuredViewer().getStructuredSelection().toArray();
+				for (Object selected : selectedElements) {
+					if (selected instanceof ContributedAadlStorage) {
+						editorOpener.open(((ContributedAadlStorage) selected).getUri(), true);
+					} else if (selected instanceof AObject) {
+						editorOpener.open(EcoreUtil.getURI((AObject) selected), true);
+					}
+				}
+			}
+		};
+	}
+
+	@Override
+	public void fillContextMenu(final IMenuManager aMenu) {
+		if (getContext().getSelection().isEmpty()) {
+			return;
+		}
+
+		if (openFileAction.isEnabled()) {
+			aMenu.insertAfter(ICommonMenuConstants.GROUP_OPEN, openFileAction);
+		}
 	}
 
 	@Override
 	public void fillActionBars(IActionBars actionBars) {
 		super.fillActionBars(actionBars);
-		actionBars.setGlobalActionHandler(ICommonActionConstants.OPEN, new Action() {
-			@Override
-			public void run() {
-				Object[] selectedElements = getActionSite().getStructuredViewer().getStructuredSelection().toArray();
-				Stream<ContributedAadlStorage> files = Arrays.stream(selectedElements)
-						.filter(file -> file instanceof ContributedAadlStorage)
-						.map(file -> (ContributedAadlStorage) file);
-				files.forEach(file -> editorOpener.open(file.getUri(), true));
-			}
-		});
+		actionBars.setGlobalActionHandler(ICommonActionConstants.OPEN, openFileAction);
 	}
 }
