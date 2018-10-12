5063a709dccbe2b7acbd946ec72a8a7679591690
aarong <aarong@sei.cmu.edu>
2018-10-12 15:43:28 -0400

    Started adding the skeleton for actual searching.

---
+++ core/org.osate.ui/src/org/osate/search/AADLSearchQuery.java
@@ -0,0 +1,58 @@
+package org.osate.search;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.OperationCanceledException;
+import org.eclipse.search.ui.ISearchQuery;
+import org.eclipse.search.ui.ISearchResult;
+
+public final class AADLSearchQuery implements ISearchQuery {
+	public enum SearchFor {
+		CLASSIFIER, PROPERTY;
+	}
+
+	public enum LimitTo {
+		ALL, REFERENCES, DECLARATIONS;
+	}
+
+	private final String substring;
+	private final SearchFor searchFor;
+	private final LimitTo limitTo;
+	private final int scope;
+	private final AADLSearchResult searchResult;
+
+	public AADLSearchQuery(final String substring, final SearchFor searchFor, final LimitTo limitTo, final int scope) {
+		this.substring = substring;
+		this.searchFor = searchFor;
+		this.limitTo = limitTo;
+		this.scope = scope;
+		this.searchResult = new AADLSearchResult(this);
+	}
+
+	@Override
+	public IStatus run(IProgressMonitor monitor) throws OperationCanceledException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getLabel() {
+		return "Search for \"" + substring + "\" in " + searchFor + " " + limitTo;
+	}
+
+	@Override
+	public boolean canRerun() {
+		return true;
+	}
+
+	@Override
+	public boolean canRunInBackground() {
+		return true;
+	}
+
+	@Override
+	public ISearchResult getSearchResult() {
+		return searchResult;
+	}
+
+}
---
+++ core/org.osate.ui/src/org/osate/search/AADLSearchResultEvent.java
@@ -0,0 +1,11 @@
+package org.osate.search;
+
+import org.eclipse.search.ui.SearchResultEvent;
+
+@SuppressWarnings("serial")
+public final class AADLSearchResultEvent extends SearchResultEvent {
+	// TODO Add stuff here
+	protected AADLSearchResultEvent(final AADLSearchResult searchResult) {
+		super(searchResult);
+	}
+}
