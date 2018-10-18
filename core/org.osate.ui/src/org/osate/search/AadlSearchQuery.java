faa4a4df958fe6d354aee323789a301318557831
aarong <aarong@sei.cmu.edu>
2018-10-18 14:41:00 -0400

    Added functionality actually use the "search for" and "limit to" fields. Making heavy use of enumerations to parameterize the functionality.

--- core/org.osate.ui/src/org/osate/search/AadlSearchQuery.java
+++ core/org.osate.ui/src/org/osate/search/AadlSearchQuery.java
@@ -4,17 +4,69 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.emf.ecore.EClass;
 import org.eclipse.search.ui.ISearchQuery;
 import org.eclipse.search.ui.ISearchResult;
-import org.eclipse.xtext.resource.IEObjectDescription;
+import org.osate.aadl2.Aadl2Package;
 
 public final class AadlSearchQuery implements ISearchQuery {
 	public enum SearchFor {
-		CLASSIFIER, PROPERTY;
+		CLASSIFIER {
+			@Override
+			public EClass declarationEClass() {
+				return Aadl2Package.eINSTANCE.getClassifier();
+			}
+		},
+
+		PROPERTY {
+			@Override
+			public EClass declarationEClass() {
+				return Aadl2Package.eINSTANCE.getProperty();
+			}
+		};
+
+		public abstract EClass declarationEClass();
 	}
 
 	public enum LimitTo {
-		ALL, REFERENCES, DECLARATIONS;
+		ALL {
+			@Override
+			public boolean references() {
+				return true;
+			}
+
+			@Override
+			public boolean declarations() {
+				return true;
+			}
+		},
+
+		REFERENCES {
+			@Override
+			public boolean references() {
+				return true;
+			}
+
+			@Override
+			public boolean declarations() {
+				return false;
+			}
+		},
+
+		DECLARATIONS {
+			@Override
+			public boolean references() {
+				return false;
+			}
+
+			@Override
+			public boolean declarations() {
+				return true;
+			}
+		};
+
+		public abstract boolean references();
+		public abstract boolean declarations();
 	}
 
 	private final String substring;
@@ -33,29 +85,27 @@ public final class AadlSearchQuery implements ISearchQuery {
 
 	@Override
 	public IStatus run(final IProgressMonitor monitor) throws OperationCanceledException {
-		final AadlFinder aadlFinder = AadlFinder.getInstance();
+		// TDOD Make a proper task out of this!
 
 		System.out.println("Searching for " + searchFor + " " + limitTo + " \"" + substring + "\"; scope = "
 				+ scope);
 
-		System.out.println("== Classifier Declarations ==");
-		for (final IEObjectDescription objectDesc : aadlFinder.getAllClassifiersInWorkspace()) {
-			for (final String segment : objectDesc.getName().getSegments()) {
-				System.out.print("[" + segment + "]");
-			}
-			System.out.println(" -- " + objectDesc.getEObjectURI());
+		final AadlFinder aadlFinder = AadlFinder.getInstance();
+
+		if (limitTo.declarations()) {
+			System.out.println("== Declarations ==");
+			aadlFinder.getAllObjectsOfTypeInWorkspace(searchFor.declarationEClass(), objectDesc -> {
+				for (final String segment : objectDesc.getName().getSegments()) {
+					System.out.print("[" + segment + "]");
+				}
+				System.out.println(" -- " + objectDesc.getEObjectURI());
+			});
 		}
 
-		System.out.println("== Property Declarations ==");
-		for (final IEObjectDescription objectDesc : aadlFinder.getAllPropertyDeclarationsInWorkspace()) {
-			for (final String segment : objectDesc.getName().getSegments()) {
-				System.out.print("[" + segment + "]");
-			}
-			System.out.println(" -- " + objectDesc.getEObjectURI());
+		if (limitTo.references()) {
+			System.out.println("== References ==");
+//			aadlFinder.doStuff();
 		}
-
-		System.out.println("== References ==");
-		aadlFinder.doStuff();
 
 		return Status.OK_STATUS;
 	}
