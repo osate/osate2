55a0467d3b9407baff7b236b8a8bc86b562fc471
aarong <aarong@sei.cmu.edu>
2018-10-23 15:42:18 -0400

    Fleshed out the AadlSearchResult and AadlSearchResultEvent.  REady to start on the results view.

--- core/org.osate.ui/src/org/osate/search/AadlSearchResult.java
+++ core/org.osate.ui/src/org/osate/search/AadlSearchResult.java
@@ -1,14 +1,21 @@
 package org.osate.search;
 
 import org.eclipse.core.runtime.ListenerList;
+import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.search.ui.ISearchQuery;
 import org.eclipse.search.ui.ISearchResult;
 import org.eclipse.search.ui.ISearchResultListener;
+import org.eclipse.xtext.resource.IEObjectDescription;
+import org.eclipse.xtext.resource.IReferenceDescription;
+import org.osate.ui.OsateUiPlugin;
 
 public final class AadlSearchResult implements ISearchResult {
 	private final AadlSearchQuery searchQuery;
 	private final ListenerList<ISearchResultListener> listeners = new ListenerList<>();
+
+	private int numFoundDeclarations = 0;
+	private int numFoundReferences = 0;
 
 	public AadlSearchResult(final AadlSearchQuery searchQuery) {
 		this.searchQuery = searchQuery;
@@ -26,9 +33,7 @@ public final class AadlSearchResult implements ISearchResult {
 
 	// TODO Use this eventually
 	@SuppressWarnings("unused")
-	private void notifyListeners() {
-		final AadlSearchResultEvent event = new AadlSearchResultEvent(this);
-
+	private void notifyListeners(final AadlSearchResultEvent event) {
 		for (ISearchResultListener listener : listeners) {
 			listener.searchResultChanged(event);
 		}
@@ -36,20 +41,20 @@ public final class AadlSearchResult implements ISearchResult {
 
 	@Override
 	public String getLabel() {
-		// TODO Auto-generated method stub
-		return null;
+		// TODO Only show the counts for things that were explicitly searched for
+		return "Here is the AadlSearchResultLabel: " + searchQuery.getLabel() + "; Found " + numFoundDeclarations
+				+ " matching declarations and " + numFoundReferences + " matching references";
 	}
 
 	@Override
 	public String getTooltip() {
-		// TODO Auto-generated method stub
-		return null;
+		// TODO This is junk
+		return "Here is the AadlSearchResultToolTip";
 	}
 
 	@Override
 	public ImageDescriptor getImageDescriptor() {
-		// TODO Auto-generated method stub
-		return null;
+		return OsateUiPlugin.getImageDescriptor("icons/aadl.gif");
 	}
 
 	@Override
@@ -57,4 +62,13 @@ public final class AadlSearchResult implements ISearchResult {
 		return searchQuery;
 	}
 
+	public void addFoundDeclaration(final ResourceSet resourceSet, final IEObjectDescription objDesc) {
+		numFoundDeclarations += 1;
+		notifyListeners(new FoundDeclarationEvent(this, resourceSet, objDesc));
+	}
+
+	public void addFoundReference(final ResourceSet resourceSet, final IReferenceDescription refDesc) {
+		numFoundReferences += 1;
+		notifyListeners(new FoundReferenceEvent(this, resourceSet, refDesc));
+	}
 }
