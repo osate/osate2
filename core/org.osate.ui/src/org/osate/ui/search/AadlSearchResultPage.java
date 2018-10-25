457bab67f2d20d5fdaebbd7fe13442d64ebabf99
aarong <aarong@sei.cmu.edu>
2018-10-25 15:18:27 -0400

    Starting adding a tree to the results viewer.  primitive right now so that I can get the input changing correct.

--- core/org.osate.ui/src/org/osate/ui/search/AadlSearchResultPage.java
+++ core/org.osate.ui/src/org/osate/ui/search/AadlSearchResultPage.java
@@ -1,14 +1,27 @@
 package org.osate.ui.search;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.jface.layout.TreeColumnLayout;
+import org.eclipse.jface.viewers.ColumnWeightData;
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.search.ui.ISearchResult;
 import org.eclipse.search.ui.ISearchResultListener;
 import org.eclipse.search.ui.ISearchResultPage;
 import org.eclipse.search.ui.ISearchResultViewPart;
 import org.eclipse.search.ui.SearchResultEvent;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeColumn;
 import org.eclipse.ui.IActionBars;
 import org.eclipse.ui.IMemento;
 import org.eclipse.ui.PartInitException;
@@ -21,10 +34,18 @@ import org.osate.search.FoundDeclarationEvent;
 import org.osate.search.FoundReferenceEvent;
 
 public final class AadlSearchResultPage implements ISearchResultListener, ISearchResultPage {
+	private static final Object[] NO_CHILDREN = new Object[0];
+
 	private AadlSearchResult searchResult;
-	private Composite root;
 	private IPageSite pageSite;
 	private String id;
+
+	private Composite root;
+	private TreeViewer treeViewer;
+
+	private final List<URI> declsList = new ArrayList<>();
+	private final List<URI> refsList = new ArrayList<>();
+	private final List[] inputNode = { declsList, refsList };
 
 	public AadlSearchResultPage() {
 		super();
@@ -43,7 +64,21 @@ public final class AadlSearchResultPage implements ISearchResultListener, ISearc
 	@Override
 	public void createControl(final Composite parent) {
 		root = new Composite(parent, SWT.NULL);
+		final Tree tree = new Tree(root, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
+		tree.setLayoutData(new GridData(GridData.FILL_BOTH));
+		tree.setLinesVisible(false);
+		tree.setHeaderVisible(false);
+		tree.setFont(parent.getFont());
 
+		final TreeColumn column = new TreeColumn(tree, SWT.LEFT);
+		final TreeColumnLayout treeLayout = new TreeColumnLayout();
+		treeLayout.setColumnData(column, new ColumnWeightData(1, true));
+		root.setLayout(treeLayout);
+
+		treeViewer = new TreeViewer(tree);
+		treeViewer.setLabelProvider(new TreeLabelProvider());
+		treeViewer.setContentProvider(new TreeContentProvider());
+		treeViewer.setInput(inputNode);
 	}
 
 	@Override
@@ -75,6 +110,7 @@ public final class AadlSearchResultPage implements ISearchResultListener, ISearc
 
 	@Override
 	public void setInput(final ISearchResult search, final Object uiState) {
+		System.out.println("setInput: " + search);
 		if (search == null) {
 			// clean up old search
 			if (searchResult != null) {
@@ -130,13 +166,77 @@ public final class AadlSearchResultPage implements ISearchResultListener, ISearc
 					System.out.print("[" + segment + "]");
 				}
 				System.out.println(" -- " + uri);
+
+				declsList.add(uri);
 			} else if (e instanceof FoundReferenceEvent) {
 				final IReferenceDescription refDesc = ((FoundReferenceEvent) e).getReferenceDescription();
 				final URI sourceURI = refDesc.getSourceEObjectUri();
 				System.out.println("Found reference in " + sourceURI.lastSegment() + ": " + sourceURI + " -> "
 						+ refDesc.getTargetEObjectUri());
+
+				refsList.add(sourceURI);
 			}
+
+			Display.getDefault().asyncExec(() -> treeViewer.refresh());
 		}
 	}
 
+	private final class TreeContentProvider implements ITreeContentProvider {
+		@Override
+		public Object[] getElements(final Object inputElement) {
+			return (Object[]) inputElement;
+		}
+
+		@Override
+		public Object[] getChildren(final Object parentElement) {
+			if (parentElement instanceof List) {
+				return ((List) parentElement).toArray();
+			} else {
+				return NO_CHILDREN;
+			}
+		}
+
+		@Override
+		public Object getParent(final Object element) {
+			if (element instanceof List) {
+				return inputNode;
+			} else if (element instanceof URI) {
+				if (declsList.contains(element)) {
+					return declsList;
+				} else if (refsList.contains(element)) {
+					return refsList;
+				}
+			}
+			return null;
+		}
+
+		@Override
+		public boolean hasChildren(final Object element) {
+			if (element instanceof List) {
+				return true;
+			}
+			return false;
+		}
+	}
+
+	private final class TreeLabelProvider extends LabelProvider {
+		@Override
+		public Image getImage(final Object element) {
+			// TODO Auto-generated method stub
+			return null;
+		}
+
+		@Override
+		public String getText(final Object element) {
+			if (element == refsList) {
+				return "References";
+			} else if (element == declsList) {
+				return "Declarations";
+			} else if (element instanceof URI) {
+				return ((URI) element).toString();
+			}
+			return "NO LABEL";
+		}
+
+	}
 }
