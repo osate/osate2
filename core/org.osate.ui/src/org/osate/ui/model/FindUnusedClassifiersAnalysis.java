5d8cb0358d5df5e04a03d5bda360e5a8c183012f
aarong <aarong@sei.cmu.edu>
2018-11-09 15:11:44 -0500

    Added enhanced version of AadlFinder (will also be merged in later from another branch).  updated the use of the progress monitor in the analysis.

--- core/org.osate.ui/src/org/osate/ui/model/FindUnusedClassifiersAnalysis.java
+++ core/org.osate.ui/src/org/osate/ui/model/FindUnusedClassifiersAnalysis.java
@@ -1,19 +1,26 @@
 package org.osate.ui.model;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.resources.WorkspaceJob;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.SubMonitor;
 import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.osate.aadl2.Aadl2Package;
 import org.osate.aadl2.AadlPackage;
 import org.osate.aadl2.modelsupport.resources.OsateResourceUtil;
+import org.osate.search.AadlFinder;
 
 public final class FindUnusedClassifiersAnalysis {
 
@@ -22,12 +29,14 @@ public final class FindUnusedClassifiersAnalysis {
 	}
 
 	public static void doIt(final Object[] array) {
-		final IFile[] packages = new IFile[array.length];
-		System.arraycopy(array, 0, packages, 0, array.length);
+		final List<IFile> packages = new ArrayList<>(array.length);
+		for (final Object file : array) {
+			packages.add((IFile) file);
+		}
 		doIt(packages);
 	}
 
-	public static void doIt(final IFile[] packages) {
+	public static void doIt(final Collection<IFile> packages) {
 		final Job job = new FindUnusedClassifiersJob(OsateResourceUtil.getResourceSet(), packages);
 		// XXX Make the rule based on the contents of the resource set
 		job.setRule(ResourcesPlugin.getWorkspace().getRoot());
@@ -38,9 +47,9 @@ public final class FindUnusedClassifiersAnalysis {
 
 	private final static class FindUnusedClassifiersJob extends WorkspaceJob {
 		private final ResourceSet resourceSet;
-		private final IFile[] packageFiles;
+		private final Collection<IFile> packageFiles;
 
-		public FindUnusedClassifiersJob(final ResourceSet resourceSet, final IFile[] packageFiles) {
+		public FindUnusedClassifiersJob(final ResourceSet resourceSet, final Collection<IFile> packageFiles) {
 			super("Find unused classifiers");
 			this.resourceSet = resourceSet;
 			this.packageFiles = packageFiles;
@@ -48,34 +57,20 @@ public final class FindUnusedClassifiersAnalysis {
 
 		@Override
 		public IStatus runInWorkspace(IProgressMonitor monitor) throws CoreException {
-			if (monitor == null) {
-				monitor = new NullProgressMonitor();
-			}
+			final List<URI> declarationURIs = new ArrayList<>();
+			AadlFinder.getInstance().getAllObjectsOfTypeInCollection(Aadl2Package.eINSTANCE.getClassifier(),
+					packageFiles, objDesc -> declarationURIs.add(objDesc.getEObjectURI()));
 
-			monitor.beginTask("Scanning", packageFiles.length);
-			try {
-				for (final IFile packageFile : packageFiles) {
-					final String packageName = getPackageName(packageFile);
-					monitor.subTask(packageName);
-
-					System.out.println(packageName);
-
-					try {
-						Thread.sleep(2000L);
-					} catch (final InterruptedException e) {
-						// who cares
-					}
-
-					if (monitor.isCanceled()) {
-						throw new OperationCanceledException();
-					}
-
-					monitor.worked(1);
+			final SubMonitor subMonitor = SubMonitor.convert(monitor, declarationURIs.size());
+			for (final URI declURI : declarationURIs) {
+				if (subMonitor.isCanceled()) {
+					throw new OperationCanceledException();
 				}
-			} finally {
-				monitor.done();
+
+				final SubMonitor nestedMonitor = subMonitor.split(1);
+
 			}
-			// TODO Auto-generated method stub
+
 			return Status.OK_STATUS;
 		}
 
