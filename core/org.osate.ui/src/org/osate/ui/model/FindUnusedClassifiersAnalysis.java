bc0ad7fd3371d531dcf402132651f0f87ead7279
aarong <aarong@sei.cmu.edu>
2018-11-12 14:32:32 -0500

    Have the basic analysis written.  Need to add the markers

--- core/org.osate.ui/src/org/osate/ui/model/FindUnusedClassifiersAnalysis.java
+++ core/org.osate.ui/src/org/osate/ui/model/FindUnusedClassifiersAnalysis.java
@@ -2,7 +2,9 @@ package org.osate.ui.model;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.ResourcesPlugin;
@@ -17,10 +19,12 @@ import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.xtext.resource.IResourceDescription;
 import org.osate.aadl2.Aadl2Package;
 import org.osate.aadl2.AadlPackage;
 import org.osate.aadl2.modelsupport.resources.OsateResourceUtil;
 import org.osate.search.AadlFinder;
+import org.osate.search.AadlFinder.ResourceConsumer;
 
 public final class FindUnusedClassifiersAnalysis {
 
@@ -56,19 +60,60 @@ public final class FindUnusedClassifiersAnalysis {
 		}
 
 		@Override
-		public IStatus runInWorkspace(IProgressMonitor monitor) throws CoreException {
+		public IStatus runInWorkspace(final IProgressMonitor monitor) throws CoreException {
+			final SubMonitor subMonitor = SubMonitor.convert(monitor, 3);
+
+			// (1) Get all the declarations in the packages
 			final List<URI> declarationURIs = new ArrayList<>();
 			AadlFinder.getInstance().getAllObjectsOfTypeInCollection(Aadl2Package.eINSTANCE.getClassifier(),
 					packageFiles, objDesc -> declarationURIs.add(objDesc.getEObjectURI()));
 
-			final SubMonitor subMonitor = SubMonitor.convert(monitor, declarationURIs.size());
-			for (final URI declURI : declarationURIs) {
-				if (subMonitor.isCanceled()) {
+			subMonitor.worked(1);
+			if (subMonitor.isCanceled()) {
+				throw new OperationCanceledException();
+			}
+
+			// (2) Find all the uses in the workspace
+			final Set<URI> referencedThings = new HashSet<>();
+			AadlFinder.getInstance().getAllReferencesToTypeInWorkspace(
+				new ResourceConsumer<IResourceDescription>() {
+						private SubMonitor subMon;
+
+						@Override
+						protected void begin(final int count) {
+							subMon = subMonitor.split(1);
+							subMon.beginTask("Scanning references", count);
+						}
+
+						@Override
+						protected void inScope(final IResourceDescription rsrcDesc) {
+							subMon.worked(1);
+							if (subMon.isCanceled()) {
+								throw new OperationCanceledException();
+							}
+						}
+
+						@Override
+						protected void skipped(final IResourceDescription rsrcDesc) {
+							subMon.worked(1);
+						}
+					},
+
+					refDesc -> referencedThings.add(refDesc.getTargetEObjectUri()));
+
+			// (3) See if the declarations in the set of referenced things
+			final SubMonitor lastMonitor = subMonitor.split(1);
+			lastMonitor.beginTask("Looking for uses", declarationURIs.size());
+			for (final URI classifierDecl : declarationURIs) {
+				if (lastMonitor.isCanceled()) {
 					throw new OperationCanceledException();
 				}
 
-				final SubMonitor nestedMonitor = subMonitor.split(1);
+				if (!referencedThings.contains(classifierDecl)) {
+					System.out.println("UNUSED: " + classifierDecl);
+				}
 
+				lastMonitor.internalWorked(1);
 			}
 
 			return Status.OK_STATUS;
