51357010eaa0d8579876f9fd58c166259a097300
Lutz Wrage <lutz.wrage@gmail.com>
2018-12-06 15:35:45 -0500

    Regenerate from ecore model

--- core/org.osate.results/src/org/osate/result/impl/IntegerValueImpl.java
+++ core/org.osate.results/src/org/osate/result/impl/IntegerValueImpl.java
@@ -204,7 +204,7 @@ public class IntegerValueImpl extends ValueImpl implements IntegerValue {
 	public String toString() {
 		if (eIsProxy()) return super.toString();
 
-		StringBuffer result = new StringBuffer(super.toString());
+		StringBuilder result = new StringBuilder(super.toString());
 		result.append(" (value: ");
 		result.append(value);
 		result.append(", unit: ");
