a5f20eba4d4dc4bc58146ff8673d287c4323fa9e
Lutz Wrage <lutz.wrage@gmail.com>
2018-12-06 11:02:50 -0500

    Regenerate code from AADL2 metamodel

--- core/org.osate.aadl2/src/org/osate/aadl2/impl/CommentImpl.java
+++ core/org.osate.aadl2/src/org/osate/aadl2/impl/CommentImpl.java
@@ -187,7 +187,7 @@ public class CommentImpl extends ElementImpl implements Comment {
 			return super.toString();
 		}
 
-		StringBuffer result = new StringBuffer(super.toString());
+		StringBuilder result = new StringBuilder(super.toString());
 		result.append(" (body: ");
 		result.append(body);
 		result.append(')');
