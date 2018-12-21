51ed52a9d90729bbf875e29235f9d38a7396292f
Joe Seibel <1824701+joeseibel@users.noreply.github.com>
2018-12-21 13:57:07 -0500

    Visualize all projects connected to a selected project.

--- core/org.osate.ui/src/org/osate/ui/projectvisualization/FocusVisualizationHandler.java
+++ core/org.osate.ui/src/org/osate/ui/projectvisualization/FocusVisualizationHandler.java
@@ -3,6 +3,7 @@ package org.osate.ui.projectvisualization;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.ui.IWorkingSet;
 import org.eclipse.ui.PartInitException;
@@ -19,7 +20,13 @@ public class FocusVisualizationHandler extends AbstractHandler {
 		try {
 			ProjectVisualizationView view = (ProjectVisualizationView) HandlerUtil.getActiveWorkbenchWindow(event)
 					.getActivePage().showView(ProjectVisualizationView.ID);
-			view.setScope((IWorkingSet) HandlerUtil.getCurrentStructuredSelection(event).getFirstElement());
+//			view.setScope((IWorkingSet) HandlerUtil.getCurrentStructuredSelection(event).getFirstElement());
+			Object selection = HandlerUtil.getCurrentStructuredSelection(event).getFirstElement();
+			if (selection instanceof IWorkingSet) {
+				view.setScope((IWorkingSet) selection);
+			} else {
+				view.setScope((IProject) selection);
+			}
 		} catch (PartInitException e) {
 			Status status = new Status(e.getStatus().getSeverity(), OsateUiPlugin.PLUGIN_ID, e.getLocalizedMessage(),
 					e);
