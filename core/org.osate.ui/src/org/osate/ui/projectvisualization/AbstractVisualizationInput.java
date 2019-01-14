88818fa0081ae631d11c52b07224132768473f5a
Joe Seibel <1824701+joeseibel@users.noreply.github.com>
2019-01-14 16:52:41 -0500

    Refactored view to put project and package code in separate classes.

---
+++ core/org.osate.ui/src/org/osate/ui/projectvisualization/AbstractVisualizationInput.java
@@ -0,0 +1,35 @@
+package org.osate.ui.projectvisualization;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+abstract class AbstractVisualizationInput<T> {
+	protected final List<T> scopedElements;
+
+	protected AbstractVisualizationInput(List<T> scopedElements) {
+		this.scopedElements = Collections.unmodifiableList(scopedElements);
+	}
+
+	abstract Object[] getGraphElements();
+
+	abstract Object[] getConnectedTo(Object entity);
+
+	abstract String getText(Object element);
+
+	boolean isInScope(Object entity) {
+		return scopedElements.contains(entity);
+	}
+
+	protected static <T> void traverseDependencies(T start, Set<T> visited, Predicate<T> filter,
+			Function<T, Iterable<T>> getDependencies) {
+		for (T dependency : getDependencies.apply(start)) {
+			if (filter.test(dependency) && !visited.contains(dependency)) {
+				visited.add(dependency);
+				traverseDependencies(dependency, visited, filter, getDependencies);
+			}
+		}
+	}
+}
