7b4dabff77f1eaa33711594cefc973ad282e63f0
aarong <aarong@sei.cmu.edu>
2019-01-15 11:16:04 -0500

    Fixed FeatureGroupTypeImpl.getAllFeatures() to not add the features of both a feature group and its inverse.  Fixed to visit along the inverse type if there is no super type.  Fixed ClassifierInfoView to NOT sort the features of a feature group type because order matters.

--- core/org.osate.ui/src/org/osate/ui/views/ClassifierInfoView.java
+++ core/org.osate.ui/src/org/osate/ui/views/ClassifierInfoView.java
@@ -554,7 +554,7 @@ public final class ClassifierInfoView extends ViewPart implements ISelectionList
 
 	public SectionNode createFeatureGroupFeaturesSectionNode(final FeatureGroupType fgt, final List<Feature> members) {
 		final boolean isInverted = (fgt.getInverse() != null) && fgt.getOwnedFeatures().isEmpty();
-		Collections.sort(members, MEMBER_COMPARATOR);
+//		Collections.sort(members, MEMBER_COMPARATOR);
 		final List<MemberNode> memberNodes = new ArrayList<>();
 		for (final Feature member : members) {
 			memberNodes.add(createMemberNode(fgt, isInverted, member, ClassifierInfoView::getRefinedFeature));
