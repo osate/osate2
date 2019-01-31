a18778041afb946ab81ffe709c929aa86db6639c
Lutz Wrage <lutz.wrage@gmail.com>
2019-01-31 13:22:58 -0500

    Check data port label against data type label

--- analyses/org.osate.analysis.security/src/org/osate/analysis/security/SecurityLabelChecker.java
+++ analyses/org.osate.analysis.security/src/org/osate/analysis/security/SecurityLabelChecker.java
@@ -7,6 +7,8 @@ import org.osate.aadl2.NamedElement;
 import org.osate.aadl2.instance.ConnectionInstance;
 import org.osate.aadl2.instance.ConnectionKind;
 import org.osate.aadl2.instance.ConnectionReference;
+import org.osate.aadl2.instance.FeatureCategory;
+import org.osate.aadl2.instance.FeatureInstance;
 import org.osate.aadl2.instance.FlowSpecificationInstance;
 import org.osate.aadl2.instance.util.InstanceSwitch;
 import org.osate.aadl2.modelsupport.errorreporting.AnalysisErrorReporterManager;
@@ -73,6 +75,21 @@ public class SecurityLabelChecker extends AadlProcessingSwitch {
 				return DONE;
 			}
 
+			/**
+			 * Check feature for security policy compliance:
+			 * Security labels of feature and associated data type must conform
+			 * to policy.
+			 */
+			@Override
+			public String caseFeatureInstance(FeatureInstance fi) {
+				if (fi.getCategory() == FeatureCategory.DATA_PORT) {
+					NamedElement data = fi.getFeature().getAllClassifier();
+
+					return checkValid(fi, data, fi);
+				}
+				return DONE;
+			}
+
 			protected String checkValid(Element element, NamedElement src, NamedElement dst) {
 				if (src == null || dst == null) {
 					return DONE;
@@ -85,12 +102,12 @@ public class SecurityLabelChecker extends AadlProcessingSwitch {
 						SecurityLabel sl = srcLabel.get();
 						SecurityLabel dl = dstLabel.get();
 						if (!policy.allows(Access.WRITE, sl, dl)) {
-							error(element, "Security policy violation: Write access from label " + sl.toString()
-									+ " to label " + dl.toString());
+							error(element, "Security policy violation: Write access from " + sl.toString() + " to "
+									+ dl.toString());
 						}
 						if (!policy.allows(Access.READ, dl, sl)) {
-							error(element, "Security policy violation: Read access from label " + dl.toString()
-									+ " to label " + sl.toString());
+							error(element, "Security policy violation: Read access from " + dl.toString() + " to "
+									+ sl.toString());
 						}
 					} else {
 						if (!srcLabel.isPresent() && dstLabel.isPresent()) {
