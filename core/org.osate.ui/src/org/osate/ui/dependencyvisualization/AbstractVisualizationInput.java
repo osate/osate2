c8ba9680c7528775dce3156bdbae3f76a7173acc
Joe Seibel <1824701+joeseibel@users.noreply.github.com>
2019-02-04 14:14:55 -0500

    Created interface and empty implementation for visualization input.

--- core/org.osate.ui/src/org/osate/ui/dependencyvisualization/AbstractVisualizationInput.java
+++ core/org.osate.ui/src/org/osate/ui/dependencyvisualization/AbstractVisualizationInput.java
@@ -5,22 +5,16 @@ import java.util.List;
 import java.util.Set;
 import java.util.function.Function;
 import java.util.function.Predicate;
-import java.util.stream.Stream;
 
-abstract class AbstractVisualizationInput<T> {
+abstract class AbstractVisualizationInput<T> implements IVisualizationInput {
 	protected final List<T> scopedElements;
 
 	protected AbstractVisualizationInput(List<T> scopedElements) {
 		this.scopedElements = Collections.unmodifiableList(scopedElements);
 	}
 
-	abstract Object[] getGraphElements();
-
-	abstract Object[] getConnectedTo(Object entity);
-
-	abstract Stream<T> getConnectedToBothDirections(Object entity);
-
-	boolean isInScope(Object entity) {
+	@Override
+	public boolean isInScope(Object entity) {
 		return scopedElements.contains(entity);
 	}
 
