658ed4060639eb545529f8cc3e810699501aa0b4
aarong <aarong@sei.cmu.edu>
2019-02-06 13:53:07 -0500

    Added a basic editing monitor that processes selected elements, but not text selection yet.  The selected items show up as "invisible" items in the context.  Still need to address tihs problem.

---
+++ core/org.osate.mylyn.aadl.ui/src/org/osate/mylyn/aadl/ui/AadlBridgePlugin.java
@@ -0,0 +1,56 @@
+package org.osate.mylyn.aadl.ui;
+
+import org.eclipse.mylyn.context.ui.IContextUiStartup;
+import org.eclipse.mylyn.monitor.ui.MonitorUi;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+public final class AadlBridgePlugin extends AbstractUIPlugin {
+	private static AadlBridgePlugin INSTANCE = null;
+
+	private AadlEditingMonitor aadlEditingMonitor = null;
+
+	public AadlBridgePlugin() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void start(final BundleContext context) throws Exception {
+		super.start(context);
+		INSTANCE = this;
+	}
+
+	/**
+	 * Returns the shared instance.
+	 */
+	public static AadlBridgePlugin getDefault() {
+		return INSTANCE;
+	}
+
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		lazyStop();
+		super.stop(context);
+		INSTANCE = null;
+	}
+
+	private void lazyStart() {
+		aadlEditingMonitor = new AadlEditingMonitor();
+		MonitorUi.getSelectionMonitors().add(aadlEditingMonitor);
+	}
+
+	private void lazyStop() {
+		if (aadlEditingMonitor != null) {
+			MonitorUi.getSelectionMonitors().remove(aadlEditingMonitor);
+		}
+	}
+
+	public static final class Startup implements IContextUiStartup {
+		@Override
+		public void lazyStartup() {
+			AadlBridgePlugin.getDefault().lazyStart();
+		}
+
+	}
+
+}
