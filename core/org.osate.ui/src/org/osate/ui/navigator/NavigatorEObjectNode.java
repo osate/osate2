e75cb299b0bf4f3d283080778c88a613fb28357e
Joe Seibel <1824701+joeseibel@users.noreply.github.com>
2019-03-28 11:24:20 -0400

    Load EObject in abstract handler instead of implementing IAdaptable.

--- core/org.osate.ui/src/org/osate/ui/navigator/NavigatorEObjectNode.java
+++ core/org.osate.ui/src/org/osate/ui/navigator/NavigatorEObjectNode.java
@@ -2,14 +2,12 @@ package org.osate.ui.navigator;
 
 import java.util.Objects;
 
-import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EClass;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 
-public class NavigatorEObjectNode implements IAdaptable {
+public class NavigatorEObjectNode {
 	private final URI uri;
 	private final EClass eClass;
 
@@ -24,17 +22,6 @@ public class NavigatorEObjectNode implements IAdaptable {
 	
 	public EClass getEClass() {
 		return eClass;
-	}
-	
-	@Override
-	public <T> T getAdapter(Class<T> adapter) {
-		if (EObject.class.isAssignableFrom(adapter)) {
-			EObject eObject = new ResourceSetImpl().getEObject(uri, true);
-			if (adapter.isInstance(eObject)) {
-				return adapter.cast(eObject);
-			}
-		}
-		return null;
 	}
 	
 	@Override
