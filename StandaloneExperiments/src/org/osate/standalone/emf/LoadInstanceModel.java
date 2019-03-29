c53e1e6ce605fe4eb5278443405d649117fa7e1a
aarong <aarong@sei.cmu.edu>
2019-03-29 15:47:43 -0400

    Started simple program to load an instance model file

---
+++ StandaloneExperiments/src/org/osate/standalone/emf/LoadInstanceModel.java
@@ -0,0 +1,30 @@
+package org.osate.standalone.emf;
+
+import java.io.IOException;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.osate.aadl2.Aadl2Package;
+import org.osate.aadl2.util.Aadl2ResourceFactoryImpl;
+
+public final class LoadInstanceModel {
+	public static void main(String[] args) {
+		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("aaxl2", new Aadl2ResourceFactoryImpl());
+		Aadl2Package.eINSTANCE.eClass();
+
+		final ResourceSet rs = new ResourceSetImpl();
+		final Resource resource = rs.getResource(URI.createURI(args[0]), true);
+
+		try {
+			resource.load(null);
+			for (final EObject eObj : resource.getContents()) {
+				System.out.println(eObj);
+			}
+		} catch (final IOException e) {
+			System.err.println("ERROR LOADING RESOURCE: " + e.getMessage());
+		}
+	}
+}
