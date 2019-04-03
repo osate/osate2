954c7de5ae5fb22cc9ddb8b8b5539479050007f6
aarong <aarong@sei.cmu.edu>
2019-04-03 13:03:53 -0400

    Created a second project for testing the XText/declarative model loading

--- standalone_tests/LoadDeclarativeModelToo/src/org/osate/standalone/emf/LoadDeclarativeModelToo.java
+++ standalone_tests/LoadDeclarativeModelToo/src/org/osate/standalone/emf/LoadDeclarativeModelToo.java
@@ -5,20 +5,22 @@ import java.io.IOException;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
-import org.osate.aadl2.Aadl2Package;
+import org.eclipse.xtext.resource.XtextResourceSet;
 import org.osate.aadl2.instance.InstancePackage;
 import org.osate.aadl2.util.Aadl2ResourceFactoryImpl;
+import org.osate.xtext.aadl2.Aadl2StandaloneSetup;
+
+import com.google.inject.Injector;
 
 public final class LoadDeclarativeModelToo {
 	public static void main(String[] args) {
-		final Inj
+		final Injector injector = new Aadl2StandaloneSetup().createInjectorAndDoEMFRegistration();
 
 		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("aaxl2", new Aadl2ResourceFactoryImpl());
 		InstancePackage.eINSTANCE.eClass();
 
-		final ResourceSet rs = new ResourceSetImpl();
+//		final ResourceSet rs = new ResourceSetImpl();
+		XtextResourceSet rs = injector.getInstance(XtextResourceSet.class);
 
 		final Resource[] resources = new Resource[args.length];
 		for (int i = 0; i < args.length; i++ ) {
