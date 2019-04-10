1925ac0096b393c707d4a1fc12f0cfa9c41c35aa
Sam Procter <sprocter@sei.cmu.edu>
2019-04-10 16:53:40 -0400

    Baseline version of tree testing is working

--- alisa/org.osate.alisa2.view.tests/src/test/java/org/osate/alisa2/view/tests/FundamentalsTreeTest.java
+++ alisa/org.osate.alisa2.view.tests/src/test/java/org/osate/alisa2/view/tests/FundamentalsTreeTest.java
@@ -4,26 +4,34 @@ import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.sirius.tests.support.api.TestsUtil;
 import org.eclipse.sirius.tests.support.api.TreeTestCase;
 import org.eclipse.sirius.tree.DTree;
-import org.eclipse.sirius.tree.description.TreeDescription;
 import org.eclipse.sirius.tree.ui.business.api.helper.TreeUIHelper;
-import org.eclipse.sirius.tree.ui.tools.api.editor.DTreeEditor;
 import org.eclipse.sirius.ui.business.api.dialect.DialectUIManager;
+import org.eclipse.sirius.ui.tools.internal.editor.AbstractDTreeEditor;
 import org.eclipse.swt.widgets.Tree;
 import org.eclipse.ui.IEditorPart;
 
 public class FundamentalsTreeTest extends TreeTestCase {
 
-	private static final String SEMANTIC_MODEL_PATH = "/org.osate.alisa2.model/model/safe2.ecore";
-	private static final String MODELER_PATH = "/org.osate.alisa2.view/description/view.odesign";
-	private static final String SESSION_PATH = "/org.osate.alisa2.view/description/representations.aird";
+	private static final String PLUGIN_ID = "org.osate.alisa2.view.tests";
 
-	private static final String TREE_DESCRIPTION_ID = "HackOverview";
+	private static final String AIRD_FILE = "representations.aird";
+
+	private static final String DATA_UNIT_DIR = PLUGIN_ID + "/src/test/resources/BoundaryDiagramTests/";
+
+	private static final String SEMANTIC_MODEL_PATH = "org.osate.alisa2.model/model/safe2.ecore";
+	private static final String MODELER_PATH = "org.osate.alisa2.view/description/view.odesign";
+
+	private IEditorPart openedEditor;
+
+	private static final String TREE_REPRESENTATION_NAME = "Containment";
 
 	@Override
 	protected void setUp() throws Exception {
 		super.setUp();
-
-		genericSetUp(SEMANTIC_MODEL_PATH, MODELER_PATH, SESSION_PATH);
+		genericSetUp(SEMANTIC_MODEL_PATH, MODELER_PATH, DATA_UNIT_DIR + AIRD_FILE);
+		DTree tree = (DTree) getRepresentations(TREE_REPRESENTATION_NAME).toArray()[0];
+		openedEditor = DialectUIManager.INSTANCE.openEditor(session, tree, new NullProgressMonitor());
+		TestsUtil.synchronizationWithUIThread();
 	}
 
 	@Override
@@ -31,18 +39,21 @@ public class FundamentalsTreeTest extends TreeTestCase {
 		super.tearDown();
 	}
 
-// Adapted from org.eclipse.sirius.tests.api.tools.TreeItemRefreshTest.testRefreshElement()
 	public void testFundamentalsTree() {
-		final TreeDescription treeDescription = find(TREE_DESCRIPTION_ID);
-		DTree newTree = (DTree) getRepresentations(TREE_DESCRIPTION_ID).toArray()[0];
-		final TreeDescription desc = newTree.getDescription();
-		IEditorPart openedEditor = DialectUIManager.INSTANCE.openEditor(session, newTree, new NullProgressMonitor());
-		TestsUtil.synchronizationWithUIThread();
-		DTreeEditor treeEditor = (DTreeEditor) openedEditor;
-		Tree tree = (Tree) treeEditor.getControl();
+		AbstractDTreeEditor treeEditor = (AbstractDTreeEditor) openedEditor;
+		Tree tree = treeEditor.getTableViewer().getTreeViewer().getTree();
 		String currentHtml = TreeUIHelper.toContentHTMl(tree);
-
-		assertEquals("yuss.", currentHtml);
+		// @formatter:off
+		assertEquals("<table>\n" +
+				"<tr><td>patient</td></tr>\n" +
+				"<tr><td>clinician</td></tr>\n" +
+				"<tr><td>pulseOx</td></tr>\n" +
+				"<tr><td>appLogic</td></tr>\n" +
+				"<tr><td></td></tr>\n" +
+				"<tr><td>appDisplay</td></tr>\n" +
+				"<tr><td></td></tr>\n" +
+				"</table>", currentHtml);
+		// @formatter:on
 	}
 
 }
