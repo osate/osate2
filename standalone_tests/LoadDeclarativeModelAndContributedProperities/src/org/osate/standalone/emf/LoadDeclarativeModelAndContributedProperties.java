d526263c88197fbf56a695d63f61e8cbbf946f5c
aarong <aarong@sei.cmu.edu>
2019-05-06 13:34:03 -0400

    Updated test to try and load a file and a predeclared property set. Loading of the property set work, but the linking is broken.

--- standalone_tests/LoadDeclarativeModelAndContributedProperities/src/org/osate/standalone/emf/LoadDeclarativeModelAndContributedProperties.java
+++ standalone_tests/LoadDeclarativeModelAndContributedProperities/src/org/osate/standalone/emf/LoadDeclarativeModelAndContributedProperties.java
@@ -1,7 +1,9 @@
 package org.osate.standalone.emf;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.eclipse.emf.common.util.TreeIterator;
 import org.eclipse.emf.common.util.URI;
@@ -22,14 +24,50 @@ public final class LoadDeclarativeModelAndContributedProperties {
 		final Injector injector = new Aadl2StandaloneSetup().createInjectorAndDoEMFRegistration();
 
 		final XtextResourceSet rs = injector.getInstance(XtextResourceSet.class);
-		final Resource[] resources = new Resource[args.length];
+
+		// Add predeclared resources
+
+		/*
+		 * String fullpath = configElems[j].getDeclaringExtension().getContributor().getName()
+		 * + (path.charAt(0) == '/' ? "" : "/") + path;
+		 *
+		 * result.add(URI.createPlatformPluginURI(fullpath, false));
+		 *
+		 */
+
+		final URI psURI1 = URI.createPlatformPluginURI(
+				"org.osate.workspace/resources/properties/Predeclared_Property_Sets/Programming_Properties.aadl",
+				false);
+
+		final URI psURI2 = URI.createPlatformPluginURI(
+				"org.osate.workspace/resources/properties/Predeclared_Property_Sets/AADL_Project.aadl",
+				false);
+
+		/*
+		 * Map<URI, URI> uriMap = set.getURIConverter().getURIMap();
+		 *
+		 * set.getResource(uri, true);
+		 * uriMap.put(uri, URI.createPlatformResourceURI(uri.path().substring(7), false));
+		 *
+		 */
+
+		final List<Resource> resources = new ArrayList<>();
+
+		final Map<URI, URI> uriMap = rs.getURIConverter().getURIMap();
+		resources.add(rs.getResource(psURI1, true));
+		uriMap.put(psURI1, URI.createPlatformResourceURI(psURI1.path().substring(7), false));
+		resources.add(rs.getResource(psURI2, true));
+		uriMap.put(psURI2, URI.createPlatformResourceURI(psURI2.path().substring(7), false));
+
+		// Load the resources
 		for (int i = 0; i < args.length; i++ ) {
-			resources[i] = rs.getResource(URI.createURI(args[i]), true);
+			resources.add(rs.getResource(URI.createURI(args[i]), true));
 		}
 
 		System.out.println("Loading...");
 		for (final Resource resource : resources) {
 			try {
+				System.out.println("..." + resource.getURI());
 				resource.load(null);
 			} catch (final IOException e) {
 				System.err.println("ERROR LOADING RESOURCE: " + e.getMessage());
