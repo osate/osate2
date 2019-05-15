9bbf2a3874157b4a532d616566284529fdabfdc4
Lutz Wrage <lutz.wrage@gmail.com>
2019-05-15 08:17:42 -0400

    initial version
    generated projects
    dummy grammar
    generated code
    set up plugin dependencies
    set up gitignore

---
+++ core/org.osate.xtext.aadl2/src/org/osate/xtext/aadl2/validation/Aadl2Validator.java
@@ -0,0 +1,14 @@
+package org.osate.xtext.aadl2.validation;
+
+import java.util.List;
+
+import org.eclipse.emf.ecore.EPackage;
+
+public class Aadl2Validator extends Aadl2JavaValidator {
+
+	@Override
+	protected List<EPackage> getEPackages() {
+		return super.getEPackages();
+	}
+
+}
