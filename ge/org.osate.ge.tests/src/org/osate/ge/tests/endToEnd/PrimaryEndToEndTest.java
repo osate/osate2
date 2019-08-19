687a2b175028c9149488689c47474b0ecf5d44e5
Philip Alldredge <pwa0001@uah.edu>
2019-08-19 15:00:03 -0500

    Experiment with new tests for GE.

---
+++ ge/org.osate.ge.tests/src/org/osate/ge/tests/endToEnd/PrimaryEndToEndTest.java
@@ -0,0 +1,20 @@
+package org.osate.ge.tests.endToEnd;
+
+import static org.osate.ge.tests.endToEnd.util.HighLevelFunctions.createAadlProject;
+
+import org.junit.Test;
+
+/**
+ * This class is the primary end to end test. It creates a complete model and exercises a large part of the graphical editor.
+ *
+ */
+public class PrimaryEndToEndTest {
+	@Test
+	public void testGraphicalEditor() {
+		// Create Projects
+		createAadlProject("shared");
+		createAadlProject("hardware");
+		createAadlProject("software");
+		createAadlProject("integrated");
+	}
+}
