e87203ecaaf642683fc44454218f5540633185f1
Joe Seibel <1824701+joeseibel@users.noreply.github.com>
2019-11-05 10:46:38 -0500

    Restored old handler names.

---
+++ analyses/org.osate.analysis.resource.budgets/src/org/osate/analysis/resource/budgets/handlers/DoBoundResourceAnalysis.java
@@ -0,0 +1,8 @@
+package org.osate.analysis.resource.budgets.handlers;
+
+/**
+ * @deprecated Use {@link BoundResourceAnalysisHandler} instead. Will be removed in 2.7.
+ */
+@Deprecated
+public class DoBoundResourceAnalysis extends BoundResourceAnalysisHandler {
+}
---
+++ analyses/org.osate.analysis.resource.budgets/src/org/osate/analysis/resource/budgets/handlers/DoPowerAnalysis.java
@@ -0,0 +1,8 @@
+package org.osate.analysis.resource.budgets.handlers;
+
+/**
+ * @deprecated Use {@link PowerAnalysisHandler} instead. Will be removed in 2.7.
+ */
+@Deprecated
+public class DoPowerAnalysis extends PowerAnalysisHandler {
+}
