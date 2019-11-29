9c13cc009c2b6069d0c80c4884f1f53f3d9c0d18
Lutz Wrage <lutz.wrage@gmail.com>
2019-11-29 12:46:43 -0500

    Rename component implementations with component type

--- core/org.osate.xtext.aadl2.ui/src/org/osate/xtext/aadl2/ui/refactoring/Aadl2DependentElementsCalculator.java
+++ core/org.osate.xtext.aadl2.ui/src/org/osate/xtext/aadl2/ui/refactoring/Aadl2DependentElementsCalculator.java
@@ -6,7 +6,12 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.util.EcoreUtil;
+import org.eclipse.xtext.EcoreUtil2;
 import org.eclipse.xtext.ui.refactoring.impl.DefaultDependentElementsCalculator;
+import org.osate.aadl2.AadlPackage;
+import org.osate.aadl2.ComponentImplementation;
+import org.osate.aadl2.ComponentType;
 
 /**
  * @author lw
@@ -19,20 +24,21 @@ public class Aadl2DependentElementsCalculator extends DefaultDependentElementsCa
 	public List<URI> getDependentElementURIs(EObject baseElement, IProgressMonitor monitor) {
 		List<URI> dependentElementURIs = super.getDependentElementURIs(baseElement, monitor);
 
-//		List<ComponentImplementation> impls = Aadl2GlobalScopeUtil.getAll(baseElement,
-//				Aadl2Package.eINSTANCE.getComponentImplementation());
-//		impls.stream()
-//				.filter((impl) -> impl.getType() == baseElement)
-//				.map((impl) -> EcoreUtil.getURI(impl))
-//				.forEach(dependentElementURIs::add);
-//
-//		AadlPackage pkg = EcoreUtil2.getContainerOfType(baseElement, AadlPackage.class);
-//		impls = EcoreUtil2.getAllContentsOfType(pkg, ComponentImplementation.class);
-//		impls.stream()
-//				.filter((impl) -> impl.getType() == baseElement)
-//				.map((impl) -> EcoreUtil.getURI(impl))
-//				.forEach(dependentElementURIs::add);
+		if (baseElement instanceof ComponentType) {
+//			List<ComponentImplementation> impls = Aadl2GlobalScopeUtil.getAll(baseElement,
+//					Aadl2Package.eINSTANCE.getComponentImplementation());
+//			impls.stream()
+//					.filter((impl) -> impl.getType() == baseElement)
+//					.map((impl) -> EcoreUtil.getURI(impl))
+//					.forEach(dependentElementURIs::add);
 
+			AadlPackage pkg = EcoreUtil2.getContainerOfType(baseElement, AadlPackage.class);
+			List<ComponentImplementation> impls = EcoreUtil2.getAllContentsOfType(pkg, ComponentImplementation.class);
+			impls.stream()
+					.filter((impl) -> impl.getType() == baseElement)
+					.map((impl) -> EcoreUtil.getURI(impl))
+					.forEach(dependentElementURIs::add);
+		}
 		if (monitor.isCanceled()) {
 			throw new OperationCanceledException();
 		}
