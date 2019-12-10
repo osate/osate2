b13bc683cf7978bff5d050385988cc014125cfcb
Lutz Wrage <lutz.wrage@gmail.com>
2019-12-10 16:27:03 -0500

    Delete class QuickSort and replace with library method

--- analyses/org.osate.analysis.resource.management/src/org/osate/analysis/scheduling/inversion/PriorityInversion.java
+++ analyses/org.osate.analysis.resource.management/src/org/osate/analysis/scheduling/inversion/PriorityInversion.java
@@ -26,12 +26,12 @@ package org.osate.analysis.scheduling.inversion;
 import java.util.Iterator;
 import java.util.List;
 
+import org.eclipse.emf.common.util.ECollections;
 import org.eclipse.emf.common.util.EList;
 import org.osate.aadl2.ComponentCategory;
 import org.osate.aadl2.Element;
 import org.osate.aadl2.instance.ComponentInstance;
 import org.osate.aadl2.instance.SystemInstance;
-import org.osate.aadl2.modelsupport.QuickSort;
 import org.osate.aadl2.modelsupport.errorreporting.AnalysisErrorReporterManager;
 import org.osate.aadl2.modelsupport.modeltraversal.ForAllElement;
 import org.osate.aadl2.properties.PropertyNotPresentException;
@@ -44,19 +44,6 @@ import org.osate.xtext.aadl2.properties.util.InstanceModelUtil;
  */
 public class PriorityInversion {
 	private AnalysisErrorReporterManager errManager;
-
-	// Only meant to be used with a list of Threads.
-	private QuickSort periodSort = new QuickSort() {
-		protected int compare(Object obj1, Object obj2) {
-			final double a = GetProperties.getPeriodinMS((ComponentInstance) obj1);
-			final double b = GetProperties.getPeriodinMS((ComponentInstance) obj2);
-			if (a > b)
-				return 1;
-			if (a == b)
-				return 0;
-			return -1;
-		}
-	};
 
 	public PriorityInversion(AnalysisErrorReporterManager errMgr) {
 		errManager = errMgr;
@@ -79,11 +66,12 @@ public class PriorityInversion {
 	public void checkPriorityInversion(ComponentInstance curProcessor) {
 		SystemInstance root = curProcessor.getSystemInstance();
 		final ComponentInstance currentProcessor = curProcessor;
-		EList boundThreads = new ForAllElement() {
+		EList<Element> boundThreads = new ForAllElement() {
 			@Override
 			protected boolean suchThat(Element obj) {
-				if (!InstanceModelUtil.isPeriodicThread((ComponentInstance) obj))
+				if (!InstanceModelUtil.isPeriodicThread((ComponentInstance) obj)) {
 					return false;
+				}
 				List<ComponentInstance> boundProcessor;
 				try {
 					boundProcessor = GetProperties.getActualProcessorBinding((ComponentInstance) obj);
@@ -95,14 +83,25 @@ public class PriorityInversion {
 		}.processPreOrderComponentInstance(root, ComponentCategory.THREAD);
 		// we will sort the thread list by period and
 		// check to make sure the assigned priority is monotonically decreasing
-		periodSort.quickSort(boundThreads);
+		ECollections.sort(boundThreads, (obj1, obj2) -> {
+			final double a = GetProperties.getPeriodinMS((ComponentInstance) obj1);
+			final double b = GetProperties.getPeriodinMS((ComponentInstance) obj2);
+			if (a > b) {
+				return 1;
+			}
+			if (a == b) {
+				return 0;
+			}
+			return -1;
+		});
 		checkIncreasingMonotonicity(boundThreads);
 	}
 
-	public void checkIncreasingMonotonicity(EList threadList) {
-		if (threadList.isEmpty())
+	public void checkIncreasingMonotonicity(List<Element> threadList) {
+		if (threadList.isEmpty()) {
 			return;
-		Iterator it = threadList.iterator();
+		}
+		Iterator<Element> it = threadList.iterator();
 		ComponentInstance curThread = (ComponentInstance) it.next();
 		double currentPeriod = GetProperties.getPeriodinMS(curThread);
 		long periodMaxPriority = GetProperties.getPriority(curThread, 0);
