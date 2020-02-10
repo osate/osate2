816d2e620accc4311252a0cccd358b94db961903
aarong <aarong@sei.cmu.edu>
2020-02-10 15:19:42 -0500

    Fixed InstantiateComopnentHandler to make sure the output directories exist before the instantiation jobs are scheduled.

--- core/org.osate.ui/src/org/osate/ui/handlers/InstantiateComponentHandler.java
+++ core/org.osate.ui/src/org/osate/ui/handlers/InstantiateComponentHandler.java
@@ -24,15 +24,19 @@
 package org.osate.ui.handlers;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResourceRuleFactory;
+import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.resources.WorkspaceJob;
 import org.eclipse.core.runtime.CoreException;
@@ -41,11 +45,13 @@ import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.SubMonitor;
+import org.eclipse.core.runtime.jobs.ISchedulingRule;
 import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.core.runtime.jobs.JobGroup;
 import org.eclipse.core.runtime.jobs.MultiRule;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.xtext.ui.editor.outline.impl.EObjectNode;
@@ -71,73 +77,130 @@ public final class InstantiateComponentHandler extends AbstractMultiJobHandler {
 
 	@Override
 	public Object execute(final ExecutionEvent event) throws ExecutionException {
-		final List<?> selectionAsList = HandlerUtil.getCurrentStructuredSelection(event).toList();
-		final int size = selectionAsList.size();
+		// Get a COPY of the selection for thread safety
+		@SuppressWarnings("unchecked")
+		final List<?> selectionAsList = new ArrayList<>(HandlerUtil.getCurrentStructuredSelection(event).toList());
 
-		/*
-		 * This map is shared by all the jobs to build the set of results. It is created here,
-		 * given to all the jobs, and then forgotten.
-		 */
-		final Map<ComponentImplementation, Result> results = new ConcurrentHashMap<>(size);
-
-		final List<ComponentImplementation> selectedCompImpls = new ArrayList<>(size);
-		final List<IFile> outputFiles = new ArrayList<>(size);
-		for (final Object selection : selectionAsList) {
-			final URI uri;
-			if (selection instanceof EObjectNode) {
-				uri = ((EObjectNode) selection).getEObjectURI();
-			} else if (selection instanceof EObjectURIWrapper) {
-				uri = ((EObjectURIWrapper) selection).getUri();
-			} else {
-				throw new AssertionError("Unexpected selection: " + selection);
-			}
-
-			final IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(uri.segment(1));
-			final ResourceSet resourceSet = resourceSetProvider.get(project);
-			final ComponentImplementation impl = (ComponentImplementation) resourceSet.getEObject(uri, true);
-			selectedCompImpls.add(impl);
-			outputFiles.add(OsateResourceUtil.toIFile(InstantiateModel.getInstanceModelURI(impl)));
-
-			/*
-			 * Init each result as cancelled because if the job is cancelled before it starts, it will never
-			 * add a new result record to the map. This way those jobs that never run are accounted for.
-			 */
-			results.put(impl, new Result(false, true, null, null));
-		}
-
-		final IResourceRuleFactory factory = ResourcesPlugin.getWorkspace().getRuleFactory();
-		final JobGroup jobGroup = new JobGroup("Instantiation", 0, 0);
-		final Job myJobs[] = new Job[size];
-		for (int i = 0; i < size; i++) {
-			final IFile outputFile = outputFiles.get(i);
-			final Job job = new InstantiationJob(selectedCompImpls.get(i), outputFile, results);
-			/*
-			 * NB. According to <https://www.eclipse.org/articles/Article-Concurrency/jobs-api.html> locking
-			 * is only needed for modification, not for reading from resources. This seems sketchy to me
-			 * but I'm going to go with it. Readers are supposed to written defensively, to expect that
-			 * things might go wonky.
-			 *
-			 * We create and possibly remove the aaxl file in the case of errors.
-			 */
-			job.setRule(MultiRule.combine(factory.createRule(outputFile), factory.deleteRule(outputFile)));
-			job.setUser(true);
-			job.setJobGroup(jobGroup);
-			myJobs[i] = job;
-		}
-
-		final Job resultJob = new ResultJob(jobGroup, results);
-		resultJob.setRule(null); // doesn't use resources
-		resultJob.setUser(false); // background helper job, don't let the user see it
-		resultJob.setSystem(true); // background helper job, don't let the user see it
-		resultJob.schedule();
-
-		// now launch the main jobs
-		for (final Job job : myJobs) {
-			job.schedule();
-		}
+		final Job job = new KickoffJob(selectionAsList);
+		job.setRule(null); // doesn't use resources
+		job.setUser(false); // background helper job, don't let the user see it
+		job.setSystem(true); // background helper job, don't let the user see it
+		job.schedule();
 
 		// Supposed to always return null
 		return null;
+	}
+
+	private final class KickoffJob extends Job {
+		private final List<?> selectionAsList;
+
+		public KickoffJob(final List<?> selectionAsList) {
+			super("Instantiation Handler (hidden)");
+			this.selectionAsList = selectionAsList;
+		}
+
+		@Override
+		protected IStatus run(final IProgressMonitor monitor) {
+			final int size = selectionAsList.size();
+
+			/*
+			 * This map is shared by all the jobs to build the set of results. It is created here,
+			 * given to all the jobs, and then forgotten.
+			 */
+			final Map<ComponentImplementation, Result> results = new ConcurrentHashMap<>(size);
+
+			final List<ComponentImplementation> selectedCompImpls = new ArrayList<>(size);
+			final List<IFile> outputFiles = new ArrayList<>(size);
+			final Set<IFolder> outputFolders = new HashSet<>();
+
+			final IResourceRuleFactory factory = ResourcesPlugin.getWorkspace().getRuleFactory();
+			ISchedulingRule prereqRule = null;
+
+			for (final Object selection : selectionAsList) {
+				final URI uri;
+				if (selection instanceof EObjectNode) {
+					uri = ((EObjectNode) selection).getEObjectURI();
+				} else if (selection instanceof EObjectURIWrapper) {
+					uri = ((EObjectURIWrapper) selection).getUri();
+				} else {
+					throw new AssertionError("Unexpected selection: " + selection);
+				}
+
+				final IProject project = ResourcesPlugin.getWorkspace().getRoot().getProject(uri.segment(1));
+				final ResourceSet resourceSet = resourceSetProvider.get(project);
+				final ComponentImplementation impl = (ComponentImplementation) resourceSet.getEObject(uri, true);
+				selectedCompImpls.add(impl);
+				final IFile outputFile = OsateResourceUtil.toIFile(InstantiateModel.getInstanceModelURI(impl));
+				outputFiles.add(outputFile);
+				final IFolder outputFolder = (IFolder) outputFile.getParent(); // N.B. We KNOW there is an "Instances" folder about the .aaxl file
+				outputFolders.add(outputFolder);
+				prereqRule = MultiRule.combine(prereqRule, factory.createRule(outputFolder));
+				/*
+				 * Init each result as cancelled because if the job is cancelled before it starts, it will never
+				 * add a new result record to the map. This way those jobs that never run are accounted for.
+				 */
+				results.put(impl, new Result(false, true, null, null));
+			}
+
+			/*
+			 * Make sure all the output folders exists before hand. Could add the folder creation rules to the
+			 * jobs below, but they would limit the parallelism to much. So we create them atomically here first,
+			 * before we launch the main worker jobs.
+			 */
+			boolean prereqFailed = false;
+			try {
+				ResourcesPlugin.getWorkspace().run(m -> {
+					for (final IFolder folder : outputFolders) {
+						if (!folder.exists()) {
+							folder.create(false, true, null);
+						}
+					}
+				}, prereqRule, IWorkspace.AVOID_UPDATE, null);
+			} catch (CoreException e) {
+				prereqFailed = true;
+				OsateUiPlugin.log(e);
+
+				PlatformUI.getWorkbench().getDisplay().asyncExec(() -> {
+					MessageDialog.openError(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(),
+							"Error staring instantiation",
+							"Excepting starting model instantiation, see the error log: " + e.getMessage());
+				});
+			}
+
+			if (!prereqFailed) {
+				final JobGroup jobGroup = new JobGroup("Instantiation", 0, 0);
+				final Job myJobs[] = new Job[size];
+				for (int i = 0; i < size; i++) {
+					final IFile outputFile = outputFiles.get(i);
+					final Job job = new InstantiationJob(selectedCompImpls.get(i), outputFile, results);
+					/*
+					 * NB. According to <https://www.eclipse.org/articles/Article-Concurrency/jobs-api.html> locking
+					 * is only needed for modification, not for reading from resources. This seems sketchy to me
+					 * but I'm going to go with it. Readers are supposed to written defensively, to expect that
+					 * things might go wonky.
+					 *
+					 * We create and possibly remove the aaxl file in the case of errors.
+					 */
+					job.setRule(MultiRule.combine(factory.createRule(outputFile), factory.deleteRule(outputFile)));
+					job.setUser(true);
+					job.setJobGroup(jobGroup);
+					myJobs[i] = job;
+				}
+
+				final Job resultJob = new ResultJob(jobGroup, results);
+				resultJob.setRule(null); // doesn't use resources
+				resultJob.setUser(false); // background helper job, don't let the user see it
+				resultJob.setSystem(true); // background helper job, don't let the user see it
+				resultJob.schedule();
+
+				// now launch the main jobs
+				for (final Job job : myJobs) {
+					job.schedule();
+				}
+			}
+
+			return Status.OK_STATUS;
+		}
 	}
 
 	private static final class InstantiationJob extends WorkspaceJob {
@@ -198,12 +261,11 @@ public final class InstantiateComponentHandler extends AbstractMultiJobHandler {
 
 			final Result result = new Result(successful, cancelled, errorMessage, exception);
 			results.put(compImpl, result);
-
 			return cancelled ? Status.CANCEL_STATUS : Status.OK_STATUS;
 		}
 	}
 
-	private static final class ResultJob extends WorkspaceJob {
+	private static final class ResultJob extends Job {
 		private final JobGroup instantiationJobs;
 		private final Map<ComponentImplementation, Result> results;
 
@@ -214,7 +276,7 @@ public final class InstantiateComponentHandler extends AbstractMultiJobHandler {
 		}
 
 		@Override
-		public IStatus runInWorkspace(final IProgressMonitor monitor) {
+		public IStatus run(final IProgressMonitor monitor) {
 			// Wait for the instantiation jobs to finish
 			boolean cancelled = false;
 			try {
