d76e987b00fb0cb332394806120d3d07a7147dd9
RyanMcilnay <rm0016@uah.edu>
2020-03-12 07:56:32 -0500

    1774 ge flow creation fix (#2228)
    Closes #1774 #1202
    
    Initial selection from tool dialogs fixed and elements can be selected using the outline view (elements that are not on the diagram).

--- ge/org.osate.ge/src/org/osate/ge/internal/ui/tools/SetBindingTool.java
+++ ge/org.osate.ge/src/org/osate/ge/internal/ui/tools/SetBindingTool.java
@@ -1,18 +1,18 @@
 /**
- * Copyright (c) 2004-2020 Carnegie Mellon University and others. (see Contributors file). 
+ * Copyright (c) 2004-2020 Carnegie Mellon University and others. (see Contributors file).
  * All Rights Reserved.
- * 
+ *
  * NO WARRANTY. ALL MATERIAL IS FURNISHED ON AN "AS-IS" BASIS. CARNEGIE MELLON UNIVERSITY MAKES NO WARRANTIES OF ANY
  * KIND, EITHER EXPRESSED OR IMPLIED, AS TO ANY MATTER INCLUDING, BUT NOT LIMITED TO, WARRANTY OF FITNESS FOR PURPOSE
  * OR MERCHANTABILITY, EXCLUSIVITY, OR RESULTS OBTAINED FROM USE OF THE MATERIAL. CARNEGIE MELLON UNIVERSITY DOES NOT
  * MAKE ANY WARRANTY OF ANY KIND WITH RESPECT TO FREEDOM FROM PATENT, TRADEMARK, OR COPYRIGHT INFRINGEMENT.
- * 
+ *
  * This program and the accompanying materials are made available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
  * SPDX-License-Identifier: EPL-2.0
- * 
+ *
  * Created, in part, with funding and support from the United States Government. (see Acknowledgments file).
- * 
+ *
  * This program includes and/or can make use of certain third party source code, object code, documentation and other
  * files ("Third Party Software"). The Third Party Software that is used by this program is dependent upon your system
  * configuration. By using this program, You agree to comply with any and all relevant Third Party Software terms and
@@ -71,13 +71,12 @@ import org.osate.aadl2.ReferenceType;
 import org.osate.aadl2.ReferenceValue;
 import org.osate.ge.BusinessObjectContext;
 import org.osate.ge.di.Activate;
+import org.osate.ge.di.Names;
 import org.osate.ge.internal.AgeDiagramProvider;
 import org.osate.ge.internal.di.Deactivate;
-import org.osate.ge.internal.di.InternalNames;
 import org.osate.ge.internal.di.SelectionChanged;
 import org.osate.ge.internal.diagram.runtime.AgeDiagram;
 import org.osate.ge.internal.diagram.runtime.DiagramConfigurationBuilder;
-import org.osate.ge.internal.diagram.runtime.DiagramElement;
 import org.osate.ge.internal.services.AadlModificationService;
 import org.osate.ge.internal.services.AadlModificationService.Modification;
 import org.osate.ge.internal.services.AadlModificationService.SimpleModifier;
@@ -96,7 +95,7 @@ public class SetBindingTool {
 	private SetBindingWindow currentWindow = null;
 
 	@Activate
-	public void activate(@Named(InternalNames.SELECTED_DIAGRAM_ELEMENTS) final BusinessObjectContext[] selectedBocs,
+	public void activate(@Named(Names.BUSINESS_OBJECT_CONTEXTS) final BusinessObjectContext[] selectedBocs,
 			final AgeDiagramProvider diagramProvider,
 			final AadlModificationService aadlModService,
 			final UiService uiService) {
@@ -132,9 +131,9 @@ public class SetBindingTool {
 	}
 
 	@SelectionChanged
-	public void onSelectionChanged(@Named(InternalNames.SELECTED_DIAGRAM_ELEMENTS) final DiagramElement[] selectedDiagramElements) {
-		if(currentWindow != null && currentWindow.getShell() != null && currentWindow.getShell().isVisible()) {
-			currentWindow.setTargetBusinessObjectContexts(selectedDiagramElements);
+	public void onSelectionChanged(@Named(Names.BUSINESS_OBJECT_CONTEXTS) final BusinessObjectContext[] selectedBocs) {
+		if (currentWindow != null && currentWindow.getShell() != null && !currentWindow.getShell().isDisposed()) {
+			currentWindow.setTargetBusinessObjectContexts(selectedBocs);
 		}
 	}
 
