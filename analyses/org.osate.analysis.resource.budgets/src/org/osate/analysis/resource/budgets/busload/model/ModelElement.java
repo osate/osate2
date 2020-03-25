1960b66d7645b9dd5a5c98f79eb04c8c75885cf7
aarong <aarong@sei.cmu.edu>
2020-03-25 12:06:10 -0400

    Redid the visitor because I realized I cannot do something simple like control indenting during output of the model using a visitor.

--- analyses/org.osate.analysis.resource.budgets/src/org/osate/analysis/resource/budgets/busload/model/ModelElement.java
+++ analyses/org.osate.analysis.resource.budgets/src/org/osate/analysis/resource/budgets/busload/model/ModelElement.java
@@ -3,42 +3,19 @@ package org.osate.analysis.resource.budgets.busload.model;
 import java.util.List;
 
 abstract class ModelElement {
-	@FunctionalInterface
-	public static interface Traversal {
-		@FunctionalInterface
-		public static interface SimpleAction {
-			public void doIt();
-		}
-
-		public void traverse(SimpleAction visitChildren, SimpleAction visitSelf);
-
-		public static final Traversal PREORDER = (visitChildren, visitSelf) -> {
-			visitSelf.doIt();
-			visitChildren.doIt();
-		};
-		public static final Traversal POSTORDER = (visitChildren, visitSelf) -> {
-			visitChildren.doIt();
-			visitSelf.doIt();
-		};
+	public final void visit(final Visitor visitor) {
+		visitSelfPrefix(visitor);
+		visitChildren(visitor);
+		visitSelfPostfix(visitor);
 	}
 
-	public final void visitPostOrder(final Visitor visitor) {
-		visit(visitor, Traversal.POSTORDER);
+	final <E extends ModelElement> void visit(final List<E> list, final Visitor visitor) {
+		list.forEach(e -> e.visit(visitor));
 	}
 
-	public final void visitPreOrder(final Visitor visitor) {
-		visit(visitor, Traversal.PREORDER);
-	}
+	abstract void visitSelfPrefix(Visitor visitor);
 
-	final void visit(final Visitor visitor, final Traversal traversal) {
-		traversal.traverse(() -> visitChildren(visitor, traversal), () -> visitSelf(visitor));
-	}
+	abstract void visitChildren(Visitor visitor);
 
-	final <E extends ModelElement> void visit(final List<E> list, final Visitor visitor, final Traversal traversal) {
-		list.forEach(e -> e.visit(visitor, traversal));
-	}
-
-	protected abstract void visitChildren(Visitor visitor, Traversal traversal);
-
-	protected abstract void visitSelf(Visitor visitor);
+	abstract void visitSelfPostfix(Visitor visitor);
 }
