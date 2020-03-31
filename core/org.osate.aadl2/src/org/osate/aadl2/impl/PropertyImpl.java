osate/osate2

commit ab2ea76273e8b8cb4a4200b70fd3597cbc9bc9e0

* Properties on Connection are fully applied, avoid   to process all properties

core/org.osate.aadl2/src/org/osate/aadl2/impl/PropertyImpl.java
@@ -60,6 +60,7 @@ import org.osate.aadl2.PropertyAssociation;
 import org.osate.aadl2.PropertyExpression;
 import org.osate.aadl2.PropertyOwner;
 import org.osate.aadl2.Subcomponent;
+import org.osate.aadl2.instance.ConnectionReference;
 import org.osate.aadl2.instance.InstanceObject;
 import org.osate.aadl2.instance.util.InstanceUtil.InstantiatedClassifier;
 import org.osate.aadl2.properties.EvaluatedProperty;
@@ -513,8 +514,11 @@ public class PropertyImpl extends BasicPropertyImpl implements Property {
 		for (PropertyAssociation pa : pas) {
 			//			OsateDebug.osateDebug("pa" + pa);
 			vals.add(pa.evaluate(ctx));
-			if (!pa.isAppend())
+			
+			if (( ! (ctx.getInstanceObject() instanceof ConnectionReference )) && (!pa.isAppend()))
+			{
 				break;
+			}
 		}
 		return vals;
 	}
@@ -553,6 +557,7 @@ public class PropertyImpl extends BasicPropertyImpl implements Property {
 			return;
 		}
 
+		
 		getPropertyValueFromDeclarativeModel(ctx, paa);
 
 		/*
@@ -562,7 +567,7 @@ public class PropertyImpl extends BasicPropertyImpl implements Property {
 		 * be attached to an ancestor instance and that might be inherited by
 		 * this instance.
 		 */
-		if (isInherit()) {
+		if (isInherit() && ( ! (io instanceof ConnectionReference)))  {
 			io = (InstanceObject) io.eContainer();
 			if (io != null) {
 				getPropertyValueInternal(
@@ -599,8 +604,7 @@ public class PropertyImpl extends BasicPropertyImpl implements Property {
 				((FeatureImpl) compDecl).getPropertyValue(this, pas, cl);
 			} else if (compDecl instanceof PortConnection) {
 				((PortConnectionImpl) compDecl).getPropertyValue(this, pas);
-
-			} else {
+			}else {
 				compDecl.getPropertyValueInternal(this, pas, true);
 			}
 		}

core/org.osate.aadl2/src/org/osate/aadl2/instance/impl/ConnectionReferenceImpl.java
@@ -429,6 +429,7 @@ public class ConnectionReferenceImpl extends InstanceObjectImpl implements Conne
 
 				    	return true;
 				    }
+
 			}
 		}
 //		OsateDebug.osateDebug("[ConnectionReferenceImpl] acceptsProperty" + property);

core/org.osate.instantiation/src/org/osate/aadl2/instantiation/CacheContainedPropertyAssociationsSwitch.java
@@ -212,8 +212,9 @@ class CacheContainedPropertyAssociationsSwitch extends AadlProcessingSwitchWithP
 				if (cpes != null && !cpes.isEmpty()) {
 					final NamedElement last = cpes.get(cpes.size() - 1).getNamedElement();
 					final List<InstanceObject> ios = ci.findInstanceObjects(cpes);
-					//OsateDebug.osateDebug ("   ios=" + ios);
 					for (InstanceObject io : ios) {
+						//OsateDebug.osateDebug ("   io=" + io);
+
 						PropertyAssociation newPA = Aadl2Factory.eINSTANCE.createPropertyAssociation();
 
 						newPA.setProperty(prop);

core/org.osate.instantiation/src/org/osate/aadl2/instantiation/CachePropertyAssociationsSwitch.java
@@ -157,11 +157,10 @@ class CachePropertyAssociationsSwitch extends AadlProcessingSwitchWithProgress {
 		//OsateDebug.osateDebug ("[CachePropertyAssociation] io=" + io);
 		
 		for (Property property : propertyFilter) {
-			//OsateDebug.osateDebug  ("   property=" + property);
-			//OsateDebug.osateDebug  ("   filter=" + propertyFilter);
 
 			if (io.acceptsProperty(property))
 			{
+				
 				/*
 				 * Just look up the property. The property doesn't yet have a
 				 * local association, so lookup will get the value from the
@@ -222,6 +221,8 @@ class CachePropertyAssociationsSwitch extends AadlProcessingSwitchWithProgress {
 		
 		for (Property prop : propertyFilter) 
 		{
+			//OsateDebug.osateDebug  ("   filter=" + prop);
+
 			PropertyAssociation setPA = null;
 
 			PropertyExpression defaultvalue = prop.getDefaultValue();
@@ -248,6 +249,7 @@ class CachePropertyAssociationsSwitch extends AadlProcessingSwitchWithProgress {
 				 * is consistent for each connection.
 				 */
 				if (connRef.acceptsProperty(prop)) {
+//					OsateDebug.osateDebug  ("   accepts=" + prop);
 
 					/*
 					 * Just look up the property. The property doesn't yet have
@@ -257,14 +259,14 @@ class CachePropertyAssociationsSwitch extends AadlProcessingSwitchWithProgress {
 					 */
 					try {
 						//OsateDebug.osateDebug("try" + prop);
-						final EvaluationContext ctx = new EvaluationContext(connRef, classifierCache,
-								scProps.retrieveSCProperty(conni, prop, connRef.getConnection()));
+						final PropertyAssociation propAssociation = scProps.retrieveSCProperty(conni, prop, connRef.getConnection());
+						
+						final EvaluationContext ctx = new EvaluationContext(connRef, classifierCache, propAssociation);
 						List<EvaluatedProperty> value = prop.evaluate(ctx);
-
 
 						if (!value.isEmpty()) 
 						{
-							//OsateDebug.osateDebug ("[CachePropertyAssociation] connRef=" + connRef.getConnection().getName() + ";property=" + prop + ";value=" + value);
+//							OsateDebug.osateDebug ("[CachePropertyAssociation] connRef=" + connRef.getConnection().getName() + ";property=" + prop + ";value=" + value);
 
 							//OsateDebug.osateDebug("connref=" + connRef.getConnection() + "value=" +  value);
 
@@ -370,6 +372,7 @@ class CachePropertyAssociationsSwitch extends AadlProcessingSwitchWithProgress {
 				checkIfCancelled();
 				if (cancelled())
 				{
+//
 					OsateDebug.osateDebug("cancel" );
 
 					break;
@@ -381,15 +384,22 @@ class CachePropertyAssociationsSwitch extends AadlProcessingSwitchWithProgress {
 			 * on all connection references of the connection instance.
 			 * If not, we put a warning in the connection instance itself.
 			 */
-			if ( (setPA != null) && (nConnRefsUseProperty < nConnRefs))
-			{
-				warning(conni, "The property " + setPA.getProperty().getQualifiedName()
-						+ " should be defined on each connection reference");
-				break;
-			}
-			if (setPA != null)
-			{
-			}
+			
+			
+			/*
+			 * The following code has been commented since it can created also
+			 * some issues with the binding properties.
+			 * JD
+			 */
+			//if ( (setPA != null) && (nConnRefsUseProperty < nConnRefs))
+			//{
+				//warning(conni, "The property " + setPA.getProperty().getQualifiedName()
+			//		+ " should be defined on each connection reference");
+				//break;
+			//}
+			//if (setPA != null)
+		//	{
+		//	}
 			
 		}
 	}

core/org.osate.instantiation/src/org/osate/aadl2/instantiation/InstantiateModel.java
@@ -1459,21 +1459,21 @@ public class InstantiateModel {
 	 * @return List holding the used property definitions
 	 */
 	private void addUsedPropertyDefinitions(Element root, List<Property> result) {
-		//OsateDebug.osateDebug ("[InstantiateModel] addUsedPropertyDefinitions=" + root);
+//		OsateDebug.osateDebug ("[InstantiateModel] addUsedPropertyDefinitions=" + root);
 
 		TreeIterator<Element> it = EcoreUtil.getAllContents(Collections.singleton(root));
 		while (it.hasNext()) {
 			EObject ao = it.next();
-		//	OsateDebug.osateDebug ("[InstantiateModel] ao=" + ao);
+			//OsateDebug.osateDebug ("[InstantiateModel]    ao=" + ao);
 
 			if (ao instanceof PropertyAssociation) {
 			//	OsateDebug.osateDebug ("[InstantiateModel] ao=" + ao);
 
 				Property pd = ((PropertyAssociation) ao).getProperty();
-				//OsateDebug.osateDebug ("[InstantiateModel] pd=" + pd);
+				//OsateDebug.osateDebug ("[InstantiateModel]    pd=" + pd);
 
 				if (pd != null) {
-					//OsateDebug.osateDebug ("[InstanceModel] AddUsedProperty " + pd + " to " + root);
+//					OsateDebug.osateDebug ("[InstanceModel] AddUsedProperty " + pd + " to " + root);
 					result.add(pd);
 				}
 			}

commit 090de539db79dd0466334b9443c01f3e23f49e04

Don't append local contained and local property value for connections
Was caused by a change (ab2ea76273e8b8cb4a4200b70fd3597cbc9bc9e0) that
seems to make no sense at all

core/org.osate.aadl2/src/org/osate/aadl2/impl/PropertyImpl.java
@@ -1,18 +1,18 @@
 /**
- * Copyright (c) 2004-2020 Carnegie Mellon University and others. (see Contributors file). 
+ * Copyright (c) 2004-2020 Carnegie Mellon University and others. (see Contributors file).
  * All Rights Reserved.
- * 
+ *
  * NO WARRANTY. ALL MATERIAL IS FURNISHED ON AN "AS-IS" BASIS. CARNEGIE MELLON UNIVERSITY MAKES NO WARRANTIES OF ANY
  * KIND, EITHER EXPRESSED OR IMPLIED, AS TO ANY MATTER INCLUDING, BUT NOT LIMITED TO, WARRANTY OF FITNESS FOR PURPOSE
  * OR MERCHANTABILITY, EXCLUSIVITY, OR RESULTS OBTAINED FROM USE OF THE MATERIAL. CARNEGIE MELLON UNIVERSITY DOES NOT
  * MAKE ANY WARRANTY OF ANY KIND WITH RESPECT TO FREEDOM FROM PATENT, TRADEMARK, OR COPYRIGHT INFRINGEMENT.
- * 
+ *
  * This program and the accompanying materials are made available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
  * SPDX-License-Identifier: EPL-2.0
- * 
+ *
  * Created, in part, with funding and support from the United States Government. (see Acknowledgments file).
- * 
+ *
  * This program includes and/or can make use of certain third party source code, object code, documentation and other
  * files ("Third Party Software"). The Third Party Software that is used by this program is dependent upon your system
  * configuration. By using this program, You agree to comply with any and all relevant Third Party Software terms and
@@ -152,6 +152,7 @@ public class PropertyImpl extends BasicPropertyImpl implements Property {
 	 * <!-- end-user-doc -->
 	 * @generated
 	 */
+	@Override
 	protected EClass eStaticClass() {
 		return Aadl2Package.eINSTANCE.getProperty();
 	}
@@ -173,8 +174,9 @@ public class PropertyImpl extends BasicPropertyImpl implements Property {
 	public void setInherit(boolean newInherit) {
 		boolean oldInherit = inherit;
 		inherit = newInherit;
-		if (eNotificationRequired())
+		if (eNotificationRequired()) {
 			eNotify(new ENotificationImpl(this, Notification.SET, Aadl2Package.PROPERTY__INHERIT, oldInherit, inherit));
+		}
 	}
 
 	/**
@@ -197,10 +199,11 @@ public class PropertyImpl extends BasicPropertyImpl implements Property {
 		if (eNotificationRequired()) {
 			ENotificationImpl notification = new ENotificationImpl(this, Notification.SET,
 					Aadl2Package.PROPERTY__DEFAULT_VALUE, oldDefaultValue, newDefaultValue);
-			if (msgs == null)
+			if (msgs == null) {
 				msgs = notification;
-			else
+			} else {
 				msgs.add(notification);
+			}
 		}
 		return msgs;
 	}
@@ -213,18 +216,22 @@ public class PropertyImpl extends BasicPropertyImpl implements Property {
 	public void setDefaultValue(PropertyExpression newDefaultValue) {
 		if (newDefaultValue != defaultValue) {
 			NotificationChain msgs = null;
-			if (defaultValue != null)
+			if (defaultValue != null) {
 				msgs = ((InternalEObject) defaultValue).eInverseRemove(this,
 						EOPPOSITE_FEATURE_BASE - Aadl2Package.PROPERTY__DEFAULT_VALUE, null, msgs);
-			if (newDefaultValue != null)
+			}
+			if (newDefaultValue != null) {
 				msgs = ((InternalEObject) newDefaultValue).eInverseAdd(this,
 						EOPPOSITE_FEATURE_BASE - Aadl2Package.PROPERTY__DEFAULT_VALUE, null, msgs);
+			}
 			msgs = basicSetDefaultValue(newDefaultValue, msgs);
-			if (msgs != null)
+			if (msgs != null) {
 				msgs.dispatch();
-		} else if (eNotificationRequired())
+			}
+		} else if (eNotificationRequired()) {
 			eNotify(new ENotificationImpl(this, Notification.SET, Aadl2Package.PROPERTY__DEFAULT_VALUE, newDefaultValue,
 					newDefaultValue));
+		}
 	}
 
 	/**
@@ -268,9 +275,10 @@ public class PropertyImpl extends BasicPropertyImpl implements Property {
 	public void setEmptyListDefault(boolean newEmptyListDefault) {
 		boolean oldEmptyListDefault = emptyListDefault;
 		emptyListDefault = newEmptyListDefault;
-		if (eNotificationRequired())
+		if (eNotificationRequired()) {
 			eNotify(new ENotificationImpl(this, Notification.SET, Aadl2Package.PROPERTY__EMPTY_LIST_DEFAULT,
 					oldEmptyListDefault, emptyListDefault));
+		}
 	}
 
 	/**
@@ -325,6 +333,7 @@ public class PropertyImpl extends BasicPropertyImpl implements Property {
 	 * <!-- end-user-doc -->
 	 * @generated
 	 */
+	@Override
 	public NotificationChain eInverseRemove(InternalEObject otherEnd, int featureID, NotificationChain msgs) {
 		switch (featureID) {
 		case Aadl2Package.PROPERTY__DEFAULT_VALUE:
@@ -340,6 +349,7 @@ public class PropertyImpl extends BasicPropertyImpl implements Property {
 	 * <!-- end-user-doc -->
 	 * @generated
 	 */
+	@Override
 	public Object eGet(int featureID, boolean resolve, boolean coreType) {
 		switch (featureID) {
 		case Aadl2Package.PROPERTY__INHERIT:
@@ -363,6 +373,7 @@ public class PropertyImpl extends BasicPropertyImpl implements Property {
 	 * <!-- end-user-doc -->
 	 * @generated
 	 */
+	@Override
 	@SuppressWarnings("unchecked")
 	public void eSet(int featureID, Object newValue) {
 		switch (featureID) {
@@ -396,6 +407,7 @@ public class PropertyImpl extends BasicPropertyImpl implements Property {
 	 * <!-- end-user-doc -->
 	 * @generated
 	 */
+	@Override
 	public void eUnset(int featureID) {
 		switch (featureID) {
 		case Aadl2Package.PROPERTY__INHERIT:
@@ -425,6 +437,7 @@ public class PropertyImpl extends BasicPropertyImpl implements Property {
 	 * <!-- end-user-doc -->
 	 * @generated
 	 */
+	@Override
 	public boolean eIsSet(int featureID) {
 		switch (featureID) {
 		case Aadl2Package.PROPERTY__INHERIT:
@@ -448,9 +461,11 @@ public class PropertyImpl extends BasicPropertyImpl implements Property {
 	 * <!-- end-user-doc -->
 	 * @generated
 	 */
+	@Override
 	public String toString() {
-		if (eIsProxy())
+		if (eIsProxy()) {
 			return super.toString();
+		}
 
 		StringBuilder result = new StringBuilder(super.toString());
 		result.append(" (inherit: ");
@@ -466,10 +481,9 @@ public class PropertyImpl extends BasicPropertyImpl implements Property {
 		List<EvaluatedProperty> vals = new LinkedList<EvaluatedProperty>();
 
 		for (PropertyAssociation pa : pas) {
-			// OsateDebug.osateDebug("pa" + pa);
 			vals.add(pa.evaluate(ctx, depth));
 
-			if ((!(ctx.getInstanceObject() instanceof ConnectionReference)) && (!pa.isAppend())) {
+			if (!pa.isAppend()) {
 				break;
 			}
 		}
@@ -607,6 +621,7 @@ public class PropertyImpl extends BasicPropertyImpl implements Property {
 	// }
 	// }
 
+	@Override
 	public boolean equals(Object p) {
 		if (p instanceof Property) {
 			String p1Name = getQualifiedName();
@@ -618,6 +633,7 @@ public class PropertyImpl extends BasicPropertyImpl implements Property {
 		return this == p;
 	}
 
+	@Override
 	public int hashCode() {
 		if (eIsProxy()) {
 			return eProxyURI().toString().hashCode();