2b5c73d2fb943122724ab81c4d2cba0e909dd8da
Ryan Mcilnay <rm0016@uah.edu>
2020-04-30 09:29:44 -0500

    ge aadl properties of refined elements fix

--- ge/org.osate.ge/src/org/osate/ge/internal/ui/properties/ChangeSubcomponentTypePropertySection.java
+++ ge/org.osate.ge/src/org/osate/ge/internal/ui/properties/ChangeSubcomponentTypePropertySection.java
@@ -1,18 +1,18 @@
 /**
- * Copyright (c) 2004-2020 Carnegie Mellon University and others. (see Contributors file). 
+ * Copyright (c) 2004-2020 Carnegie Mellon University and others. (see Contributors file).
  * All Rights Reserved.
- * 
+ *
  * NO WARRANTY. ALL MATERIAL IS FURNISHED ON AN "AS-IS" BASIS. CARNEGIE MELLON UNIVERSITY MAKES NO WARRANTIES OF ANY
  * KIND, EITHER EXPRESSED OR IMPLIED, AS TO ANY MATTER INCLUDING, BUT NOT LIMITED TO, WARRANTY OF FITNESS FOR PURPOSE
  * OR MERCHANTABILITY, EXCLUSIVITY, OR RESULTS OBTAINED FROM USE OF THE MATERIAL. CARNEGIE MELLON UNIVERSITY DOES NOT
  * MAKE ANY WARRANTY OF ANY KIND WITH RESPECT TO FREEDOM FROM PATENT, TRADEMARK, OR COPYRIGHT INFRINGEMENT.
- * 
+ *
  * This program and the accompanying materials are made available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
  * SPDX-License-Identifier: EPL-2.0
- * 
+ *
  * Created, in part, with funding and support from the United States Government. (see Acknowledgments file).
- * 
+ *
  * This program includes and/or can make use of certain third party source code, object code, documentation and other
  * files ("Third Party Software"). The Third Party Software that is used by this program is dependent upon your system
  * configuration. By using this program, You agree to comply with any and all relevant Third Party Software terms and
@@ -49,6 +49,7 @@ import org.osate.aadl2.NamedElement;
 import org.osate.aadl2.Subcomponent;
 import org.osate.ge.BusinessObjectSelection;
 import org.osate.ge.internal.ui.util.InternalPropertySectionUtil;
+import org.osate.ge.internal.util.AadlHelper;
 import org.osate.ge.internal.util.AgeEmfUtil;
 import org.osate.ge.internal.util.StringUtil;
 import org.osate.ge.internal.util.SubcomponentUtil;
@@ -140,11 +141,7 @@ public class ChangeSubcomponentTypePropertySection extends AbstractPropertySecti
 	}
 
 	private static boolean isCompatibleSupcomponentType(final NamedElement ne, final EClass subcomponentType) {
-		Subcomponent sc = (Subcomponent) ne;
-		if (sc.getRefined() != null) {
-			sc = sc.getRefined();
-		}
-
+		final Subcomponent sc = (Subcomponent) AadlHelper.getRootRefinedElement(ne);
 		final ComponentImplementation ci = (ComponentImplementation) sc.getContainingClassifier();
 		return SubcomponentUtil.canContainSubcomponentType(ci, subcomponentType)
 				&& (sc.getRefined() == null || sc.getRefined() instanceof AbstractSubcomponent);
