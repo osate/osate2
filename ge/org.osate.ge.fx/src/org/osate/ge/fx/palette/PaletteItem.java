96fc6279e18f9b47818bc3c75f755f59dc765fe9
BrendenMcHan <btm0023@uah.edu>
2020-05-04 08:16:19 -0500

    Attempt at listening to the activeItemProperty

--- ge/org.osate.ge.fx/src/org/osate/ge/fx/palette/PaletteItem.java
+++ ge/org.osate.ge.fx/src/org/osate/ge/fx/palette/PaletteItem.java
@@ -1,25 +1,41 @@
 package org.osate.ge.fx.palette;
 
+import javafx.beans.value.ChangeListener;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.control.Button;
+import javafx.scene.control.Toggle;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Region;
 import javafx.scene.text.Font;
 
 class PaletteItem<I> extends Region {
 
-	Button itemButton;
+	ToggleButton itemButton;
+	ToggleGroup itemGroup = new ToggleGroup();
 	final String HOVER_ITEM_STYLE = "-fx-background-color: rgba(252,228,179,1.0);";
 	final String SELECTED_ITEM_STYLE = "-fx-background-color: rgba(207,227,250,1.0);";
 	final String IDLE_ITEM_STYLE = "-fx-background-color: white;";
 
 	public PaletteItem(final PaletteModel<?, I> model, I item) {
-		itemButton = new Button(model.getItemLabel(item));
+		itemButton = new ToggleButton(model.getItemLabel(item));
 		itemButton.setPadding(new Insets(0, 0, 0, 20));
 		itemButton.setFont(new Font(14));
+		itemButton.setToggleGroup(itemGroup);
 
 		itemButton.setStyle(IDLE_ITEM_STYLE);
+
+		// TODO: Find a way to listen to the activeItemProperty and change the style based on that.
+		model.activeItemProperty().addListener((ChangeListener<I>) (o, oldValue, newValue) -> {
+			for (Toggle t : itemGroup.getToggles()) {
+				if (t.isSelected()) {
+					itemButton.setStyle(SELECTED_ITEM_STYLE);
+				} else {
+					itemButton.setStyle(IDLE_ITEM_STYLE);
+				}
+			}
+		});
 		itemButton.setOnMouseEntered(e -> {
 
 			if (model.getActiveItem() != item) {
