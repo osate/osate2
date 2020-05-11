44203a3e65d0147f3024c2c9b2ac09c19dbe0c8a
BrendenMcHan <btm0023@uah.edu>
2020-05-11 06:36:39 -0500

    Preparation for upgrade.

--- ge/org.osate.ge.fx/src/org/osate/ge/fx/palette/PaletteItem.java
+++ ge/org.osate.ge.fx/src/org/osate/ge/fx/palette/PaletteItem.java
@@ -2,56 +2,60 @@ package org.osate.ge.fx.palette;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.control.ToggleButton;
+import javafx.scene.control.Button;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Region;
 import javafx.scene.text.Font;
 
 class PaletteItem<I> extends Region {
 
-	final ToggleButton itemButton;
-	boolean itemHovered[] = { false };
-	final String HOVER_ITEM_STYLE = "-fx-background-color: rgba(252,228,179,1.0);";
-	final String SELECTED_ITEM_STYLE = "-fx-background-color: rgba(207,227,250,1.0);";
-	final String IDLE_ITEM_STYLE = "-fx-background-color: white;";
+	private final Button itemButton;
+	private boolean itemHovered = false;
+	private static final String HOVER_ITEM_STYLE = "-fx-background-color: rgba(252,228,179,1.0);";
+	private static final String SLECTED_ITEM_STYLE = "-fx-background-color: rgba(207,227,250,1.0);";
+	private static final String IDLE_ITEM_STYLE = "-fx-background-color: white;";
 
 	public PaletteItem(final PaletteModel<?, I> model, I item) {
-		itemButton = new ToggleButton(model.getItemLabel(item));
+		itemButton = new Button(model.getItemLabel(item));
 		itemButton.setPadding(new Insets(0, 0, 0, 20));
 		itemButton.setFont(new Font(14));
 
 		itemButton.setStyle(IDLE_ITEM_STYLE);
 
 		itemButton.setOnMouseEntered(e -> {
-			itemHovered[0] = true;
-			updateStyle(itemHovered[0], model, item);
+			itemHovered = true;
+			updateStyle();
 		});
 		itemButton.setOnMouseExited(e -> {
-			itemHovered[0] = false;
-			updateStyle(itemHovered[0], model, item);
+			itemHovered = false;
+			updateStyle();
 		});
 
 		itemButton.setAlignment(Pos.BASELINE_LEFT);
 		itemButton.setGraphic(new ImageView(model.getItemIcon(item)));
 		itemButton.setOnAction(e -> {
 
-			if (model.getActiveItem() != item) {
-				model.activateItem(item);
-
-				updateStyle(itemHovered[0], model, item);
-			}
+			model.activateItem(item);
+			updateStyle();
 
 		});
 		this.getChildren().add(itemButton);
 	}
 
-	public void updateStyle(boolean itemHovered, PaletteModel<?, I> model, I item) {
+	public void updateStyle() {
 
 		if (itemHovered && model.getActiveItem() != item) {
 			itemButton.setStyle(HOVER_ITEM_STYLE);
-		} else if (model.getActiveItem() == item) {
-			itemButton.setStyle(SELECTED_ITEM_STYLE);
 		}
+
+		for (G group : model.getGroups()) {
+			for (PaletteItem I : model.getItems(G)) {
+				if (model.getActiveItem() == item) {
+					itemButton.setStyle(SELECTED_ITEM_STYLE);
+				}
+			}
+		}
+
 	}
 
 	@Override
