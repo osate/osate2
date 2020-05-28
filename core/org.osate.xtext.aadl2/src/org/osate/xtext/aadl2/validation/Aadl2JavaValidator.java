b038d156fb69bb6f3d73a2a9e36918e4c23e37c4
aarong <aarong@sei.cmu.edu>
2020-05-28 13:12:12 -0400

    Fixed testClassifierMatchRule() to work with bidirectional connections

--- core/org.osate.xtext.aadl2/src/org/osate/xtext/aadl2/validation/Aadl2JavaValidator.java
+++ core/org.osate.xtext.aadl2/src/org/osate/xtext/aadl2/validation/Aadl2JavaValidator.java
@@ -5350,7 +5350,10 @@ public class Aadl2JavaValidator extends AbstractAadl2JavaValidator {
 	private boolean testClassifierMatchRule(Connection connection, ConnectionEnd source, Classifier sourceClassifier,
 			ConnectionEnd destination, Classifier destinationClassifier) {
 		if (sourceClassifier != destinationClassifier) {
-			if (sourceClassifier instanceof ComponentImplementation
+			// bidirectional connections must have equal classifiers
+			if (connection.isAllBidirectional()) {
+				return false;
+			} else if (sourceClassifier instanceof ComponentImplementation
 					&& destinationClassifier instanceof ComponentType) {
 				if (!destinationClassifier.equals(((ComponentImplementation) sourceClassifier).getType())) {
 					error(connection, "The types of '" + source.getName() + "' and '"
