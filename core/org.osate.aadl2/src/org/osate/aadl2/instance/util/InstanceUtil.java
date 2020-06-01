03e6b7c21a3e3248e166dbff402be3492ae7ad92
kehanna <kehanna@RM-CAMPUS-21.ad.sei.cmu.edu>
2020-06-01 16:25:58 -0400

    Made classifier and bindings non-public

--- core/org.osate.aadl2/src/org/osate/aadl2/instance/util/InstanceUtil.java
+++ core/org.osate.aadl2/src/org/osate/aadl2/instance/util/InstanceUtil.java
@@ -1,18 +1,18 @@
 /**
- * Copyright (c) 2004-2020 Carnegie Mellon University and others. (see Contributors file). 
+ * Copyright (c) 2004-2020 Carnegie Mellon University and others. (see Contributors file).
  * All Rights Reserved.
- * 
+ *
  * NO WARRANTY. ALL MATERIAL IS FURNISHED ON AN "AS-IS" BASIS. CARNEGIE MELLON UNIVERSITY MAKES NO WARRANTIES OF ANY
  * KIND, EITHER EXPRESSED OR IMPLIED, AS TO ANY MATTER INCLUDING, BUT NOT LIMITED TO, WARRANTY OF FITNESS FOR PURPOSE
  * OR MERCHANTABILITY, EXCLUSIVITY, OR RESULTS OBTAINED FROM USE OF THE MATERIAL. CARNEGIE MELLON UNIVERSITY DOES NOT
  * MAKE ANY WARRANTY OF ANY KIND WITH RESPECT TO FREEDOM FROM PATENT, TRADEMARK, OR COPYRIGHT INFRINGEMENT.
- * 
+ *
  * This program and the accompanying materials are made available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
  * SPDX-License-Identifier: EPL-2.0
- * 
+ *
  * Created, in part, with funding and support from the United States Government. (see Acknowledgments file).
- * 
+ *
  * This program includes and/or can make use of certain third party source code, object code, documentation and other
  * files ("Third Party Software"). The Third Party Software that is used by this program is dependent upon your system
  * configuration. By using this program, You agree to comply with any and all relevant Third Party Software terms and
@@ -55,6 +55,7 @@ import org.osate.aadl2.instance.FeatureInstance;
 import org.osate.aadl2.instance.InstanceObject;
 import org.osate.aadl2.instance.SystemInstance;
 import org.osate.aadl2.instance.SystemOperationMode;
+import org.osate.aadl2.instance.util.InstanceUtil.InstantiatedClassifier;
 
 /**
  * @author lwrage
@@ -72,11 +73,11 @@ public class InstanceUtil {
 		/**
 		 *
 		 */
-		public Classifier classifier;
+		private Classifier classifier;
 		/**
 		 *
 		 */
-		public EList<PrototypeBinding> bindings;
+		private EList<PrototypeBinding> bindings;
 
 		InstantiatedClassifier() {
 		}
@@ -84,6 +85,26 @@ public class InstanceUtil {
 		public InstantiatedClassifier(Classifier classifier, EList<PrototypeBinding> bindings) {
 			this.classifier = classifier;
 			this.bindings = bindings;
+		}
+
+		/**
+		 * return the classifier variable
+		 * @param
+		 * @return Classifier
+		 * @since 3.0
+		 */
+		public Classifier getClassifier() {
+			return this.classifier;
+		}
+
+		/**
+		 * return the bindings variable
+		 * @param
+		 * @return EList<PrototypeBinding>
+		 * @since 3.0
+		 */
+		public EList<PrototypeBinding> getBindings() {
+			return this.bindings;
 		}
 	}
 
@@ -492,5 +513,6 @@ public class InstanceUtil {
 		}
 		return null;
 	}
+
 
 }
