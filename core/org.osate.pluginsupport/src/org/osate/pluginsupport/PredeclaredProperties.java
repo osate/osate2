dad36b77c9d3d8ed4e1ed52d38ff32b3583a6092
Joe Seibel <1824701+joeseibel@users.noreply.github.com>
2020-06-23 11:22:27 -0400

    Added missing since tag.

--- core/org.osate.pluginsupport/src/org/osate/pluginsupport/PredeclaredProperties.java
+++ core/org.osate.pluginsupport/src/org/osate/pluginsupport/PredeclaredProperties.java
@@ -8,6 +8,9 @@ import java.util.function.Function;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.jface.preference.IPreferenceStore;
 
+/**
+ * @since 4.0
+ */
 public final class PredeclaredProperties {
 	private static final String IS_VISIBLE_PREFIX = "contributed.resource.isVisible.";
 	public static final String NUMBER_OF_WORKSPACE_CONTRIBUTIONS = "contributed.resource.numWorkspace";
