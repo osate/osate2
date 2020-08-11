4ee4627a22a266cb02fa970543c10f17c00d6b0b
aarong <aarong@sei.cmu.edu>
2020-08-11 18:16:03 -0400

    Switched REinstantationHandler to tuse the REinstantiationEngine

---
+++ core/org.osate.ui/src/org/osate/ui/internal/instantiate/ReinstantiationEngine.java
@@ -0,0 +1,255 @@
+/**
+ * Copyright (c) 2004-2020 Carnegie Mellon University and others. (see Contributors file).
+ * All Rights Reserved.
+ *
+ * NO WARRANTY. ALL MATERIAL IS FURNISHED ON AN "AS-IS" BASIS. CARNEGIE MELLON UNIVERSITY MAKES NO WARRANTIES OF ANY
+ * KIND, EITHER EXPRESSED OR IMPLIED, AS TO ANY MATTER INCLUDING, BUT NOT LIMITED TO, WARRANTY OF FITNESS FOR PURPOSE
+ * OR MERCHANTABILITY, EXCLUSIVITY, OR RESULTS OBTAINED FROM USE OF THE MATERIAL. CARNEGIE MELLON UNIVERSITY DOES NOT
+ * MAKE ANY WARRANTY OF ANY KIND WITH RESPECT TO FREEDOM FROM PATENT, TRADEMARK, OR COPYRIGHT INFRINGEMENT.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Created, in part, with funding and support from the United States Government. (see Acknowledgments file).
+ *
+ * This program includes and/or can make use of certain third party source code, object code, documentation and other
+ * files ("Third Party Software"). The Third Party Software that is used by this program is dependent upon your system
+ * configuration. By using this program, You agree to comply with any and all relevant Third Party Software terms and
+ * conditions contained in any such Third Party Software or separate license file distributed with such Third Party
+ * Software. The parties who own the Third Party Software ("Third Party Licensors") are intended third party benefici-
+ * aries to this license with respect to the terms applicable to their Third Party Software. Third Party Software li-
+ * censes only apply to the Third Party Software and not any other portion of this program or this program as a whole.
+ */
+package org.osate.ui.internal.instantiate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResourceRuleFactory;
+import org.eclipse.core.resources.WorkspaceJob;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.OperationCanceledException;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.SubMonitor;
+import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.core.runtime.jobs.JobGroup;
+import org.eclipse.core.runtime.jobs.MultiRule;
+import org.eclipse.ui.IWorkingSet;
+import org.eclipse.ui.PlatformUI;
+import org.osate.aadl2.instance.SystemInstance;
+import org.osate.aadl2.instantiation.InstantiateModel;
+import org.osate.aadl2.instantiation.RootMissingException;
+import org.osate.core.AadlNature;
+import org.osate.core.OsateCorePlugin;
+import org.osate.ui.OsateUiPlugin;
+import org.osate.workspace.WorkspacePlugin;
+
+/**
+ * @since 3.0
+ */
+public final class ReinstantiationEngine extends AbstractInstantiationEngine<IFile> {
+	public ReinstantiationEngine(final List<?> selectionAsList) {
+		super(selectionAsList);
+	}
+
+	@Override
+	protected PrereqHelper getPrereqHelper(final int size, final IResourceRuleFactory ruleFactory) {
+		return new PrereqHelper() {
+			final List<IFile> inputs = new ArrayList<>(size);
+
+			@Override
+			public void handleInput(final IFile input) {
+				inputs.add(input);
+			}
+
+			@Override
+			public boolean performPrereqs() {
+				// nothing to do here
+				return true;
+			}
+
+			@Override
+			public Job getJob(final int i, final Map<IFile, InternalJobResult> results) {
+				final IFile aaxlFile = inputs.get(i);
+				final Job job = new ReinstantiationJob(aaxlFile, results);
+				/*
+				 * NB. According to <https://www.eclipse.org/articles/Article-Concurrency/jobs-api.html> locking
+				 * is only needed for modification, not for reading from resources. This seems sketchy to me
+				 * but I'm going to go with it. Readers are supposed to written defensively, to expect that
+				 * things might go wonky.
+				 *
+				 * We create and possibly remove the aaxl file in the case of errors.
+				 */
+				job.setRule(MultiRule.combine(ruleFactory.modifyRule(aaxlFile), ruleFactory.deleteRule(aaxlFile)));
+				return job;
+			}
+		};
+	}
+
+	@Override
+	protected Job getResultJob(final JobGroup instantiationJobs, final Map<IFile, InternalJobResult> results) {
+		return new ResultJob(instantiationJobs, results);
+	}
+
+	@Override
+	protected Set<IFile> getInputsFromSelection(final List<?> selectionAsList) {
+		final List<IFile> instanceFiles = new ArrayList<>();
+		findAllInstanceFiles(selectionAsList.toArray(new Object[selectionAsList.size()]), instanceFiles);
+		// remove duplicates
+		return instanceFiles.stream().distinct().collect(Collectors.toSet());
+	}
+
+	private static void findAllInstanceFiles(final Object[] rsrcs, final List<IFile> instanceFiles) {
+		for (final Object rsrc : rsrcs) {
+			if (rsrc instanceof IWorkingSet) {
+				findAllInstanceFiles(((IWorkingSet) rsrc).getElements(), instanceFiles);
+			} else if (rsrc instanceof IFile) {
+				final IFile file = (IFile) rsrc;
+				final String ext = file.getFileExtension();
+				if (ext.equals(WorkspacePlugin.INSTANCE_FILE_EXT)) {
+					instanceFiles.add(file);
+				}
+			} else if (rsrc instanceof IContainer) {
+				final IContainer container = (IContainer) rsrc;
+				if (container instanceof IProject) {
+					final IProject project = (IProject) container;
+					if (!project.isOpen() || !AadlNature.hasNature(project)) {
+						// Project is closed or is not an AADL project, so ignore it
+						continue;
+					}
+				}
+
+				if (!container.getName().startsWith(".")) {
+					try {
+						findAllInstanceFiles(container.members(), instanceFiles);
+					} catch (CoreException e) {
+						WorkspacePlugin.log(e);
+					}
+				}
+			}
+		}
+	}
+
+	private final class ReinstantiationJob extends WorkspaceJob {
+		private final IFile modelFile;
+		private final Map<IFile, InternalJobResult> results;
+
+		public ReinstantiationJob(final IFile modelFile, final Map<IFile, InternalJobResult> results) {
+			super("Reinstantiate " + modelFile.getName());
+			this.modelFile = modelFile;
+			this.results = results;
+		}
+
+		@Override
+		public IStatus runInWorkspace(final IProgressMonitor monitor) {
+			final SubMonitor subMonitor = SubMonitor.convert(monitor, 1);
+
+			/*
+			 * Error handling in buildIntanceModel is complicated and probably should not be handled the
+			 * way it is, but I don't want to fix that right now, so we are going to capture all the information
+			 * we can from it and display it to the user at the end of the operation.
+			 */
+			boolean successful = false;
+			String errorMessage = null;
+			Exception exception = null;
+			boolean cancelled = false;
+			SystemInstance systemInstance = null;
+
+			boolean delete = false;
+			try {
+				systemInstance = InstantiateModel.rebuildInstanceModelFile(modelFile, subMonitor.split(1));
+				successful = systemInstance != null;
+				errorMessage = InstantiateModel.getErrorMessage();
+				delete = !successful;
+			} catch (final InterruptedException | OperationCanceledException e) {
+				// Instantiation was canceled by the user.
+				cancelled = true;
+				systemInstance = null;
+				delete = true;
+			} catch (final RootMissingException e) {
+				successful = false;
+				errorMessage = "Root component implementation declaration no longer exists; instance model removed";
+				systemInstance = null;
+				delete = true;
+			} catch (final Exception e) {
+				OsateUiPlugin.log(e);
+				successful = false;
+				exception = e;
+				systemInstance = null;
+				delete = true;
+			}
+
+			if (delete) {
+				// Remove the partially instantiated resource
+				try {
+					if (modelFile.exists()) {
+						modelFile.delete(0, null);
+					}
+				} catch (final CoreException ce) {
+					System.out.println();
+					// eat it
+				}
+			}
+
+			final InternalJobResult result = new InternalJobResult(successful, cancelled, errorMessage, exception,
+					systemInstance);
+			results.put(modelFile, result);
+
+			return cancelled ? Status.CANCEL_STATUS : Status.OK_STATUS;
+		}
+	}
+
+	private static final class ResultJob extends Job {
+		private final JobGroup instantiationJobs;
+		private final Map<IFile, InternalJobResult> results;
+
+		public ResultJob(final JobGroup intantiationJob, final Map<IFile, InternalJobResult> results) {
+			super("Instantiation Result Job (hidden)");
+			this.instantiationJobs = intantiationJob;
+			this.results = results;
+		}
+
+		@Override
+		public IStatus run(final IProgressMonitor monitor) {
+			// Wait for the instantiation jobs to finish
+			boolean cancelled = false;
+			try {
+				instantiationJobs.join(0L, null);
+
+				/* User can suppress the dialog if all the results are successful */
+				if (OsateCorePlugin.getDefault().getAlwaysShowInstantiationResults()
+						|| !InternalJobResult.allSuccessful(results.values())) {
+					/* Get the results and display them */
+					PlatformUI.getWorkbench().getDisplay().asyncExec(() -> {
+						final InstantiationResultsDialog<?> d = new InstantiationResultsDialog<IFile>(
+								PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), "Reinstantiation",
+								"Instance Model", modelFile -> modelFile.getFullPath().toString(), results,
+								OsateCorePlugin.getDefault().getPreferenceStore());
+						d.open();
+					});
+				}
+			} catch (final InterruptedException | OperationCanceledException e) {
+				/*
+				 * InterruptedException thrown if we are somehow cancelled. Not sure if
+				 * or how this can happen, but if it does, just give up.
+				 *
+				 * OperationCancelledException is thrown if the progress monitor given
+				 * to join() is cancelled, but we didn't give it one, so it should
+				 * never occur.
+				 */
+				cancelled = true;
+			}
+
+			return cancelled ? Status.CANCEL_STATUS : Status.OK_STATUS;
+		}
+	}
+}
