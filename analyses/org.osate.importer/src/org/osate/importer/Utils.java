078d2eaef2bf876b623b078c74084da15093011d
Anna Sazonova <alsazonova@sei.cmu.edu>
2020-08-18 09:21:49 -0400

    replace exception printStackTrace with error logging and show message to user if appropriate

--- analyses/org.osate.importer/src/org/osate/importer/Utils.java
+++ analyses/org.osate.importer/src/org/osate/importer/Utils.java
@@ -1,18 +1,18 @@
 /**
- * Copyright (c) 2004-2020 Carnegie Mellon University and others. (see Contributors file). 
+ * Copyright (c) 2004-2020 Carnegie Mellon University and others. (see Contributors file).
  * All Rights Reserved.
- * 
+ *
  * NO WARRANTY. ALL MATERIAL IS FURNISHED ON AN "AS-IS" BASIS. CARNEGIE MELLON UNIVERSITY MAKES NO WARRANTIES OF ANY
  * KIND, EITHER EXPRESSED OR IMPLIED, AS TO ANY MATTER INCLUDING, BUT NOT LIMITED TO, WARRANTY OF FITNESS FOR PURPOSE
  * OR MERCHANTABILITY, EXCLUSIVITY, OR RESULTS OBTAINED FROM USE OF THE MATERIAL. CARNEGIE MELLON UNIVERSITY DOES NOT
  * MAKE ANY WARRANTY OF ANY KIND WITH RESPECT TO FREEDOM FROM PATENT, TRADEMARK, OR COPYRIGHT INFRINGEMENT.
- * 
+ *
  * This program and the accompanying materials are made available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
  * SPDX-License-Identifier: EPL-2.0
- * 
+ *
  * Created, in part, with funding and support from the United States Government. (see Acknowledgments file).
- * 
+ *
  * This program includes and/or can make use of certain third party source code, object code, documentation and other
  * files ("Third Party Software"). The Third Party Software that is used by this program is dependent upon your system
  * configuration. By using this program, You agree to comply with any and all relevant Third Party Software terms and
@@ -41,7 +41,6 @@ import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.ui.PlatformUI;
 import org.osate.aadl2.Element;
 import org.osate.aadl2.instance.ComponentInstance;
-import org.osate.aadl2.util.OsateDebug;
 import org.osate.importer.properties.InspectProperty;
 
 public class Utils {
@@ -92,7 +91,7 @@ public class Utils {
 	/**
 	 * Refresh the complete Workspace. Useful when adding
 	 * files in the workspace.
-	 * 
+	 *
 	 * @param monitor The monitor used by the underlying action
 	 * that performs the refresh.
 	 */
@@ -103,6 +102,7 @@ public class Utils {
 			} catch (CoreException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
+
 			}
 		}
 	}
@@ -192,7 +192,7 @@ public class Utils {
 
 						if (lastSegmentObj instanceof IFolder) {
 
-							selectedIFolder = (IFolder) ((IAdaptable) lastSegmentObj).getAdapter(IFolder.class);
+							selectedIFolder = ((IAdaptable) lastSegmentObj).getAdapter(IFolder.class);
 							if (selectedIFolder != null) {
 								selectedFolder = selectedIFolder.getRawLocation().toOSString();
 							}
