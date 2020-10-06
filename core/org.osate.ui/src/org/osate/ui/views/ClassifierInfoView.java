488e2f5eb16e9128f124aabc8bb31e90585b6c24
aarong <aarong@sei.cmu.edu>
2020-10-06 16:35:05 -0400

    Added a resource change listener so the view can update itself when the viewed hierarchy changes.

--- core/org.osate.ui/src/org/osate/ui/views/ClassifierInfoView.java
+++ core/org.osate.ui/src/org/osate/ui/views/ClassifierInfoView.java
@@ -27,11 +27,22 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
 
+import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IResourceChangeEvent;
+import org.eclipse.core.resources.IResourceChangeListener;
+import org.eclipse.core.resources.IResourceDelta;
+import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IAction;
 import org.eclipse.jface.layout.TreeColumnLayout;
@@ -67,6 +78,7 @@ import org.osate.aadl2.ModeFeature;
 import org.osate.aadl2.NamedElement;
 import org.osate.aadl2.Prototype;
 import org.osate.aadl2.Subcomponent;
+import org.osate.aadl2.modelsupport.resources.OsateResourceUtil;
 import org.osate.ui.OsateUiPlugin;
 import org.osate.ui.UiUtil;
 
@@ -90,6 +102,19 @@ public final class ClassifierInfoView extends ViewPart {
 
 	private volatile boolean syncWithEditor = true;
 
+	/*
+	 * Keep track of the URI of the classifier being viewed so that we can refresh if
+	 * the workspace changes. Need to keep the URI and not the classifier object
+	 * itself so that we can reparse it and pick up the changes.
+	 */
+	private URI viewedClassifierURI = null;
+	/*
+	 * Synchronized because it is used by the dispaly thread and whatever thread exeuctes
+	 * the resource change listener.
+	 */
+	private final Set<IResource> classifierResources = Collections.synchronizedSet(new HashSet<>());
+	private IResourceChangeListener rsrcListener = null;
+
 	public ClassifierInfoView() {
 		modelElementLabelProvider = UiUtil.getModelElementLabelProvider();
 	}
@@ -105,7 +130,6 @@ public final class ClassifierInfoView extends ViewPart {
 		/* I basically stole this from org.eclipse.jdt.internal.ui.util.OpenTypeHiearchyUtil.openInViewPart() */
 		final IWorkbenchPage page = window.getActivePage();
 		try {
-//			ClassifierInfoView result = (ClassifierInfoView) page.findView(VIEW_ID);
 			final ClassifierInfoView result = (ClassifierInfoView) page.showView(VIEW_ID);
 			return result;
 		} catch (CoreException e) {
@@ -148,10 +172,16 @@ public final class ClassifierInfoView extends ViewPart {
 		aadlImage = new Image(site.getShell().getDisplay(),
 				ClassifierInfoView.class.getResourceAsStream("/icons/aadl.gif"));
 		super.init(site);
+
+		rsrcListener = new Listener();
+		ResourcesPlugin.getWorkspace().addResourceChangeListener(rsrcListener);
 	}
 
 	@Override
 	public void dispose() {
+		if (rsrcListener != null) {
+			ResourcesPlugin.getWorkspace().removeResourceChangeListener(rsrcListener);
+		}
 		aadlImage.dispose();
 		aadlImage = null;
 	}
@@ -325,10 +355,25 @@ public final class ClassifierInfoView extends ViewPart {
 	// == Set the view input
 	// ======================================================================
 
-	public void setInput(final Classifier input) {
-		if (input != null) {
-			final AncestorTree tree = createAncestorTree(input);
-			ancestorTree.setInput(tree);
+	public void setInput(final URI classifierURI) {
+		if (classifierURI != null) {
+			viewedClassifierURI = classifierURI;
+			final Classifier classifier = (Classifier) new ResourceSetImpl().getEObject(classifierURI, true);
+			updateDisplay(classifier);
+		}
+	}
+
+	private void refresh() {
+		if (viewedClassifierURI != null) {
+			final Classifier classifier = (Classifier) new ResourceSetImpl().getEObject(viewedClassifierURI, true);
+			updateDisplay(classifier);
+		}
+	}
+
+	private void updateDisplay(final Classifier input) {
+		getViewSite().getShell().getDisplay().asyncExec(() -> {
+			classifierResources.clear();
+			ancestorTree.setInput(createAncestorTree(input));
 			ancestorTree.expandToLevel(2);
 			if (input instanceof ComponentType) {
 				memberTree.setInput(createMemberTree((ComponentType) input));
@@ -340,12 +385,51 @@ public final class ClassifierInfoView extends ViewPart {
 				memberTree.setInput(createMemberTree((FeatureGroupType) input));
 				memberTree.expandToLevel(2);
 			}
-		}
+		});
 	}
 
 	// ======================================================================
 	// == Helper classes
 	// ======================================================================
+
+	// ----------------------------------------------------------------------
+	// -- Resource Listener
+	// ----------------------------------------------------------------------
+
+	/**
+	 * When a resource in the workspace is moved or deleted we check to see if it is overrides a
+	 * plug-in contributed resource.  If so, we need to update the global preferences.
+	 */
+	private class Listener implements IResourceChangeListener {
+		@Override
+		public void resourceChanged(final IResourceChangeEvent event) {
+			/*
+			 * Going to be quite dumb about this. If any of the resources we care
+			 * about change in any way, then we update the view.
+			 */
+			final AtomicBoolean changed = new AtomicBoolean(false);
+			if (event.getType() == IResourceChangeEvent.POST_CHANGE) {
+				final IResourceDelta docDelta = event.getDelta();
+				if (docDelta != null) {
+					try {
+						docDelta.accept(delta -> {
+							final IResource resource = delta.getResource();
+							if (classifierResources.contains(resource)) {
+								changed.set(true);
+							}
+							return true;
+						});
+					} catch (final CoreException e) {
+						OsateUiPlugin.log(e);
+					}
+				}
+			}
+
+			if (changed.get()) {
+				refresh();
+			}
+		}
+	}
 
 	// ----------------------------------------------------------------------
 	// -- Ancestor tree
@@ -412,6 +496,13 @@ public final class ClassifierInfoView extends ViewPart {
 	}
 
 	private AncestorTreeNode createAncestorTreeNode(final Classifier classifier, final String prefix) {
+		final Resource eResource = classifier.eResource();
+		// If the AADL file has parse errors then we may get a null result here
+		if (eResource != null) {
+			final URI uri = eResource.getURI();
+			classifierResources.add(OsateResourceUtil.toIFile(uri));
+		}
+
 		AncestorTreeNode[] children = new AncestorTreeNode[0];
 		if (classifier instanceof ComponentType) {
 			final ComponentType extended = ((ComponentType) classifier.getExtended());
