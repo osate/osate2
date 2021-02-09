faac14c049e634d50ce1b32cb703ce0c1cbe4e62
aarong <aarong@sei.cmu.edu>
2021-02-09 15:36:08 -0500

    Forgot actually add the new project org.osate.analysis.model to the repository!

---
+++ analyses/org.osate.analysis.model/src-gen/org/osate/analysis/model/analysis/AnalysisFactory.java
@@ -0,0 +1,33 @@
+/**
+ */
+package org.osate.analysis.model.analysis;
+
+import org.eclipse.emf.ecore.EFactory;
+
+/**
+ * <!-- begin-user-doc -->
+ * The <b>Factory</b> for the model.
+ * It provides a create method for each non-abstract class of the model.
+ * <!-- end-user-doc -->
+ * @see org.osate.analysis.model.analysis.AnalysisPackage
+ * @generated
+ */
+public interface AnalysisFactory extends EFactory {
+	/**
+	 * The singleton instance of the factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	AnalysisFactory eINSTANCE = org.osate.analysis.model.analysis.impl.AnalysisFactoryImpl.init();
+
+	/**
+	 * Returns the package supported by this factory.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @return the package supported by this factory.
+	 * @generated
+	 */
+	AnalysisPackage getAnalysisPackage();
+
+} //AnalysisFactory
