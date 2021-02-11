38c8265005b1d31418ca29e3dc66ccc21b7e65f3
Ryan Mcilnay <rm0016@uah.edu>
2021-02-11 10:54:03 -0600

    improve ge ba test

--- ge/org.osate.ge.tests/src/org/osate/ge/tests/endToEnd/util/OsateGeTestCommands.java
+++ ge/org.osate.ge.tests/src/org/osate/ge/tests/endToEnd/util/OsateGeTestCommands.java
@@ -385,16 +385,16 @@ public class OsateGeTestCommands {
 	}
 
 	/**
-	 * Creates a behavior annex and a state using the palette tool.
+	 * Creates a behavior annex and an initial state using the palette tool.
 	 * Preconditions: OSATE shell is active.  Specified classifier element exists.
-	 * Postconditions: A new behavior annex with a state has been created.  The state will be renamed to the specified name.
+	 * Postconditions: A new behavior annex with an initial state has been created.  The state will be renamed to the specified name.
 	 * @param diagram is the diagram in which to create the behavior annex and behavior state
 	 * @param pkgRef is the package reference in which the classifier belongs to
 	 * @param classifierName is the name of the parent classifier for the behavior specification
 	 * @param behaviorSpecification is the relative reference to the new behavior specification
 	 * @param newStateName is the name to which the behavior state should be renamed to
 	 */
-	public static void createBehaviorAnnexWithState(final DiagramReference diagram,
+	public static void createBehaviorAnnexWithInitialState(final DiagramReference diagram,
 			final RelativeBusinessObjectReference pkgRef, final String classifierName,
 			final RelativeBusinessObjectReference behaviorSpecification, final String newStateName) {
 		final RelativeBusinessObjectReference classifierRef = getClassifierRelativeReference(classifierName);
@@ -404,8 +404,6 @@ public class OsateGeTestCommands {
 
 		final DiagramElementReference behaviorSpecDiagramRef = element(pkgRef, classifierRef, behaviorSpecification);
 
-		clickCheckboxInPropertiesView(diagram, "AADL", 0, behaviorSpecDiagramRef);
-		
 		// Show contents of specification
 		showContentsAndLayout(diagram, behaviorSpecDiagramRef);
 
