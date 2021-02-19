aa9ab1d45a181a970fcfd41c4d6ab6f87b759a5a
Lutz Wrage <lutz.wrage@gmail.com>
2021-02-19 15:02:26 -0500

    Fix: "save as..." before modification

--- core/ease-scripts/src/generate-core.js
+++ core/ease-scripts/src/generate-core.js
@@ -1,48 +1,3 @@
-importPackage(org.eclipse.swtbot.eclipse.finder);
-bot = new SWTWorkbenchBot();
-
-// open uml file
-print("Open UML file");
-explorerView = bot.viewByTitle("Package Explorer");
-explorerView.show();
-explorer = explorerView.bot().tree()
-node = findTreeItem(explorer.getAllItems(), "core")
-waitExpand(node);
-node = findTreeItem(node.getItems(), "org.osate.aadl2.metamodel");
-waitExpand(node);
-node = findTreeItem(node.getItems(), "Models");
-waitExpand(node);
-node = findTreeItem(node.getItems(), "AADL2.EMF.uml");
-node.contextMenu("Open With").menu("UML Model Editor").click();
-
-// select top package
-print("Select model package");
-editor = bot.editorByTitle("AADL2.EMF.uml");
-editor.show();
-editor.setFocus();
-tree = editor.bot().tree();
-root = tree.getTreeItem("platform:/resource/org.osate.aadl2.metamodel/Models/AADL2.EMF.uml");
-waitExpand(root);
-root.select("<<EPackage, Metamodel>> <Model> AADL2");
-
-// package merge
-print("Package merge");
-umlMenu = bot.menu("UML Editor");
-umlMenu.menu("Package").menu("Merge...").click();
-
-shell = bot.shell("Merge Package 'AADL2 - AADL2.EMF.uml'");
-shell.bot().button("OK").click();
-bot.waitUntil(org.eclipse.swtbot.swt.finder.waits.Conditions.shellCloses(shell));
-shell = bot.shell("Merge Package 'AADL2 - AADL2.EMF.uml'");
-shell.bot().button("OK").click();
-bot.waitUntil(org.eclipse.swtbot.swt.finder.waits.Conditions.shellCloses(shell));
-
-// convert to metamodel
-print("Convert to metamodel");
-umlMenu.menu("Convert To").menu("Metamodel").click();
-bot.sleep(1000);
-
-// save as AADL.EMF.merged.uml
 /**
  * Copyright (c) 2004-2021 Carnegie Mellon University and others. (see Contributors file). 
  * All Rights Reserved.
@@ -66,6 +21,25 @@ bot.sleep(1000);
  * aries to this license with respect to the terms applicable to their Third Party Software. Third Party Software li-
  * censes only apply to the Third Party Software and not any other portion of this program or this program as a whole.
  */
+
+importPackage(org.eclipse.swtbot.eclipse.finder);
+bot = new SWTWorkbenchBot();
+
+// open uml file
+print("Open UML file");
+explorerView = bot.viewByTitle("Package Explorer");
+explorerView.show();
+explorer = explorerView.bot().tree()
+node = findTreeItem(explorer.getAllItems(), "core")
+waitExpand(node);
+node = findTreeItem(node.getItems(), "org.osate.aadl2.metamodel");
+waitExpand(node);
+node = findTreeItem(node.getItems(), "Models");
+waitExpand(node);
+node = findTreeItem(node.getItems(), "AADL2.EMF.uml");
+node.contextMenu("Open With").menu("UML Model Editor").click();
+
+// save as AADL.EMF.merged.uml
 print("Save merged model");
 bot.menu("File").menu("Save As...").click();
 
@@ -81,6 +55,37 @@ save.bot().button("OK").click();
 // overwrite existing file
 overwrite = bot.shell("Question");
 overwrite.bot().button("Yes").click();
+
+// select top package
+print("Select model package");
+editor = bot.editorByTitle("AADL2.EMF.merged.uml");
+editor.show();
+editor.setFocus();
+tree = editor.bot().tree();
+root = tree.getTreeItem("platform:/resource/org.osate.aadl2.metamodel/Models/AADL2.EMF.merged.uml");
+waitExpand(root);
+root.select("<<EPackage, Metamodel>> <Model> AADL2");
+
+// package merge
+print("Package merge");
+umlMenu = bot.menu("UML Editor");
+umlMenu.menu("Package").menu("Merge...").click();
+
+shell = bot.shell("Merge Package 'AADL2 - AADL2.EMF.merged.uml'");
+shell.bot().button("OK").click();
+bot.waitUntil(org.eclipse.swtbot.swt.finder.waits.Conditions.shellCloses(shell));
+shell = bot.shell("Merge Package 'AADL2 - AADL2.EMF.merged.uml'");
+shell.bot().button("OK").click();
+bot.waitUntil(org.eclipse.swtbot.swt.finder.waits.Conditions.shellCloses(shell));
+
+// convert to metamodel
+print("Convert to metamodel");
+umlMenu.menu("Convert To").menu("Metamodel").click();
+bot.sleep(1000);
+
+// save AADL.EMF.merged.uml
+print("Save merged model");
+bot.menu("File").menu("Save").click();
 
 // close editor
 editor.close();
