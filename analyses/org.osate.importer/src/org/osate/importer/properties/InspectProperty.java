838275b3414e936accfe336fd80185b865d80b3f
Aaron Greenhouse <aaron.greenhouse@gmail.com>
2021-02-24 10:08:41 -0500

    Replaced use of contribution.sei property sets with the new generated helper classes.  Marked old uses as deprecated.

--- analyses/org.osate.importer/src/org/osate/importer/properties/InspectProperty.java
+++ analyses/org.osate.importer/src/org/osate/importer/properties/InspectProperty.java
@@ -1,18 +1,18 @@
 /**
- * Copyright (c) 2004-2021 Carnegie Mellon University and others. (see Contributors file). 
+ * Copyright (c) 2004-2021 Carnegie Mellon University and others. (see Contributors file).
  * All Rights Reserved.
- * 
+ *
  * NO WARRANTY. ALL MATERIAL IS FURNISHED ON AN "AS-IS" BASIS. CARNEGIE MELLON UNIVERSITY MAKES NO WARRANTIES OF ANY
  * KIND, EITHER EXPRESSED OR IMPLIED, AS TO ANY MATTER INCLUDING, BUT NOT LIMITED TO, WARRANTY OF FITNESS FOR PURPOSE
  * OR MERCHANTABILITY, EXCLUSIVITY, OR RESULTS OBTAINED FROM USE OF THE MATERIAL. CARNEGIE MELLON UNIVERSITY DOES NOT
  * MAKE ANY WARRANTY OF ANY KIND WITH RESPECT TO FREEDOM FROM PATENT, TRADEMARK, OR COPYRIGHT INFRINGEMENT.
- * 
+ *
  * This program and the accompanying materials are made available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
  * SPDX-License-Identifier: EPL-2.0
- * 
+ *
  * Created, in part, with funding and support from the United States Government. (see Acknowledgments file).
- * 
+ *
  * This program includes and/or can make use of certain third party source code, object code, documentation and other
  * files ("Third Party Software"). The Third Party Software that is used by this program is dependent upon your system
  * configuration. By using this program, You agree to comply with any and all relevant Third Party Software terms and
@@ -24,39 +24,12 @@
 
 package org.osate.importer.properties;
 
-import org.osate.aadl2.BooleanLiteral;
 import org.osate.aadl2.NamedElement;
-import org.osate.aadl2.Property;
-import org.osate.aadl2.PropertyExpression;
-import org.osate.aadl2.properties.PropertyLookupException;
-import org.osate.contribution.sei.names.SEI;
-import org.osate.xtext.aadl2.properties.util.GetProperties;
 
+@Deprecated
 public class InspectProperty {
-
-	private static Property inspectProperty = null;
-
 	public static boolean shallInspect(final NamedElement ph) {
-		PropertyExpression pv;
-		boolean val;
-
-		if (inspectProperty == null) {
-			inspectProperty = GetProperties.lookupPropertyDefinition(ph, SEI._NAME, SEI.VDID_INSPECT);
-		}
-
-		val = false;
-
-		try {
-			pv = ph.getSimplePropertyValue(inspectProperty);
-
-			if (pv instanceof BooleanLiteral) {
-				val = ((BooleanLiteral) pv).getValue();
-			}
-		} catch (PropertyLookupException e) {
-			return false;
-		}
-		System.out.println("return inspect: " + val + " on comp " + ph);
-		return val;
+		return org.osate.contribution.sei.sei.Sei.getVdidInspect(ph).orElse(false);
 	}
 
 }
