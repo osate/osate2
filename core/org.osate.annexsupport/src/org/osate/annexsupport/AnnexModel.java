ad10a6d33bd5fd46f144fea4de1e417a057eae93
Anna Sazonova <alsazonova@sei.cmu.edu>
2021-03-02 14:03:10 -0500

    Add comments to describe each function

--- core/org.osate.annexsupport/src/org/osate/annexsupport/AnnexModel.java
+++ core/org.osate.annexsupport/src/org/osate/annexsupport/AnnexModel.java
@@ -31,6 +31,10 @@ import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.preferences.IScopeContext;
 import org.eclipse.jface.preference.IPreferenceStore;
+import org.osate.aadl2.AnnexLibrary;
+import org.osate.aadl2.AnnexSubclause;
+import org.osate.aadl2.DefaultAnnexLibrary;
+import org.osate.aadl2.DefaultAnnexSubclause;
 import org.osate.aadl2.NamedElement;
 import org.osate.core.OsateCorePlugin;
 import org.osgi.service.prefs.BackingStoreException;
@@ -52,18 +56,37 @@ public class AnnexModel {
 	public static final String PREFS_QUALIFIER = "org.osate.ui.internal.annex";
 	public static final String PREF_ANNEX_USE_WORKSPACE = "org.osate.ui.internal.annex_use_workspace";
 
+	private static enum UserSetting {
+		USE_WORKSPACE_PREFERENCE, USE_PROJECT_PREFERENCE
+	};
+
 	// Methods
-	public static final boolean getWorkspacePref(IProject project) {
+	/**
+	 * User can choose to set preference per workspace or per project.
+	 * The preference is stored in PREF_ANNEX_USE_WORKSPACE in project scope
+	 *
+	 * @param project that is being parsed
+	 * @return True if workspace preference should be used OR if @param project is NULL; False if project preference should be used
+	 */
+	public static final UserSetting getUserPreference(IProject project) {
 		if (project == null) {
-			return true; // if can not access project preferences, return use workspace preference = true
+			return UserSetting.USE_WORKSPACE_PREFERENCE; // if can not access project preferences, return use workspace preference = true
 		}
 
 		final IScopeContext context = new ProjectScope(project);
 		final Preferences prefs = context.getNode(PREFS_QUALIFIER);
-		return prefs.getBoolean(PREF_ANNEX_USE_WORKSPACE, DEFAULT_PREF_VALUE);
+		boolean useWorkspace = prefs.getBoolean(PREF_ANNEX_USE_WORKSPACE, DEFAULT_PREF_VALUE);
+
+		return useWorkspace ? UserSetting.USE_WORKSPACE_PREFERENCE : UserSetting.USE_PROJECT_PREFERENCE;
 	}
 
-	public static final boolean getAnnex(String annexExtensionId) {
+	/**
+	 * Check if workspace preference is set to enabled for provided annex name
+	 *
+	 * @param annexExtensionId Annex name to check
+	 * @return True if @param annexExtensionId is enabled per workspace preference
+	 */
+	public static final boolean getAnnexEnabled(String annexExtensionId) {
 		final IPreferenceStore store = OsateCorePlugin.getDefault().getPreferenceStore();
 		if (store.contains(annexExtensionId.toLowerCase())) {
 			return store.getBoolean(annexExtensionId.toLowerCase());
@@ -72,7 +95,14 @@ public class AnnexModel {
 		return DEFAULT_PREF_VALUE;
 	}
 
-	public static final boolean getAnnex(final IProject project, String annexExtensionId) {
+	/**
+	 * Check if project preference is set to enabled for provided annex name
+	 *
+	 * @param project that is being parsed
+	 * @param annexExtensionId Annex name to check
+	 * @return True if @param annexExtensionId is enabled per workspace preference
+	 */
+	public static final boolean getAnnexEnabled(final IProject project, String annexExtensionId) {
 		final IScopeContext context = new ProjectScope(project);
 		final Preferences prefs = context.getNode(PREFS_QUALIFIER);
 
@@ -83,26 +113,40 @@ public class AnnexModel {
 
 		// It's possible the above may have failed for some reason, in which case we revert to the workspace preferences
 		if (annexTurnedOn == null) {
-			annexTurnedOn = getAnnex(annexExtensionId);
+			annexTurnedOn = getAnnexEnabled(annexExtensionId);
 		}
 
 		return annexTurnedOn;
 	}
 
-	public static final boolean getAnnexBasedOnWorkspacePreference(final IProject project, String annexExtensionId) {
-		// return true if preference for annex is enabled, return false if preference for annex is disabled
-		if (getWorkspacePref(project)) { // preferences set per workspace
-			return getAnnex(annexExtensionId);
+	/**
+	 * Check if this annex plugin is enabled in osate annex preferences
+	 * This methods decides if workspace or project preference needs to be used
+	 *
+	 * @param project that is being parsed
+	 * @param annexExtensionId annex name to check user preference for
+	 * @return True if annex is enabled, False if annex is disabled
+	 */
+
+	public static final boolean isAnnexNameEnabled(final IProject project, String annexExtensionId) {
+		if (getUserPreference(project).equals(UserSetting.USE_WORKSPACE_PREFERENCE)) {
+			return getAnnexEnabled(annexExtensionId); // preferences set per workspace
 		}
 
-		return getAnnex(project, annexExtensionId); // preference set per project
+		return getAnnexEnabled(project, annexExtensionId); // preference set per project
 	}
 
-	public static final void setAnnex(boolean value, IProject project, String annexExtensionId) {
+	/**
+	 *
+	 * @param enabled True if @param annexExtensionId is enabled in @param project; False if disabled
+	 * @param project Project for which to set a setting for
+	 * @param annexExtensionId Annex name to set a preference for
+	 */
+	public static final void setAnnexEnabled(boolean enabled, IProject project, String annexExtensionId) {
 		final IScopeContext context = new ProjectScope(project);
 		final Preferences prefs = context.getNode(PREFS_QUALIFIER);
 		if (!prefs.getBoolean(PREF_ANNEX_USE_WORKSPACE, DEFAULT_PREF_VALUE)) {
-			prefs.putBoolean(annexExtensionId.toLowerCase(), value);
+			prefs.putBoolean(annexExtensionId.toLowerCase(), enabled);
 			try {
 				prefs.flush();
 			} catch (BackingStoreException e) {
@@ -111,17 +155,37 @@ public class AnnexModel {
 		}
 	}
 
-	public static final void setAnnex(boolean value, String annexExtensionId) {
+	/**
+	 *
+	 * @param enabled True if @param annexExtensionId is enabled in workspace; False if disabled
+	 * @param annexExtensionId Annex name to set a preference for
+	 */
+	public static final void setAnnexEnabled(boolean enabled, String annexExtensionId) {
 		final IPreferenceStore store = OsateCorePlugin.getDefault().getPreferenceStore();
-		store.setValue(annexExtensionId.toLowerCase(), value);
+		store.setValue(annexExtensionId.toLowerCase(), enabled);
 	}
 
+	/**
+	 * Set PREF_ANNEX_USE_WORKSPACE setting that indicates if workspace OR project preferences should be used for @param project
+	 *
+	 * @param useWorkspace Set to True if Workspace preferences should be applied for this @param project; Set to False if project preferences are applied to this @param project
+	 * @param project to which the setting is applied
+	 */
 	public static final void setWorkspacePref(boolean useWorkspace, IProject project) {
 		final IScopeContext context = new ProjectScope(project);
 		final Preferences prefs = context.getNode(PREFS_QUALIFIER);
 		prefs.putBoolean(PREF_ANNEX_USE_WORKSPACE, useWorkspace);
 	}
 
+	/**
+	 *
+	 * @param <A> Type of the resulting annex section. Expected to be {@link AnnexLibrary} or {@link AnnexSubclause}.
+	 * @param <D> Type of the default annex section. Expected to be {@link DefaultAnnexLibrary} or
+	 *            {@link DefaultAnnexSubclause}.
+	 * @param defaultAnnexSection Either the {@link DefaultAnnexLibrary} or {@link DefaultAnnexSubclause}
+	 * @param annexName for which the check is done to see if it is enabled
+	 * @return @param annexName if Annex is enabled; default annex name if @param annexName is disabled
+	 */
 	public static <A extends NamedElement, D extends A> String filterDisabledAnnexes(
 			D defaultAnnexSection,
 			String annexName) {
@@ -134,7 +198,7 @@ public class AnnexModel {
 			project = file.getProject();
 		}
 		// check if this annex plugin is enabled in osate annex preferences
-		if (getAnnexBasedOnWorkspacePreference(project, annexName)) {
+		if (isAnnexNameEnabled(project, annexName)) {
 			return annexName;
 		}
 
