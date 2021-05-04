8062a72167d330f172f37be2680436e8d9969f38
Anna Sazonova <alsazonova@sei.cmu.edu>
2021-05-04 10:03:01 -0400

    new batch of icons

--- core/org.osate.aadl2.edit/src/org/osate/aadl2/provider/AbstractFeatureItemProvider.java
+++ core/org.osate.aadl2.edit/src/org/osate/aadl2/provider/AbstractFeatureItemProvider.java
@@ -104,7 +104,7 @@ public class AbstractFeatureItemProvider extends DirectedFeatureItemProvider {
 	 * @generated
 	 */
 	public Object getImage(Object object) {
-		return overlayImage(object, getResourceLocator().getImage("full/obj16/AbstractFeature"));
+		return overlayImage(object, getResourceLocator().getImage("full/obj16/AbstractFeature.png"));
 	}
 
 	/**
