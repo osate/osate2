070d778b09ce9822224d4aeaccd9cf3a32952ed2
Ryan Mcilnay <rm0016@uah.edu>
2021-06-08 07:43:28 -0500

    improve and cleanup behavior transition properties section

--- ge/org.osate.ge.ba/src/org/osate/ge/ba/util/BehaviorAnnexXtextUtil.java
+++ ge/org.osate.ge.ba/src/org/osate/ge/ba/util/BehaviorAnnexXtextUtil.java
@@ -23,13 +23,16 @@
  */
 package org.osate.ge.ba.util;
 
+import java.util.Iterator;
 import java.util.stream.Collectors;
 
 import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.ui.editor.model.IXtextDocument;
 import org.eclipse.xtext.xbase.lib.Pair;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterators;
+import com.google.common.collect.Lists;
 import com.google.common.collect.PeekingIterator;
 
 public class BehaviorAnnexXtextUtil {
@@ -46,18 +49,27 @@ public class BehaviorAnnexXtextUtil {
 	}
 
 	/**
-	 * Find offset of character in string that is not commented out
+	 * Finds the offset of the specified character in the specified string.  The character
+	 * must be outside of any text that is bracketed by "{[" and "]}".
+	 *
+	 * @param str the string to search for the character
+	 * @param ch the character to find in the string
+	 *
+	 * @throws RuntimeException if the character cannot be found
+	 *
+	 * @return the offset of the character in the string
 	 */
-	public static int findUncommentedTerminationChar(final String str, final char delim) {
-		final String openBrackets = "{[";
-		final String closeBrackets = "]}";
+	public static int findUncommentedTerminationChar(final String str, final char ch) {
+		final ImmutableList<Character> openBrackets = Lists.charactersOf("{[");
+		final ImmutableList<Character> closeBrackets = Lists.charactersOf("]}");
 		final PeekingIterator<Character> charPeekingIt = Iterators
 				.peekingIterator(str.chars().mapToObj(e -> (char) e).collect(Collectors.toList()).iterator());
 		for (int offset = 0; charPeekingIt.hasNext(); offset++) {
 			final Character c = charPeekingIt.next();
-			if (contains(openBrackets, c)) {
+			if (openBrackets.contains(c)) {
+				// Find closing bracket before finding termination char
 				offset = findClosingBracket(charPeekingIt, openBrackets, closeBrackets, offset);
-			} else if (c == delim) {
+			} else if (c == ch) {
 				return offset;
 			} else if (c == '-' && charPeekingIt.peek() == '-') {
 				// Found comment, increase offset until end of line
@@ -73,15 +85,15 @@ public class BehaviorAnnexXtextUtil {
 		throw new RuntimeException("Cannot find terminating character");
 	}
 
-	private static int findClosingBracket(final PeekingIterator<Character> charPeekingIt, final String openBrackets,
-			final String closeBrackets,
+	private static int findClosingBracket(final Iterator<Character> charPeekingIt,
+			final ImmutableList<Character> openBrackets, final ImmutableList<Character> closeBrackets,
 			int offset) {
 		int bracketsOpenCount = 1;
 		for (; charPeekingIt.hasNext(); offset++) {
 			final Character c = charPeekingIt.next();
-			if (contains(openBrackets, c)) {
+			if (openBrackets.contains(c)) {
 				bracketsOpenCount++;
-			} else if (contains(closeBrackets, c)) {
+			} else if (closeBrackets.contains(c)) {
 				if (bracketsOpenCount == 1) {
 					return offset + 1;
 				} else {
@@ -93,12 +105,15 @@ public class BehaviorAnnexXtextUtil {
 		throw new RuntimeException("Could not find closing brackets.");
 	}
 
-	private static boolean contains(final String str, final char chr) {
-		return str.indexOf(chr) != -1;
-	}
-
 	/**
-	 * Find offset of a pair characters in string that is not commented out
+	 * Finds the offset of the specified character pair in the specified string.
+	 *
+	 * @param str the string to search for the character pair
+	 * @param charsToMatch the character pair to find in the string
+	 *
+	 * @throws RuntimeException if the character pair cannot be found
+	 *
+	 * @return the offset of the character pair in the string
 	 */
 	public static int findUncommentedCharPair(final String str, final Pair<Character, Character> charsToMatch) {
 		final PeekingIterator<Character> charPeekingIt = Iterators
