9202703f3ed2813266ce68b073c17cdce0935c25
RyanMcilnay <rm0016@uah.edu>
2021-06-08 09:14:36 -0500

    2655 fix ba properties section text display (#2670)
    * fix ge BehaviorTransition properties section.
     - Format action block text
     - Add timer for validation for editing action blocks and dispatch
    conditions

--- ge/org.osate.ge.ba/src/org/osate/ge/ba/util/BehaviorAnnexXtextUtil.java
+++ ge/org.osate.ge.ba/src/org/osate/ge/ba/util/BehaviorAnnexXtextUtil.java
@@ -23,13 +23,16 @@
  */
 package org.osate.ge.ba.util;
 
+import java.util.Iterator;
 import java.util.stream.Collectors;
 
 import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.ui.editor.model.IXtextDocument;
 import org.eclipse.xtext.xbase.lib.Pair;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterators;
+import com.google.common.collect.Lists;
 import com.google.common.collect.PeekingIterator;
 
 public class BehaviorAnnexXtextUtil {
@@ -46,16 +49,29 @@ public class BehaviorAnnexXtextUtil {
 	}
 
 	/**
-	 * Find offset of character in string that is not commented out
+	 * Finds the offset of the specified character in the specified string.  The character
+	 * must be outside of any text that is bracketed by "{[" and "]}".
+	 *
+	 * @param str the string to search for the character
+	 * @param ch the character to find in the string
+	 *
+	 * @throws RuntimeException if the character cannot be found
+	 *
+	 * @return the offset of the character in the string
 	 */
-	public static int findUncommentedChar(final String str, final char delim) {
-		final PeekingIterator<Character> charPeekingIt = Iterators
-				.peekingIterator(str.chars().mapToObj(e -> (char) e).collect(Collectors.toList()).iterator());
+	public static int findUncommentedTerminationChar(final String str, final char ch) {
+		final ImmutableList<Character> openBrackets = ImmutableList.of('{', '[');
+		final ImmutableList<Character> closeBrackets = ImmutableList.of('}', ']');
+		final PeekingIterator<Character> charPeekingIt = Iterators.peekingIterator(Lists.charactersOf(str).iterator());
 		for (int offset = 0; charPeekingIt.hasNext(); offset++) {
 			final Character c = charPeekingIt.next();
-			if (c == delim) {
+			if (openBrackets.contains(c)) {
+				// Find closing bracket before finding termination char
+				offset = findClosingBracket(charPeekingIt, openBrackets, closeBrackets, offset);
+			} else if (c == ch) {
 				return offset;
 			} else if (c == '-' && charPeekingIt.peek() == '-') {
+				// Found comment, increase offset until end of line
 				for (offset = offset + 1; charPeekingIt.hasNext(); offset++) {
 					final Character tmp = charPeekingIt.next();
 					if (tmp == '\n') {
@@ -68,8 +84,35 @@ public class BehaviorAnnexXtextUtil {
 		throw new RuntimeException("Cannot find terminating character");
 	}
 
+	private static int findClosingBracket(final Iterator<Character> charPeekingIt,
+			final ImmutableList<Character> openBrackets, final ImmutableList<Character> closeBrackets,
+			int offset) {
+		int bracketsOpenCount = 1;
+		for (; charPeekingIt.hasNext(); offset++) {
+			final Character c = charPeekingIt.next();
+			if (openBrackets.contains(c)) {
+				bracketsOpenCount++;
+			} else if (closeBrackets.contains(c)) {
+				if (bracketsOpenCount == 1) {
+					return offset + 1;
+				} else {
+					bracketsOpenCount--;
+				}
+			}
+		}
+
+		throw new RuntimeException("Could not find closing brackets.");
+	}
+
 	/**
-	 * Find offset of a pair characters in string that is not commented out
+	 * Finds the offset of the specified character pair in the specified string.
+	 *
+	 * @param str the string to search for the character pair
+	 * @param charsToMatch the character pair to find in the string
+	 *
+	 * @throws RuntimeException if the character pair cannot be found
+	 *
+	 * @return the offset of the character pair in the string
 	 */
 	public static int findUncommentedCharPair(final String str, final Pair<Character, Character> charsToMatch) {
 		final PeekingIterator<Character> charPeekingIt = Iterators
