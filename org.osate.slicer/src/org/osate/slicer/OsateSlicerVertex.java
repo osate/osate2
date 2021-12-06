97fb4ec202c5ee435d83f1753017994f468cb8fd
Sam Procter <sprocter@sei.cmu.edu>
2021-12-06 11:50:56 -0500

    Add support for hierarchy and graph visualization

--- org.osate.slicer/src/org/osate/slicer/OsateSlicerVertex.java
+++ org.osate.slicer/src/org/osate/slicer/OsateSlicerVertex.java
@@ -1,5 +1,7 @@
 package org.osate.slicer;
 
+import org.eclipse.emf.ecore.EObject;
+
 /**
  * The vertex type used by the slicer. See the
  * <a href="https://jgrapht.org/guide/VertexAndEdgeTypes#vertices-as-pointers">
@@ -12,19 +14,30 @@ public class OsateSlicerVertex {
 	/**
 	 * A globally unique name for the vertex
 	 */
-	final String name; // key
+	final private String name; // key
+
+	/**
+	 * The container for the port represented by this vertex
+	 */
+	private EObject container;
 
 	/**
 	 * Make a new vertex with the given name
-	 * @param name The vertex's name
+	 * @param name The feature's name
+	 * @param container The container for the feature
 	 */
-	public OsateSlicerVertex(String name) {
+	public OsateSlicerVertex(String name, EObject container) {
 		this.name = name;
+		this.container = container;
+	}
+
+	public EObject getContainer() {
+		return container;
 	}
 
 	@Override
 	public String toString() {
-		return name;
+		return getName();
 	}
 
 	@Override
@@ -35,5 +48,9 @@ public class OsateSlicerVertex {
 	@Override
 	public boolean equals(Object o) {
 		return (o instanceof OsateSlicerVertex) && (toString().equals(o.toString()));
+	}
+
+	public String getName() {
+		return name;
 	}
 }
--- org.osate.slicer/src/org/osate/slicer/SlicerRepresentation.java
+++ org.osate.slicer/src/org/osate/slicer/SlicerRepresentation.java
@@ -1,11 +1,15 @@
 package org.osate.slicer;
 
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.util.EcoreUtil;
 import org.jgrapht.Graph;
 import org.jgrapht.Graphs;
@@ -14,12 +18,14 @@ import org.jgrapht.graph.AsSubgraph;
 import org.jgrapht.graph.DefaultDirectedGraph;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.EdgeReversedGraph;
+import org.jgrapht.nio.dot.DOTExporter;
 import org.jgrapht.traverse.BreadthFirstIterator;
 import org.osate.aadl2.DirectionType;
 import org.osate.aadl2.Element;
 import org.osate.aadl2.NamedElement;
+import org.osate.aadl2.instance.ComponentInstance;
 import org.osate.aadl2.instance.ConnectionInstance;
-import org.osate.aadl2.instance.ConnectionInstanceEnd;
+import org.osate.aadl2.instance.ConnectionReference;
 import org.osate.aadl2.instance.FeatureInstance;
 import org.osate.aadl2.instance.SystemInstance;
 import org.osate.aadl2.instance.util.InstanceSwitch;
@@ -29,6 +35,8 @@ import org.osate.aadl2.instance.util.InstanceSwitch;
  * This API provides functionality for building the graph needed for slicing AADL models, as well as performing the slices.
  *
  * @author sprocter
+ *
+ * TODO: Find a way to mark this unstable / discourage use, look into marking "internal" so you get non-API warnings
  *
  */
 public class SlicerRepresentation {
@@ -54,33 +62,31 @@ public class SlicerRepresentation {
 	private SlicerSwitch mySwitch = new SlicerSwitch();
 
 	public void buildGraph(SystemInstance si) {
-
 		// Add vertices and explicit edges
 		EcoreUtil.getAllContents(si.eResource(), true).forEachRemaining(elem -> {
 			mySwitch.doSwitch((Element) elem);
 		});
 
 		// Add implicit edges
-		buildIntraConnections();
+		buildIntraConnections(mySwitch.hasExplicitDecomp);
 
 		// Create a separate graph, rather than a wrapper, saving CPU at the expense of memory
 		Graphs.addGraphReversed(rg, g);
 
-		System.out.println(toString());
-		System.out.println(forwardReachability("sys_impl_Instance.b.output"));
-		System.out.println(backwardReachability("sys_impl_Instance.b.output"));
+		System.out.println(this.toDot());
 	}
 
 	/**
 	 * Adds a new vertex by the given name to the graph.
 	 *
 	 * @param name The name of the new vertex
+	 * @param eObject
 	 */
-	public void addVertex(String name) {
+	public void addVertex(String name, EObject eObject) {
 		if (vertexMap.containsKey(name)) {
 			return; // No duplicates allowed
 		}
-		OsateSlicerVertex v = new OsateSlicerVertex(name);
+		OsateSlicerVertex v = new OsateSlicerVertex(name, eObject);
 		g.addVertex(v);
 		vertexMap.put(name, v);
 	}
@@ -93,6 +99,26 @@ public class SlicerRepresentation {
 	 */
 	public void addEdge(String src, String tgt) {
 		g.addEdge(vertexMap.get(src), vertexMap.get(tgt));
+	}
+
+	public Collection<EObject> forwardReach(ComponentInstance component) {
+		Set<EObject> retSet = new HashSet<>();
+		for (FeatureInstance fi : component.getAllFeatureInstances()) {
+			forwardReachability(getCompleteFeatureName(fi)).vertexSet().forEach(v -> {
+				retSet.add(v.getContainer());
+			});
+		}
+		return retSet;
+	}
+
+	public Collection<EObject> backwardReach(ComponentInstance component) {
+		Set<EObject> retSet = new HashSet<>();
+		for (FeatureInstance fi : component.getAllFeatureInstances()) {
+			backwardReachability(getCompleteFeatureName(fi)).vertexSet().forEach(v -> {
+				retSet.add(v.getContainer());
+			});
+		}
+		return retSet;
 	}
 
 	/**
@@ -136,18 +162,43 @@ public class SlicerRepresentation {
 		return reachableSubgraph;
 	}
 
-	private void buildIntraConnections() {
+	/**
+	 * This method connect component inports to component outports.
+	 * @param noInternalConnections Set of components which don't need inports connected to outports
+	 */
+	private void buildIntraConnections(Set<String> noInternalConnections) {
 		Set<String> intraConnectedComponents = new HashSet<>(mySwitch.inFeats.keySet());
 		intraConnectedComponents.retainAll(mySwitch.outFeats.keySet());
 		intraConnectedComponents.forEach(container -> {
 			mySwitch.inFeats.get(container).forEach(i -> {
 				mySwitch.outFeats.get(container).forEach(o -> {
-					if (!i.equals(o)) {
+					if (!i.equals(o) && !noInternalConnections.contains(container)) {
 						addEdge(i, o);
 					}
 				});
 			});
 		});
+	}
+
+	/**
+	 * Builds a dot-qualified full name from the given named element
+	 * @param ne
+	 * @return The full name / path to the component
+	 */
+	private String getCompleteFeatureName(NamedElement ne) {
+		StringBuilder sb = new StringBuilder(ne.getFullName());
+		do {
+			ne = (NamedElement) ne.eContainer();
+			sb.insert(0, ne.getFullName() + ".");
+		} while (ne.eContainer() != null);
+		return sb.toString();
+	}
+
+	public String toDot() {
+		DOTExporter<OsateSlicerVertex, DefaultEdge> exporter = new DOTExporter<>(v -> v.getName().replace('.', '_'));
+		Writer writer = new StringWriter();
+		exporter.exportGraph(g, writer);
+		return writer.toString();
 	}
 
 	@Override
@@ -172,11 +223,16 @@ public class SlicerRepresentation {
 		 */
 		private Map<String, Set<String>> outFeats = new HashMap<>();
 
+		/**
+		 * This stores container names which have explicit decompositions provided by the modeler
+		 */
+		private Set<String> hasExplicitDecomp = new HashSet<>();
+
 		@Override
 		public Void caseFeatureInstance(FeatureInstance fi) {
 			String fullFeatureName = getCompleteFeatureName(fi);
 			String fullContainerName = fullFeatureName.substring(0, fullFeatureName.lastIndexOf('.'));
-			addVertex(fullFeatureName);
+			addVertex(fullFeatureName, fi.eContainer());
 			if (fi.getDirection() == DirectionType.IN || fi.getDirection() == DirectionType.IN_OUT) {
 				if (!inFeats.containsKey(fullContainerName)) {
 					inFeats.put(fullContainerName, new HashSet<String>());
@@ -194,23 +250,17 @@ public class SlicerRepresentation {
 
 		@Override
 		public Void caseConnectionInstance(ConnectionInstance ci) {
-			addEdge(getCompleteFeatureName(ci.getSource()), getCompleteFeatureName(ci.getDestination()));
-			return null;
-		}
+			for (ConnectionReference cr : ci.getConnectionReferences()) {
+				addEdge(getCompleteFeatureName(cr.getSource()), getCompleteFeatureName(cr.getDestination()));
+			}
 
-		/**
-		 * Builds a dot-qualified full name from the given feature / connection end
-		 * @param cie The connection end / feature
-		 * @return The full name / path to the component
-		 */
-		private String getCompleteFeatureName(ConnectionInstanceEnd cie) {
-			NamedElement ne = cie;
-			StringBuilder sb = new StringBuilder(ne.getFullName());
-			do {
-				ne = (NamedElement) ne.eContainer();
-				sb.insert(0, ne.getFullName() + ".");
-			} while (ne.eContainer() != null);
-			return sb.toString();
+			// If the component has a decomposition specified, we need to record that
+			if (ci.getConnectionReferences().size() > 1) {
+				String fullFeatureName = getCompleteFeatureName(ci.getConnectionReferences().get(0).getDestination());
+				String fullContainerName = fullFeatureName.substring(0, fullFeatureName.lastIndexOf('.'));
+				hasExplicitDecomp.add(fullContainerName);
+			}
+			return null;
 		}
 	};
 }
