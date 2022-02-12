f8c346abeb87003419e697fb0c76fb352857949d
Joe Seibel <1824701+joeseibel@users.noreply.github.com>
2022-02-11 23:10:24 -0500

    Converted InstanceFormatter from Xtend to Java.

--- core/org.osate.aadl2.instance.textual/src/org/osate/aadl2/instance/textual/formatting/InstanceFormatter.java
+++ core/org.osate.aadl2.instance.textual/src/org/osate/aadl2/instance/textual/formatting/InstanceFormatter.java
@@ -1,18 +1,18 @@
 /**
- * Copyright (c) 2004-2022 Carnegie Mellon University and others. (see Contributors file). 
+ * Copyright (c) 2004-2022 Carnegie Mellon University and others. (see Contributors file).
  * All Rights Reserved.
- * 
+ *
  * NO WARRANTY. ALL MATERIAL IS FURNISHED ON AN "AS-IS" BASIS. CARNEGIE MELLON UNIVERSITY MAKES NO WARRANTIES OF ANY
  * KIND, EITHER EXPRESSED OR IMPLIED, AS TO ANY MATTER INCLUDING, BUT NOT LIMITED TO, WARRANTY OF FITNESS FOR PURPOSE
  * OR MERCHANTABILITY, EXCLUSIVITY, OR RESULTS OBTAINED FROM USE OF THE MATERIAL. CARNEGIE MELLON UNIVERSITY DOES NOT
  * MAKE ANY WARRANTY OF ANY KIND WITH RESPECT TO FREEDOM FROM PATENT, TRADEMARK, OR COPYRIGHT INFRINGEMENT.
- * 
+ *
  * This program and the accompanying materials are made available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
  * SPDX-License-Identifier: EPL-2.0
- * 
+ *
  * Created, in part, with funding and support from the United States Government. (see Acknowledgments file).
- * 
+ *
  * This program includes and/or can make use of certain third party source code, object code, documentation and other
  * files ("Third Party Software"). The Third Party Software that is used by this program is dependent upon your system
  * configuration. By using this program, You agree to comply with any and all relevant Third Party Software terms and
@@ -21,42 +21,44 @@
  * aries to this license with respect to the terms applicable to their Third Party Software. Third Party Software li-
  * censes only apply to the Third Party Software and not any other portion of this program or this program as a whole.
  */
-package org.osate.aadl2.instance.textual.formatting
+package org.osate.aadl2.instance.textual.formatting;
 
-import com.google.inject.Inject
-import org.eclipse.xtext.formatting.impl.AbstractDeclarativeFormatter
-import org.eclipse.xtext.formatting.impl.FormattingConfig
-import org.osate.aadl2.instance.textual.services.InstanceGrammarAccess
+import org.eclipse.xtext.formatting.impl.AbstractDeclarativeFormatter;
+import org.eclipse.xtext.formatting.impl.FormattingConfig;
+import org.osate.aadl2.instance.textual.services.InstanceGrammarAccess;
+
+import com.google.inject.Inject;
 
 /**
  * This class contains custom formatting declarations.
- * 
+ *
  * See https://www.eclipse.org/Xtext/documentation/303_runtime_concepts.html#formatting
  * on how and when to use it.
- * 
+ *
  * Also see {@link org.eclipse.xtext.xtext.XtextFormattingTokenSerializer} as an example
  */
-class InstanceFormatter extends AbstractDeclarativeFormatter {
+public class InstanceFormatter extends AbstractDeclarativeFormatter {
+	@Inject
+	private InstanceGrammarAccess grammarAccess;
 
-	@Inject extension InstanceGrammarAccess
-	
-	override protected void configureFormatting(FormattingConfig c) {
-		c.autoLinewrap = 1000
-		findKeywordPairs("{", "}").forEach[
-			c.setIndentationIncrement.after(first)
-			c.setLinewrap.after(first)
-			c.setIndentationDecrement.before(second)
-			c.setLinewrap.before(second)
-		]
-		c.setLinewrap(1).after(systemOperationModeRule)
-		c.setLinewrap(1).after(componentInstanceRule)
-		c.setLinewrap(1).after(featureInstanceRule)
-		c.setLinewrap(1).after(connectionInstanceRule)
-		c.setLinewrap(1).after(connectionReferenceRule)
-		c.setLinewrap(1).after(flowSpecificationInstanceRule)
-		c.setLinewrap(1).after(endToEndFlowInstanceRule)
-		c.setLinewrap(1).after(modeInstanceRule)
-		c.setLinewrap(1).after(modeTransitionInstanceRule)
-		c.setLinewrap(1).after(propertyAssociationInstanceRule)
+	@Override
+	protected void configureFormatting(FormattingConfig c) {
+		c.setAutoLinewrap(1000);
+		grammarAccess.findKeywordPairs("{", "}").forEach(pair -> {
+			c.setIndentationIncrement().after(pair.getFirst());
+			c.setLinewrap().after(pair.getFirst());
+			c.setIndentationDecrement().before(pair.getSecond());
+			c.setLinewrap().before(pair.getSecond());
+		});
+		c.setLinewrap(1).after(grammarAccess.getSystemOperationModeRule());
+		c.setLinewrap(1).after(grammarAccess.getComponentInstanceRule());
+		c.setLinewrap(1).after(grammarAccess.getFeatureInstanceRule());
+		c.setLinewrap(1).after(grammarAccess.getConnectionInstanceRule());
+		c.setLinewrap(1).after(grammarAccess.getConnectionReferenceRule());
+		c.setLinewrap(1).after(grammarAccess.getFlowSpecificationInstanceRule());
+		c.setLinewrap(1).after(grammarAccess.getEndToEndFlowInstanceRule());
+		c.setLinewrap(1).after(grammarAccess.getModeInstanceRule());
+		c.setLinewrap(1).after(grammarAccess.getModeTransitionInstanceRule());
+		c.setLinewrap(1).after(grammarAccess.getPropertyAssociationInstanceRule());
 	}
 }
