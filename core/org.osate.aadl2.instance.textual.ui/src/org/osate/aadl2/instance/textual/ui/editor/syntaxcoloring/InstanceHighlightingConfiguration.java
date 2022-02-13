f38e58f6db9739af4b3c1dc116f2c38711311dd9
Joe Seibel <1824701+joeseibel@users.noreply.github.com>
2022-02-12 21:39:11 -0500

    Converted InstanceHighlightingConfiguration from Xtend to Java.

--- core/org.osate.aadl2.instance.textual.ui/src/org/osate/aadl2/instance/textual/ui/editor/syntaxcoloring/InstanceHighlightingConfiguration.java
+++ core/org.osate.aadl2.instance.textual.ui/src/org/osate/aadl2/instance/textual/ui/editor/syntaxcoloring/InstanceHighlightingConfiguration.java
@@ -1,18 +1,18 @@
 /**
- * Copyright (c) 2004-2022 Carnegie Mellon University and others. (see Contributors file). 
+ * Copyright (c) 2004-2022 Carnegie Mellon University and others. (see Contributors file).
  * All Rights Reserved.
- * 
+ *
  * NO WARRANTY. ALL MATERIAL IS FURNISHED ON AN "AS-IS" BASIS. CARNEGIE MELLON UNIVERSITY MAKES NO WARRANTIES OF ANY
  * KIND, EITHER EXPRESSED OR IMPLIED, AS TO ANY MATTER INCLUDING, BUT NOT LIMITED TO, WARRANTY OF FITNESS FOR PURPOSE
  * OR MERCHANTABILITY, EXCLUSIVITY, OR RESULTS OBTAINED FROM USE OF THE MATERIAL. CARNEGIE MELLON UNIVERSITY DOES NOT
  * MAKE ANY WARRANTY OF ANY KIND WITH RESPECT TO FREEDOM FROM PATENT, TRADEMARK, OR COPYRIGHT INFRINGEMENT.
- * 
+ *
  * This program and the accompanying materials are made available under the terms of the Eclipse Public License 2.0
  * which is available at https://www.eclipse.org/legal/epl-2.0/
  * SPDX-License-Identifier: EPL-2.0
- * 
+ *
  * Created, in part, with funding and support from the United States Government. (see Acknowledgments file).
- * 
+ *
  * This program includes and/or can make use of certain third party source code, object code, documentation and other
  * files ("Third Party Software"). The Third Party Software that is used by this program is dependent upon your system
  * configuration. By using this program, You agree to comply with any and all relevant Third Party Software terms and
@@ -21,21 +21,28 @@
  * aries to this license with respect to the terms applicable to their Third Party Software. Third Party Software li-
  * censes only apply to the Third Party Software and not any other portion of this program or this program as a whole.
  */
-package org.osate.aadl2.instance.textual.ui.editor.syntaxcoloring
+package org.osate.aadl2.instance.textual.ui.editor.syntaxcoloring;
 
-import org.eclipse.swt.SWT
-import org.eclipse.swt.graphics.RGB
-import org.eclipse.xtext.ui.editor.syntaxcoloring.DefaultHighlightingConfiguration
-import org.eclipse.xtext.ui.editor.syntaxcoloring.IHighlightingConfigurationAcceptor
-import org.eclipse.xtext.ui.editor.utils.TextStyle
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.xtext.ui.editor.syntaxcoloring.DefaultHighlightingConfiguration;
+import org.eclipse.xtext.ui.editor.syntaxcoloring.IHighlightingConfigurationAcceptor;
+import org.eclipse.xtext.ui.editor.utils.TextStyle;
 
-class InstanceHighlightingConfiguration extends DefaultHighlightingConfiguration {
-	val public static String NAME_ID = "NAME_ID"
-	val public static String DECLARATIVE_ID = "DECLARATIVE_ID"
-	
-	override configure(IHighlightingConfigurationAcceptor acceptor) {
-		super.configure(acceptor)
-		acceptor.acceptDefaultHighlighting(NAME_ID, "Name", new TextStyle => [style = SWT.BOLD])
-		acceptor.acceptDefaultHighlighting(DECLARATIVE_ID, "Declarative Reference", new TextStyle => [color = new RGB(128, 128, 128)])
+public class InstanceHighlightingConfiguration extends DefaultHighlightingConfiguration {
+	public static final String NAME_ID = "NAME_ID";
+	public static final String DECLARATIVE_ID = "DECLARATIVE_ID";
+
+	@Override
+	public void configure(IHighlightingConfigurationAcceptor acceptor) {
+		super.configure(acceptor);
+
+		var bold = new TextStyle();
+		bold.setStyle(SWT.BOLD);
+		acceptor.acceptDefaultHighlighting(NAME_ID, "Name", bold);
+
+		var gray = new TextStyle();
+		gray.setColor(new RGB(128, 128, 128));
+		acceptor.acceptDefaultHighlighting(DECLARATIVE_ID, "Declarative Reference", gray);
 	}
 }
