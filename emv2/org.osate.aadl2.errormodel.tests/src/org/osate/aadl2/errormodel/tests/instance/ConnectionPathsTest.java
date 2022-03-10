4d86bce3e8fa647601dc1847144ae28e100215cb
Joe Seibel <1824701+joeseibel@users.noreply.github.com>
2022-03-10 17:23:27 -0500

    Created connection path tests related to feature groups.

--- emv2/org.osate.aadl2.errormodel.tests/src/org/osate/aadl2/errormodel/tests/instance/ConnectionPathsTest.java
+++ emv2/org.osate.aadl2.errormodel.tests/src/org/osate/aadl2/errormodel/tests/instance/ConnectionPathsTest.java
@@ -71,4 +71,79 @@ public class ConnectionPathsTest {
 		// Tests that up connection paths are not instantiated.
 		assertEquals(0, InstantiateModel.instantiate(system).getAnnexInstances().size());
 	}
+
+	@Test
+	public void testFeatureInFeatureGroup() throws Exception {
+		var pkg = testHelper.parseFile(PATH + "feature_in_feature_group_test.aadl");
+		var system = (SystemImplementation) pkg.getPublicSection().getOwnedClassifiers().get(1);
+		var annexInstance = (EMV2AnnexInstance) InstantiateModel.instantiate(system).getAnnexInstances().get(0);
+		assertEquals(1, annexInstance.getPropagationPaths().size());
+		with((ConnectionPath) annexInstance.getPropagationPaths().get(0), connectionPath -> {
+			assertEquals("left.left_feature_group.out_f -> right.right_feature_group.in_f", connectionPath.getName());
+			assertEquals("left.left_feature_group.out_f -> right.right_feature_group.in_f",
+					connectionPath.getConnection().getName());
+			assertEquals("left_feature_group.out_f", connectionPath.getSourcePropagation().getName());
+			assertEquals("right_feature_group.in_f", connectionPath.getDestinationPropagation().getName());
+		});
+	}
+
+	@Test
+	public void testFeatureInMultipleFeatureGroups() throws Exception {
+		var pkg = testHelper.parseFile(PATH + "feature_in_multiple_feature_groups_test.aadl");
+		var system = (SystemImplementation) pkg.getPublicSection().getOwnedClassifiers().get(1);
+		var annexInstance = (EMV2AnnexInstance) InstantiateModel.instantiate(system).getAnnexInstances().get(0);
+		assertEquals(1, annexInstance.getPropagationPaths().size());
+		with((ConnectionPath) annexInstance.getPropagationPaths().get(0), connectionPath -> {
+			assertEquals(
+					"left.left_feature_group_1.left_feature_group_2.left_feature_group_3.out_f -> right.right_feature_group_1.right_feature_group_2.right_feature_group_3.in_f",
+					connectionPath.getName());
+			assertEquals(
+					"left.left_feature_group_1.left_feature_group_2.left_feature_group_3.out_f -> right.right_feature_group_1.right_feature_group_2.right_feature_group_3.in_f",
+					connectionPath.getConnection().getName());
+			assertEquals("left_feature_group_1.left_feature_group_2.left_feature_group_3.out_f",
+					connectionPath.getSourcePropagation().getName());
+			assertEquals("right_feature_group_1.right_feature_group_2.right_feature_group_3.in_f",
+					connectionPath.getDestinationPropagation().getName());
+		});
+	}
+
+	@Test
+	public void testEmptyFeatureGroup() throws Exception {
+		var pkg = testHelper.parseFile(PATH + "empty_feature_group_test.aadl");
+		var system = (SystemImplementation) pkg.getPublicSection().getOwnedClassifiers().get(1);
+		var annexInstance = (EMV2AnnexInstance) InstantiateModel.instantiate(system).getAnnexInstances().get(0);
+		assertEquals(1, annexInstance.getPropagationPaths().size());
+		with((ConnectionPath) annexInstance.getPropagationPaths().get(0), connectionPath -> {
+			assertEquals("left.out_fg -> right.in_fg", connectionPath.getName());
+			assertEquals("left.out_fg -> right.in_fg", connectionPath.getConnection().getName());
+			assertEquals("out_fg", connectionPath.getSourcePropagation().getName());
+			assertEquals("in_fg", connectionPath.getDestinationPropagation().getName());
+		});
+	}
+
+	@Test
+	public void testFeatureGroupWithFeatures() throws Exception {
+		var pkg = testHelper.parseFile(PATH + "feature_group_with_features_test.aadl");
+		var system = (SystemImplementation) pkg.getPublicSection().getOwnedClassifiers().get(1);
+		var annexInstance = (EMV2AnnexInstance) InstantiateModel.instantiate(system).getAnnexInstances().get(0);
+		assertEquals(3, annexInstance.getPropagationPaths().size());
+		with((ConnectionPath) annexInstance.getPropagationPaths().get(0), connectionPath -> {
+			assertEquals("left.out_fg.f_1 -> right.in_fg.f_1", connectionPath.getName());
+			assertEquals("left.out_fg.f_1 -> right.in_fg.f_1", connectionPath.getConnection().getName());
+			assertEquals("out_fg.f_1", connectionPath.getSourcePropagation().getName());
+			assertEquals("in_fg.f_1", connectionPath.getDestinationPropagation().getName());
+		});
+		with((ConnectionPath) annexInstance.getPropagationPaths().get(1), connectionPath -> {
+			assertEquals("left.out_fg.f_2 -> right.in_fg.f_2", connectionPath.getName());
+			assertEquals("left.out_fg.f_2 -> right.in_fg.f_2", connectionPath.getConnection().getName());
+			assertEquals("out_fg.f_2", connectionPath.getSourcePropagation().getName());
+			assertEquals("in_fg.f_2", connectionPath.getDestinationPropagation().getName());
+		});
+		with((ConnectionPath) annexInstance.getPropagationPaths().get(2), connectionPath -> {
+			assertEquals("left.out_fg.f_3 -> right.in_fg.f_3", connectionPath.getName());
+			assertEquals("left.out_fg.f_3 -> right.in_fg.f_3", connectionPath.getConnection().getName());
+			assertEquals("out_fg.f_3", connectionPath.getSourcePropagation().getName());
+			assertEquals("in_fg.f_3", connectionPath.getDestinationPropagation().getName());
+		});
+	}
 }
