e1359fea68db3f0b019ca3a692a73d95114bf0f4
Lutz Wrage <lwrage@sei.cmu.edu>
2026-09-04 11:32:34 -0400

    Add regression test for issue #3153
    Validate a standalone AADL model with a Behavior Annex that declares two initial thread states. The test requires the user-visible D.3 legality diagnostic and demonstrates that the generic AADL validation path currently returns no issues.

---
+++ ba/org.osate.ba.tests/src/org/osate/ba/tests/Issue3153Test.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2004-2026 Carnegie Mellon University and others. (see Contributors file).
+ * All Rights Reserved.
+ *
+ * NO WARRANTY. ALL MATERIAL IS FURNISHED ON AN "AS-IS" BASIS. CARNEGIE MELLON UNIVERSITY MAKES NO WARRANTIES OF ANY
+ * KIND, EITHER EXPRESSED OR IMPLIED, AS TO ANY MATTER INCLUDING, BUT NOT LIMITED TO, WARRANTY OF FITNESS FOR PURPOSE
+ * OR MERCHANTABILITY, EXCLUSIVITY, OR RESULTS OBTAINED FROM USE OF THE MATERIAL. CARNEGIE MELLON UNIVERSITY DOES NOT
+ * MAKE ANY WARRANTY OF ANY KIND WITH RESPECT TO FREEDOM FROM PATENT, TRADEMARK, OR COPYRIGHT INFRINGEMENT.
+ *
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Created, in part, with funding and support from the United States Government. (see Acknowledgments file).
+ *
+ * This program includes and/or can make use of certain third party source code, object code, documentation and other
+ * files ("Third Party Software"). The Third Party Software that is used by this program is dependent upon your system
+ * configuration. By using this program, You agree to comply with any and all relevant Third Party Software terms and
+ * conditions contained in any such Third Party Software or separate license file distributed with such Third Party
+ * Software. The parties who own the Third Party Software ("Third Party Licensors") are intended third party benefici-
+ * aries to this license with respect to the terms applicable to their Third Party Software. Third Party Software li-
+ * censes only apply to the Third Party Software and not any other portion of this program or this program as a whole.
+ */
+package org.osate.ba.tests;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.eclipse.xtext.testing.InjectWith;
+import org.eclipse.xtext.testing.XtextRunner;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.osate.aadl2.AadlPackage;
+import org.osate.testsupport.Aadl2InjectorProvider;
+import org.osate.testsupport.TestHelper;
+
+import com.google.inject.Inject;
+
+/**
+ * Verifies that validation of an ordinary AADL resource dispatches to the embedded Behavior Annex validator. This
+ * protects user-visible BA legality diagnostics, which disappeared when the annex migrated to Xtext.
+ */
+@RunWith(XtextRunner.class)
+@InjectWith(Aadl2InjectorProvider.class)
+public class Issue3153Test {
+	private static final String MODEL = "org.osate.ba.tests/models/issue3153/Issue3153.aadl";
+
+	@Inject
+	private TestHelper<AadlPackage> testHelper;
+
+	@Test
+	public void aadlValidationReportsBehaviorAnnexLegalityErrors() throws Exception {
+		var result = testHelper.testFile(MODEL);
+		assertEquals(List.of("ERROR: issue3153::example can't have more than one initial state : first, second : "
+				+ "Behavior Annex D.3.(L3) legality rule failed."), result.getIssues()
+				.stream()
+				.map(issue -> issue.getSeverity() + ": " + issue.getMessage())
+				.toList());
+	}
+}
